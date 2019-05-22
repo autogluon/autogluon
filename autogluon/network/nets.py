d39d4d31db27295fafb704a53d1d97f9d6ddb129
cgraywang <wangcg.pku@gmail.com>
2019-05-22 11:28:54 -0700

    Demo (#20)
    * fix
    
    * fix 1
    
    * add notebook

--- autogluon/network/nets.py
+++ autogluon/network/nets.py
@@ -3,6 +3,8 @@ import ConfigSpace as CS
 from ..space import *
 from .utils import Net
 
+from ..task.image_classification import get_model
+
 __all__ = ['Nets']
 
 
@@ -25,6 +27,8 @@ class Nets(object):
         cs.add_hyperparameter(net_list_hyper_param)
         for net in self.net_list:
             #TODO(cgraywang): distinguish between different nets, only support resnet for now
+            if isinstance(net, str):
+                net = get_model(net)
             net_hyper_params = net.get_hyper_params()
             conds = []
             for net_hyper_param in net_hyper_params:
@@ -45,6 +49,10 @@ class Nets(object):
         for net in self.net_list:
             if isinstance(net, Net):
                 net_strs.append(net.name)
+            elif isinstance(net, str):
+                net_strs.append(net)
+            else:
+                pass
         return net_strs
 
     def __repr__(self):
