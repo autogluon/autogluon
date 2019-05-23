470347f6aafba1c0bbb506a65793ba54c1076aae
Piyush Ghai <ghai.8@osu.edu>
2019-05-23 13:52:38 -0700

    Added results obejct in core.py

--- autogluon/task/text_classification/core.py
+++ autogluon/task/text_classification/core.py
@@ -1,1 +1,16 @@
+import logging
+from typing import Any
 __all__ = []
+
+logger = logging.getLogger(__name__)
+
+
+class Results(object):
+    """
+    Python class to hold the results for the trials
+    """
+    def __init__(self, model: Any, metric: Any, config:Any, time: int):
+        self.model = model
+        self.metric = metric
+        self.config = config
+        self.time = time
