7f7a4dc31efc200825c5267110d5497f21d4bc3d
Piyush Ghai <ghai.8@osu.edu>
2019-05-24 16:26:17 -0700

    Added TextData Transforms and Dataset Utils

---
+++ autogluon/dataset/transforms.py
@@ -0,0 +1,100 @@
+import re
+
+import spacy
+from nltk.stem.porter import *
+
+from .utils import *
+
+__all__ = ['TextDataTransform']
+
+stemmer = PorterStemmer()
+
+spacy_nlp = spacy.load('en')
+
+STOP_WORDS = spacy.lang.en.stop_words.STOP_WORDS
+
+NUM_REGEX = re.compile("[0-9]+")
+
+PUNCTUATION_REGEX = re.compile("[.;:!\'?,\"()\[\]]")
+
+HTML_REGEX = re.compile("(<br\s*/><br\s*/>)")  # TODO Refine this further
+
+
+def replace_nums(text: AnyStr) -> AnyStr:
+    """Removes numbers in a string"""
+    return NUM_REGEX.sub('', text)
+
+
+def replace_html(text: AnyStr) -> AnyStr:
+    return HTML_REGEX.sub('', text)
+
+
+def replace_punctuations(text: AnyStr) -> AnyStr:
+    return PUNCTUATION_REGEX.sub('', text)
+
+
+def stop_words(textlist: Collection[AnyStr]) -> Collection[AnyStr]:
+    """Utility method for stop word removal"""
+    tokens = [token for token in textlist if token not in STOP_WORDS]
+    return tokens
+
+
+def stemming(textlist: Collection[AnyStr]) -> Collection[AnyStr]:
+    """Utility method for word stemming"""
+    tokens = [stemmer.stem(token) for token in textlist]
+    return tokens
+
+
+def lemmatization():
+    # TODO
+    pass
+
+
+class TextDataTransform(object):
+    """
+    Python class for performing data pre-processing on the text dataset.
+    This class is constructed using
+    """
+
+    def __init__(self, vocab, tokenizer=nlp.data.SpacyTokenizer('en'), max_sequence_length=100, pre_tokenize_rules=None,
+                 post_tokenize_rules=None):
+        """
+        Init method for TextDataTransform. This is a utility class for defining custom transforms on the text dataset.
+        :param vocab:
+        :param tokenizer:
+        :param max_sequence_length:
+        :param pre_tokenize_rules:
+        :param post_tokenize_rules:
+        """
+        self._vocab = vocab
+        self._tokenizer = tokenizer
+        self._pre_tokenize_rules = get_or_else(pre_tokenize_rules, [])
+        self._post_tokenize_rules = get_or_else(post_tokenize_rules, [])
+        self._length_clip = nlp.data.ClipSequence(max_sequence_length)
+
+    def __call__(self, sample):
+        text, label = sample
+
+        text = self._tokenizer(self.pre_process(text))
+
+        for rule in self._post_tokenize_rules:
+            text = globals()[rule](text)
+
+        text = self._vocab[self.post_process(text)]
+
+        return text, label
+
+    def pre_process(self, text):
+        # text = self._pattern.sub('', text)
+        for rule in self._pre_tokenize_rules:
+            text = globals()[rule](text)
+            if len(text) == 0:
+                text = self._vocab.unknown_token
+
+        return text
+
+    def post_process(self, text):
+        for rule in self._post_tokenize_rules:
+            text = globals()[rule](text)
+
+        return self._length_clip(text)
---
+++ autogluon/dataset/utils.py
@@ -0,0 +1,86 @@
+from typing import AnyStr, Any, Collection
+import os
+import mxnet as mx
+from mxnet import gluon
+import gluonnlp as nlp
+
+
+def read_text_from_file(path:AnyStr) -> AnyStr:
+    text = []
+    for line in open(path, 'r'):
+        line = line.lower().strip()
+        text.append(line)
+    return ' '.join(line for line in text)
+
+
+def get_dataset_from_files(path:AnyStr,
+                           label_folders:Collection[AnyStr] = None) -> (gluon.data.SimpleDataset, Collection[int]):
+    """
+    Utility method that reads a dataset from files and returns text, label pairs if labels=True
+    :param path:
+    :param label_folders: Whether labels are needed or not
+    :return:
+    """
+
+    include_labels = label_folders is not None
+    lines = list()
+    label_set = set()
+    for folder in os.listdir(os.path.join(path)):
+        if include_labels and folder not in label_folders:
+            continue
+        if folder.startswith('.'):
+            # Ignore .DS_Store or something similar
+            continue
+
+        for filename in os.listdir(os.path.join(path, folder)):
+            if not filename.endswith('.txt'):
+                continue
+            line = read_text_from_file(os.path.join(path, folder, filename))
+            if include_labels:
+                # TODO: remove hardcoding!
+                lbl = 0 if folder=='neg' else 1
+                lines.append([line, lbl])  # Text , Label pair
+                label_set.add(lbl)
+            else:
+                lines.append(line)
+
+    return gluon.data.SimpleDataset(lines), label_set
+
+
+def get_dataset_from_json_files(path: AnyStr) -> (gluon.data.SimpleDataset, Collection[int]):
+    import json
+    label_set = set()
+    if not path.endswith('.json'):
+        path = '.'.join([path, 'json'])
+    with open(path) as f:
+        content = json.load(f)
+
+    for elem in content:
+        label_set.add(elem[1])
+
+    return gluon.data.SimpleDataset(content), label_set
+
+
+def get_or_else(x: Any, y: Any) -> Any:
+    return y if x is None else x
+
+
+def flatten_dataset(dataset: gluon.data.SimpleDataset,
+                    transform_fn: callable) -> Collection[AnyStr]:
+    """
+    This flattens and compresses the dataset to a single list of each individual words.
+    This will be useful to fine-tune a pre-trained LM model on this dataset.
+    The fine-tuning method requires this format for the data.
+    :param:dataset
+    :param: transform_fn
+    :return:
+    """
+
+    flattened_data = list()
+    print ("Original LEN", len(dataset))
+    for (sent, label) in dataset:
+        sent = transform_fn(sent)
+        for word in sent.split():
+            flattened_data.append(word)
+    print ("Flattened LEN", len(flattened_data))
+    return flattened_data
