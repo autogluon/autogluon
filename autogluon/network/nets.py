e4fa195340679fa93f78692097e9996e96a79fe4
Piyush Ghai <ghai.8@osu.edu>
2019-05-24 14:26:47 -0700

    Added typing hints to nets.py

--- autogluon/network/nets.py
+++ autogluon/network/nets.py
@@ -1,31 +1,33 @@
-import ConfigSpace as CS
+from typing import Collection, AnyStr
 
+import ConfigSpace as CS
 import autogluon as ag
-from ..space import *
+
 from .utils import Net
+from ..space import *
 
 __all__ = ['Nets']
 
 
 class Nets(object):
-    def __init__(self, net_list):
-        #TODO(cgraywang): add model instance, for now, use a list of model names
+    def __init__(self, net_list: Collection[Net]):
+        # TODO(cgraywang): add model instance, for now, use a list of model names
         assert isinstance(net_list, list), type(net_list)
         self.net_list = net_list
         self.search_space = None
         self.add_search_space()
 
-    def _set_search_space(self, cs):
+    def _set_search_space(self, cs: CS.ConfigurationSpace):
         self.search_space = cs
 
     def add_search_space(self):
         cs = CS.ConfigurationSpace()
         net_list_hyper_param = List('model',
-                                    choices=self.get_net_strs())\
+                                    choices=self.get_net_strs()) \
             .get_hyper_param()
         cs.add_hyperparameter(net_list_hyper_param)
         for net in self.net_list:
-            #TODO(cgraywang): distinguish between different nets, only support resnet for now
+            # TODO(cgraywang): distinguish between different nets, only support resnet for now
             if isinstance(net, str):
                 net = ag.task.get_model(net)
             net_hyper_params = net.get_hyper_params()
@@ -33,17 +35,17 @@ class Nets(object):
             for net_hyper_param in net_hyper_params:
                 if net_hyper_param not in cs.get_hyperparameters():
                     cs.add_hyperparameter(net_hyper_param)
-                #TODO(cgraywang): put condition in presets? split task settings out
+                # TODO(cgraywang): put condition in presets? split task settings out
                 cond = CS.InCondition(net_hyper_param, net_list_hyper_param,
                                       self.get_net_strs())
                 conds.append(cond)
             cs.add_conditions(conds)
         self._set_search_space(cs)
 
-    def get_search_space(self):
+    def get_search_space(self) -> CS.ConfigurationSpace:
         return self.search_space
 
-    def get_net_strs(self):
+    def get_net_strs(self) -> Collection[AnyStr]:
         net_strs = []
         for net in self.net_list:
             if isinstance(net, Net):
