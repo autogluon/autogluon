a0fb6199ae63a042c5d4d34ceac49e5d8a0b1edb
Karan Jariwala <karankjariwala@gmail.com>
2019-06-12 11:38:14 -0700

    Added WNUT2017 dataset support

--- autogluon/task/named_entity_recognition/dataset.py
+++ autogluon/task/named_entity_recognition/dataset.py
@@ -15,70 +15,64 @@ logger = logging.getLogger(__name__)
 __all__ = ['Dataset']
 NULL_TAG = "X"
 
+
 class Dataset(dataset.Dataset):
     def __init__(self, name: AnyStr = None, train_path: AnyStr = None, val_path: AnyStr = None,
                  lazy: bool = True, vocab: nlp.Vocab = None, max_sequence_length: int = 180,
-                 tokenizer: nlp.data.transforms = None,
-                 batch_size: int = 8):
+                 tokenizer: nlp.data.transforms = None, indexes_format: dict = None,
+                 batch_size: int = 32):
         super(Dataset, self).__init__(name, train_path, val_path)
         # TODO : This currently works only for datasets from GluonNLP. This needs to be made more generic.
         # TODO : add search space, handle batch_size, num_workers
-        self._num_classes: int = 0
+        # self._num_classes: int = 0
         self._vocab: nlp.Vocab = vocab
         self._train_ds_transformed = None
         self._val_ds_transformed = None
-        # self._train_data_lengths = None  # TODO There is an alternative way possible to avoid creating this list
-        # self.data_format = 'json'  # TODO This should come from config
-        # self._label_set = set()
         self.batch_size = batch_size
-
-        # self.cased = True
-        self.max_sequence_length = max_sequence_length # TODO This should come from config
-        # self.tokenizer = getattr(nlp.data.transforms, 'BERTTokenizer')
+        self.max_sequence_length = max_sequence_length  # TODO This should come from config
         self.tokenizer = tokenizer
+        self.indexes_format = indexes_format
         self._download_dataset()
         self.add_search_space()
 
         if vocab is None and lazy is False:
-            raise ValueError("Please specify a vocabulary object to init the dataset.")
+            raise ValueError("Please specify a vocabulary object to initialize the dataset.")
 
         if self._vocab is None:
             _, self._vocab = nlp.model.get_model(name='bert_12_768_12',
-                                                dataset_name='book_corpus_wiki_en_cased')
+                                                 dataset_name='book_corpus_wiki_en_cased')
             logger.info("Taking default vocabulary of pre-trained model `bert_12_768_12` "
-                        "on `book_corpus_wiki_en_cased`. If you would like to use different "
-                        "vocabulary then please pass parameter `vocab` as an instance of "
+                        "on `book_corpus_wiki_en_cased`. If you would like to use different"
+                        "vocabulary then please pass parameter `vocab` as an instance of"
                         "`nlp.Vocab`.")
 
         # if not lazy:
-        self._init_()
+        if not lazy:
+            self._init_()
+        else:
+            self._init_()
 
     def _init_(self):
         self._read_dataset()
         self.add_search_space()
-        # self._num_classes = len(self._label_set)
-
-    @property
-    def num_classes(self) -> int:
-        return self._num_classes
 
     @property
     def vocab(self) -> nlp.vocab:
         return self._vocab
 
-    # @vocab.setter
-    # def vocab(self, value: nlp.vocab) -> None:
-    #     self.vocab = value
-    #     self._init_()
+    @vocab.setter
+    def vocab(self, value: nlp.vocab) -> None:
+        self.vocab = value
+        self._init_()
 
     def _download_dataset(self) -> None:
-        if self.name.lower() == 'conll2003':
+        if self.name.lower() == 'conll2003' or self.name.lower() == 'wnut2017':
             if self.train_path is None or self.val_path is None:
-                raise ValueError("CoNLL-2003 can't be downloaded directly from Gluon due to"
+                raise ValueError("{} can't be downloaded directly from Gluon due to"
                                  "license Issue. Please provide the downloaded filepath"
-                                 "in `train_path` and `val_path`")
+                                 "in `train_path` and `val_path`".format(self.name.lower()))
         else:
-            raise NotImplementedError # TODO: Add support for more dataset
+            raise NotImplementedError  # TODO: Add support for more dataset
 
     def _read_dataset(self) -> None:
         """
@@ -90,32 +84,27 @@ class Dataset(dataset.Dataset):
         self._preprocess()
         self._prepare_data_loader()
 
-
     def _load_dataset(self):
         print("Loading the dataset...")
         if self.tokenizer is None:
             self.tokenizer = getattr(nlp.data.transforms, 'BERTTokenizer')
-        self.tokenizer = self.tokenizer(vocab=self.vocab, lower= False)
-
-        self.train_dataset = load_segment(file_path=self.train_path, tokenizer=self.tokenizer)
-        self.val_dataset = load_segment(file_path=self.val_path, tokenizer=self.tokenizer)
-        # self.test_dataset = load_segment(file_path=self.train_path, tokenizer=self.tokenizer)
+        self.tokenizer = self.tokenizer(vocab=self.vocab, lower=False)
+        self.train_dataset = load_segment(dataset_name=self.name, file_path=self.train_path,
+                                          tokenizer=self.tokenizer, indexes_format=self.indexes_format)
+        self.val_dataset = load_segment(dataset_name=self.name, file_path=self.val_path,
+                                        tokenizer=self.tokenizer, indexes_format=self.indexes_format)
 
         print("Train data length: {}".format(len(self.train_dataset)))
         print("Validation data length: {}".format(len(self.val_dataset)))
-        # print("Test data length: {}".format(len(self.test_dataset)))
 
         # Build tag vocab
         print("\nBuilding tag vocabulary...")
         all_sentences = self.train_dataset + self.val_dataset
-        # all_sentences = self.train_dataset + self.val_dataset + self.test_dataset # TODO: Remove test dataset
         tag_counter = nlp.data.count_tokens(token.tag for sentence in all_sentences for token in sentence)
         self.tag_vocab = nlp.Vocab(tag_counter, padding_token=NULL_TAG,
                                    bos_token=None, eos_token=None, unknown_token=None)
         self.null_tag_index = self.tag_vocab[NULL_TAG]
-        self._num_classes = len(self.tag_vocab)
-        print("Number of tag types: {}".format(self._num_classes))
-
+        print("Number of tag types: {}".format(len(self.tag_vocab)))
 
     def encode_as_input(self, sentence):
         # check whether the given sequence can be fit into `seq_len`.
@@ -152,12 +141,11 @@ class Dataset(dataset.Dataset):
                 flag_nonnull_tag)
 
     def _preprocess(self):
-        print("\nPreprocessing dataset ...")
         self.train_dataset = [self.encode_as_input(sentence) for sentence in self.train_dataset]
         self.val_dataset = [self.encode_as_input(sentence) for sentence in self.val_dataset]
 
     def _prepare_data_loader(self):
-        print("\nPreparing dataloaders ...")
+        print("Preparing dataloaders...")
         self.train_dataloader = gluon.data.DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True,
                                                       last_batch='keep', num_workers=cpu_count())
         self.valid_dataloader = gluon.data.DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=False,
--- autogluon/task/named_entity_recognition/utils.py
+++ autogluon/task/named_entity_recognition/utils.py
@@ -1,20 +1,194 @@
+import re
+import sys
 from collections import namedtuple
 
+import logging
 
-# NER utils
+log = logging.getLogger(__name__)
 
 TaggedToken = namedtuple('TaggedToken', ['text', 'tag'])
-PredictedToken = namedtuple('PredictedToken', ['text', 'true_tag', 'pred_tag'])
 
 NULL_TAG = "X"
 
-def bio_bioes(tokens):
-    """Convert a list of TaggedTokens in BIO(2) scheme to BIOES scheme.
+
+def read_data(name, file_path, column_format):
+    """Calls specific read method based on dataset.
+    
+    Parameters
+    ----------
+    name : str
+        Name of the dataset
+    file_path : str
+        A file path
+    column_format : dict
+        A dictionary contains key as an index and value as text/tag to find
+        location in a file
+
+    Returns
+    -------
+    List[List[TaggedToken]]:
+        A list of sentences with each sentence breaks down into TaggedToken
+    """
+    if name.lower() == 'conll2003':
+        return read_conll2003(file_path, column_format)
+    elif name.lower() == 'wnut2017':
+        return read_wnut2017(file_path, column_format)
+    else:
+        print(name.lower())
+        raise NotImplementedError  # TODO: More dataset support
+
+
+def read_wnut2017(file_path, column_format):
+    """Reads the data from the file and convert into list of sentences.
+
+    Parameters
+    ----------
+    file_path : str
+        A file path
+    column_format : dict
+        A dictionary contains key as an index and value as text/tag to find
+        location in a file
+
+    Returns
+    -------
+    List[List[TaggedToken]]:
+        A list of sentences with each sentence breaks down into TaggedToken
+    """
+    sentence_list = []
+    current_sentence = []
+
+    try:
+        lines = open(
+            str(file_path), encoding="utf-8"
+        ).read().strip().split("\n")
+    except:
+        log.info(
+            'UTF-8 can\'t read: {} ... using "latin-1" instead.'.format(
+                file_path
+            )
+        )
+        lines = open(
+            str(file_path), encoding="latin1"
+        ).read().strip().split("\n")
+
+    # get the text and ner column
+    text_column: int = sys.maxsize
+    ner_column: int = sys.maxsize
+    for column in column_format:
+        if column_format[column].lower() == "text":
+            text_column = column
+        elif column_format[column].lower() == 'ner':
+            ner_column = column
+        else:
+            raise ValueError("Invalid column type")
+
+    for line in lines:
+        if line.startswith("#"):
+            continue
+
+        if line.strip().replace("﻿", "") == "":
+            if len(current_sentence) > 0:
+                sentence_list.append(current_sentence)
+            current_sentence = []
+
+        else:
+            fields = re.split("\s+", line)
+            # token = TaggedToken(text=fields[text_column], tag=None)
+            for column in column_format:
+                if len(fields) > column:
+                    if column != text_column:
+                        token = TaggedToken(text=fields[text_column], tag=fields[column])
+
+            current_sentence.append(token)
+
+    if len(current_sentence) > 0:
+        sentence_list.append(current_sentence)
+
+    return sentence_list
+
+
+def read_conll2003(file_path, column_format):
+    """Reads the data from the file and convert into list of sentences.
+
+    Parameters
+    ----------
+    file_path : str
+        A file path
+    column_format : dict
+        A dictionary contains key as an index and value as text/tag to find
+        location in a file
+
+    Returns
+    -------
+    List[List[TaggedToken]]:
+        A list of sentences with each sentence breaks down into TaggedToken
+    """
+    with open(file_path, 'r') as ifp:
+        sentence_list = []
+        current_sentence = []
+
+        for line in ifp:
+            if len(line.strip()) > 0:
+                word, _, _, tag = line.rstrip().split(" ")
+                current_sentence.append(TaggedToken(text=word, tag=tag))
+            else:
+                # the sentence was completed if an empty line occurred; flush the current sentence.
+                sentence_list.append(current_sentence)
+                current_sentence = []
+
+        # check if there is a remaining token. in most CoNLL data files, this does not happen.
+        if len(current_sentence) > 0:
+            sentence_list.append(current_sentence)
+        return sentence_list
+
+
+def bio_to_bio2(sentences):
+    """Check that tags have a valid IOB format.
+    Tags in IOB1 format are converted to IOB2.
+
+    Parameters
+    ----------
+    sentences : List[List[TaggedToken]]
+        A list of sentences in BIO format with each sentence breaks down into TaggedToken
+
+    Returns
+    -------
+    List[List[TaggedToken]]:
+        A list of sentences in BIO-2 format with each sentence breaks down into TaggedToken
+    """
+    sentence_list = []
+    current_sentence = []
+    prev_tag = 'O'
+
+    for sentence in sentences:
+        for i, token in enumerate(sentence):
+            tag = token.tag
+            if tag == 'O':
+                bio2_tag = 'O'
+            else:
+                if prev_tag == 'O' or tag[2:] != prev_tag[2:]:
+                    bio2_tag = 'B' + tag[1:]
+                else:
+                    bio2_tag = tag
+            current_sentence.append(TaggedToken(text=token.text, tag=bio2_tag))
+            prev_tag = tag
+        sentence_list.append(current_sentence)
+        current_sentence = []
+        prev_tag = 'O'
+
+    # check if there is a remaining token. in most CoNLL data files, this does not happen.
+    if len(current_sentence) > 0:
+        sentence_list.append(current_sentence)
+    return sentence_list
+
+
+def bio2_to_bioes(tokens):
+    """Convert a list of TaggedTokens from BIO-2 scheme to BIOES scheme.
 
     Parameters
     ----------
     tokens: List[TaggedToken]
-        A list of tokens in BIO(2) scheme
+        A list of tokens in BIO-2 scheme
 
     Returns
     -------
@@ -41,49 +215,6 @@ def bio_bioes(tokens):
                 ret.append(TaggedToken(text=token.text, tag="E" + token.tag[1:]))
     return ret
 
-def read_bio_as_bio2(data_path):
-    """Read CoNLL-formatted text file in BIO scheme in given path as sentences in BIO2 scheme.
-
-    Parameters
-    ----------
-    data_path: str
-        Path of the data file to read
-
-    Returns
-    -------
-    List[List[TaggedToken]]:
-        List of sentences, each of which is a List of TaggedTokens
-    """
-
-    with open(data_path, 'r') as ifp:
-        sentence_list = []
-        current_sentence = []
-        prev_tag = 'O'
-
-        for line in ifp:
-            if len(line.strip()) > 0:
-                word, _, _, tag = line.rstrip().split(" ")
-                # convert BIO tag to BIO2 tag
-                if tag == 'O':
-                    bio2_tag = 'O'
-                else:
-                    if prev_tag == 'O' or tag[2:] != prev_tag[2:]:
-                        bio2_tag = 'B' + tag[1:]
-                    else:
-                        bio2_tag = tag
-                current_sentence.append(TaggedToken(text=word, tag=bio2_tag))
-                prev_tag = tag
-            else:
-                # the sentence was completed if an empty line occurred; flush the current sentence.
-                sentence_list.append(current_sentence)
-                current_sentence = []
-                prev_tag = 'O'
-
-        # check if there is a remaining token. in most CoNLL data files, this does not happen.
-        if len(current_sentence) > 0:
-            sentence_list.append(current_sentence)
-        return sentence_list
-
 
 def remove_docstart_sentence(sentences):
     """Remove -DOCSTART- sentences in the list of sentences.
@@ -91,11 +222,13 @@ def remove_docstart_sentence(sentences):
     Parameters
     ----------
     sentences: List[List[TaggedToken]]
-        List of sentences, each of which is a List of TaggedTokens; this list may contain DOCSTART sentences.
+        List of sentences, each of which is a List of TaggedTokens;
+        this list may contain DOCSTART sentences.
 
     Returns
     -------
-        List of sentences, each of which is a List of TaggedTokens; this list does not contain DOCSTART sentences.
+        List of sentences, each of which is a List of TaggedTokens;
+        this list does not contain DOCSTART sentences.
     """
     ret = []
     for sentence in sentences:
@@ -107,7 +240,22 @@ def remove_docstart_sentence(sentences):
             ret.append(current_sentence)
     return ret
 
+
 def bert_tokenize_sentence(sentence, bert_tokenizer):
+    """Convert the text word into BERTTokenize format.
+
+    Parameters
+    ----------
+    sentence : List[List[TaggedToken]]
+        A list of sentences with each sentence breaks down into TaggedToken
+    bert_tokenizer : gluonnlp.data.transforms
+        A Bert Tokenize instance
+
+    Returns
+    -------
+    List[List[TaggedToken]]:
+        A list of sentences in BERT scheme
+    """
     ret = []
     for token in sentence:
         # break a word into sub-word tokens
@@ -118,8 +266,10 @@ def bert_tokenize_sentence(sentence, bert_tokenizer):
                 for sub_token_text in sub_token_texts[1:]]
     return ret
 
-def load_segment(file_path, tokenizer):
-    bio2_sentences = remove_docstart_sentence(read_bio_as_bio2(file_path))
-    bioes_sentences = [bio_bioes(sentence) for sentence in bio2_sentences]
+
+def load_segment(dataset_name, file_path, tokenizer, indexes_format):
+    sentences = read_data(dataset_name, file_path, indexes_format)
+    bio2_sentences = remove_docstart_sentence(bio_to_bio2(sentences))
+    bioes_sentences = [bio2_to_bioes(sentence) for sentence in bio2_sentences]
     subword_sentences = [bert_tokenize_sentence(sentence, tokenizer) for sentence in bioes_sentences]
     return subword_sentences
