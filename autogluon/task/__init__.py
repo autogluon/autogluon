e73198fed087db5459bf8111b1a63c1fa1f12510
Jonas Mueller <1390638+jwmueller@users.noreply.github.com>
2019-09-16 14:47:25 -0700

    First commit for tabular module. Still need to move auxiliary f3_grail_data_frame_utilities package to unify tabular codebase into single module.

--- autogluon/task/__init__.py
+++ autogluon/task/__init__.py
@@ -1,2 +1,3 @@
 from .base import BaseTask
-from .image_classification import ImageClassification 
+from .image_classification import ImageClassification
+from .predict_table_column import PredictTableColumn
