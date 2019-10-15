8fc766810e79dcec93699ec79a55b7a66b19b3ba
Jonas Mueller <1390638+jwmueller@users.noreply.github.com>
2019-10-15 13:42:16 -0700

    added toy regression benchmark. updated learner to handle missing labels during training and evaluation

--- tabular/benchmark/SelfBenchmarks.py
+++ tabular/benchmark/SelfBenchmarks.py
@@ -3,22 +3,22 @@
     Lower performance-values = better, normalized to [0,1] for each dataset to enable cross-dataset comparisons.
     Classification performance = error-rate, Regression performance = 1 - R^2
     
-    # TODO: we want to assess that Autogluon has correctly inferred the type of each feature (continuous vs categorical vs text)
+    # TODO: assess that Autogluon has correctly inferred the type of each feature (continuous vs categorical vs text)
     
     # TODO: suppress internal AutoGluon print statements, so that only benchmark-info is printed
     
     # TODO: may want to take allowed run-time of AutoGluon into account? Eg. can produce performance vs training time curves for each dataset.
     
     # TODO: We'd like to add extra benchmark datasets with the following properties:
-    - one-dimensional features
-    - extreme-multiclass classification
+    - test dataset with just one data point
+    - test dataset where order of columns different than in training data (same column names)
+    - extreme-multiclass classification (500+ classes)
     - high-dimensional features + low-sample size
-    - missing labels in training data
     - parquet file format
-    - extreme levels of missingness
-    - classification severe class imbalance
+    - high levels of missingness in test data only, no missingness in train data
+    - classification w severe class imbalance
     - regression with severely skewed Y-values (eg. predicting count data)
-    - trivial prediction problem where y = simple deterministic function of x
+    - text features in dataset
 """
 
 import numpy as np
@@ -43,26 +43,32 @@ EPS = 1e-10
 
 # Information about each dataset in benchmark is stored in dict.
 # performance_val = expected performance on this dataset (lower = better), Should update based on previously run benchmarks
-binary_dataset = {'folder': '/Users/jonasmue/WorkDocs/AutoGluon/githubAutogluon/auto-ml-with-gluon/tabular/datasets/AdultIncomeData/',
+binary_dataset = {'folder': '~/WorkDocs/AutoGluon/githubAutogluon/auto-ml-with-gluon/tabular/datasets/AdultIncomeData/',
                   'name': 'AdultIncomeBinary',
                   'problem_type': BINARY,
                   'label_column': 'class',
                   'performance_val': 0.129} # mixed types of features
 
-multi_dataset = {'folder': '/Users/jonasmue/WorkDocs/Datasets/CoverTypeMulticlassClassification/',
+multi_dataset = {'folder': '~/WorkDocs/Datasets/CoverTypeMulticlassClassification/',
                   'name': 'CoverTypeMulticlass',
                   'problem_type': MULTICLASS,
                   'label_column': 'Cover_Type',
                   'performance_val': 0.032} # 7 classes, all features are numeric
 
-regression_dataset = {'folder': '/Users/jonasmue/WorkDocs/Datasets/AmesHousingPriceRegression/',
+regression_dataset = {'folder': '~/WorkDocs/Datasets/AmesHousingPriceRegression/',
                    'name': 'AmesHousingRegression',
                   'problem_type': REGRESSION,
                   'label_column': 'SalePrice',
-                  'performance_val': 0.076}
+                  'performance_val': 0.076} # Regression with mixed feature-types, skewed Y-values
 
- # List containing dicts of information on each dataset
-datasets = [binary_dataset, multi_dataset, regression_dataset]
+toyregres_dataset = {'folder': '~/WorkDocs/Datasets/AutogluonToyBenchmark/toyRegression/', 
+                     'name': 'ToyRegression',
+                     'problem_type': REGRESSION,
+                    'label_column': 'y', 
+                    'performance_val': 0.183} # 1d toy deterministic regression task with heavy label+feature missingness + extra distraction columns in test data
+
+ # List containing dicts for each dataset to include in benchmark (try to order based on runtimes)
+datasets = [toyregres_dataset, binary_dataset, regression_dataset, multi_dataset]
 
 # Aggregate performance summaries obtained in previous benchmark run:
 prev_perf_vals = [dataset['performance_val'] for dataset in datasets]
@@ -107,6 +113,7 @@ with warnings.catch_warnings(record=True) as caught_warnings:
         if performance_vals[idx] > dataset['performance_val'] * perf_threshold:
             warnings.warn("Performance on dataset %s is %s times worse than previous performance." % (dataset['name'], performance_vals[idx]/(EPS+dataset['performance_val'])))
 
+# Summarize:
 avg_perf = np.mean(performance_vals)
 median_perf = np.median(performance_vals)
 worst_perf = np.max(performance_vals)
@@ -130,5 +137,6 @@ print("Worst performance: %s" % worst_perf)
 print("\n\n WARNINGS:")
 for w in caught_warnings:
     warnings.warn(w.message)
+
 
 
