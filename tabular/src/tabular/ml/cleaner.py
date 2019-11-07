87d37d05cc065682567f26a330d56db4f389ff52
Nick Erickson <neerick@amazon.com>
2019-11-06 21:52:45 -0800

    Enabled infrequent class removal for multiclass problems

--- tabular/src/tabular/ml/cleaner.py
+++ tabular/src/tabular/ml/cleaner.py
@@ -9,7 +9,7 @@ class Cleaner:
         if problem_type == BINARY:
             return CleanerDummy()
         elif problem_type == MULTICLASS:
-            return CleanerDummy() # CleanerMulticlass(label=label, threshold=threshold) # Do not want to remove classes!
+            return CleanerMulticlass(label=label, threshold=threshold)
         elif problem_type == REGRESSION:
             return CleanerDummy()
         elif problem_type == LANGUAGE_MODEL:
