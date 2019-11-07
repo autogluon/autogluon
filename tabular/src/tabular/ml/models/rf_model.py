39f324ae11d181213aa85136b15bea99a162835f
Nick Erickson <neerick@amazon.com>
2019-11-06 21:50:27 -0800

    Fixed sklearn RF models failing due to NaN values

--- tabular/src/tabular/ml/models/rf_model.py
+++ tabular/src/tabular/ml/models/rf_model.py
@@ -8,6 +8,11 @@ class RFModel(SKLearnModel):
     def __init__(self, path, name, model, problem_type, objective_func, debug=0):
         super().__init__(path=path, name=name, model=model, problem_type=problem_type, objective_func=objective_func, debug=debug)
 
+    def preprocess(self, X):
+        X = super().preprocess(X)
+        X = X.fillna(0)
+        return X
+
     def hyperparameter_tune(self, X, y, spaces=None):
         if spaces is None:
             spaces = RFSpaces(problem_type=self.problem_type, objective_func=self.objective_func, num_classes=None).get_hyperparam_spaces_baseline()
