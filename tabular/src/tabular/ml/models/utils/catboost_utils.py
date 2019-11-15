5203310b84234b18df40e32b51f2c58f25c90b66
Nick Erickson <neerick@amazon.com>
2019-11-14 23:35:56 -0800

    Added catboost model to tabular (Unused)

---
+++ tabular/src/tabular/ml/models/utils/catboost_utils.py
@@ -0,0 +1,89 @@
+
+import numpy as np
+
+from tabular.ml.constants import BINARY, MULTICLASS, REGRESSION
+
+
+# TODO: Add weight support?
+# TODO: Can these be optimized? What computational cost do they have compared to the default catboost versions?
+class CustomMetric:
+    def __init__(self, metric, is_higher_better, needs_pred_proba):
+        self.metric = metric
+        self.is_higher_better = is_higher_better
+        self.needs_pred_proba = needs_pred_proba
+
+    def get_final_error(self, error, weight):
+        return error
+
+    def is_max_optimal(self):
+        return self.is_higher_better
+
+    def evaluate(self, approxes, target, weight):
+        raise NotImplementedError
+
+
+class BinaryCustomMetric(CustomMetric):
+    def _get_y_pred_proba(self, approxes):
+        return np.array(approxes[0])
+
+    def _get_y_pred(self, y_pred_proba):
+        return np.round(y_pred_proba)
+
+    def evaluate(self, approxes, target, weight):
+        y_pred_proba = self._get_y_pred_proba(approxes=approxes)
+
+        if self.needs_pred_proba:
+            score = self.metric(np.array(target), y_pred_proba)
+        else:
+            raise NotImplementedError('Custom Catboost Binary prob metrics are not supported by AutoGluon.')
+            # y_pred = self._get_y_pred(y_pred_proba=y_pred_proba)  # This doesn't work at the moment because catboost returns some strange valeus in approxes which are not the probabilities
+            # score = self.metric(np.array(target), y_pred)
+
+        return score, 1
+
+
+class MulticlassCustomMetric(CustomMetric):
+    def _get_y_pred_proba(self, approxes):
+        return np.array(approxes)
+
+    def _get_y_pred(self, y_pred_proba):
+        return y_pred_proba.argmax(axis=0)
+
+    def evaluate(self, approxes, target, weight):
+        y_pred_proba = self._get_y_pred_proba(approxes=approxes)
+        if self.needs_pred_proba:
+            raise NotImplementedError('Custom Catboost Multiclass proba metrics are not supported by AutoGluon.')
+            # y_pred_proba = y_pred_proba.reshape(len(np.unique(np.array(target))), -1).T
+            # score = self.metric(np.array(target), y_pred_proba)  # This doesn't work at the moment because catboost returns some strange valeus in approxes which are not the probabilities
+        else:
+            y_pred = self._get_y_pred(y_pred_proba=y_pred_proba)
+            score = self.metric(np.array(target), y_pred)
+
+        return score, 1
+
+
+class RegressionCustomMetric(CustomMetric):
+    def _get_y_pred(self, approxes):
+        return np.array(approxes[0])
+
+    def evaluate(self, approxes, target, weight):
+        y_pred = self._get_y_pred(approxes=approxes)
+        score = self.metric(np.array(target), y_pred)
+
+        return score, 1
+
+
+metric_classes_dict = {
+    BINARY: BinaryCustomMetric,
+    MULTICLASS: MulticlassCustomMetric,
+    REGRESSION: RegressionCustomMetric,
+}
+
+
+def construct_custom_catboost_metric(metric, is_higher_better, needs_pred_proba, problem_type):
+    if (metric.name == 'log_loss') and (problem_type == MULTICLASS) and needs_pred_proba:
+        return 'MultiClass'
+    if metric.name == 'accuracy':
+        return 'Accuracy'
+    metric_class = metric_classes_dict[problem_type]
+    return metric_class(metric=metric, is_higher_better=is_higher_better, needs_pred_proba=needs_pred_proba)
---
+++ tabular/src/tabular/ml/tuning/hyperparameters/defaults/catboost/parameters.py
@@ -0,0 +1,72 @@
+""" Lists the default (fixed) hyperparameter values we use in our Gradient Boosting model. """
+
+from tabular.ml.constants import BINARY, MULTICLASS, REGRESSION
+
+DEFAULT_NUM_BOOST_ROUND = 10000
+
+def get_param_baseline(problem_type, num_classes=None):
+    if problem_type == BINARY:
+        return get_param_binary_baseline()
+    elif problem_type == MULTICLASS:
+        return get_param_multiclass_baseline(num_classes=num_classes)
+    elif problem_type == REGRESSION:
+        return get_param_regression_baseline()
+    else:
+        return get_param_binary_baseline()
+
+
+def get_param_baseline_dummy(problem_type, num_classes=None):
+    if problem_type == BINARY:
+        return get_param_binary_baseline_dummy()
+    elif problem_type == MULTICLASS:
+        return get_param_multiclass_baseline_dummy(num_classes=num_classes)
+    elif problem_type == REGRESSION:
+        return get_param_regression_baseline_dummy()
+    else:
+        return get_param_binary_baseline_dummy()
+
+
+def get_param_binary_baseline():
+    params = {
+
+    }
+    return params
+
+
+def get_param_binary_baseline_dummy():
+    params = {
+        'iterations': 10000,
+        'learning_rate': 0.1,
+    }
+    return params
+
+
+def get_param_multiclass_baseline(num_classes):
+    params = {
+
+    }
+    return params
+
+
+
+def get_param_multiclass_baseline_dummy(num_classes):
+    params = {
+        'iterations': 10000,
+        'learning_rate': 0.1,
+    }
+    return params
+
+
+def get_param_regression_baseline():
+    params = {
+
+    }
+    return params
+
+
+def get_param_regression_baseline_dummy():
+    params = {
+        'iterations': 10000,
+        'learning_rate': 0.1,
+    }
+    return params
