4ee9b4f99a76f284ffb5710d207a4c41423fef9b
cgraywang <wangcg.pku@gmail.com>
2019-11-18 13:33:44 -0800

    add test

--- tests/unittests/test_text_classification.py
+++ tests/unittests/test_text_classification.py
@@ -1,34 +1,19 @@
-import pytest
 import logging
-
+import autogluon as ag
 from autogluon import TextClassification as task
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 
-@pytest.mark.serial
-def test_text_classification_dataset():
-    logger.debug('Start testing autogluon fit')
+def test_text_classification():
+    logger.debug('Start testing autogluon text classification')
     dataset = task.Dataset(name='SST')
     logger.debug(dataset)
     logger.debug('Finished.')
 
 
-@pytest.mark.serial
-def test_text_classification_fit():
-    dataset = task.Dataset(name='SST')
-    predictor = task.fit(dataset)
-    logger.debug('Top-1 val acc: %.3f' % predictor.results['best_reward'])
-    logger.debug('The best configuration is:')
-    logger.debug(predictor.results['best_config'])
-    logger.debug('Finished.')
-
-
-@pytest.mark.serial
-def test_text_classification_predictor():
-    dataset = task.Dataset(name='SST')
-    predictor = task.fit(dataset)
-    test_acc = predictor.evaluate(dataset)
-    logger.debug('Top-1 test acc: %.3f' % test_acc)
-    logger.debug('Finished.')
+if __name__ == '__main__':
+    import nose
+    nose.runmodule()
+    ag.done()
