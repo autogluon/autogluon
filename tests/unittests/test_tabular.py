0301b5e4c36bf78ce14f71b8f199b4bc13f1fd60
Nick Erickson <neerick@amazon.com>
2019-11-22 22:15:40 -0800

    Fixed test_tabular, removed legacy version

--- tests/unittests/test_tabular.py
+++ tests/unittests/test_tabular.py
@@ -31,7 +31,7 @@ from autogluon import TabularPrediction as task
 import autogluon as ag
 
 # TODO: move this file
-from autogluon.tabular.ml.constants import BINARY, MULTICLASS, REGRESSION
+from autogluon.utils.tabular.ml.constants import BINARY, MULTICLASS, REGRESSION
 
  
 ############ Benchmark options you can set: ########################
