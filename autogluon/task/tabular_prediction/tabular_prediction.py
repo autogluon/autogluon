cf989326314b9e59f94bff2dcd0b28c23c74fa90
Nick Erickson <neerick@amazon.com>
2019-12-11 04:35:36 -0800

    Disabled NN in tabular by default until fix to hang defect is found

--- autogluon/task/tabular_prediction/tabular_prediction.py
+++ autogluon/task/tabular_prediction/tabular_prediction.py
@@ -55,7 +55,8 @@ class TabularPrediction(BaseTask):
     @staticmethod
     def fit(train_data, label, tuning_data=None, output_directory=None, problem_type=None, eval_metric=None,
             hyperparameter_tune=False, feature_prune=False, holdout_frac=None, num_bagging_folds=0, stack_ensemble_levels=0,
-            hyperparameters = {'NN': {'num_epochs': 500}, 
+            hyperparameters = {
+                               # 'NN': {'num_epochs': 500},  # TODO: Disabled by default due to defect recently discovered causing process to hang
                                'GBM': {'num_boost_round': 10000},
                                'CAT': {'iterations': 10000},
                                'RF': {'n_estimators': 300},
