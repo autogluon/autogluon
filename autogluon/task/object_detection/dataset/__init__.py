76eb33e0daf1e59353ef1bf4e90843e000941f70
chongruo <chongruo@gmail.com>
2019-12-31 00:35:07 +0000

    support more datasets for object detection

--- autogluon/task/object_detection/dataset/__init__.py
+++ autogluon/task/object_detection/dataset/__init__.py
@@ -6,12 +6,41 @@ from .coco import *
 import logging
 logger = logging.getLogger(__name__)
 
-def get_dataset(root, index_file_name='trainval', name=None, classes=None, \
-                format='voc', Train=True, **kwargs):
+def get_dataset(root='~/.mxnet/datasets/voc', index_file_name='trainval', name=None, \
+                classes=None, format='voc', Train=True, **kwargs):
+    """
+    Parameters
+    ----------
+    root : str
+        root path to dataset folder
+    index_file_name : str
+        the name of the .txt file which constains images for training or testing. 
+        this is only for custom dataset.
+    name: str
+        name for built-in dataset, ('voc', 'voc2007' or 'voc2012')
+        when use built-in dataset, the index_file_name should be None.
+    classes: tuple of strs
+        users can specify classes for custom dataset 
+        ex. classes = ('bike', 'bird', 'cat', ...)
+    format: str
+        data format. ('voc', 'coco')
+    Train : bool
+        specify Train/Test mode. It is only valid when name is not None.  
+    """
     if format=='voc':
         logger.info(">>> create dataset(VOC format) ")
 
-        splits = [('', index_file_name)]
+        # built-in dataset
+        if name:
+            if Train:
+                if name=='voc':
+                    splits = [('VOC2007', 'trainval'), ('VOC2012', 'trainval')]
+                elif name=='voc2007':
+                    splits = [('VOC2007', 'trainval')]
+            else:
+                splits= [('VOC2007', 'test')] 
+        else:  # custom dataset
+            splits = [('', index_file_name)]
         return CustomVOCDetection(root, splits, name, classes, **kwargs)
 
     elif format=='coco':
