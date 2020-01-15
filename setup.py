a7348e854d630b32148d8c92803d2960b186c46f
Hang Zhang <zhang.hang@rutgers.edu>
2020-01-14 21:57:31 -0800

    bump up to 0.0.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import setuptools.command.install
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 
-version = '0.0.3'
+version = '0.0.4'
 try:
     sha = subprocess.check_output(['git', 'rev-parse', 'HEAD'],
         cwd=cwd).decode('ascii').strip()
