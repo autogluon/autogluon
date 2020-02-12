f4a0fc6e951ed518901d6008abc25379f8f39895
Jerry Zhang <jerryzh.cn@gmail.com>
2020-02-12 13:20:58 -0800

    add Faster RCNN to autogluon

--- autogluon/task/object_detection/nets.py
+++ autogluon/task/object_detection/nets.py
@@ -1,19 +1,15 @@
-import mxnet as mx
-from mxnet import gluon, init
-from gluoncv.model_zoo import get_model
 import gluoncv as gcv
+import mxnet as mx
 
-from ...core import *
 
 def get_built_in_network(name, *args, **kwargs):
-
     def _get_network(name, transfer_classes, ctx=mx.cpu()):
         name = name.lower()
         net_name = '_'.join(('yolo3', name, 'custom'))
-        net = gcv.model_zoo.get_model(net_name, 
-                                  classes=transfer_classes,
-                                  pretrained_base=False, 
-                                  transfer='coco')
+        net = gcv.model_zoo.get_model(net_name,
+                                      classes=transfer_classes,
+                                      pretrained_base=False,
+                                      transfer='coco')
         net.initialize(ctx=ctx, force_reinit=True)
         return net
 
