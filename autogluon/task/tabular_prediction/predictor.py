215b44a80b3744919bf9b06c9d5dde9ae52d9906
Razmik Melikbekyan <melikbekyan@yahoo.com>
2020-02-12 03:03:27 +0100

    Basic pep 8 corrections, type checking correction. (#284)
    * FIX: type checking correction
    
    * FIX: refactoring : indentation correction
    
    * FIX: basic pep8 corrections, bug fix
    
    * FIX: type checking correction, refactoring
    
    * FIX: tabular dataset type checking

--- autogluon/task/tabular_prediction/predictor.py
+++ autogluon/task/tabular_prediction/predictor.py
@@ -1,12 +1,13 @@
 import logging
+
 import pandas as pd
 
 from .dataset import TabularDataset
 from ..base.base_predictor import BasePredictor
 from ...utils import plot_performance_vs_trials, plot_summary_of_models, plot_tabular_models, verbosity2loglevel
+from ...utils.tabular.ml.constants import REGRESSION
 from ...utils.tabular.ml.learner.default_learner import DefaultLearner as Learner
 from ...utils.tabular.ml.utils import setup_outputdir
-from ...utils.tabular.ml.constants import BINARY, MULTICLASS, REGRESSION
 
 __all__ = ['TabularPredictor']
 
@@ -60,8 +61,8 @@ class TabularPredictor(BasePredictor):
             To access any learner method `func()` from this Predictor, use: `predictor._learner.func()`.
             To access any trainer method `func()` from this `Predictor`, use: `predictor._trainer.func()`.
         """
-        self._learner = learner # Learner object
-        self._trainer = self._learner.load_trainer() # Trainer object
+        self._learner = learner  # Learner object
+        self._trainer = self._learner.load_trainer()  # Trainer object
         self.output_directory = self._learner.path_context
         self.problem_type = self._learner.problem_type
         self.eval_metric = self._learner.objective_func
@@ -96,13 +97,9 @@ class TabularPredictor(BasePredictor):
             Array of predictions, one corresponding to each row in given dataset. Either numpy Ndarray or pandas Series depending on `as_pandas` argument.
 
         """
-        if type(dataset) == str:
-            dataset = TabularDataset(file_path=dataset)
-        if isinstance(dataset, pd.Series):
-            raise TypeError("dataset must be TabularDataset or pandas.DataFrame, not pandas.Series. \
-                To predict on just single example (ith row of table), use dataset.iloc[[i]] rather than dataset.iloc[i]")
+        dataset = self.__get_dataset(dataset)
         return self._learner.predict(X_test=dataset, model=model, as_pandas=as_pandas, use_pred_cache=use_pred_cache, add_to_pred_cache=add_to_pred_cache)
-    
+
     def predict_proba(self, dataset, model=None, as_pandas=False):
         """ Use trained models to produce predicted class probabilities rather than class-labels (if task is classification).
 
@@ -123,11 +120,7 @@ class TabularPredictor(BasePredictor):
             Array of predicted class-probabilities, corresponding to each row in the given dataset. 
             May be a numpy Ndarray or pandas Series/Dataframe depending on `as_pandas` argument and the type of prediction problem.
         """
-        if type(dataset) == str:
-            dataset = TabularDataset(file_path=dataset)
-        if isinstance(dataset, pd.Series):
-            raise TypeError("dataset must be TabularDataset or pandas.DataFrame, not pandas.Series. \
-                To predict on just single example (ith row of table), use dataset.iloc[[i]] rather than dataset.iloc[i]")
+        dataset = self.__get_dataset(dataset)
         return self._learner.predict_proba(X_test=dataset, model=model, as_pandas=as_pandas)
 
     def evaluate(self, dataset, silent=False):
@@ -148,8 +141,7 @@ class TabularPredictor(BasePredictor):
             -------
             Predictive performance value on the given dataset, based on the `eval_metric` used by this Predictor.
         """
-        if type(dataset) == str:
-            dataset = TabularDataset(file_path=dataset)
+        dataset = self.__get_dataset(dataset)
         perf = self._learner.score(dataset)
         sign = self._learner.objective_func._sign
         perf = perf * sign  # flip negative once again back to positive (so higher is no longer necessarily better)
@@ -179,7 +171,7 @@ class TabularPredictor(BasePredictor):
             Scalar performance value if `auxiliary_metrics = False`. 
             If `auxiliary_metrics = True`, returns dict where keys = metrics, values = performance along each metric.
         """
-        return self._learner.evaluate(y_true=y_true, y_pred=y_pred, silent=silent, 
+        return self._learner.evaluate(y_true=y_true, y_pred=y_pred, silent=silent,
                                       auxiliary_metrics=auxiliary_metrics, detailed_report=detailed_report)
 
     def leaderboard(self, dataset=None, silent=False):
@@ -201,8 +193,7 @@ class TabularPredictor(BasePredictor):
             -------
             Pandas `pandas.DataFrame` of model performance summary information.
         """
-        if type(dataset) == str:
-            dataset = TabularDataset(file_path=dataset)
+        dataset = self.__get_dataset(dataset) if dataset is not None else dataset
         return self._learner.leaderboard(X=dataset, silent=silent)
 
     def fit_summary(self, verbosity=3):
@@ -223,13 +214,13 @@ class TabularPredictor(BasePredictor):
         """
         hpo_used = len(self._trainer.hpo_results) > 0
         model_typenames = {key: self._trainer.model_types[key].__name__ for key in self._trainer.model_types}
-        unique_model_types = set(model_typenames.values()) # no more class info
+        unique_model_types = set(model_typenames.values())  # no more class info
         # all fit() information that is returned:
         results = {
-            'model_types': model_typenames, # dict with key = model-name, value = type of model (class-name)
-            'model_performance': self.model_performance, # dict with key = model-name, value = validation performance
-            'model_best': self._trainer.model_best, # the name of the best model (on validation data)
-            'model_paths': self._trainer.model_paths, # dict with key = model-name, value = path to model file
+            'model_types': model_typenames,  # dict with key = model-name, value = type of model (class-name)
+            'model_performance': self.model_performance,  # dict with key = model-name, value = validation performance
+            'model_best': self._trainer.model_best,  # the name of the best model (on validation data)
+            'model_paths': self._trainer.model_paths,  # dict with key = model-name, value = path to model file
             'model_fit_times': self._trainer.model_fit_times,
             'model_pred_times': self._trainer.model_pred_times,
             'num_bagging_folds': self._trainer.kfolds,
@@ -249,7 +240,7 @@ class TabularPredictor(BasePredictor):
             model_hyperparams[model_name] = model_obj.params
         results['model_hyperparams'] = model_hyperparams
 
-        if verbosity > 0: # print stuff
+        if verbosity > 0:  # print stuff
             print("*** Summary of fit() ***")
             print("Number of models trained: %s" % len(results['model_performance']))
             print("Types of models trained: ")
@@ -260,36 +251,39 @@ class TabularPredictor(BasePredictor):
             num_fold_str = ""
             bagging_used = results['num_bagging_folds'] > 0
             if bagging_used:
-                num_fold_str = " (with "+str(results['num_bagging_folds'])+" folds)"
+                num_fold_str = f" (with {results['num_bagging_folds']} folds)"
             print("Bagging used: %s %s" % (bagging_used, num_fold_str))
             num_stack_str = ""
             stacking_used = results['stack_ensemble_levels'] > 0
             if stacking_used:
-                num_stack_str = " (with "+str(results['stack_ensemble_levels'])+" levels)"
+                num_stack_str = f" (with {results['stack_ensemble_levels']} levels)"
             print("Stack-ensembling used: %s %s" % (stacking_used, num_stack_str))
             # TODO: uncomment once feature_prune is functional:  self._summarize('feature_prune', 'feature-selection used', results)
             print("User-specified hyperparameters:")
             print(results['hyperparameters_userspecified'])
-        if verbosity > 1: # create plots
-            plot_tabular_models(results, output_directory=self.output_directory, 
-                save_file="SummaryOfModels.html", plot_title="Models produced during fit()")
+        if verbosity > 1:  # create plots
+            plot_tabular_models(results, output_directory=self.output_directory,
+                                save_file="SummaryOfModels.html",
+                                plot_title="Models produced during fit()")
             if hpo_used:
                 for model_type in results['hpo_results']:
-                    plot_summary_of_models(results['hpo_results'][model_type], 
-                        output_directory=self.output_directory, save_file = model_type+"_HPOmodelsummary.html",
-                        plot_title= "Models produced during " +model_type+" HPO")
-                    plot_performance_vs_trials(results['hpo_results'][model_type], 
-                        output_directory=self.output_directory, save_file = model_type+"_HPOperformanceVStrials.png",
-                        plot_title = "HPO trials for "+model_type+" models")
-        if verbosity > 2: # print detailed information
+                    plot_summary_of_models(
+                        results['hpo_results'][model_type],
+                        output_directory=self.output_directory, save_file=model_type + "_HPOmodelsummary.html",
+                        plot_title=f"Models produced during {model_type} HPO")
+                    plot_performance_vs_trials(
+                        results['hpo_results'][model_type],
+                        output_directory=self.output_directory, save_file=model_type + "_HPOperformanceVStrials.png",
+                        plot_title=f"HPO trials for {model_type} models")
+        if verbosity > 2:  # print detailed information
             if hpo_used:
                 hpo_results = results['hpo_results']
                 print("*** Details of Hyperparameter optimization ***")
                 for model_type in hpo_results:
                     hpo_model = hpo_results[model_type]
-                    print("HPO for %s model:  Num. configurations tried = %s, Time spent = %s, Search strategy = %s" 
+                    print("HPO for %s model:  Num. configurations tried = %s, Time spent = %s, Search strategy = %s"
                           % (model_type, len(hpo_model['trial_info']), hpo_model['total_time'], hpo_model['search_strategy']))
-                    print("Best hyperparameter-configuration (validation-performance: %s = %s):" 
+                    print("Best hyperparameter-configuration (validation-performance: %s = %s):"
                           % (self.eval_metric, hpo_model['validation_performance']))
                     print(hpo_model['best_config'])
             """
@@ -324,11 +318,11 @@ class TabularPredictor(BasePredictor):
         -------
         :class:`TabularPredictor` object
         """
-        logger.setLevel(verbosity2loglevel(verbosity)) # Reset logging after load (may be in new Python session)
+        logger.setLevel(verbosity2loglevel(verbosity))  # Reset logging after load (may be in new Python session)
         if output_directory is None:
             raise ValueError("output_directory cannot be None in load()")
 
-        output_directory = setup_outputdir(output_directory) # replace ~ with absolute path if it exists
+        output_directory = setup_outputdir(output_directory)  # replace ~ with absolute path if it exists
         learner = Learner.load(output_directory)
         return cls(learner=learner)
 
@@ -340,6 +334,21 @@ class TabularPredictor(BasePredictor):
         self._learner.save()
         logger.log(20, "TabularPredictor saved. To load, use: TabularPredictor.load(%s)" % self.output_directory)
 
-    def _summarize(self, key, msg, results):
+    @staticmethod
+    def _summarize(key, msg, results):
         if key in results:
             print(msg + ": " + str(results[key]))
+
+    @staticmethod
+    def __get_dataset(dataset):
+        if isinstance(dataset, TabularDataset):
+            return dataset
+        if isinstance(dataset, str):
+            return TabularDataset(file_path=dataset)
+        if isinstance(dataset, pd.DataFrame):
+            return TabularDataset(df=dataset)
+        if isinstance(dataset, pd.Series):
+            raise TypeError("dataset must be TabularDataset or pandas.DataFrame, not pandas.Series. \
+                   To predict on just single example (ith row of table), use dataset.iloc[[i]] rather than dataset.iloc[i]")
+        else:
+            raise TypeError("dataset must be TabularDataset or pandas.DataFrame or str file path to dataset")
