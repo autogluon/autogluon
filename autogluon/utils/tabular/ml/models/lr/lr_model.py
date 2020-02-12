03ad9860ebdd0d9d25e50adbe6bfc835327fcef4
Alexander Shirkov <sgdread@gmail.com>
2020-02-11 22:20:53 -0800

    Adjusted pre-processing pipeline to adjust for skew, added imputation

--- autogluon/utils/tabular/ml/models/lr/lr_model.py
+++ autogluon/utils/tabular/ml/models/lr/lr_model.py
@@ -1,11 +1,13 @@
 import re
 import time
 
+import numpy as np
 from pandas import DataFrame
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import FeatureUnion, Pipeline
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, QuantileTransformer
 
 from autogluon.utils.tabular.ml.constants import BINARY, MULTICLASS
 from autogluon.utils.tabular.ml.models.abstract.abstract_model import AbstractModel
@@ -18,44 +20,92 @@ class LRModel(AbstractModel):
                  feature_types_metadata=None, debug=0):
         super().__init__(path=path, name=name, problem_type=problem_type, objective_func=objective_func, hyperparameters=hyperparameters, features=features,
                          feature_types_metadata=feature_types_metadata, debug=debug)
+        self.types_of_features = None
         self.pipeline = None
         self.cat_one_hot = None
 
     def tokenize(self, s):
         return re.split('[ ]+', s)
 
+    def _get_types_of_features(self, df):
+        """ Returns dict with keys: : 'continuous', 'skewed', 'onehot', 'embed', 'language', values = ordered list of feature-names falling into each category.
+            Each value is a list of feature-names corresponding to columns in original dataframe.
+            TODO: ensure features with zero variance have already been removed before this function is called.
+        """
+        if self.types_of_features is not None:
+            Warning("Attempting to _get_types_of_features for TabularNeuralNetModel, but previously already did this.")
+        categorical_featnames = self.__get_feature_type_if_present('object') + self.__get_feature_type_if_present('bool')
+        continuous_featnames = self.__get_feature_type_if_present('float') + self.__get_feature_type_if_present('int') + self.__get_feature_type_if_present(
+            'datetime')
+        language_featnames = self.feature_types_metadata['nlp']
+        valid_features = categorical_featnames + continuous_featnames + language_featnames
+        if len(categorical_featnames) + len(continuous_featnames) \
+                + len(language_featnames) \
+                != df.shape[1]:
+            unknown_features = [feature for feature in df.columns if feature not in valid_features]
+            df = df.drop(columns=unknown_features)
+            self.features = list(df.columns)
+
+        types_of_features = {'continuous': [], 'skewed': [], 'onehot': [], 'language': []}
+        # continuous = numeric features to rescale
+        # skewed = features to which we will apply power (ie. log / box-cox) transform before normalization
+        # onehot = features to one-hot encode (unknown categories for these features encountered at test-time are encoded as all zeros). We one-hot encode any features encountered that only have two unique values.
+        one_hot_threshold = 2 if len(language_featnames) > 0 else 10000
+        for feature in self.features:
+            feature_data = df[feature]
+            num_unique_vals = len(feature_data.unique())
+            if feature in language_featnames:
+                types_of_features['language'].append(feature)
+            elif feature in continuous_featnames:
+                if np.abs(feature_data.skew()) > self.params['proc.skew_threshold']:
+                    types_of_features['skewed'].append(feature)
+                else:
+                    types_of_features['continuous'].append(feature)
+            elif (feature in categorical_featnames) and (num_unique_vals <= one_hot_threshold):
+                types_of_features['onehot'].append(feature)
+        return types_of_features
+
+    def __get_feature_type_if_present(self, feature_type):
+        """ Returns crude categorization of feature types """
+        return self.feature_types_metadata[feature_type] if feature_type in self.feature_types_metadata else []
+
     def preprocess(self, X: DataFrame, is_train=False, vect_max_features=1000):
         X = X.copy()
-        nlp_cols = self.feature_types_metadata['nlp']
-        cats = self.feature_types_metadata['object']
-        cats = [c for c in cats if c in self.cat_one_hot]
-
-        cont = self.feature_types_metadata['int'] + self.feature_types_metadata['float']
-        cont = [c for c in cont if '__nlp__' not in c]
-
-        X[cont] = X[cont].fillna(0)
-
-        # TODO: reuse the code from NNs - imputation and quantile transform
+        feature_types = self._get_types_of_features(X)
         if is_train:
-            vectorizer_pipeline = Pipeline(steps=[
-                ("preparator", NlpDataPreprocessor(nlp_cols=nlp_cols)),
-                ("vectorizer", TfidfVectorizer(ngram_range=(1, 3), sublinear_tf=True, max_features=vect_max_features, tokenizer=self.tokenize))
-            ])
+            transformer_list = []
 
-            cats_pipeline = Pipeline(steps=[
-                ('generator', OheFeaturesGenerator(cats_cols=cats)),
-            ])
+            if len(feature_types['language']) > 0:
+                pipeline = Pipeline(steps=[
+                    ("preparator", NlpDataPreprocessor(nlp_cols=feature_types['language'])),
+                    ("vectorizer",
+                     TfidfVectorizer(ngram_range=self.params['proc.ngram_range'], sublinear_tf=True, max_features=vect_max_features, tokenizer=self.tokenize))
+                ])
+                transformer_list.append(('vect', pipeline))
 
-            numeric_feats_pipeline = Pipeline(steps=[
-                ('generator', NumericDataPreprocessor(cont_cols=cont)),
-                ('scaler', StandardScaler())
-            ])
+            if len(feature_types['onehot']) > 0:
+                pipeline = Pipeline(steps=[
+                    ('generator', OheFeaturesGenerator(cats_cols=feature_types['onehot'])),
+                ])
+                # transformer_list.append(('cats', pipeline))
 
-            self.pipeline = FeatureUnion(transformer_list=[
-                ('cats', cats_pipeline),
-                ('vect', vectorizer_pipeline),
-                ('cont', numeric_feats_pipeline),
-            ])
+            if len(feature_types['continuous']) > 0:
+                pipeline = Pipeline(steps=[
+                    ('generator', NumericDataPreprocessor(cont_cols=feature_types['continuous'])),
+                    ('imputer', SimpleImputer(strategy=self.params['proc.impute_strategy'])),
+                    ('scaler', StandardScaler())
+                ])
+                transformer_list.append(('cont', pipeline))
+
+            if len(feature_types['skewed']) > 0:
+                pipeline = Pipeline(steps=[
+                    ('generator', NumericDataPreprocessor(cont_cols=feature_types['skewed'])),
+                    ('imputer', SimpleImputer(strategy=self.params['proc.impute_strategy'])),
+                    ('quantile', QuantileTransformer(output_distribution='normal')),  # Or output_distribution = 'uniform'
+                ])
+                transformer_list.append(('skew', pipeline))
+
+            self.pipeline = FeatureUnion(transformer_list=transformer_list)
             self.pipeline.fit(X)
 
         X = self.pipeline.transform(X)
@@ -66,6 +116,9 @@ class LRModel(AbstractModel):
             'model_type': 'LR',
             'C': 1,
             'vectorizer_dict_size': 3000,
+            'proc.ngram_range': (1, 3),
+            'proc.skew_threshold': 0.99,
+            'proc.impute_strategy': 'median',  # strategy argument of sklearn.SimpleImputer() used to impute missing numeric values
         }
         for param, val in default_params.items():
             self._set_default_param_value(param, val)
