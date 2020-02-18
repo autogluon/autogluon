1c1571efc67710f62c358880e7802fb5d76df102
Jerry Zhang <zhangz6@cs.washington.edu>
2020-02-18 15:11:44 -0800

    add Faster RCNN to autogluon (#290)
    * add Faster RCNN to autogluon
    
    * revert unnecessary changes
    
    * small fix
    
    * fix bug
    
    * small change

---
+++ autogluon/task/object_detection/data_parallel.py
@@ -0,0 +1,62 @@
+from gluoncv.utils.parallel import Parallelizable
+from mxnet import autograd
+from mxnet.contrib import amp
+
+
+class ForwardBackwardTask(Parallelizable):
+    def __init__(self, net, optimizer, rpn_cls_loss, rpn_box_loss, rcnn_cls_loss, rcnn_box_loss,
+                 mix_ratio, enable_amp):
+        super(ForwardBackwardTask, self).__init__()
+        self.net = net
+        self._optimizer = optimizer
+        self.rpn_cls_loss = rpn_cls_loss
+        self.rpn_box_loss = rpn_box_loss
+        self.rcnn_cls_loss = rcnn_cls_loss
+        self.rcnn_box_loss = rcnn_box_loss
+        self.mix_ratio = mix_ratio
+        self.amp = enable_amp
+
+    def forward_backward(self, x):
+        data, label, rpn_cls_targets, rpn_box_targets, rpn_box_masks = x
+        with autograd.record():
+            gt_label = label[:, :, 4:5]
+            gt_box = label[:, :, :4]
+            cls_pred, box_pred, roi, samples, matches, rpn_score, rpn_box, anchors, cls_targets, \
+                box_targets, box_masks, _ = self.net(data, gt_box, gt_label)
+            # losses of rpn
+            rpn_score = rpn_score.squeeze(axis=-1)
+            num_rpn_pos = (rpn_cls_targets >= 0).sum()
+            rpn_loss1 = self.rpn_cls_loss(rpn_score, rpn_cls_targets,
+                                          rpn_cls_targets >= 0) * rpn_cls_targets.size / num_rpn_pos
+            rpn_loss2 = self.rpn_box_loss(rpn_box, rpn_box_targets,
+                                          rpn_box_masks) * rpn_box.size / num_rpn_pos
+            # rpn overall loss, use sum rather than average
+            rpn_loss = rpn_loss1 + rpn_loss2
+            # losses of rcnn
+            num_rcnn_pos = (cls_targets >= 0).sum()
+            rcnn_loss1 = self.rcnn_cls_loss(cls_pred, cls_targets,
+                                            cls_targets.expand_dims(-1) >= 0) * cls_targets.size / \
+                num_rcnn_pos
+            rcnn_loss2 = self.rcnn_box_loss(box_pred, box_targets, box_masks) * box_pred.size / \
+                num_rcnn_pos
+            rcnn_loss = rcnn_loss1 + rcnn_loss2
+            # overall losses
+            total_loss = rpn_loss.sum() * self.mix_ratio + rcnn_loss.sum() * self.mix_ratio
+
+            rpn_loss1_metric = rpn_loss1.mean() * self.mix_ratio
+            rpn_loss2_metric = rpn_loss2.mean() * self.mix_ratio
+            rcnn_loss1_metric = rcnn_loss1.mean() * self.mix_ratio
+            rcnn_loss2_metric = rcnn_loss2.mean() * self.mix_ratio
+            rpn_acc_metric = [[rpn_cls_targets, rpn_cls_targets >= 0], [rpn_score]]
+            rpn_l1_loss_metric = [[rpn_box_targets, rpn_box_masks], [rpn_box]]
+            rcnn_acc_metric = [[cls_targets], [cls_pred]]
+            rcnn_l1_loss_metric = [[box_targets, box_masks], [box_pred]]
+
+            if self.amp:
+                with amp.scale_loss(total_loss, self._optimizer) as scaled_losses:
+                    autograd.backward(scaled_losses)
+            else:
+                total_loss.backward()
+
+        return rpn_loss1_metric, rpn_loss2_metric, rcnn_loss1_metric, rcnn_loss2_metric, \
+            rpn_acc_metric, rpn_l1_loss_metric, rcnn_acc_metric, rcnn_l1_loss_metric
--- autogluon/task/object_detection/nets.py
+++ autogluon/task/object_detection/nets.py
@@ -1,21 +1,35 @@
-import mxnet as mx
-from mxnet import gluon, init
-from gluoncv.model_zoo import get_model
 import gluoncv as gcv
+import mxnet as mx
 
-from ...core import *
 
-def get_built_in_network(name, *args, **kwargs):
-
-    def _get_network(name, transfer_classes, ctx=mx.cpu()):
+def get_built_in_network(meta_arch, name, *args, **kwargs):
+    def _get_network(meta_arch, name, transfer_classes, ctx=mx.cpu()):
         name = name.lower()
-        net_name = '_'.join(('yolo3', name, 'custom'))
-        net = gcv.model_zoo.get_model(net_name, 
-                                  classes=transfer_classes,
-                                  pretrained_base=False, 
-                                  transfer='coco')
-        net.initialize(ctx=ctx, force_reinit=True)
+        if meta_arch == 'yolo3':
+            net_name = '_'.join((meta_arch, name, 'custom'))
+            net = gcv.model_zoo.get_model(net_name,
+                                          classes=transfer_classes,
+                                          pretrained_base=False,
+                                          transfer='coco')
+            net.initialize(ctx=ctx, force_reinit=True)
+        elif meta_arch == 'faster_rcnn':
+            net_name = '_'.join(('custom', meta_arch, 'fpn'))
+            kwargs = {'base_network_name': args.net, 'short': 600, 'max_size': 1000,
+                      'nms_thresh': 0.5, 'nms_topk': -1, 'min_stage': 2, 'max_stage': 6,
+                      'post_nms': -1, 'roi_mode': 'align', 'roi_size': (7, 7),
+                      'strides': (4, 8, 16, 32, 64), 'clip': 4.14, 'rpn_channel': 256,
+                      'base_size': 16, 'scales': (2, 4, 8, 16, 32), 'ratios': (0.5, 1, 2),
+                      'alloc_size': (384, 384), 'rpn_nms_thresh': 0.7, 'rpn_train_pre_nms': 12000,
+                      'rpn_train_post_nms': 2000, 'rpn_test_pre_nms': 6000,
+                      'rpn_test_post_nms': 1000, 'rpn_min_size': 1,
+                      'per_device_batch_size': args.batch_size // args.num_gpus, 'num_sample': 512,
+                      'pos_iou_thresh': 0.5, 'pos_ratio': 0.25, 'max_num_gt': 100}
+            net = gcv.model_zoo.get_model(net_name, classes=transfer_classes,
+                                          pretrained_base=False,
+                                          transfer=None, **kwargs)
+        else:
+            raise NotImplementedError('%s not implemented.' % meta_arch)
         return net
 
     name = name.lower()
-    return _get_network(name, *args, **kwargs)
+    return _get_network(meta_arch, name, *args, **kwargs)
