5798b847ad4f2880565ae38e029a20d9af02360d
Nick Erickson <neerick@amazon.com>
2020-03-03 11:13:28 -0800

    Tabular: Reduced code duplication in HPO code (#347)

--- autogluon/utils/tabular/ml/models/tabular_nn/tabular_nn_trial.py
+++ autogluon/utils/tabular/ml/models/tabular_nn/tabular_nn_trial.py
@@ -1,8 +1,7 @@
-import os
-import time
 import logging
 
 from .tabular_nn_dataset import TabularNNDataset
+from ..abstract import model_trial
 from ....utils.exceptions import TimeLimitExceeded
 from ......core import args
 
@@ -13,39 +12,17 @@ logger = logging.getLogger(__name__)
 def tabular_nn_trial(args, reporter):
     """ Training and evaluation function used during a single trial of HPO """
     try:
-        task_id = args.pop('task_id')
-        util_args = args.pop('util_args')
+        model, args, util_args = model_trial.prepare_inputs(args=args)
 
         train_dataset = TabularNNDataset.load(util_args.train_path)
         test_dataset = TabularNNDataset.load(util_args.test_path)
         y_test = test_dataset.get_labels()
 
-        # Note: args.task_id may not start at 0 if HPO has been run for other models with same scheduler
-        file_prefix = "trial_" + str(task_id)  # append to all file names created during this trial. Do NOT change!
-
-        model = util_args.model
-        model.name = model.name + os.path.sep + file_prefix
-        model.set_contexts(path_context=model.path_root + model.name + os.path.sep)
-
-        time_current = time.time()
-        time_elapsed = time_current - util_args.time_start
-        if 'time_limit' in util_args:
-            time_left = util_args.time_limit - time_elapsed
-            if time_left <= 0:
-                raise TimeLimitExceeded
-        else:
-            time_left = None
-
-        model.params.update(args)
-        time_fit_start = time.time()
-        model.fit(X_train=train_dataset, Y_train=None, X_test=test_dataset, time_limit=time_left, reporter=reporter)
-        time_fit_end = time.time()
-        score = model.score(X=test_dataset, y=y_test)
-        time_pred_end = time.time()
-        model.fit_time = time_fit_end - time_fit_start
-        model.predict_time = time_pred_end - time_fit_end
-        model.val_score = score
-        model.save()
+        fit_model_args = dict(X_train=train_dataset, Y_train=None, X_test=test_dataset)
+        score_model_args = dict(X=test_dataset, y=y_test)
+        model_trial.fit_and_save_model(model=model, params=args, fit_model_args=fit_model_args, score_model_args=score_model_args,
+                                       time_start=util_args.time_start, time_limit=util_args.get('time_limit', None), reporter=reporter)
     except Exception as e:
-        logger.exception(e, exc_info=True)
+        if not isinstance(e, TimeLimitExceeded):
+            logger.exception(e, exc_info=True)
         reporter.terminate()
