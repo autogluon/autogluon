6cf463aaccda465dc9e1c80dd55b184bfc159f5b
Nick Erickson <neerick@amazon.com>
2020-03-02 16:12:50 -0800

    Tabular: Reduced code duplication in HPO code

--- autogluon/utils/tabular/ml/models/abstract/model_trial.py
+++ autogluon/utils/tabular/ml/models/abstract/model_trial.py
@@ -22,42 +22,52 @@ def model_trial(args, reporter: LocalStatusReporter):
             - model.save() must have return_filename, file_prefix, directory options
     """
     try:
-        task_id = args.pop('task_id')
-        util_args = args.pop('util_args')
+        model, args, util_args = prepare_inputs(args=args)
 
-        file_prefix = f"trial_{task_id}"  # append to all file names created during this trial. Do NOT change!
-        model = util_args.model  # the model object must be passed into model_trial() here
-        model.name = model.name + os.path.sep + file_prefix
-        model.set_contexts(path_context=model.path_root + model.name + os.path.sep)
+        X_train, y_train = load_pkl.load(util_args.directory + util_args.dataset_train_filename)
+        X_val, y_val = load_pkl.load(util_args.directory + util_args.dataset_val_filename)
 
-        X_train, Y_train = load_pkl.load(util_args.directory + util_args.dataset_train_filename)
-        X_val, Y_val = load_pkl.load(util_args.directory + util_args.dataset_val_filename)
-
-        time_start = util_args.pop('time_start')
-        time_limit = util_args.pop('time_limit', None)
-        time_current = time.time()
-        time_elapsed = time_current - time_start
-        if time_limit is not None:
-            time_left = time_limit - time_elapsed
-            if time_left <= 0:
-                raise TimeLimitExceeded
-        else:
-            time_left = None
-
-        model.params.update(args)
-        time_fit_start = time.time()
-        model.fit(X_train, Y_train, X_val, Y_val, time_limit=time_left)
-        time_fit_end = time.time()
-        val_perf = model.score(X_val, Y_val)
-        time_pred_end = time.time()
-        model.fit_time = time_fit_end - time_fit_start
-        model.predict_time = time_pred_end - time_fit_end
-        model.val_score = val_perf
-        model.save()
-
+        fit_model_args = dict(X_train=X_train, Y_train=y_train, X_test=X_val, Y_test=y_val)
+        score_model_args = dict(X=X_val, y=y_val)
+        model = fit_and_save_model(model=model, params=args, fit_model_args=fit_model_args, score_model_args=score_model_args,
+                                   time_start=util_args.time_start, time_limit=util_args.get('time_limit', None), reporter=None)
     except Exception as e:
         if not isinstance(e, TimeLimitExceeded):
             logger.exception(e, exc_info=True)
         reporter.terminate()
     else:
-        reporter(epoch=0, validation_performance=val_perf)
+        reporter(epoch=0, validation_performance=model.val_score)
+
+
+def prepare_inputs(args):
+    task_id = args.pop('task_id')
+    util_args = args.pop('util_args')
+
+    file_prefix = f"trial_{task_id}"  # append to all file names created during this trial. Do NOT change!
+    model = util_args.model  # the model object must be passed into model_trial() here
+    model.name = model.name + os.path.sep + file_prefix
+    model.set_contexts(path_context=model.path_root + model.name + os.path.sep)
+    return model, args, util_args
+
+
+def fit_and_save_model(model, params, fit_model_args, score_model_args, time_start, time_limit=None, reporter=None):
+    time_current = time.time()
+    time_elapsed = time_current - time_start
+    if time_limit is not None:
+        time_left = time_limit - time_elapsed
+        if time_left <= 0:
+            raise TimeLimitExceeded
+    else:
+        time_left = None
+
+    model.params.update(params)
+    time_fit_start = time.time()
+    model.fit(**fit_model_args, time_limit=time_left, reporter=reporter)
+    time_fit_end = time.time()
+    val_score = model.score(**score_model_args)
+    time_pred_end = time.time()
+    model.fit_time = time_fit_end - time_fit_start
+    model.predict_time = time_pred_end - time_fit_end
+    model.val_score = val_score
+    model.save()
+    return model
