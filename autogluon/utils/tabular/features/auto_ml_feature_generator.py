2c7c616e102bb6b25ba4b957e70ed23876cb7a41
Razmik Melikbekyan <melikbekyan@yahoo.com>
2020-03-06 23:11:26 +0100

    FIX: tabular utils and lgb clean up (#353)

--- autogluon/utils/tabular/features/auto_ml_feature_generator.py
+++ autogluon/utils/tabular/features/auto_ml_feature_generator.py
@@ -1,17 +1,22 @@
-import logging, copy, traceback, psutil
-import pandas as pd
-from pandas import DataFrame
+import copy
+import logging
+import traceback
+
 import numpy as np
-# from fastai.tabular.transform import add_datepart # TODO: remove
+import pandas as pd
+import psutil
+from pandas import DataFrame
 
 from .abstract_feature_generator import AbstractFeatureGenerator
-from .vectorizers import vectorizer_auto_ml_default
 from .vectorizers import get_ngram_freq, downscale_vectorizer
+from .vectorizers import vectorizer_auto_ml_default
 
 logger = logging.getLogger(__name__)
 
+
 class AutoMLFeatureGenerator(AbstractFeatureGenerator):
-    def __init__(self, enable_nlp_vectorizer_features=True, enable_nlp_ratio_features=True, enable_categorical_features=True, enable_raw_features=True, enable_datetime_features=True,
+    def __init__(self, enable_nlp_vectorizer_features=True, enable_nlp_ratio_features=True,
+                 enable_categorical_features=True, enable_raw_features=True, enable_datetime_features=True,
                  vectorizer=None):
         super().__init__()
         self.enable_nlp_features = enable_nlp_vectorizer_features
@@ -67,11 +72,6 @@ class AutoMLFeatureGenerator(AbstractFeatureGenerator):
         if self.enable_nlp_features and self.features_nlp:
             # Combine Text Fields
             txt = ['. '.join(row) for row in X[self.features_nlp].values]
-            # print(txt[:10])
-            # txt_cleaned = LemmatizerPreprocessor().process(txt)
-            # print(txt_cleaned[:10])
-            # txt_cleaned_2 = StopWordsRemover().process(txt_cleaned)
-            # print(txt_cleaned_2[:10])
 
             X['__nlp__'] = txt  # Could potentially find faster methods if this ends up being slow
 
@@ -79,11 +79,11 @@ class AutoMLFeatureGenerator(AbstractFeatureGenerator):
 
             if not self.fit:
                 features_nlp_to_remove = []
-                logger.log(15, 'Fitting vectorizer for nlp features: '+str(self.features_nlp))
+                logger.log(15, 'Fitting vectorizer for nlp features: ' + str(self.features_nlp))
                 for nlp_feature in features_nlp_current:
                     # TODO: Preprocess text?
                     # print('fitting vectorizer for', nlp_feature, '...')
-                    text_list = (list(X[nlp_feature].drop_duplicates().values))
+                    text_list = list(X[nlp_feature].drop_duplicates().values)
                     vectorizer_raw = copy.deepcopy(self.vectorizer_default_raw)
                     try:
                         vectorizer_fit, _ = self.train_vectorizer(text_list, vectorizer_raw)
@@ -91,14 +91,13 @@ class AutoMLFeatureGenerator(AbstractFeatureGenerator):
                     except ValueError:
                         logger.debug('Removing nlp features due to error')
                         features_nlp_to_remove = self.features_nlp
-                    except:
-                        raise
-                self.features_nlp = [feature for feature in self.features_nlp if feature not in features_nlp_to_remove]
-            X_features_cols_prior_to_nlp = list(X_features.columns)
-            keep_trying_nlp = True
 
+                self.features_nlp = [feature for feature in self.features_nlp if feature not in features_nlp_to_remove]
+
+            X_features_cols_prior_to_nlp = list(X_features.columns)
             downsample_ratio = None
             nlp_failure_count = 0
+            keep_trying_nlp = True
             while keep_trying_nlp:
                 try:
                     X_nlp_features_combined = self.generate_nlp_ngrams(X=X, features_nlp_current=features_nlp_current, downsample_ratio=downsample_ratio)
@@ -116,15 +115,17 @@ class AutoMLFeatureGenerator(AbstractFeatureGenerator):
                         raise
                     traceback.print_tb(err.__traceback__)
 
-                    X_nlp_features_combined = None
                     X_features = X_features[X_features_cols_prior_to_nlp]
                     skip_nlp = False
                     for vectorizer in self.vectorizers:
                         vocab_size = len(vectorizer.vocabulary_)
                         if vocab_size <= 50:
                             skip_nlp = True
-                    if nlp_failure_count >= 3:
-                        skip_nlp = True
+                            break
+                    else:
+                        if nlp_failure_count >= 3:
+                            skip_nlp = True
+
                     if skip_nlp:
                         logger.log(15, 'Warning: ngrams generation resulted in OOM error, removing ngrams features. If you want to use ngrams for this problem, increase memory allocation for AutoGluon.')
                         logger.debug(str(err))
@@ -162,10 +163,10 @@ class AutoMLFeatureGenerator(AbstractFeatureGenerator):
 
                 if downsample_ratio is not None:
                     if (downsample_ratio >= 1) or (downsample_ratio <= 0):
-                        raise ValueError('downsample_ratio must be <1 and >0, but downsample_ratio is ' + str(downsample_ratio))
+                        raise ValueError(f'downsample_ratio must be >0 and <1, but downsample_ratio is {downsample_ratio}')
                     vocab_size = len(vectorizer_fit.vocabulary_)
                     downsampled_vocab_size = int(np.floor(vocab_size * downsample_ratio))
-                    logger.debug('Reducing Vectorizer vocab size from '+str(vocab_size)+' to '+str(downsampled_vocab_size)+' to avoid OOM error')
+                    logger.debug(f'Reducing Vectorizer vocab size from {vocab_size} to {downsampled_vocab_size} to avoid OOM error')
                     ngram_freq = get_ngram_freq(vectorizer=vectorizer_fit, transform_matrix=transform_matrix)
                     downscale_vectorizer(vectorizer=vectorizer_fit, ngram_freq=ngram_freq, vocab_size=downsampled_vocab_size)
                     # TODO: This doesn't have to be done twice, can update transform matrix based on new vocab instead of calling .transform
@@ -175,7 +176,7 @@ class AutoMLFeatureGenerator(AbstractFeatureGenerator):
             nlp_features_names = vectorizer_fit.get_feature_names()
 
             X_nlp_features = pd.DataFrame(transform_matrix.toarray())  # FIXME
-            X_nlp_features.columns = [nlp_feature + '.' + str(x) for x in nlp_features_names]
+            X_nlp_features.columns = [f'{nlp_feature}.{x}' for x in nlp_features_names]
             X_nlp_features[nlp_feature + '._total_'] = X_nlp_features.gt(0).sum(axis=1)
 
             X_nlp_features_combined.append(X_nlp_features)
--- autogluon/utils/tabular/ml/utils.py
+++ autogluon/utils/tabular/ml/utils.py
@@ -1,15 +1,15 @@
-import gc, os, multiprocessing, logging
+import logging
+import multiprocessing
+import os
+from collections import defaultdict
+from datetime import datetime
+
 import numpy as np
 import pandas as pd
-from collections import defaultdict
 from pandas import DataFrame, Series
-from datetime import datetime
 from sklearn.model_selection import KFold, StratifiedKFold, RepeatedKFold, RepeatedStratifiedKFold, train_test_split
 
-from .constants import BINARY, REGRESSION, MULTICLASS, SOFTCLASS
-from ..utils.savers import save_pd
-from ..utils.decorators import calculate_time
-from ...try_import import try_import_lightgbm
+from .constants import BINARY, REGRESSION, SOFTCLASS
 
 logger = logging.getLogger(__name__)
 
@@ -25,7 +25,6 @@ def get_pred_from_proba(y_pred_proba, problem_type=BINARY):
 
 
 def generate_kfold(X, y=None, n_splits=5, random_state=0, stratified=False, n_repeats=1):
-    kfolds = []
     if stratified and (y is not None):
         if n_repeats > 1:
             kf = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state=random_state)
@@ -33,22 +32,21 @@ def generate_kfold(X, y=None, n_splits=5, random_state=0, stratified=False, n_re
             kf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=random_state)
 
         kf.get_n_splits(X, y)
-        for train_index, test_index in kf.split(X, y):
-            kfolds.append([train_index, test_index])
+        return [[train_index, test_index] for train_index, test_index in kf.split(X, y)]
     else:
         if n_repeats > 1:
             kf = RepeatedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state=random_state)
         else:
             kf = KFold(n_splits=n_splits, shuffle=True, random_state=random_state)
+
         kf.get_n_splits(X)
-        for train_index, test_index in kf.split(X):
-            kfolds.append([train_index, test_index])
-    return kfolds
+        return [[train_index, test_index] for train_index, test_index in kf.split(X)]
 
 
 def generate_train_test_split(X: DataFrame, y: Series, problem_type: str, test_size: float = 0.1, random_state=42) -> (DataFrame, DataFrame, Series, Series):
     if (test_size <= 0.0) or (test_size >= 1.0):
         raise ValueError("fraction of data to hold-out must be specified between 0 and 1")
+
     if problem_type in [REGRESSION, SOFTCLASS]:
         stratify = None
     else:
@@ -68,126 +66,9 @@ def generate_train_test_split(X: DataFrame, y: Series, problem_type: str, test_s
     return X_train, X_test, y_train, y_test
 
 
-# TODO: Move to lgb
-def construct_dataset(x: DataFrame, y: Series, location=None, reference=None, params=None, save=False, weight=None):
-    try_import_lightgbm()
-    import lightgbm as lgb
-    # save_pd.save(path=location + '.csv', df=x, header=False)
-    feature_list = list(x.columns.values)
-    # dataset = lgb.Dataset(data=location + '.csv', label=y, reference=reference, feature_name=feature_list)
-    dataset = lgb.Dataset(data=x, label=y, reference=reference, free_raw_data=True, params=params, weight=weight)
-
-    if save:
-        if os.path.exists(location + '.bin'):
-            os.remove(location + '.bin')
-        else:
-            pass
-
-        os.makedirs(os.path.dirname(location + '.bin'), exist_ok=True)
-        dataset.save_binary(location + '.bin')
-        # dataset_binary = lgb.Dataset(location + '.bin', reference=reference, free_raw_data=False)# .construct()
-
-
-    return dataset
-
-
-# TODO: Move to lgb
-def construct_dataset_low_memory(X: DataFrame, y: Series, location, reference=None, params=None):
-    try_import_lightgbm()
-    import lightgbm as lgb
-    cat_columns = list(X.select_dtypes(include='category').columns.values)
-    # X = X.drop(columns_categorical, axis=1)
-
-    X[cat_columns] = X[cat_columns].apply(lambda x: x.cat.codes)
-
-    columns = list(X.columns)
-    for column in columns:
-        column_data = X[column]
-
-    split_train = len(X)
-    # split_train = 11111111  # border between train/test in pickle (length of our train)
-    n_attrs = len(X.columns)
-    # n_attrs = 25  # as is
-
-    pickle_list = [X]
-    # pickle_list = ['attrs_xxxx', 'attrs_base_cnt', 'attrs_nunique']  # list of pickled attrs
-    # del_cols = ['click_time', 'day', ]  # attrs to be deleted in final train
-
-
-    si = 0
-    os.makedirs(os.path.dirname(location + '.mmp'), exist_ok=True)
-    mmap = np.memmap(location + '.mmp', dtype='float32', mode='w+', shape=(split_train, n_attrs))
-
-
-    columns = []
-    for pkl in pickle_list:
-        _temp = pkl
-        # _temp = load_attrs(pkl)
-
-        _columns = [x for x in _temp.columns]
-        columns = columns + _columns
-
-        nodel_ind = [_temp.columns.tolist().index(x) for x in _temp.columns]
-
-        _temp = _temp.iloc[:split_train, nodel_ind]
-
-        ei = _temp.values.shape[1]
-        mmap[:, si:si+ei] = _temp.values
-        si += ei
-
-        del _temp
-        gc.collect()
-
-    mmap.flush()
-    del mmap
-    gc.collect()
-
-    mmap = np.memmap(location + '.mmp', dtype='float32', mode='r', shape=(split_train, n_attrs))
-    _train = np.array(mmap[:split_train])
-    # _val = np.array(mmap[split_train:])
-
-    # _train = _train[:, columns.index('is_attributed')]
-    # _val = _val[:, columns.index('is_attributed')]
-
-    use_columns = columns
-    # muse_columns = [columns.index(x) for x in use_columns]
-
-    # d_train = _train[:, muse_columns]
-    xgtrain = lgb.Dataset(_train, label=y, params=params, reference=reference, categorical_feature=cat_columns, feature_name=columns)
-    # d_val = _val[:, muse_columns]
-    # xgvalid = lgb.Dataset(d_val, label=y_test, reference=xgtrain, **params)
-
-    # bst = lgb.train(model_params, xgtrain, valid_sets=[xgvalid], valid_names=['valid'], evals_result=evals_results, **fit_params)
-
-    return xgtrain
-
-
-# TODO: Move to lgb
-@calculate_time
-def construct_dataset_lowest_memory(X: DataFrame, y: Series, location, reference=None, params=None):
-
-    try_import_lightgbm()
-    import lightgbm as lgb
-    cat_columns = list(X.select_dtypes(include='category').columns.values)
-
-    columns = list(X.columns)
-    X[cat_columns] = X[cat_columns].apply(lambda x: x.cat.codes)
-
-    cat_columns_index = [columns.index(cat) for cat in cat_columns]
-
-    logger.log(15, 'Saving... '+str(location)+'.csv')
-    save_pd.save(path=location + '.csv', df=X, header=False, index=True)
-
-    xgtrain = lgb.Dataset(location + '.csv', label=y, params=params, reference=reference, categorical_feature=cat_columns_index,
-                          feature_name=columns,
-                          )
-
-    return xgtrain
-
-
 def convert_categorical_to_int(X):
     X = X.copy()
-    cat_columns = X.select_dtypes(['category']).columns
+    cat_columns = X.select_dtypes(include=['category']).columns
     X[cat_columns] = X[cat_columns].apply(lambda x: x.cat.codes)
     return X
 
@@ -196,21 +77,22 @@ def setup_outputdir(output_directory):
     if output_directory is None:
         utcnow = datetime.utcnow()
         timestamp = utcnow.strftime("%Y%m%d_%H%M%S")
-        output_directory = "AutogluonModels/ag-" + timestamp + '/'
+        output_directory = f"AutogluonModels/ag-{timestamp}{os.path.sep}"
         os.makedirs(output_directory)
-        logger.log(25, "No output_directory specified. Models will be saved in: %s" % output_directory)
+        logger.log(25, f"No output_directory specified. Models will be saved in: {output_directory}")
     output_directory = os.path.expanduser(output_directory)  # replace ~ with absolute path if it exists
-    if output_directory[-1] != '/':
-        output_directory = output_directory + '/'
+    if output_directory[-1] != os.path.sep:
+        output_directory = output_directory + os.path.sep
     return output_directory
 
 
 def setup_compute(nthreads_per_trial, ngpus_per_trial):
     if nthreads_per_trial is None:
         nthreads_per_trial = multiprocessing.cpu_count()  # Use all of processing power / trial by default. To use just half: # int(np.floor(multiprocessing.cpu_count()/2))
+
     if ngpus_per_trial is None:
-        ngpus_per_trial = 0 # do not use GPU by default
-    if ngpus_per_trial > 1:
+        ngpus_per_trial = 0  # do not use GPU by default
+    elif ngpus_per_trial > 1:
         ngpus_per_trial = 1
         logger.debug("tabular_prediction currently doesn't use >1 GPU per training run. ngpus_per_trial set = 1")
     return nthreads_per_trial, ngpus_per_trial
@@ -221,12 +103,13 @@ def setup_trial_limits(time_limits, num_trials, hyperparameters={'NN': None}):
     if num_trials is None:
         if time_limits is None:
             time_limits = 10 * 60  # run for 10min by default
-        time_limits /= float(len(hyperparameters.keys()))  # each model type gets half the available time
+        time_limits /= float(len(hyperparameters))  # each model type gets half the available time
         num_trials = 1000  # run up to 1000 trials (or as you can within the given time_limits)
     elif time_limits is None:
         time_limits = int(1e6)  # user only specified num_trials, so run all of them regardless of time-limits
     else:
-        time_limits /= float(len(hyperparameters.keys()))  # each model type gets half the available time
+        time_limits /= float(len(hyperparameters))  # each model type gets half the available time
+
     if time_limits <= 10:  # threshold = 10sec, ie. too little time to run >1 trial.
         num_trials = 1
     time_limits *= 0.9  # reduce slightly to account for extra time overhead
@@ -243,16 +126,16 @@ def combine_pred_and_true(y_predprob, y_true, upweight_factor=0.25):
     """
     if len(y_predprob) != len(y_true):
         raise ValueError("y_predprob and y_true cannot have different lengths for distillation. Perhaps some classes' data was deleted during label cleaning.")
-    
-    y_trueprob = np.zeros((y_true.size, y_true.max()+1))
-    y_trueprob[np.arange(y_true.size),y_true] = upweight_factor
+
+    y_trueprob = np.zeros((y_true.size, y_true.max() + 1))
+    y_trueprob[np.arange(y_true.size), y_true] = upweight_factor
     y_predprob = y_predprob + y_trueprob
-    y_predprob = y_predprob / y_predprob.sum(axis=1, keepdims=1) # renormalize
+    y_predprob = y_predprob / y_predprob.sum(axis=1, keepdims=1)  # renormalize
     return y_predprob
 
 
-# Returns DataFrame with rows shuffled based on seed value
 def shuffle_df_rows(X: DataFrame, seed=0, reset_index=True):
+    """Returns DataFrame with rows shuffled based on seed value."""
     row_count = X.shape[0]
     np.random.seed(seed)
     rand_shuffle = np.random.randint(0, row_count, size=row_count)
