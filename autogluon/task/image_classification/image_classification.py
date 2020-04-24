d8b2e37517348f494c875db469114d29bbb8d878
Joshua Z. Zhang <cheungchih@gmail.com>
2020-04-24 16:12:29 -0700

    address comments

--- autogluon/task/image_classification/image_classification.py
+++ autogluon/task/image_classification/image_classification.py
@@ -27,22 +27,22 @@ class ImageClassification(BaseTask):
     Classifier = Classifier
 
     @staticmethod
-    def Dataset(path=None, train=True, name=None, input_size=224, crop_ratio=0.875, *args, **kwargs):
+    def Dataset(path=None, name=None, train=True, input_size=224, crop_ratio=0.875, *args, **kwargs):
         """Dataset for AutoGluon image classification tasks.
            May either be a :class:`autogluon.task.image_classification.ImageFolderDataset`, :class:`autogluon.task.image_classification.RecordDataset`,
            or a popular dataset already built into AutoGluon ('mnist', 'fashionmnist', 'cifar10', 'cifar100', 'imagenet').
 
         Parameters
         ----------
-        path : str
+        path : str, optional
             The data location. If using :class:`ImageFolderDataset`,
             image folder`path/to/the/folder` should be provided.
             If using :class:`RecordDataset`, the `path/to/*.rec` should be provided.
-        train : bool, default = True
-            Whether this dataset should be used for training or validation.
         name : str, optional
             Which built-in dataset to use, will override all other options if specified.
             The options are: 'mnist', 'fashionmnist', 'cifar', 'cifar10', 'cifar100', 'imagenet'
+        train : bool, optional, default = True
+            Whether this dataset should be used for training or validation.
         input_size : int
             The input image size.
         crop_ratio : float
@@ -55,7 +55,8 @@ class ImageClassification(BaseTask):
         """
         if name is None:
             if path is None:
-                raise ValueError("Either `path` or `name` must be present in Dataset()")
+                raise ValueError("Either `path` or `name` must be present in Dataset(). "
+                                 "If `name` is provided, it will override the rest of the arguments.")
         return get_dataset(path=path, train=train, name=name,
                            input_size=input_size, crop_ratio=crop_ratio,
                            *args, **kwargs)
