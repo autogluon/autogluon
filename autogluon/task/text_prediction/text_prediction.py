c05d1dc0a7b6d884c855b02a8ed6bfc16679bbc1
Xingjian Shi <xshiab@connect.ust.hk>
2020-07-18 01:36:16 -0700

    Update text_prediction.py

--- autogluon/task/text_prediction/text_prediction.py
+++ autogluon/task/text_prediction/text_prediction.py
@@ -23,6 +23,7 @@ def default():
     ret = {
         'BertForTextPredictionBasic': {
             'model.backbone.name': 'google_electra_base',
+            'optimization.batch_size': core.space.Categorical(32, 64),
             'optimization.num_train_epochs': core.space.Categorical(3, 10),
             'optimization.lr': core.space.Real(1E-5, 1E-4)
         }
