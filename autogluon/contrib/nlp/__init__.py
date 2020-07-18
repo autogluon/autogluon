8d7fa774204af01767d5067311c394161cddf759
Xingjian Shi <xshiab@connect.ust.hk>
2020-07-18 00:06:39 -0700

    Update __init__.py

---
+++ autogluon/contrib/nlp/__init__.py
@@ -0,0 +1,13 @@
+from . import base
+from . import data
+from . import models
+from . import utils
+from . import attention_cell
+from . import initializer as init
+from . import layers
+from . import loss
+from . import lr_scheduler
+from . import op
+from . import optimizers
+from . import registry
+from . import sequence_sampler
