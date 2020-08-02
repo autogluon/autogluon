463c923a1137b6ebf19e54e394369a7950150a86
Xingjian Shi <xshiab@connect.ust.hk>
2020-08-01 18:06:14 -0700

    try to use tqdm auto

--- autogluon/task/text_prediction/models/basic_v1.py
+++ autogluon/task/text_prediction/models/basic_v1.py
@@ -7,7 +7,7 @@ import collections
 import time
 import json
 import functools
-import tqdm
+from tqdm.auto import tqdm
 import mxnet as mx
 from mxnet.util import use_np
 from mxnet.lr_scheduler import PolyScheduler, CosineScheduler
@@ -433,7 +433,7 @@ def train_function(args, reporter, train_data, tuning_data,
     no_better_rounds = 0
     report_idx = 0
     start_tick = time.time()
-    for update_idx in tqdm.tqdm(range(max_update)):
+    for update_idx in tqdm(range(max_update)):
         num_samples_per_update_l = [0 for _ in ctx_l]
         for accum_idx in range(num_accumulated):
             sample_l = next(train_loop_dataloader)
