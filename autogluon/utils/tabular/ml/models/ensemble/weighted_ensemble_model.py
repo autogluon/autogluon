5aaafad8c835325ac8bd5fe4a08af1e4492e257b
Nick Erickson <neerick@amazon.com>
2020-08-18 18:49:09 -0700

    Tabular: Fixed defect in feature importance when duplicated indices in input, fixed defect in lgbm feature importance

--- autogluon/utils/tabular/ml/models/ensemble/weighted_ensemble_model.py
+++ autogluon/utils/tabular/ml/models/ensemble/weighted_ensemble_model.py
@@ -43,7 +43,7 @@ class WeightedEnsembleModel(StackerEnsembleModel):
             weights_dict[key] = weights_dict[key] / num_models
         return weights_dict
 
-    def compute_feature_importance(self, X, y, features_to_use=None, preprocess=True, is_oof=True, **kwargs):
+    def compute_feature_importance(self, X, y, features_to_use=None, preprocess=True, is_oof=True, **kwargs) -> pd.Series:
         logger.warning('Warning: non-raw feature importance calculation is not valid for weighted ensemble since it does not have features, returning ensemble weights instead...')
         if is_oof:
             feature_importance = pd.Series(self._get_model_weights()).sort_values(ascending=False)
