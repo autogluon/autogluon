c6b0710ec69017e1c14506e181f7c5665461e503
Nick Erickson <neerick@amazon.com>
2020-09-23 18:03:43 -0700

    Updated docs

--- autogluon/task/tabular_prediction/tabular_prediction.py
+++ autogluon/task/tabular_prediction/tabular_prediction.py
@@ -221,6 +221,7 @@ class TabularPrediction(BaseTask):
                     'GBM': [
                         {},
                         {'extra_trees': True, 'AG_args': {'name_suffix': 'XT'}},
+                        {'boosting_type': 'dart', 'AG_args': {'name_suffix': 'DART', 'priority': -5}},
                     ],
                     'CAT': {},
                     'RF': [
