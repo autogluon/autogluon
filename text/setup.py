5bb8b1296b3e26ab4b43ce0cb19d55d2c60fdeb3
Nick Erickson <neerick@amazon.com>
2020-10-22 12:42:55 -0700

    Cleaned setup.py dependencies

--- text/setup.py
+++ text/setup.py
@@ -40,34 +40,17 @@ long_description = open(os.path.join('..', 'README.md')).read()
 MIN_PYTHON_VERSION = '>=3.6.*'
 
 requirements = [
-    'Pillow<=6.2.1',
     'numpy>=1.16.0',
     'scipy>=1.3.3',
-    'cython',
-    'tornado>=5.0.1',
-    'requests',
-    'matplotlib',
     'tqdm>=4.38.0',
-    'paramiko>=2.4',
-    'dask>=2.6.0',
-    'cryptography>=2.8',
-    'distributed>=2.6.0',
-    'ConfigSpace<=0.4.10',
-    'gluoncv>=0.5.0,<1.0',
-    'graphviz<0.9.0,>=0.8.1',
-    'fastparquet==0.4.1',
-    'scikit-optimize',
-    'pyarrow<=1.0.0',
-    'boto3',
     'pandas>=1.0.0,<2.0',
-    'psutil>=5.0.0,<=5.7.0',  # TODO: psutil 5.7.1/5.7.2 has non-deterministic error on CI doc build -  ImportError: cannot import name '_psutil_linux' from 'psutil'
     'scikit-learn>=0.22.0,<0.24',
-    'networkx>=2.3,<3.0',
     'autogluon.core',
     'autogluon.mxnet'
 ]
 
 text_requirements = [
+    'pyarrow<=1.0.0',
     'autogluon-contrib-nlp'
 ]
 
