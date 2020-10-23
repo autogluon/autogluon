0c2688091891091a5075a51eea2c552f088c2c42
Jaroo <david.oh0126@gmail.com>
2020-10-23 15:23:14 +0900

    Updated small changes to import and setup

--- tabular/setup.py
+++ tabular/setup.py
@@ -56,11 +56,13 @@ requirements = [
     'fastparquet==0.4.1',
     'scikit-optimize',
     'catboost>=0.23.0,<0.25',
+    'xgboost>=1.2,<1.3',
     'pyarrow<=1.0.0',
     'boto3',
     'lightgbm>=3.0,<4.0',
     'pandas>=1.0.0,<2.0',
-    'psutil>=5.0.0,<=5.7.0',  # TODO: psutil 5.7.1/5.7.2 has non-deterministic error on CI doc build -  ImportError: cannot import name '_psutil_linux' from 'psutil'
+    'psutil>=5.0.0,<=5.7.0',
+    # TODO: psutil 5.7.1/5.7.2 has non-deterministic error on CI doc build -  ImportError: cannot import name '_psutil_linux' from 'psutil'
     'scikit-learn>=0.22.0,<0.24',
     'networkx>=2.3,<3.0',
     'autogluon.core'
