ce1232245cdf99f9fa03259f14cce28a5712fb15
Alexander Shirkov <sgdread@gmail.com>
2020-10-23 17:22:23 -0700

    [Modularizaton] added back required dependency

--- tabular/setup.py
+++ tabular/setup.py
@@ -46,6 +46,7 @@ requirements = [
     'psutil>=5.0.0,<=5.7.0',  # TODO: psutil 5.7.1/5.7.2 has non-deterministic error on CI doc build -  ImportError: cannot import name '_psutil_linux' from 'psutil'
     'scikit-learn>=0.22.0,<0.24',
     'networkx>=2.3,<3.0',
+    'gluoncv>=0.5.0,<1.0',
     f'autogluon.core=={version}'
 ]
 
