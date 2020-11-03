32ced7c8596e92b3f4bfcea88dff509c5e38347c
Xingjian Shi <xshiab@connect.ust.hk>
2020-11-03 00:11:41 -0800

    Update basic_v1.py

--- text/src/autogluon/text/text_prediction/models/basic_v1.py
+++ text/src/autogluon/text/text_prediction/models/basic_v1.py
@@ -22,13 +22,13 @@ from autogluon_contrib_nlp.utils.misc import logging_config, grouper,\
 from autogluon_contrib_nlp.utils.parameter import move_to_ctx, clip_grad_global_norm
 from autogluon.core import args, space
 from autogluon.core.scheduler import FIFOScheduler, HyperbandScheduler
-from .. import version
 from ..metrics import calculate_metric_by_expr
 from .. import constants as _C
 from ..column_property import get_column_property_metadata, get_column_properties_from_metadata
 from ..preprocessing import TabularBasicBERTPreprocessor
 from ..modules.basic_prediction import BERTForTabularBasicV1
 from ..dataset import TabularDataset
+from .... import version
 
 
 @use_np
