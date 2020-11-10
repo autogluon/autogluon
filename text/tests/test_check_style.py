2d5e8738945b5d2bb26774bc719cf52a8bd5e22b
Nick Erickson <neerick@amazon.com>
2020-11-09 17:05:18 -0800

    fix rootdir

--- text/tests/test_check_style.py
+++ text/tests/test_check_style.py
@@ -1,5 +1,4 @@
 import logging
-import os
 import warnings
 from subprocess import Popen, PIPE
 
@@ -7,10 +6,7 @@ from subprocess import Popen, PIPE
 def test_check_style():
     logging.getLogger().setLevel(logging.INFO)
     logging.info("PEP8 Style check")
-    wd = os.getcwd()
-    os.chdir("..")
     flake8_proc = Popen(['flake8', '--count', '--max-line-length', '300'], stdout=PIPE)
-    os.chdir(wd)
     flake8_out = flake8_proc.communicate()[0]
     lines = flake8_out.splitlines()
     count = int(lines[-1].decode())
