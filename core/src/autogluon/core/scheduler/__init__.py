548418410294a2e2e7dfa59c3bb41e058179563c
Xingjian Shi <xshiab@connect.ust.hk>
2020-11-16 08:15:47 -0800

    fix

--- core/src/autogluon/core/scheduler/__init__.py
+++ core/src/autogluon/core/scheduler/__init__.py
@@ -1,8 +1,8 @@
 from .import remote, resource
+from .resource import get_cpu_count, get_gpu_count
 
 # schedulers
 from .scheduler import *
 from .fifo import *
 from .hyperband import *
 from .rl_scheduler import *
-from ..utils.utils import get_cpu_count, get_gpu_count
