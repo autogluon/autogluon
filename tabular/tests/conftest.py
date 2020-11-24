79629280e0ca54794a7b111a5b69b4a2f6f834df
Xingjian Shi <xshiab@connect.ust.hk>
2020-11-24 01:21:59 -0800

    fix

--- tabular/tests/conftest.py
+++ tabular/tests/conftest.py
@@ -55,7 +55,7 @@ class DatasetLoaderHelper:
         # Regression with multiple text field and categorical
         sts={
             'url': 'https://autogluon-text.s3-us-west-2.amazonaws.com/glue_sts.zip',
-            'name': 'Semantic Textual Similarity Dataset',
+            'name': 'SemanticTextualSimilarity',
             'problem_type': REGRESSION,
             'label_column': 'score',
         }
