e1f560fd6a1d511c1d1e9213f838d359dedd564f
Nick Erickson <neerick@amazon.com>
2020-12-16 15:40:53 -0800

    refactoring

--- core/src/autogluon/core/task/base/base_task.py
+++ core/src/autogluon/core/task/base/base_task.py
@@ -10,6 +10,7 @@ from ...utils import in_ipynb, try_import_mxnet
 __all__ = [
     'BaseTask',
     'compile_scheduler_options',
+    'compile_scheduler_options_v2',
     'create_scheduler']
 
 Results = collections.namedtuple('Results', 'model reward config time metadata')
@@ -97,9 +98,9 @@ searcher_for_hyperband_strategy = {
 
 
 def compile_scheduler_options(
-        scheduler_options, search_strategy, search_options, nthreads_per_trial,
-        ngpus_per_trial, checkpoint, num_trials, time_out, resume, visualizer,
-        time_attr, reward_attr, dist_ip_addrs, epochs=None):
+        scheduler_options, search_strategy, nthreads_per_trial,
+        ngpus_per_trial, num_trials, time_out, search_options=None, checkpoint=None, resume=False, visualizer=None,
+        time_attr=None, reward_attr=None, dist_ip_addrs=None, epochs=None):
     """
     Updates a copy of scheduler_options (scheduler-specific options, can be
     empty) with general options. The result can be passed to __init__ of the
@@ -133,6 +134,8 @@ def compile_scheduler_options(
         scheduler_options = dict()
     else:
         assert isinstance(scheduler_options, dict)
+    if dist_ip_addrs is None:
+        dist_ip_addrs = []
     assert isinstance(search_strategy, str)
     if search_options is None:
         search_options = dict()
@@ -141,7 +144,7 @@ def compile_scheduler_options(
     if time_attr is None:
         time_attr = 'epoch'
     if reward_attr is None:
-        reward_attr = 'accuracy'
+        reward_attr = 'validation_performance'
     scheduler_options = copy.copy(scheduler_options)
     scheduler_options.update({
         'resource': {
@@ -162,3 +165,97 @@ def compile_scheduler_options(
         if epochs is not None:
             scheduler_options['max_t'] = epochs
     return scheduler_options
+
+
+# TODO: Migrate TextPrediction to use this version, delete old version
+def compile_scheduler_options_v2(
+        scheduler_options, nthreads_per_trial,
+        ngpus_per_trial, num_trials, time_out, search_strategy=None, search_options=None, checkpoint=None, resume=False, visualizer=None,
+        time_attr=None, reward_attr=None, dist_ip_addrs=None, epochs=None):
+    """
+    Updates a copy of scheduler_options (scheduler-specific options, can be
+    empty) with general options. The result can be passed to __init__ of the
+    scheduler.
+
+    Special role of epochs for HyperbandScheduler: If the search_strategy
+    involves HyperbandScheduler and epochs is given, then this value is
+    copied to scheduler_options['max_t']. Pass epochs for applications
+    where the time_attr is epoch, and epochs is the maximum number of
+    epochs.
+
+    :param scheduler_options:
+    :param search_strategy:
+    :param search_options:
+    :param nthreads_per_trial:
+    :param ngpus_per_trial:
+    :param checkpoint:
+    :param num_trials:
+    :param time_out:
+    :param resume:
+    :param visualizer:
+    :param time_attr:
+    :param reward_attr:
+    :param dist_ip_addrs:
+    :param kwargs:
+    :param epochs: See above. Optional
+    :return: Copy of scheduler_options with updates
+
+    """
+    if scheduler_options is None:
+        scheduler_options = dict()
+    else:
+        assert isinstance(scheduler_options, dict)
+    if dist_ip_addrs is None:
+        dist_ip_addrs = []
+    if search_strategy is None:
+        search_strategy = 'random'
+    assert isinstance(search_strategy, str)
+    if search_options is None:
+        search_options = dict()
+    if visualizer is None:
+        visualizer = 'none'
+    if time_attr is None:
+        time_attr = 'epoch'
+    if reward_attr is None:
+        reward_attr = 'validation_performance'
+    _scheduler_options = {
+        'resource': {
+            'num_cpus': nthreads_per_trial, 'num_gpus': ngpus_per_trial},
+        'searcher': search_strategy,
+        'search_options': search_options,
+        'checkpoint': checkpoint,
+        'resume': resume,
+        'num_trials': num_trials,
+        'time_out': time_out,
+        'reward_attr': reward_attr,
+        'time_attr': time_attr,
+        'visualizer': visualizer,
+        'dist_ip_addrs': dist_ip_addrs,
+    }
+    _scheduler_options.update(copy.copy(scheduler_options))
+
+    searcher = searcher_for_hyperband_strategy.get(_scheduler_options['searcher'])
+    if searcher is not None:
+        _scheduler_options['searcher'] = searcher
+        if epochs is not None:
+            _scheduler_options['max_t'] = epochs
+    required_options = [
+        'resource',
+        'searcher',
+        'search_options',
+        'checkpoint',
+        'resume',
+        'num_trials',
+        'time_out',
+        'reward_attr',
+        'time_attr',
+        'visualizer',
+        'dist_ip_addrs',
+    ]
+    missing_options = []
+    for option in required_options:
+        if option not in _scheduler_options:
+            missing_options.append(option)
+    if missing_options:
+        raise AssertionError(f'Missing required keys in scheduler_options: {missing_options}')
+    return _scheduler_options
--- tabular/src/autogluon/tabular/task/tabular_prediction/predictor_v2.py
+++ tabular/src/autogluon/tabular/task/tabular_prediction/predictor_v2.py
@@ -5,7 +5,7 @@ import math
 import numpy as np
 import pandas as pd
 
-from autogluon.core.task.base import compile_scheduler_options
+from autogluon.core.task.base import compile_scheduler_options_v2
 from autogluon.core.task.base.base_task import schedulers
 from autogluon.core.utils import verbosity2loglevel
 from autogluon.core.utils.loaders import load_pkl
@@ -70,21 +70,17 @@ class TabularPredictorV2(TabularPredictor):
             hyperparameters=None,
             feature_generator="auto",
             feature_metadata=None,
-            # verbosity=None,  # TODO: Add
-            hyperparameter_tune=False,  # TODO: Remove but add string default options to scheduler_kwargs? Rename to hyperparameter_tune_kwargs
-            num_trials=None,  # TODO: REMOVE
-            search_strategy='random',  # TODO: REMOVE
+            hyperparameter_tune=False,  # TODO: Remove but add string default options to hyperparameter_tune_kwargs?
+            hyperparameter_tune_kwargs=None,
             **kwargs):
         """
         Fit models to predict a column of data table based on the other columns.
 
         # TODO: Move documentation from TabularPrediction.fit to here
-        # TODO: Move all scheduler/searcher specific arguments into search_options and scheduler_options to simplify documentation.
+        # TODO: Move all scheduler/searcher specific arguments into hyperparameter_tune_kwargs
 
         """
         self._validate_fit_kwargs(kwargs)
-
-        assert search_strategy != 'bayesopt_hyperband', "search_strategy == 'bayesopt_hyperband' not yet supported"
 
         verbosity = kwargs.get('verbosity', self.verbosity)
         if verbosity < 0:
@@ -94,39 +90,32 @@ class TabularPredictorV2(TabularPredictor):
 
         logger.setLevel(verbosity2loglevel(verbosity))
 
-        # TODO: v0.1 - time_limits -> time_limit?
-        # TODO: v0.1 - stack_ensemble_levels -> num_stack_levels / num_stack_layers?
-        # TODO: v0.1 - id_columns -> ignored_columns?
-        # TODO: v0.1 - nthreads_per_trial/ngpus_per_trial -> rename/rework
-        # TODO: v0.1 - visualizer -> consider reworking/removing
+        # TODO: Stopping metric -> Default to None for models, let model choose if None as if hyperparameter
+        # AG_args_fit = {'num_gpus': -1} -> 'auto' by default
+        # TODO: v0.1 - time_limits -> time_limit? -> +1
+        # TODO: v0.1 - stack_ensemble_levels -> num_stack_levels / num_stack_layers? -> num_stack_levels
+        # TODO: v0.1 - id_columns -> ignored_columns? -> +1
+        # TODO: v0.1 - num_cpus/num_gpus -> rename/rework -> num_threads, num_gpus in AG_args_fit -> HPO overrides
+        # TODO: v0.1 - visualizer -> consider reworking/removing -> AG_args_fit argument
         # TODO: v0.1 - HPO arguments to a generic hyperparameter_tune_kwargs parameter?
         # TODO: v0.1 - stack_ensemble_levels is silently ignored if num_bagging_folds < 2, ensure there is a warning printed
 
         holdout_frac = kwargs.get('holdout_frac', None)
-        num_bagging_folds = kwargs.get('num_bagging_folds', 0)
+        num_bagging_folds = kwargs.get('num_bagging_folds', None)
         num_bagging_sets = kwargs.get('num_bagging_sets', None)
-        stack_ensemble_levels = kwargs.get('stack_ensemble_levels', 0)
+        stack_ensemble_levels = kwargs.get('stack_ensemble_levels', None)
         auto_stack = kwargs.get('auto_stack', False)
-
-        feature_prune = kwargs.get('feature_prune', False)
-        scheduler_options = kwargs.get('scheduler_options', None)
-        search_options = kwargs.get('search_options', None)
-        nthreads_per_trial = kwargs.get('nthreads_per_trial', None)
-        ngpus_per_trial = kwargs.get('ngpus_per_trial', None)
-        dist_ip_addrs = kwargs.get('dist_ip_addrs', None)
-        visualizer = kwargs.get('visualizer', None)
+        num_cpus = kwargs.get('num_cpus', None)
+        num_gpus = kwargs.get('num_gpus', None)
         unlabeled_data = kwargs.get('unlabeled_data', None)
+
+        ag_args = kwargs.get('AG_args', None)
+        ag_args_fit = kwargs.get('AG_args_fit', None)
+        ag_args_ensemble = kwargs.get('AG_args_ensemble', None)
+        excluded_model_types = kwargs.get('excluded_model_types', [])
 
         self._set_feature_generator(feature_generator=feature_generator, feature_metadata=feature_metadata)
         train_data, tuning_data, unlabeled_data = self._validate_fit_data(train_data=train_data, tuning_data=tuning_data, unlabeled_data=unlabeled_data)
-
-        if feature_prune:
-            feature_prune = False  # TODO: Fix feature pruning to add back as an option
-            # Currently disabled, needs to be updated to align with new model class functionality
-            logger.log(30, 'Warning: feature_prune does not currently work, setting to False.')
-        # TODO: Fix or remove in v0.1
-        if dist_ip_addrs is not None:
-            logger.log(30, 'Warning: dist_ip_addrs does not currently work. Distributed instances will not be utilized.')
 
         refit_full = kwargs.get('refit_full', False)
         if refit_full and not self._learner.cache_data:
@@ -141,95 +130,37 @@ class TabularPredictorV2(TabularPredictor):
         if hyperparameter_tune:
             logger.log(30, 'Warning: `hyperparameter_tune=True` is currently experimental and may cause the process to hang. Setting `auto_stack=True` instead is recommended to achieve maximum quality models.')
 
-        if dist_ip_addrs is None:
-            dist_ip_addrs = []
-
-        if search_options is None:
-            search_options = dict()
-
         if hyperparameters is None:
             hyperparameters = 'default'
         if isinstance(hyperparameters, str):
             hyperparameters = get_hyperparameter_config(hyperparameters)
 
         # Process kwargs to create trainer, schedulers, searchers:
-        ag_args = kwargs.get('AG_args', None)
-        ag_args_fit = kwargs.get('AG_args_fit', None)
-        ag_args_ensemble = kwargs.get('AG_args_ensemble', None)
-        excluded_model_types = kwargs.get('excluded_model_types', [])
-        nthreads_per_trial, ngpus_per_trial = setup_compute(nthreads_per_trial, ngpus_per_trial)
         num_train_rows = len(train_data)
-        if auto_stack:
-            # TODO: What about datasets that are 100k+? At a certain point should we not bag?
-            # TODO: What about time_limits? Metalearning can tell us expected runtime of each model, then we can select optimal folds + stack levels to fit time constraint
-            if num_bagging_folds is None:
-                num_bagging_folds = min(10, max(5, math.floor(num_train_rows / 100)))
-            if stack_ensemble_levels is None:
-                stack_ensemble_levels = min(1, max(0, math.floor(num_train_rows / 750)))
-        if num_bagging_folds is None:
-            num_bagging_folds = 0
-        if stack_ensemble_levels is None:
-            stack_ensemble_levels = 0
-        if not isinstance(num_bagging_folds, int):
-            raise ValueError(f'num_bagging_folds must be an integer. (num_bagging_folds={num_bagging_folds})')
-        if not isinstance(stack_ensemble_levels, int):
-            raise ValueError(f'stack_ensemble_levels must be an integer. (stack_ensemble_levels={stack_ensemble_levels})')
-        if num_bagging_folds < 2 and num_bagging_folds != 0:
-            raise ValueError(f'num_bagging_folds must be equal to 0 or >=2. (num_bagging_folds={num_bagging_folds})')
-        if stack_ensemble_levels != 0 and num_bagging_folds == 0:
-            raise ValueError(f'stack_ensemble_levels must be 0 if num_bagging_folds is 0. (stack_ensemble_levels={stack_ensemble_levels}, num_bagging_folds={num_bagging_folds})')
-        if num_bagging_sets is None:
-            if num_bagging_folds >= 2:
-                if time_limits is not None:
-                    num_bagging_sets = 20  # TODO: v0.1 Reduce to 5 or 3 as 20 is unnecessarily extreme as a default.
-                else:
-                    num_bagging_sets = 1
-            else:
-                num_bagging_sets = 1
-        if not isinstance(num_bagging_sets, int):
-            raise ValueError(f'num_bagging_sets must be an integer. (num_bagging_sets={num_bagging_sets})')
 
-        time_limits_orig = copy.deepcopy(time_limits)
-        time_limits_hpo = copy.deepcopy(time_limits)
-
-        if num_bagging_folds >= 2 and (time_limits_hpo is not None):
-            time_limits_hpo = time_limits_hpo / (1 + num_bagging_folds * (1 + stack_ensemble_levels))
-        # FIXME: Incorrect if user specifies custom level-based hyperparameter config!
-        time_limits_hpo, num_trials = setup_trial_limits(time_limits_hpo, num_trials, hyperparameters)  # TODO: Move HPO time allocation to Trainer
-
-        if (num_trials is not None) and hyperparameter_tune and (num_trials == 1):
-            hyperparameter_tune = False
-            logger.log(30, 'Warning: Specified num_trials == 1 or time_limits is too small for hyperparameter_tune, setting to False.')
+        num_bagging_folds, num_bagging_sets, stack_ensemble_levels = self._sanitize_stack_args(
+            num_bagging_folds=num_bagging_folds, num_bagging_sets=num_bagging_sets, stack_ensemble_levels=stack_ensemble_levels,
+            time_limits=time_limits, auto_stack=auto_stack, num_train_rows=num_train_rows,
+        )
 
         if holdout_frac is None:
             holdout_frac = default_holdout_frac(num_train_rows, hyperparameter_tune)
 
-        # Add visualizer to NN hyperparameters:
-        if (visualizer is not None) and (visualizer != 'none') and ('NN' in hyperparameters):
-            hyperparameters['NN']['visualizer'] = visualizer
+        scheduler_options = self._init_scheduler(hyperparameter_tune_kwargs, time_limits, hyperparameters, num_cpus, num_gpus, num_bagging_folds, stack_ensemble_levels)
 
-        # All models use the same scheduler:
-        scheduler_options = compile_scheduler_options(
-            scheduler_options=scheduler_options,
-            search_strategy=search_strategy,
-            search_options=search_options,
-            nthreads_per_trial=nthreads_per_trial,
-            ngpus_per_trial=ngpus_per_trial,
-            checkpoint=None,
-            num_trials=num_trials,
-            time_out=time_limits_hpo,
-            resume=False,
-            visualizer=visualizer,
-            time_attr='epoch',
-            reward_attr='validation_performance',
-            dist_ip_addrs=dist_ip_addrs)
-        scheduler_cls = schedulers[search_strategy.lower()]
-        scheduler_options = (scheduler_cls, scheduler_options)  # wrap into tuple
+        # TODO: visualizer to args_fit?
+        # TODO: Does not work with advanced model hyperparameters option
+        if scheduler_options is not None:
+            visualizer = scheduler_options[1]['visualizer']
+            # Add visualizer to NN hyperparameters:
+            if (visualizer is not None) and (visualizer != 'none') and ('NN' in hyperparameters):
+                hyperparameters['NN']['visualizer'] = visualizer
+
         self._learner.fit(X=train_data, X_val=tuning_data, X_unlabeled=unlabeled_data, scheduler_options=scheduler_options,
-                          hyperparameter_tune=hyperparameter_tune, feature_prune=feature_prune,
+                          hyperparameter_tune=hyperparameter_tune,
                           holdout_frac=holdout_frac, num_bagging_folds=num_bagging_folds, num_bagging_sets=num_bagging_sets, stack_ensemble_levels=stack_ensemble_levels,
                           hyperparameters=hyperparameters, ag_args=ag_args, ag_args_fit=ag_args_fit, ag_args_ensemble=ag_args_ensemble, excluded_model_types=excluded_model_types,
-                          time_limit=time_limits_orig, save_bagged_folds=save_bagged_folds, verbosity=verbosity)
+                          time_limit=time_limits, save_bagged_folds=save_bagged_folds, verbosity=verbosity)
         self._set_post_fit_vars()
 
         keep_only_best = kwargs.get('keep_only_best', False)
@@ -263,7 +194,69 @@ class TabularPredictorV2(TabularPredictor):
             self.save_space()
         self.save()
 
-        # TODO: v0.1: return newly trained model names?
+    def _init_scheduler(self, hyperparameter_tune_kwargs, time_limit, hyperparameters, num_cpus, num_gpus, num_bagging_folds, stack_ensemble_levels):
+        num_cpus, num_gpus = setup_compute(num_cpus, num_gpus)
+        time_limits_hpo = time_limit
+        if num_bagging_folds >= 2 and (time_limits_hpo is not None):
+            time_limits_hpo = time_limits_hpo / (1 + num_bagging_folds * (1 + stack_ensemble_levels))
+        # FIXME: Incorrect if user specifies custom level-based hyperparameter config!
+        time_limits_hpo, num_trials = setup_trial_limits(time_limits_hpo, None, hyperparameters)  # TODO: Move HPO time allocation to Trainer
+
+        # All models use the same scheduler:
+        scheduler_options = compile_scheduler_options_v2(
+            scheduler_options=hyperparameter_tune_kwargs,
+            nthreads_per_trial=num_cpus,
+            ngpus_per_trial=num_gpus,
+            num_trials=num_trials,
+            time_out=time_limits_hpo,
+        )
+        if scheduler_options is None:
+            return None
+
+        assert scheduler_options['searcher'] != 'bayesopt_hyperband', "searcher == 'bayesopt_hyperband' not yet supported"
+        # TODO: Fix or remove in v0.1
+        if scheduler_options.get('dist_ip_addrs', None):
+            logger.log(30, 'Warning: dist_ip_addrs does not currently work. Distributed instances will not be utilized.')
+
+        if scheduler_options['num_trials'] == 1:
+            logger.log(30, 'Warning: Specified num_trials == 1 or time_limits is too small for hyperparameter_tune, disabling HPO.')
+            return None  # FIXME
+
+        scheduler_cls = schedulers[scheduler_options['searcher'].lower()]
+        scheduler_options = (scheduler_cls, scheduler_options)  # wrap into tuple
+        return scheduler_options
+
+    def _sanitize_stack_args(self, num_bagging_folds, num_bagging_sets, stack_ensemble_levels, time_limits, auto_stack, num_train_rows):
+        if auto_stack:
+            # TODO: What about datasets that are 100k+? At a certain point should we not bag?
+            # TODO: What about time_limits? Metalearning can tell us expected runtime of each model, then we can select optimal folds + stack levels to fit time constraint
+            if num_bagging_folds is None:
+                num_bagging_folds = min(10, max(5, math.floor(num_train_rows / 100)))
+            if stack_ensemble_levels is None:
+                stack_ensemble_levels = min(1, max(0, math.floor(num_train_rows / 750)))
+        if num_bagging_folds is None:
+            num_bagging_folds = 0
+        if stack_ensemble_levels is None:
+            stack_ensemble_levels = 0
+        if not isinstance(num_bagging_folds, int):
+            raise ValueError(f'num_bagging_folds must be an integer. (num_bagging_folds={num_bagging_folds})')
+        if not isinstance(stack_ensemble_levels, int):
+            raise ValueError(f'stack_ensemble_levels must be an integer. (stack_ensemble_levels={stack_ensemble_levels})')
+        if num_bagging_folds < 2 and num_bagging_folds != 0:
+            raise ValueError(f'num_bagging_folds must be equal to 0 or >=2. (num_bagging_folds={num_bagging_folds})')
+        if stack_ensemble_levels != 0 and num_bagging_folds == 0:
+            raise ValueError(f'stack_ensemble_levels must be 0 if num_bagging_folds is 0. (stack_ensemble_levels={stack_ensemble_levels}, num_bagging_folds={num_bagging_folds})')
+        if num_bagging_sets is None:
+            if num_bagging_folds >= 2:
+                if time_limits is not None:
+                    num_bagging_sets = 20  # TODO: v0.1 Reduce to 5 or 3 as 20 is unnecessarily extreme as a default.
+                else:
+                    num_bagging_sets = 1
+            else:
+                num_bagging_sets = 1
+        if not isinstance(num_bagging_sets, int):
+            raise ValueError(f'num_bagging_sets must be an integer. (num_bagging_sets={num_bagging_sets})')
+        return num_bagging_folds, num_bagging_sets, stack_ensemble_levels
 
     def _set_post_fit_vars(self, learner: AbstractLearner = None):
         if learner is not None:
@@ -353,13 +346,8 @@ class TabularPredictorV2(TabularPredictor):
             'keep_only_best',
             'save_space',
             'refit_full',
-            'feature_prune',
-            'scheduler_options',
-            'search_options',
-            'nthreads_per_trial',
-            'ngpus_per_trial',
-            'dist_ip_addrs',
-            'visualizer',
+            'num_cpus',
+            'num_gpus',
             'unlabeled_data',
             'verbosity',
         }
