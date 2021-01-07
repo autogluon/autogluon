91d987e96d75ae89289723b022ea29aa3ad58548
Alexander Shirkov <sgdread@gmail.com>
2021-01-06 18:02:32 -0800

    [RayTune] added trials results schedulers

--- core/src/autogluon/core/tune/tune_scheduler.py
+++ core/src/autogluon/core/tune/tune_scheduler.py
@@ -1,16 +1,39 @@
+import logging
+from collections import OrderedDict
 from functools import partial
 
 from ray import tune
+from ray.tune import Callback
 
 from autogluon.core.space import *
 from autogluon.core.utils.edict import EasyDict
 
+logger = logging.getLogger(__name__)
+
+
+class ResultsHistoryCallback(Callback):
+
+    def __init__(self, collector: dict):
+        self.collector = collector
+
+    def on_trial_result(self, iteration, trials, trial, result, **info):
+        result['time_this_iter'] = result['time_this_iter_s']
+        result['time_since_start'] = result['time_total_s']
+        if trial in self.collector:
+            self.collector[trial].append(result)
+        else:
+            self.collector[trial] = [result]
+
 
 class RayTuneScheduler(object):
 
-    def __init__(self, task_fn, **tune_args):
+    def __init__(self, task_fn, reward_attr, time_attr='epoch', **tune_args):
         self.task_fn = task_fn
         self.tune_args = tune_args
+        self.tune_args['metric'] = reward_attr
+        self.training_history = None
+        self.reward_attr = reward_attr
+        self.time_attr = time_attr
 
     @classmethod
     def wrap_space(cls, space):
@@ -38,8 +61,12 @@ class RayTuneScheduler(object):
         return fn(config, reporter=cls.reporter_fn, **kwargs)
 
     def run(self, **kwargs):
+        self.training_history = OrderedDict()
+
+        results_history_callback = ResultsHistoryCallback(self.training_history)
         result = tune.run(
             partial(self.train_fn_wrapper, self.task_fn.f),
+            callbacks=[results_history_callback],
             config=self.wrap_space(self.task_fn.kwvars),
             **self.tune_args
         )
@@ -52,3 +79,38 @@ class RayTuneScheduler(object):
     def join_jobs(self, timeout=None):
         # Keep for compatibility
         pass
+
+    def get_training_curves(self, filename=None, plot=False, use_legend=True):
+        """Get Training Curves
+
+        Parameters
+        ----------
+            filename : str
+            plot : bool
+            use_legend : bool
+
+        Examples
+        --------
+        >>> scheduler.run()
+        >>> scheduler.join_jobs()
+        >>> scheduler.get_training_curves(plot=True)
+
+            .. image:: https://github.com/zhanghang1989/AutoGluonWebdata/blob/master/doc/api/autogluon.1.png?raw=true
+        """
+        if filename is None and not plot:
+            logger.warning('Please either provide filename or allow plot in get_training_curves')
+        import matplotlib.pyplot as plt
+        plt.ylabel(self.reward_attr)
+        plt.xlabel(self.time_attr)
+        plt.title("Performance vs Training-Time in each HPO Trial")
+        for task_id, task_res in self.training_history.items():
+            rewards = [x[self.reward_attr] for x in task_res]
+            x = list(range(len(task_res)))
+            plt.plot(x, rewards, label=f'task {task_id}')
+        if use_legend:
+            plt.legend(loc='best')
+        if filename:
+            logger.info(f'Saving Training Curve in {filename}')
+            plt.savefig(filename)
+        if plot:
+            plt.show()
