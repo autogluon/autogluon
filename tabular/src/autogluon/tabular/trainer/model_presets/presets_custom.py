e35d9d54e15f23845ffa01a6e240b602e1864dba
Nick Erickson <neerick@amazon.com>
2021-01-15 18:19:06 -0800

    addressed comments

--- tabular/src/autogluon/tabular/trainer/model_presets/presets_custom.py
+++ tabular/src/autogluon/tabular/trainer/model_presets/presets_custom.py
@@ -12,28 +12,19 @@ def get_preset_custom(name, problem_type, num_classes):
         model[AG_ARGS] = dict(model_type='GBM', name_suffix='Custom', disable_in_hpo=True)
         return [model]
     elif name == 'ELECTRA_BASE':
-        from autogluon.text.text_prediction.text_prediction import ag_text_prediction_params
-        text_backbone = 'google_electra_base'
-        model = ag_text_prediction_params.create('default_no_hpo')
-        model['models']['BertForTextPredictionBasic']['search_space']['model.backbone.name'] = text_backbone
-        model['models']['BertForTextPredictionBasic']['search_space']['optimization.num_train_epochs'] = 10
-        model[AG_ARGS] = dict(model_type='TEXT_NN_V1', name_suffix='Base', disable_in_hpo=True)
-        return [model]
+        return _get_preset_electra('google_electra_base', 'default_no_hpo', 'Base')
     elif name == 'ELECTRA_BASE_HPO':
-        from autogluon.text.text_prediction.text_prediction import ag_text_prediction_params
-        text_backbone = 'google_electra_base'
-        model = ag_text_prediction_params.create('default')
-        model['models']['BertForTextPredictionBasic']['search_space']['model.backbone.name'] = text_backbone
-        model['models']['BertForTextPredictionBasic']['search_space']['optimization.num_train_epochs'] = 10
-        model[AG_ARGS] = dict(model_type='TEXT_NN_V1', name_suffix='BaseHPO', disable_in_hpo=True)
-        return [model]
+        return _get_preset_electra('google_electra_base', 'default', 'BaseHPO')
     elif name == 'ELECTRA_LARGE':
-        from autogluon.text.text_prediction.text_prediction import ag_text_prediction_params
-        text_backbone = 'google_electra_large'
-        model = ag_text_prediction_params.create('default_no_hpo')
-        model['models']['BertForTextPredictionBasic']['search_space']['model.backbone.name'] = text_backbone
-        model['models']['BertForTextPredictionBasic']['search_space']['optimization.num_train_epochs'] = 10
-        model[AG_ARGS] = dict(model_type='TEXT_NN_V1', name_suffix='Large', disable_in_hpo=True)
-        return [model]
+        return _get_preset_electra('google_electra_large', 'default_no_hpo', 'Large')
     else:
         raise ValueError(f'Unknown custom model preset: {name}')
+
+
+def _get_preset_electra(backbone, params_name, name_suffix):
+    from autogluon.text.text_prediction.text_prediction import ag_text_prediction_params
+    model = ag_text_prediction_params.create(params_name)
+    model['models']['BertForTextPredictionBasic']['search_space']['model.backbone.name'] = backbone
+    model['models']['BertForTextPredictionBasic']['search_space']['optimization.num_train_epochs'] = 10
+    model[AG_ARGS] = dict(model_type='TEXT_NN_V1', name_suffix=name_suffix, disable_in_hpo=True)
+    return [model]
