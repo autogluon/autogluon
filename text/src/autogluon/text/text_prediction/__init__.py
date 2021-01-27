87695af187d9f06ebf24bf13aa344afd5afa7163
Xingjian Shi <xshiab@connect.ust.hk>
2021-01-27 10:18:02 -0800

    Update __init__.py

--- text/src/autogluon/text/text_prediction/__init__.py
+++ text/src/autogluon/text/text_prediction/__init__.py
@@ -1,10 +1,8 @@
-from . import column_property
 from . import constants
 from . import infer_types
-from . import text_prediction
-from .metrics import *
+from . import metrics
 from . import predictor
 from .predictor.predictor import TextPredictor
 
-__all__ = ['column_property', 'constants', 'infer_types',
-           'preprocessing', 'text_prediction'] + metrics.__all__ + ['TextPrediction']
+__all__ = ['constants', 'infer_types',
+           'metrics', 'predictor'] + ['TextPredictor']
