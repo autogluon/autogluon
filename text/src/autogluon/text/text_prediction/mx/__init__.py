dc11edb52addbfa4463c2abd9f2db377fbd11a0c
Xingjian Shi <xshiab@connect.ust.hk>
2021-01-31 19:58:21 -0800

    update

--- text/src/autogluon/text/text_prediction/mx/__init__.py
+++ text/src/autogluon/text/text_prediction/mx/__init__.py
@@ -1,4 +1,3 @@
-from . import fields
 from . import models
 from . import modules
 from . import preprocessing
--- text/src/autogluon/text/text_prediction/mx/utils.py
+++ text/src/autogluon/text/text_prediction/mx/utils.py
@@ -1,6 +1,7 @@
 import mxnet as mx
+from mxnet.util import use_np
 
-
+@use_np
 def average_checkpoints(net, checkpoint_paths, out_path):
     for path in checkpoint_paths:
-
+        pass
