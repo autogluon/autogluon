2dec2f2d872e0902092b882df7d083c7fcba6d42
Xingjian Shi <xshiab@connect.ust.hk>
2021-02-01 03:02:09 -0800

    update

--- text/src/autogluon/text/text_prediction/__init__.py
+++ text/src/autogluon/text/text_prediction/__init__.py
@@ -4,6 +4,7 @@ from . import metrics
 from . import predictor
 from . import presets
 from .predictor.predictor import TextPredictor
+from .presets import ag_text_presets
 
 __all__ = ['constants', 'infer_types', 'metrics', 'predictor', 'presets',
-           'TextPredictor']
+           'TextPredictor', 'ag_text_presets']
