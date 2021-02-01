6bc24f1136553932f35b814d541b2a4c49687425
Xingjian Shi <xshiab@connect.ust.hk>
2021-01-31 21:10:40 -0800

    Update presets.py

--- text/src/autogluon/text/text_prediction/presets.py
+++ text/src/autogluon/text/text_prediction/presets.py
@@ -29,9 +29,9 @@ def default() -> dict:
                 'backend': 'gluonnlp_v0',
                 'search_space': {
                     'model.backbone.name': 'google_electra_base',
-                    'optimization.batch_size': 128,
+                    'optimization.batch_size': 64,
                     'optimization.per_device_batch_size': 8,
-                    'optimization.num_train_epochs': 4,
+                    'optimization.num_train_epochs': 10,
                     'optimization.lr': space.Choice(5E-5),
                     'optimization.layerwise_lr_decay': 0.8
                 }
