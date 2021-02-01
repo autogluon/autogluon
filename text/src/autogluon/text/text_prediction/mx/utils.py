2bd5c85cc83dde13a7832e1f982cf8405687656a
Xingjian Shi <xshiab@connect.ust.hk>
2021-02-01 02:36:41 -0800

    Update utils.py

--- text/src/autogluon/text/text_prediction/mx/utils.py
+++ text/src/autogluon/text/text_prediction/mx/utils.py
@@ -26,7 +26,7 @@ def average_checkpoints(checkpoint_paths, out_path):
 @use_np
 def set_seed(seed):
     if seed is None:
-        seed = np.random.randint(0, 1000)
+        seed = np.random.randint(0, 100000)
     mx.random.seed(seed)
     np.random.seed(seed)
     random.seed(seed)
