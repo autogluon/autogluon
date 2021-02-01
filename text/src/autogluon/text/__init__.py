7d0e61afbba82afcf228cd65ee086ce9fdf8453e
Xingjian Shi <xshiab@connect.ust.hk>
2021-02-01 03:42:01 -0800

    update

--- text/src/autogluon/text/__init__.py
+++ text/src/autogluon/text/__init__.py
@@ -1,4 +1,5 @@
 from . import text_prediction
 from .text_prediction import TextPredictor
+from .text_prediction.presets import ag_text_presets
 
-__all__ = ['text_prediction', 'TextPredictor']
+__all__ = ['text_prediction', 'TextPredictor', 'ag_text_presets']
