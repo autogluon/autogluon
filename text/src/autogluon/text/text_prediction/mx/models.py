4b199d9ff5eead312dcdaae5c9a8d91fe6a8888d
Xingjian Shi <xshiab@connect.ust.hk>
2021-02-05 22:07:28 -0800

    Update models.py

--- text/src/autogluon/text/text_prediction/mx/models.py
+++ text/src/autogluon/text/text_prediction/mx/models.py
@@ -287,7 +287,6 @@ def _classification_regression_predict(net, dataloader, problem_type, label_scal
                 if extract_embedding:
                     _, embeddings = net(batch_feature)
                     iter_pred_l.append(embeddings)
-                    print(embeddings)
                 else:
                     pred = net(batch_feature)
                     if problem_type == MULTICLASS or problem_type == BINARY:
@@ -301,6 +300,7 @@ def _classification_regression_predict(net, dataloader, problem_type, label_scal
                 for ele in iter_logits_l:
                     logits[i].append(ele.asnumpy())
         predictions[i] = np.concatenate(predictions[i], axis=0)
+        print(predictions[i])
         if problem_type == REGRESSION:
             predictions[i] = label_scaler.inverse_transform(predictions[i])[:, 0]
         if use_logits:
