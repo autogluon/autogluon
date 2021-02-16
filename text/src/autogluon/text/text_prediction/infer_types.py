97c3e4965d4b03984ef58eadec46f6f521aef1e6
Xingjian Shi <xshiab@connect.ust.hk>
2021-02-16 11:33:45 -0800

    Update infer_types.py

--- text/src/autogluon/text/text_prediction/infer_types.py
+++ text/src/autogluon/text/text_prediction/infer_types.py
@@ -66,9 +66,9 @@ def is_categorical_column(data: pd.Series,
                 if k not in key_set:
                     print('Not in key_set, k={}'.format(k))
                     oov_num += v
+            print('oov_num=', oov_num)
             if is_label and oov_num != 0:
                 return False
-            print('oov_num=', oov_num)
             if oov_num / total_valid_num > oov_ratio_threshold:
                 return False
             return True
