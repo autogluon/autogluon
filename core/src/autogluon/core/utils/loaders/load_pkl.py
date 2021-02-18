a6194b7438b78b683a215639cff8c09b8db42e74
Adrien Atallah <adrienatallah@gmail.com>
2021-02-18 18:07:08 -0500

    Add compression_fn and compression_fn_kwargs to load_pkl/save_pkl and unit tests for tabular pkl compression

--- core/src/autogluon/core/utils/loaders/load_pkl.py
+++ core/src/autogluon/core/utils/loaders/load_pkl.py
@@ -1,11 +1,16 @@
 import io, logging, pickle, boto3
 
-from . import load_pointer
-from .. import s3_utils
+from ..loaders import load_pointer
+from ...utils import s3_utils
+from ...utils import compression_utils
 
 logger = logging.getLogger(__name__)
 
-def load(path, format=None, verbose=True):
+
+def load(path, format=None, verbose=True, **kwargs):
+    compression_fn = kwargs.get('compression_fn', None)
+    compression_fn_kwargs = kwargs.get('compression_fn_kwargs', None)
+
     if path.endswith('.pointer'):
         format = 'pointer'
     elif s3_utils.is_s3_url(path):
@@ -22,8 +27,19 @@ def load(path, format=None, verbose=True):
         return pickle.loads(s3.Bucket(s3_bucket).Object(s3_prefix).get()['Body'].read())
 
     if verbose: logger.log(15, 'Loading: %s' % path)
-    with open(path, 'rb') as fin:
-        object = pickle.load(fin)
+
+    compression_fn_map = compression_utils.get_compression_map()
+    validated_path = compression_utils.get_validated_path(path, compression_fn=compression_fn)
+
+    if compression_fn_kwargs is None:
+        compression_fn_kwargs = {}
+
+    if compression_fn in compression_fn_map:
+        with compression_fn_map[compression_fn]['open'](validated_path, 'rb', **compression_fn_kwargs) as fin:
+            object = pickle.load(fin)
+    else:
+        raise ValueError(f'compression_fn={compression_fn} or compression_fn_kwargs={compression_fn_kwargs} are not valid. Valid function values: {compression_fn_map.keys()}')
+
     return object
 
 
