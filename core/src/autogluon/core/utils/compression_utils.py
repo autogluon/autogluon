a5bd759d839d10aa0899950e460285354162cb28
Adrien Atallah <AdrienAtallah@gmail.com>
2021-02-18 19:45:12 -0500

    Add compression_fn and compression_fn_kwargs to load_pkl/save_pkl and unit tests for tabular pkl compression (#940)

---
+++ core/src/autogluon/core/utils/compression_utils.py
@@ -0,0 +1,33 @@
+import gzip
+import bz2
+import lzma
+
+
+compression_fn_map = {
+    None: {
+        'open': open,
+        'extension': '',
+    },
+    'gzip': {
+        'open': gzip.open,
+        'extension': 'gz',
+    },
+    'bz2': {
+        'open': bz2.open,
+        'extension': 'bz2',
+    },
+    'lzma': {
+        'open': lzma.open,
+        'extension': 'lzma',
+    },
+}
+
+def get_validated_path(filename, compression_fn=None):
+    if compression_fn is not None:
+        validated_path = f"{filename}.{compression_fn_map[compression_fn]['extension']}"
+    else:
+        validated_path = filename
+    return validated_path
+
+def get_compression_map():
+    return compression_fn_map
