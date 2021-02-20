080c5c9657954deb2060fc1814eee46880edf595
Joshua Z. Zhang <cheungchih@gmail.com>
2021-02-19 16:55:27 -0800

    long string

--- vision/src/autogluon/vision/utils/error_handler.py
+++ vision/src/autogluon/vision/utils/error_handler.py
@@ -17,14 +17,14 @@ class MXNetErrorCatcher(contextlib.AbstractContextManager):
             if lines:
                 mxnet_err = lines[-1].strip()
             if 'GPU is not enabled' in mxnet_err:
-                self.hint += 'MXNet is not built with GPU support, make sure you have nvidia gpus enabled with' +
-                             'corresponding mxnet-cuxxx version installed, where `xxx` is the cuda version.' +
-                             'You may refer to "https://auto.gluon.ai/stable/install.html" for' +
-                             'detailed installation guide.\n'
+                self.hint += ('MXNet is not built with GPU support, make sure you have nvidia gpus enabled with'
+                              'corresponding mxnet-cuxxx version installed, where `xxx` is the cuda version.'
+                              'You may refer to "https://auto.gluon.ai/stable/install.html" for'
+                              'detailed installation guide.\n')
             if 'out of memory' in mxnet_err:
-                self.hint += 'CUDA out of memory error detected. Try reduce `batch_size` in `hyperparameters`,' +
-                             "e.g., fit(train_data, val_data, hyperparameters={'batch_size': 2}). If the error persists," +
-                             'please check if you have other processes occupying too much GPU memory by looking at `nvidia-smi`.\n'
+                self.hint += ('CUDA out of memory error detected. Try reduce `batch_size` in `hyperparameters`,'
+                              "e.g., fit(train_data, val_data, hyperparameters={'batch_size': 2}). If the error persists,"
+                              'please check if you have other processes occupying too much GPU memory by looking at `nvidia-smi`.\n')
             self.exc_value = mxnet_err
         else:
             self.exc_value = exc_value
