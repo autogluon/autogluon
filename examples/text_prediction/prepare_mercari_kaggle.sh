be6ddace74c2d779f29df263dff851d50e653bf3
Xingjian Shi <xshiab@connect.ust.hk>
2021-02-21 00:13:44 -0800

    Update prepare_mercari_kaggle.sh

--- examples/text_prediction/prepare_mercari_kaggle.sh
+++ examples/text_prediction/prepare_mercari_kaggle.sh
@@ -6,5 +6,6 @@ mv mercari-price-suggestion-challenge.zip mercari_price/
 cd mercari_price/
 unzip mercari-price-suggestion-challenge.zip
 7za e train.tsv.7z
-unzip e test_stg2.tsv.zip
+unzip test_stg2.tsv.zip
+unzip sample_submission_stg2.csv.zip
 cd ..
