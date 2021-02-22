5b870a3425fd688a0b6dc0c0adccae1e59e81935
Xingjian Shi <xshiab@connect.ust.hk>
2021-02-21 17:42:05 -0800

    Update predictor.py

--- text/src/autogluon/text/text_prediction/predictor/predictor.py
+++ text/src/autogluon/text/text_prediction/predictor/predictor.py
@@ -281,7 +281,7 @@ class TextPredictor:
         """
         return self._model.evaluate(data, metrics=metrics)
 
-    def predict(self, dataset, as_pandas=False):
+    def predict(self, dataset, as_pandas=True):
         """Get the prediction from
 
         Returns
@@ -297,7 +297,7 @@ class TextPredictor:
             output = pd.DataFrame({self.label: output})[self.label]
         return output
 
-    def predict_proba(self, dataset, as_pandas=False):
+    def predict_proba(self, dataset, as_pandas=True):
         """Predict the probability from the input
 
         Parameters
