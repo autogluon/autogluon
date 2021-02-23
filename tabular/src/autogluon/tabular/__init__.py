27449215f6de3dab92ed04cfa2ded8583456f8f8
Nick Erickson <neerick@amazon.com>
2021-02-23 11:15:23 -0800

    Tabular: Removed predictor_legacy.py and tabular_prediction.py

--- tabular/src/autogluon/tabular/__init__.py
+++ tabular/src/autogluon/tabular/__init__.py
@@ -3,7 +3,6 @@ import logging
 from autogluon.core.dataset import TabularDataset
 from autogluon.core.features.feature_metadata import FeatureMetadata
 
-from .task.tabular_prediction import TabularPrediction
 from .predictor import TabularPredictor
 
 logging.basicConfig(format='%(message)s')  # just print message in logs
--- tabular/src/autogluon/tabular/task/tabular_prediction/__init__.py
+++
@@ -1,1 +0,0 @@
-from .tabular_prediction import TabularPrediction
