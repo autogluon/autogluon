7a45d2e0e18804e1ea4509cabbc2cd089451d601
Xingjian Shi <xshiab@connect.ust.hk>
2021-02-23 19:17:14 -0800

    Update test_text_prediction_v1_model.py

--- tabular/tests/unittests/models/test_text_prediction_v1_model.py
+++ tabular/tests/unittests/models/test_text_prediction_v1_model.py
@@ -7,5 +7,5 @@ def test_text_prediction_v1_sts(fit_helper):
     dataset_name = 'sts'
     fit_helper.fit_and_validate_dataset(
         dataset_name=dataset_name, fit_args=fit_args, sample_size=100,
-        refit_full=False,  # FIXME: remove this after FIX is merged https://github.com/awslabs/autogluon/pull/936
+        refit_full=True,
     )
