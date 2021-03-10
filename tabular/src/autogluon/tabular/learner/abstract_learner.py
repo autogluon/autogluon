105c77f786b62866e54472d514a613d44f64d29f
Jonas Mueller <1390638+jwmueller@users.noreply.github.com>
2021-03-10 09:28:56 -0800

    add sample_weight to score_debug EnsembleSelection (#1007)

--- tabular/src/autogluon/tabular/learner/abstract_learner.py
+++ tabular/src/autogluon/tabular/learner/abstract_learner.py
@@ -283,7 +283,7 @@ class AbstractLearner:
         if compute_oracle:
             pred_probas = list(model_pred_proba_dict.values())
             ensemble_selection = EnsembleSelection(ensemble_size=100, problem_type=trainer.problem_type, metric=self.eval_metric)
-            ensemble_selection.fit(predictions=pred_probas, labels=y_internal, identifiers=None)  # TODO: Only fit non-nan
+            ensemble_selection.fit(predictions=pred_probas, labels=y_internal, identifiers=None, sample_weight=w)  # TODO: Only fit non-nan
             oracle_weights = ensemble_selection.weights_
             oracle_pred_time_start = time.time()
             oracle_pred_proba_norm = [pred * weight for pred, weight in zip(pred_probas, oracle_weights)]
