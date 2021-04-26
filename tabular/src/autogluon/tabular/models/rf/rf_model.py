ba08c8dc1d74bcd90de90580dc82b556c210c8f9
Nick Erickson <neerick@amazon.com>
2021-04-26 14:02:26 -0700

    Tabular: RF/XT Efficient OOB (#1082)
    * Tabular: Enabled efficient OOB for RF/XT
    
    * Tabular: Removed min_samples_leaf
    
    * 300 estimators

--- tabular/src/autogluon/tabular/models/rf/rf_model.py
+++ tabular/src/autogluon/tabular/models/rf/rf_model.py
@@ -65,10 +65,16 @@ class RFModel(AbstractModel):
 
     def _set_default_params(self):
         default_params = {
+            # TODO: 600 is much better, but increases info leakage in stacking -> therefore 300 is ~equal in stack ensemble final quality.
+            #  Consider adding targeted noise to OOF to avoid info leakage, or increase `min_samples_leaf`.
             'n_estimators': 300,
             'n_jobs': -1,
             'random_state': 0,
             'bootstrap': True,  # Required for OOB estimates, setting to False will raise exception if bagging.
+            # TODO: min_samples_leaf=5 is too large on most problems, however on some datasets it helps a lot (airlines likes >40 min_samples_leaf, adult likes 2 much better than 1)
+            #  This value would need to be tuned per dataset, likely very worthwhile.
+            #  Higher values = less OOF info leak, default = 1, which maximizes info leak.
+            # 'min_samples_leaf': 5,  # Significantly reduces info leakage to stacker models. Never use the default/1 when using as base model.
             # 'oob_score': True,  # Disabled by default as it is better to do it post-fit via custom logic.
         }
         for param, val in default_params.items():
@@ -293,7 +299,7 @@ class RFModel(AbstractModel):
     @classmethod
     def _get_default_ag_args_ensemble(cls) -> dict:
         default_ag_args_ensemble = super()._get_default_ag_args_ensemble()
-        extra_ag_args_ensemble = {'use_child_oof': False}
+        extra_ag_args_ensemble = {'use_child_oof': True}
         default_ag_args_ensemble.update(extra_ag_args_ensemble)
         return default_ag_args_ensemble
 
