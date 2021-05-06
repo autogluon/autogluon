17220afc84aa9f45c0e5b7e36a2ebfed2e885816
Nick Erickson <neerick@amazon.com>
2021-05-06 13:43:02 -0700

    Simplify custom LightGBM config

--- tabular/src/autogluon/tabular/models/lgb/hyperparameters/parameters.py
+++ tabular/src/autogluon/tabular/models/lgb/hyperparameters/parameters.py
@@ -5,15 +5,15 @@ from autogluon.core.constants import BINARY, MULTICLASS, REGRESSION, SOFTCLASS
 DEFAULT_NUM_BOOST_ROUND = 10000  # default for single training run
 
 
-def get_param_baseline_custom(problem_type, num_classes=None):
+def get_param_baseline_custom(problem_type):
     if problem_type == BINARY:
         return get_param_binary_baseline_custom()
     elif problem_type == MULTICLASS:
-        return get_param_multiclass_baseline_custom(num_classes=num_classes)
+        return get_param_multiclass_baseline_custom()
     elif problem_type == REGRESSION:
         return get_param_regression_baseline_custom()
     elif problem_type == SOFTCLASS:
-        return get_param_softclass_baseline_custom(num_classes=num_classes)
+        return get_param_softclass_baseline_custom()
     else:
         return get_param_binary_baseline_custom()
 
@@ -31,24 +31,15 @@ def get_param_baseline(problem_type, num_classes=None):
         return get_param_binary_baseline()
 
 
-def get_param_multiclass_baseline_custom(num_classes):
+def get_param_multiclass_baseline_custom():
     params = {
-        'num_boost_round': DEFAULT_NUM_BOOST_ROUND,
-        'num_threads': -1,
-        'objective': 'multiclass',
-        'num_classes': num_classes,
-        'verbose': -1,
-        'boosting_type': 'gbdt',
         'learning_rate': 0.03,
         'num_leaves': 128,
         'feature_fraction': 0.9,
         'min_data_in_leaf': 3,
-        'two_round': True,
-        'seed_value': 0,
-        # 'device': 'gpu'  # needs GPU-enabled lightGBM build
         # TODO: Bin size max increase
     }
-    return params.copy()
+    return params
 
 
 def get_param_binary_baseline():
@@ -91,61 +82,33 @@ def get_param_regression_baseline():
     return params
 
 
-def get_param_binary_baseline_dummy_gpu():
+def get_param_binary_baseline_custom():
     params = {
-        'num_boost_round': DEFAULT_NUM_BOOST_ROUND,
-        'num_threads': -1,
-        'objective': 'binary',
-        'verbose': -1,
-        'boosting_type': 'gbdt',
-        'two_round': True,
-        'device_type': 'gpu',
+        'learning_rate': 0.03,
+        'num_leaves': 128,
+        'feature_fraction': 0.9,
+        'min_data_in_leaf': 5,
     }
     return params
 
 
-def get_param_binary_baseline_custom():
-    params = {
-        'num_boost_round': DEFAULT_NUM_BOOST_ROUND,
-        'num_threads': -1,
-        'objective': 'binary',
-        'verbose': -1,
-        'boosting_type': 'gbdt',
-        'learning_rate': 0.03,
-        'num_leaves': 128,
-        'feature_fraction': 0.9,
-        'min_data_in_leaf': 5,
-        # 'is_unbalance': True,  # TODO: Set is_unbalanced: True for F1-score, AUC!
-        'two_round': True,
-        'seed_value': 0,
-    }
-    return params.copy()
-
-
 def get_param_regression_baseline_custom():
     params = {
-        'num_boost_round': DEFAULT_NUM_BOOST_ROUND,
-        'num_threads': -1,
-        'objective': 'regression',
-        'verbose': -1,
-        'boosting_type': 'gbdt',
         'learning_rate': 0.03,
         'num_leaves': 128,
         'feature_fraction': 0.9,
         'min_data_in_leaf': 5,
-        'two_round': True,
-        'seed_value': 0,
     }
-    return params.copy()
+    return params
 
 
 def get_param_softclass_baseline(num_classes):
     params = get_param_multiclass_baseline(num_classes)
     params.pop('metric', None)
-    return params.copy()
+    return params
 
 
-def get_param_softclass_baseline_custom(num_classes):
-    params = get_param_multiclass_baseline_custom(num_classes)
+def get_param_softclass_baseline_custom():
+    params = get_param_multiclass_baseline_custom()
     params.pop('metric', None)
-    return params.copy()
+    return params
