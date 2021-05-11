1eb4cb856c8fcfac8a27e9dbcc9d4563017b0629
Xingjian Shi <xshiab@connect.ust.hk>
2021-05-11 01:44:40 -0700

    Update __init__.py

--- core/src/autogluon/core/metrics/__init__.py
+++ core/src/autogluon/core/metrics/__init__.py
@@ -457,7 +457,7 @@ recall = make_scorer('recall',
                      sklearn.metrics.recall_score)
 
 # Register other metrics
-make_scorer('quadratic_kappa', quadratic_kappa, needs_proba=True)
+quadratic_kappa = make_scorer('quadratic_kappa', quadratic_kappa, needs_proba=True)
 
 
 def customized_log_loss(y_true, y_pred, eps=1e-15):
@@ -531,7 +531,8 @@ for scorer in [pinball_loss]:
         QUANTILE_METRICS[alias] = scorer
 
 CLASSIFICATION_METRICS = dict()
-for scorer in [accuracy, balanced_accuracy, mcc, roc_auc, roc_auc_ovo_macro, average_precision, log_loss, pac_score]:
+for scorer in [accuracy, balanced_accuracy, mcc, roc_auc, roc_auc_ovo_macro, average_precision,
+               log_loss, pac_score, quadratic_kappa]:
     CLASSIFICATION_METRICS[scorer.name] = scorer
     for alias in scorer.alias:
         CLASSIFICATION_METRICS[alias] = scorer
