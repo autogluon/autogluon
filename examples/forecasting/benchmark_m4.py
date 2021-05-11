0339ff4803722d710374116aeb9bfcc6098b59db
yx1215 <yx1215@nyu.edu>
2021-05-11 19:38:05 +0800

    update benchmark_m4.py

--- examples/forecasting/benchmark_m4.py
+++ examples/forecasting/benchmark_m4.py
@@ -2,23 +2,32 @@ from gluonts.dataset.repository.datasets import get_dataset
 from autogluon.forecasting import ForecastingPredictor
 import os
 import time
-
+dataset_list = ["m4_hourly",
+                "m4_daily",
+                "m4_weekly",
+                "m4_monthly",
+                "m4_quarterly",
+                "m4_yearly",
+                "traffic",
+                "electricity"]
 os.makedirs("benchmark_results", exist_ok=True)
-dataset = get_dataset("m4_daily")
-train_data = dataset.train
-test_data = dataset.test
-prediction_length = dataset.metadata.prediction_length
-freq = dataset.metadata.freq
-predictor = ForecastingPredictor(path="m4_benchmark").fit(train_data,
-                                                          prediction_length,
-                                                          freq=freq,
-                                                          hyperparameter_tune=True,
-                                                          quantiles=[0.1, 0.5, 0.9],
-                                                          refit_full=True,
-                                                          set_best_to_refit_full=True,
-                                                          num_trials=5,
-                                                          )
-leaderboard = predictor.leaderboard(dataset.test)
-leaderboard.to_csv(f"benchmark_results/results_{int(time.time())}.csv", index=False)
-print(leaderboard)
-print(predictor.evaluate(test_data))
+for dataset_name in dataset_list:
+    print(f"Evaluating dataset {dataset_name}...")
+    dataset = get_dataset(dataset_name)
+    train_data = dataset.train
+    test_data = dataset.test
+    prediction_length = dataset.metadata.prediction_length
+    freq = dataset.metadata.freq
+    predictor = ForecastingPredictor(path="m4_benchmark").fit(train_data,
+                                                              prediction_length,
+                                                              freq=freq,
+                                                              hyperparameter_tune=True,
+                                                              quantiles=[0.1, 0.5, 0.9],
+                                                              refit_full=True,
+                                                              set_best_to_refit_full=True,
+                                                              num_trials=10,
+                                                              )
+    leaderboard = predictor.leaderboard(dataset.test)
+    leaderboard.to_csv(f"benchmark_results/results_{dataset_name}_{int(time.time())}.csv", index=False)
+    print(leaderboard)
+    print(predictor.evaluate(test_data))
