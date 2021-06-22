b1806c95c8c233d43005fd18581671287eabb2e2
yx1215 <yx1215@nyu.edu>
2021-06-22 09:01:54 +0800

    add forecasting examples

---
+++ examples/forecasting/benchmark_m4.py
@@ -0,0 +1,34 @@
+from gluonts.dataset.repository.datasets import get_dataset
+from autogluon.forecasting import ForecastingPredictor
+import os
+import time
+
+dataset_list = ["m4_hourly",
+                "m4_daily",
+                "m4_weekly",
+                "m4_monthly",
+                "m4_quarterly",
+                "m4_yearly",
+                "traffic",
+                "electricity"]
+os.makedirs("benchmark_results", exist_ok=True)
+for dataset_name in dataset_list:
+    print(f"Evaluating dataset {dataset_name}...")
+    dataset = get_dataset(dataset_name)
+    train_data = dataset.train
+    test_data = dataset.test
+    prediction_length = dataset.metadata.prediction_length
+    freq = dataset.metadata.freq
+    predictor = ForecastingPredictor(path=f"autogluon_benchmark_{dataset_name}").fit(train_data,
+                                                                                     prediction_length,
+                                                                                     freq=freq,
+                                                                                     hyperparameter_tune_kwargs="auto",
+                                                                                     quantiles=[0.1, 0.5, 0.9],
+                                                                                     refit_full=True,
+                                                                                     set_best_to_refit_full=True,
+                                                                                     num_trials=10,
+                                                                                     )
+    leaderboard = predictor.leaderboard(dataset.test)
+    leaderboard.to_csv(f"benchmark_results/results_{dataset_name}_{int(time.time())}.csv", index=False)
+    print(leaderboard)
+    print(predictor.evaluate(test_data))
---
+++ examples/forecasting/example_custom_model_forecast.py
@@ -0,0 +1,64 @@
+""" Example script for defining and using custom models in AutoGluon Forecasting """
+
+from autogluon.forecasting.models.gluonts_model.abstract_gluonts.abstract_gluonts_model import AbstractGluonTSModel
+from gluonts.model.deepar import DeepAREstimator
+from gluonts.mx.distribution.neg_binomial import NegativeBinomialOutput
+from autogluon.forecasting.utils.dataset_utils import time_series_dataset
+from autogluon.forecasting import ForecastingPredictor
+from autogluon.forecasting import TabularDataset
+
+
+class CustomDeepARModel(AbstractGluonTSModel):
+
+    def __init__(self, path: str, freq: str, prediction_length: int, name: str = "DeepAR_custom",
+                 eval_metric: str = None, hyperparameters=None, model=None, **kwargs):
+        super().__init__(path=path,
+                         freq=freq,
+                         prediction_length=prediction_length,
+                         hyperparameters=hyperparameters,
+                         name=name,
+                         eval_metric=eval_metric,
+                         model=model,
+                         **kwargs)
+
+    def create_model(self):
+        self.model = DeepAREstimator.from_hyperparameters(**self.params)
+
+
+train_data = TabularDataset("https://autogluon.s3-us-west-2.amazonaws.com/datasets/CovidTimeSeries/train.csv")
+test_data = TabularDataset("https://autogluon.s3-us-west-2.amazonaws.com/datasets/CovidTimeSeries/test.csv")
+
+gluonts_train_data = time_series_dataset(train_data, index_column="name", target_column="ConfirmedCases",
+                                         time_column="Date")
+gluonts_test_data = time_series_dataset(test_data, index_column="name", target_column="ConfirmedCases",
+                                        time_column="Date")
+
+epochs = 5
+# # Use the model outside task
+customized_model = CustomDeepARModel(path="AutogluonModels/", freq="D", prediction_length=19,
+                                     hyperparameters={"epochs": epochs, "num_batches_per_epoch": 10,
+                                                      "distr_output": NegativeBinomialOutput()}, )
+customized_model.fit(gluonts_train_data)
+print(customized_model.score(gluonts_test_data))
+predictions = customized_model.predict(gluonts_test_data)
+print(predictions["Afghanistan_"])
+
+# Training custom model alongside other models using task.fit #
+custom_hyperparameters = {CustomDeepARModel: {"epochs": epochs,
+                                              "num_batches_per_epoch": 10,
+                                              "distr_output": NegativeBinomialOutput()},
+                          "MQCNN": {'context_length': 19,
+                                    'epochs': epochs,
+                                    "num_batches_per_epoch": 10}
+                          }
+
+predictor = ForecastingPredictor().fit(train_data=train_data,
+                                       prediction_length=19,
+                                       index_column="name",
+                                       target_column="ConfirmedCases",
+                                       time_column="Date",
+                                       hyperparameters=custom_hyperparameters,
+                                       quantiles=[0.1, 0.5, 0.9])
+
+print(predictor.leaderboard())
+print(predictor.predict(test_data)["Afghanistan_"])
