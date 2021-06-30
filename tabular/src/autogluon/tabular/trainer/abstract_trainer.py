529d7cc65fad411622072aa0349215a15e1e901c
Nick Erickson <neerick@amazon.com>
2021-06-30 15:41:39 -0700

    Tabular: Added experimental refit_folds option to BaggedEnsembleModel (#1216)

--- tabular/src/autogluon/tabular/trainer/abstract_trainer.py
+++ tabular/src/autogluon/tabular/trainer/abstract_trainer.py
@@ -929,7 +929,7 @@ class AbstractTrainer:
         Trains model but does not add the trained model to this Trainer.
         Returns trained model object.
         """
-        model.fit(X=X, y=y, X_val=X_val, y_val=y_val, **model_fit_kwargs)
+        model = model.fit(X=X, y=y, X_val=X_val, y_val=y_val, **model_fit_kwargs)
         return model
 
     def _train_and_save(self, X, y, model: AbstractModel, X_val=None, y_val=None, stack_name='core', level=1, **model_fit_kwargs) -> List[str]:
