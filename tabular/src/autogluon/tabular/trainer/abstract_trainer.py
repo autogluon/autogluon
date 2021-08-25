994f13bc6aeb3afc1e195d99a6af29d29fde297f
jasonyoo <jasonyoo0116@gmail.com>
2021-08-25 14:33:13 -0700

    set proxy model class default to LGB

--- tabular/src/autogluon/tabular/trainer/abstract_trainer.py
+++ tabular/src/autogluon/tabular/trainer/abstract_trainer.py
@@ -1503,7 +1503,7 @@ class AbstractTrainer:
         best_fit_models = fit_models.loc[fit_models['score_val'] == fit_models['score_val'].max()]
         proxy_model = self.load_model(best_fit_models.loc[best_fit_models['fit_time'].idxmin()]['model'])
 
-        if feature_prune_kwargs[proxy_model.name].get('proxy_model_class', None) == "LGB":
+        if feature_prune_kwargs[proxy_model.name].get('proxy_model_class', "LGB") == "LGB":
             lgb_models = leaderboard[(leaderboard['model'].str.contains('LightGBM')) & (leaderboard['can_infer']) & (leaderboard['stack_level'] == level)]
             if len(lgb_models) > 0:
                 best_lgb_models = lgb_models.loc[lgb_models['score_val'] == lgb_models['score_val'].max()]
