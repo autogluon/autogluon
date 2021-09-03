12fd6da180f58641415a61bb405e60d64717aa7b
engsarah365 <eng_sarah365@yahoo.com>
2021-09-03 11:13:04 +0200

    Add 4

--- full_install.sh
+++ full_install.sh
@@ -12,4 +12,4 @@ python3 -m pip install -e tabular_to_image/
 python3 -m pip install -e autogluon/
 sudo apt-get install graphviz libgraphviz-dev pkg-config
 sudo apt-get install python-pip python-virtualenv
-pip install pygraphviz
+python3 -m pip install  pygraphviz
