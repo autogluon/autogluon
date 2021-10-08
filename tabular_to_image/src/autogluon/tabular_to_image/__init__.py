55aebdbaca3367aafd1a7bbea3a50b3f4ae94440
aa <eng_sarah127@pg.cu.edu.eg>
2021-10-08 18:06:58 +0200

    wprk

--- tabular_to_image/src/autogluon/tabular_to_image/__init__.py
+++ tabular_to_image/src/autogluon/tabular_to_image/__init__.py
@@ -10,7 +10,7 @@ try:
 except ImportError:
     pass
 
-#from autogluon.tabular_to_image.prediction import ImagePredictions #.predictor import TabularPredictor
-from .prediction import ImagePredictions
+from autogluon.tabular_to_image.prediction import ImagePredictions #.predictor import TabularPredictor
+#from .prediction import ImagePredictions
 
 logging.basicConfig(format='%(message)s')  # just print message in logs
