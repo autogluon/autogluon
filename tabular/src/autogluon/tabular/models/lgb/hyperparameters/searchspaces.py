d305bf3f65b6d1964ba5637d32c62374840f7887
Nick Erickson <neerick@amazon.com>
2021-10-14 17:55:10 -0700

    Tabular: Updated LightGBM search space

--- tabular/src/autogluon/tabular/models/lgb/hyperparameters/searchspaces.py
+++ tabular/src/autogluon/tabular/models/lgb/hyperparameters/searchspaces.py
@@ -22,7 +22,7 @@ def get_searchspace_multiclass_baseline(num_classes):
         'num_classes': num_classes,
         'learning_rate': Real(lower=5e-3, upper=0.2, default=0.05, log=True),
         'feature_fraction': Real(lower=0.75, upper=1.0, default=1.0),
-        'min_data_in_leaf': Int(lower=2, upper=30, default=20),  # TODO: Use size of dataset to set upper, if row count is small upper should be small
+        'min_data_in_leaf': Int(lower=2, upper=60, default=20),  # TODO: Use size of dataset to set upper, if row count is small upper should be small
         'num_leaves': Int(lower=16, upper=96, default=31),  # TODO: Use row count and feature count to set this, the higher feature count the higher num_leaves upper
         'num_boost_round': DEFAULT_NUM_BOOST_ROUND,
         'boosting_type': 'gbdt',
@@ -40,7 +40,7 @@ def get_searchspace_binary_baseline():
         'objective': 'binary',
         'learning_rate': Real(lower=5e-3, upper=0.2, default=0.05, log=True),
         'feature_fraction': Real(lower=0.75, upper=1.0, default=1.0),
-        'min_data_in_leaf': Int(lower=2, upper=30, default=20),
+        'min_data_in_leaf': Int(lower=2, upper=60, default=20),
         'num_leaves': Int(lower=16, upper=96, default=31),
         'num_boost_round': DEFAULT_NUM_BOOST_ROUND,
         'boosting_type': 'gbdt',
@@ -56,7 +56,7 @@ def get_searchspace_regression_baseline():
         'objective': 'regression',
         'learning_rate': Real(lower=5e-3, upper=0.2, default=0.05, log=True),
         'feature_fraction': Real(lower=0.75, upper=1.0, default=1.0),
-        'min_data_in_leaf': Int(lower=2, upper=30, default=20),
+        'min_data_in_leaf': Int(lower=2, upper=60, default=20),
         'num_leaves': Int(lower=16, upper=96, default=31),
         'num_boost_round': DEFAULT_NUM_BOOST_ROUND,
         'boosting_type': 'gbdt',
