7e877d1d12bdfdc8aa46f72742202bb4b9e65162
Alexander Shirkov <ashyrkou@amazon.com>
2021-11-03 14:59:42 -0700

    Adjusted lock initialization on imports - now only done on use

--- core/src/autogluon/core/locks.py
+++ core/src/autogluon/core/locks.py
@@ -4,3 +4,12 @@ import multiprocessing as mp
 class TaskLock(object):
     TASK_ID = mp.Value('i', 0)
     LOCK = mp.Lock()
+
+
+class DecoratorLock(object):
+    SEED = mp.Value('i', 0)
+    LOCK = mp.Lock()
+
+class RemoteLock(object):
+    LOCK = mp.Lock()
+    REMOTE_ID = mp.Value('i', 0)
