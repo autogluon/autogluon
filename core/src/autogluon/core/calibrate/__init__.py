bf02e9b42f89b0386de7ff0161cd72c26b024926
DolanTheMFWizard <89041085+DolanTheMFWizard@users.noreply.github.com>
2021-11-16 22:45:01 -0800

    Update __init__.py

--- core/src/autogluon/core/calibrate/__init__.py
+++ core/src/autogluon/core/calibrate/__init__.py
@@ -1,3 +1,8 @@
+# All methods retrofitted from https://github.com/dirichletcal/dirichlet_python
+# More information on methods:
+# https://arxiv.org/abs/1910.12656 (Beyond temperature scaling: Obtaining well-calibrated multiclass probabilities with Dirichlet calibration)
+# https://arxiv.org/abs/1706.04599 (On Calibration of Modern Neural Networks)
+# TODO: These methods can harm calibration of out-of-distribution data. Meant to improve calibration on in-distribution data
 from .fulldirichlet import FullDirichletCalibrator
 from .matrixscaling import MatrixScaling
 from .tempscaling import TemperatureScaling
