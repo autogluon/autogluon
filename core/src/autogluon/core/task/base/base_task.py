936b3a591ac5edd7cc08653bbe907ef4d8799597
Innixma <neerick@amazon.com>
2021-12-05 17:44:14 -0800

    added BaseTask back to avoid GluonCV error

--- core/src/autogluon/core/task/base/base_task.py
+++ core/src/autogluon/core/task/base/base_task.py
@@ -1,11 +1,17 @@
 import copy
+import logging
+import time
+from abc import abstractmethod
 
 from ...scheduler import HyperbandScheduler, FIFOScheduler
 from ...scheduler.seq_scheduler import LocalSequentialScheduler
+from ...utils import in_ipynb, try_import_mxnet
 from ...utils.utils import setup_compute
 
 __all__ = [
-    'compile_scheduler_options_v2']
+    'BaseTask',
+    'compile_scheduler_options_v2',
+    'create_scheduler']
 
 schedulers = {
     'local': LocalSequentialScheduler,
@@ -13,6 +19,73 @@ schedulers = {
     'hyperband_stopping': HyperbandScheduler,
     'hyperband_promotion': HyperbandScheduler,
 }
+
+logger = logging.getLogger(__name__)
+
+
+def create_scheduler(train_fn, scheduler, scheduler_options):
+    if isinstance(scheduler, str):
+        scheduler_cls = schedulers[scheduler.lower()]
+    else:
+        assert callable(scheduler)
+        scheduler_cls = scheduler
+        scheduler_options = copy.copy(scheduler_options)
+    return scheduler_cls(train_fn, **scheduler_options)
+
+
+# FIXME: REMOVE THIS, first GluonCV needs to stop depending on AG, as it imports this class
+class BaseTask(object):
+    """BaseTask for AutoGluon applications
+    """
+    @property
+    @staticmethod
+    def Dataset():
+        try_import_mxnet()
+        import mxnet as mx
+        return mx.gluon.data.Dataset
+
+    @classmethod
+    def run_fit(cls, train_fn, search_strategy, scheduler_options,
+                plot_results=False):
+        start_time = time.time()
+        # create scheduler and schedule tasks
+        scheduler = create_scheduler(train_fn, search_strategy, scheduler_options)
+        scheduler.run()
+        scheduler.join_jobs()
+        # gather the best configuration
+        best_reward = scheduler.get_best_reward()
+        best_config = scheduler.get_best_config()
+        args = train_fn.args
+        args.final_fit = True
+        if hasattr(args, 'epochs') and hasattr(args, 'final_fit_epochs'):
+            args.epochs = args.final_fit_epochs
+        train_fn.args.update({'final_fit':True})
+        train_fn.kwvars.update({'final_fit':True})
+        scheduler_final = create_scheduler(train_fn, search_strategy, scheduler_options)
+        results = scheduler_final.run_with_config(best_config)
+        total_time = time.time() - start_time
+        if plot_results or in_ipynb():
+            plot_training_curves = scheduler_options['checkpoint'].replace('exp1.ag', 'plot_training_curves.png')
+            scheduler.get_training_curves(filename=plot_training_curves, plot=True, use_legend=False)
+        record_args = copy.deepcopy(args)
+        if results is None:
+            logger.warning('No valid results obtained with best config, the result may not be useful...')
+            results = {}
+        results.update(best_reward=best_reward,
+                       best_config=best_config,
+                       total_time=total_time,
+                       metadata=scheduler.metadata,
+                       training_history=scheduler.training_history,
+                       config_history=scheduler.config_history,
+                       reward_attr=scheduler._reward_attr,
+                       args=record_args)
+        return results
+
+    @classmethod
+    @abstractmethod
+    def fit(cls, *args, **kwargs):
+        pass
+
 
 # These search_strategies use HyperbandScheduler, along with certain
 # searchers.
