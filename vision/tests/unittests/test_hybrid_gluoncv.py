a3da29816088a12b695b11e00610439b903a6ff1
Innixma <neerick@amazon.com>
2021-12-18 16:51:42 -0800

    fix imports

--- vision/tests/unittests/test_hybrid_gluoncv.py
+++ vision/tests/unittests/test_hybrid_gluoncv.py
@@ -4,8 +4,8 @@ import autogluon.core as ag
 IMAGE_CLASS_DATASET, _, IMAGE_CLASS_TEST = ImageClassification.Dataset.from_folders(
     'https://autogluon.s3.amazonaws.com/datasets/shopee-iet.zip')
 
+
 def test_hybrid_image_classification():
-    from gluoncv.auto.tasks import ImageClassification
     model = ag.Categorical('resnet18_v1b', 'resnet18')
     task = ImageClassification({'model': model, 'num_trials': 4, 'epochs': 1, 'batch_size': 8})
     classifier = task.fit(IMAGE_CLASS_DATASET)
