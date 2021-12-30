2615ebbf9ad4fbe0675847529c39c38e05f70d99
Innixma <neerick@amazon.com>
2021-12-29 17:53:42 -0800

    Remove cython, dill, paramiko, autograd

--- core/src/autogluon/core/utils/try_import.py
+++ core/src/autogluon/core/utils/try_import.py
@@ -7,7 +7,6 @@ __all__ = [
     'try_import_faiss',
     'try_import_fastai',
     'try_import_cv2',
-    'try_import_gluonnlp',
     'try_import_torch',
     'try_import_skopt',
     'try_import_autogluon_text',
@@ -131,24 +130,6 @@ def try_import_cv2():
         raise ImportError(
             "Unable to import dependency cv2. "
             "A quick tip is to install via `pip install opencv-python`. ")
-
-
-def try_import_gluonnlp():
-    try:
-        import gluonnlp
-        # TODO After 1.0 is supported,
-        #  we will remove the checking here and use gluonnlp.utils.check_version instead.
-        from pkg_resources import parse_version  # pylint: disable=import-outside-toplevel
-        gluonnlp_version = parse_version(gluonnlp.__version__)
-        assert gluonnlp_version >= parse_version('0.8.1') and\
-               gluonnlp_version <= parse_version('0.8.3'), \
-            'Currently, we only support 0.8.1<=gluonnlp<=0.8.3'
-    except ImportError:
-        raise ImportError(
-            "Unable to import dependency gluonnlp. The NLP model won't be available "
-            "without installing gluonnlp. "
-            "A quick tip is to install via `pip install gluonnlp==0.8.1`. ")
-    return gluonnlp
 
 
 def try_import_torch():
