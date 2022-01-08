9aa6af3180d22504fb8017b348504cef331f190e
Alexander Shirkov <ashyrkou@amazon.com>
2022-01-07 17:41:50 -0800

    Efficiency updates

--- tabular/src/autogluon/tabular/models/widedeep_nn/widedeep_nn_model.py
+++ tabular/src/autogluon/tabular/models/widedeep_nn/widedeep_nn_model.py
@@ -2,12 +2,14 @@ import logging
 
 import numpy as np
 import pytorch_widedeep.training.trainer
-
+import torch
 from autogluon.common.features.types import R_OBJECT
+from pytorch_widedeep.models import FTTransformer, TabPerceiver
+
 from autogluon.core.constants import BINARY, MULTICLASS, REGRESSION
 from autogluon.core.models import AbstractModel
+from common.src.autogluon.common.features.types import R_INT, R_FLOAT, R_DATETIME, R_BOOL, R_CATEGORY
 from .utils import set_seed
-import torch
 
 logger = logging.getLogger(__name__)
 
@@ -30,8 +32,7 @@ class WideDeepNNModel(AbstractModel):
         # TODO: Add try_import_pytorch_widedeep() to enable a more helpful error message if pytorch_widedeep is not installed
         #  Refer to other model implementations for examples
         from pytorch_widedeep import Trainer
-        from pytorch_widedeep.preprocessing import WidePreprocessor, TabPreprocessor
-        from pytorch_widedeep.models import Wide, TabMlp, WideDeep, TabResnet, SAINT, TabTransformer
+        from pytorch_widedeep.preprocessing import TabPreprocessor
         from pytorch_widedeep.metrics import Accuracy, R2Score
         # TODO: Use this to get user-specified params instead of hard-coding
         # params = self._get_model_params()
@@ -44,19 +45,9 @@ class WideDeepNNModel(AbstractModel):
         # TODO: Either don't use cross_cols or find a way to automatically determine them in a fully automated fashion
         # TODO: Find a way to automatically determine embed dimensions
 
-        embed_cols = []
-        cont_cols = self.feature_metadata.get_features(valid_raw_types=['float', 'int'])
-        cat_cols = self.feature_metadata.get_features(valid_raw_types=['category'])
-        for cat_feat in cat_cols:
-            num_categories = len(X[cat_feat].cat.categories)
-            if num_categories >= 32:
-                embed_cols.append((cat_feat, 32))
-            elif num_categories >= 16:
-                embed_cols.append((cat_feat, 16))
-            elif num_categories >= 8:
-                embed_cols.append((cat_feat, 8))
-
-        wide_cols = cat_cols
+        cont_cols = self._feature_metadata.get_features(valid_raw_types=[R_INT, R_FLOAT, R_DATETIME])
+        cat_cols = self._feature_metadata.get_features(valid_raw_types=[R_OBJECT, R_CATEGORY, R_BOOL])
+        print(f'### {cat_cols}')
 
         if self.problem_type == BINARY:
             objective = 'binary'
@@ -73,75 +64,39 @@ class WideDeepNNModel(AbstractModel):
         else:
             raise ValueError(f'{self.name} does not support the problem_type {self.problem_type}.')
 
-        # wide
-        self._wide_preprocessor = WidePreprocessor(wide_cols=wide_cols)
-        X_wide = self._wide_preprocessor.fit_transform(X)
-
-        wide = Wide(wide_dim=np.unique(X_wide).shape[0], pred_dim=pred_dim)
-
         # deeptabular
-        self._tab_preprocessor = TabPreprocessor(embed_cols=embed_cols, continuous_cols=cont_cols)
+        for_transformer = self.params['type'] in ['tab_transformer', 'ft_transformer', 'tab_perciever']
+        print(f'### for_transformer = {for_transformer}')
+        if for_transformer:
+            embed_cols = cat_cols
+        else:
+            embed_cols = []
+            for cat_feat in cat_cols:
+                num_categories = len(X[cat_feat].cat.categories)
+                embed_cols.append((cat_feat, min(600, round(1.6 * num_categories ** 0.56))))
+
+        self._tab_preprocessor = TabPreprocessor(embed_cols=embed_cols, continuous_cols=cont_cols, for_transformer=for_transformer)
         X_tab = self._tab_preprocessor.fit_transform(X)
 
-        self.name = f"{self.params['type']}_{self.name}"
-        if self.params['type'] == 'tabmlp':
-            model = TabMlp(
-                # mlp_hidden_dims=[200, 100],
-                column_idx=self._tab_preprocessor.column_idx,
-                embed_input=self._tab_preprocessor.embeddings_input,
-                continuous_cols=cont_cols,
-            )
+        model = self._construct_wide_deep_model(
+            self.params['type'],
+            self._tab_preprocessor.column_idx,
+            self._tab_preprocessor.embeddings_input,
+            cont_cols,
+            pred_dim,
+            **self.params.get('model_args', {})
+        )
 
-            # wide and deep
-            # model = WideDeep(wide=wide, deeptabular=deeptabular, pred_dim=pred_dim)
-
-        elif self.params['type'] == 'tabresnet':
-            model = TabResnet(
-                # blocks_dims=[16, 16, 16],
-                column_idx=self._tab_preprocessor.column_idx,
-                embed_input=self._tab_preprocessor.embeddings_input,
-                continuous_cols=cont_cols,
-                # blocks_dims=[200,200,200],
-                # cont_norm_layer="layernorm",
-                # concat_cont_first=False,
-                # mlp_hidden_dims=[200, 100],
-                # mlp_dropout=0.1,
-            )
-        elif self.params['type'] == 'SAINT':
-            model = SAINT(
-                column_idx=self._tab_preprocessor.column_idx,
-                embed_input=self._tab_preprocessor.embeddings_input,
-                continuous_cols=cont_cols,
-                # embed_continuous_activation="leaky_relu",
-            )
-        elif self.params['type'] == 'tab_transformer':
-            model = TabTransformer(
-                column_idx=self._tab_preprocessor.column_idx,
-                embed_input=self._tab_preprocessor.embeddings_input,
-                continuous_cols=cont_cols,
-            )
-        else:
-            raise ValueError(f'Unknown model type {self.params["type"]}')
-
-        model = WideDeep(deeptabular=model, pred_dim=pred_dim)
-
-        X_train = dict()
-        X_train['X_wide'] = X_wide
-        X_train['X_tab'] = X_tab
-        X_train['target'] = y.values
+        X_train = { 'X_tab': X_tab, 'target': y.values}
 
         if X_val is not None and y_val is not None:
-            X_val_in = dict()
-            X_val_in['X_wide'] = self._wide_preprocessor.transform(X_val)
-            X_val_in['X_tab'] = self._tab_preprocessor.transform(X_val)
-            X_val_in['target'] = y_val.values
+            X_val_in = {'X_tab': self._tab_preprocessor.transform(X_val), 'target': y_val.values}
             val_split = None
         else:
             X_val_in = None
             val_split = 0.1
 
         logger.log(15, model)
-
 
         # train the model
         # TODO: Add custom metric support (Convert arbitrary AG metric)
@@ -150,9 +105,12 @@ class WideDeepNNModel(AbstractModel):
         # TODO: confirm if this is reproducible on linux
         pytorch_widedeep.training.trainer.n_cpus = 0
 
-        n_epochs = 3
-        wide_opt = torch.optim.Adam(model.deeptabular.parameters(), lr=0.02)
-        wide_sch = torch.optim.lr_scheduler.CosineAnnealingLR(wide_opt, T_max=n_epochs, eta_min=1e-5)
+        n_epochs = 10
+        lr = 1e-2
+        bs = 256
+        tab_opt = torch.optim.Adam(model.deeptabular.parameters())
+        steps_per_epoch = int(np.ceil(len(X_train)/bs))
+        tab_sch = torch.optim.lr_scheduler.OneCycleLR(tab_opt, max_lr=lr, epochs=n_epochs, steps_per_epoch=steps_per_epoch)
 
         trainer = Trainer(model, objective=objective, metrics=metrics)
         # FIXME: Does not return best epoch, instead returns final epoch
@@ -162,9 +120,9 @@ class WideDeepNNModel(AbstractModel):
             X_train=X_train,
             X_val=X_val_in,
             n_epochs=n_epochs,
-            optimizers=[wide_opt],
-            lr_schedulers=[wide_sch],
-            batch_size=256,
+            optimizers=[tab_opt],
+            lr_schedulers=[tab_sch],
+            batch_size=bs,
             val_split=val_split,
         )
 
@@ -172,12 +130,11 @@ class WideDeepNNModel(AbstractModel):
 
     def _predict_proba(self, X, **kwargs):
         X = self.preprocess(X, **kwargs)
-        X_wide = self._wide_preprocessor.transform(X)
         X_tab = self._tab_preprocessor.transform(X)
         if self.problem_type != REGRESSION:
-            preds = self.model.predict_proba(X_wide=X_wide, X_tab=X_tab)
+            preds = self.model.predict_proba(X_tab=X_tab)
         else:
-            preds = self.model.predict(X_wide=X_wide, X_tab=X_tab)
+            preds = self.model.predict(X_tab=X_tab)
 
         if self.problem_type == BINARY:
             return preds[:, 1]
@@ -191,3 +148,31 @@ class WideDeepNNModel(AbstractModel):
         )
         default_auxiliary_params.update(extra_auxiliary_params)
         return default_auxiliary_params
+
+    @staticmethod
+    def _construct_wide_deep_model(model_type, column_idx, embed_input, continuous_cols, pred_dim, **model_args):
+        from pytorch_widedeep.models import TabMlp, WideDeep, TabResnet, SAINT, TabTransformer
+
+        print(f'### model_args: {model_args}')
+
+        __MODEL_TYPES = dict(
+            tabmlp=TabMlp,
+            tabresnet=TabResnet,
+            SAINT=SAINT,
+            tab_transformer=TabTransformer,
+            ft_transformer=FTTransformer,
+            tab_perciever=TabPerceiver,
+        )
+
+        model_cls = __MODEL_TYPES.get(model_type, None)
+        if model_cls is None:
+            raise ValueError(f'Unknown model type {model_type}')
+
+        model = model_cls(
+            column_idx=column_idx,
+            embed_input=embed_input,
+            continuous_cols=continuous_cols,
+            **model_args
+        )
+        model = WideDeep(deeptabular=model, pred_dim=pred_dim)
+        return model
