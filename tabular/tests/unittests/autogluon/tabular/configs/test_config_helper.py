b4dad19c7a486c1b06c4417f39daa9742a7fcf49
Alexander Shirkov <ashyrkou@amazon.com>
2022-01-14 13:40:51 -0800

    Config helper

--- tabular/tests/unittests/autogluon/tabular/configs/test_config_helper.py
+++ tabular/tests/unittests/autogluon/tabular/configs/test_config_helper.py
@@ -3,105 +3,105 @@ import pytest
 from autogluon.tabular.configs.config_helper import ConfigBuilder
 
 
-def test_with_presets():
+def test_presets():
     expected_config = dict(presets=['best_quality'])
-    acutal_config = ConfigBuilder().with_presets('best_quality').build()
+    acutal_config = ConfigBuilder().presets('best_quality').build()
     assert acutal_config == expected_config
 
-    acutal_config = ConfigBuilder().with_presets(['best_quality']).build()
+    acutal_config = ConfigBuilder().presets(['best_quality']).build()
     assert acutal_config == expected_config
 
     expected_config = dict(presets=['best_quality', 'optimize_for_deployment'])
-    acutal_config = ConfigBuilder().with_presets(['best_quality', 'optimize_for_deployment']).build()
+    acutal_config = ConfigBuilder().presets(['best_quality', 'optimize_for_deployment']).build()
     assert acutal_config == expected_config
 
 
-def test_with_presets_invalid_option():
+def test_presets_invalid_option():
     with pytest.raises(AssertionError, match=r"The following preset are not recognized: .'unknown1'. - use one of the valid presets: .*"):
-        ConfigBuilder().with_presets('unknown1').build()
+        ConfigBuilder().presets('unknown1').build()
 
     with pytest.raises(AssertionError, match=r"The following preset are not recognized: .'unknown2', 'unknown3'. - use one of the valid presets: .*"):
-        ConfigBuilder().with_presets(['best_quality', 'unknown2',  'unknown3']).build()
+        ConfigBuilder().presets(['best_quality', 'unknown2',  'unknown3']).build()
 
 
-def test_with_excluded_model_types():
+def test_excluded_model_types():
     expected_config = dict(excluded_model_types=['NN'])
-    acutal_config = ConfigBuilder().with_excluded_model_types('NN').build()
+    acutal_config = ConfigBuilder().excluded_model_types('NN').build()
     assert acutal_config == expected_config
 
-    acutal_config = ConfigBuilder().with_excluded_model_types(['NN']).build()
+    acutal_config = ConfigBuilder().excluded_model_types(['NN']).build()
     assert acutal_config == expected_config
 
     expected_config = dict(excluded_model_types=['NN', 'LR'])
-    acutal_config = ConfigBuilder().with_excluded_model_types(['NN', 'LR']).build()
+    acutal_config = ConfigBuilder().excluded_model_types(['NN', 'LR']).build()
     assert acutal_config == expected_config
 
     expected_config = dict(excluded_model_types=['NN'])
-    acutal_config = ConfigBuilder().with_excluded_model_types(['NN', 'NN']).build()
+    acutal_config = ConfigBuilder().excluded_model_types(['NN', 'NN']).build()
     assert acutal_config == expected_config
 
 
-def test_with_excluded_model_types_invalid_option():
+def test_excluded_model_types_invalid_option():
     with pytest.raises(AssertionError, match=r"unknown1 is not one of the valid models .*"):
-        ConfigBuilder().with_excluded_model_types('unknown1').build()
+        ConfigBuilder().excluded_model_types('unknown1').build()
 
     with pytest.raises(AssertionError, match=r"unknown2 is not one of the valid models .*"):
-        ConfigBuilder().with_excluded_model_types(['unknown2']).build()
+        ConfigBuilder().excluded_model_types(['unknown2']).build()
 
 
-def test_with_included_model_types():
+def test_included_model_types():
     expected_config = dict(excluded_model_types=['RF', 'XT', 'KNN', 'GBM', 'CAT', 'XGB', 'QNN', 'LR', 'FASTAI', 'TRANSF', 'AG_TEXT_NN', 'AG_IMAGE_NN', 'FASTTEXT', 'VW'])
-    acutal_config = ConfigBuilder().with_included_model_types('NN').build()
+    acutal_config = ConfigBuilder().included_model_types('NN').build()
     assert acutal_config == expected_config
 
-    acutal_config = ConfigBuilder().with_included_model_types(['NN']).build()
+    acutal_config = ConfigBuilder().included_model_types(['NN']).build()
     assert acutal_config == expected_config
 
-    acutal_config = ConfigBuilder().with_included_model_types(['NN', 'NN']).build()
+    acutal_config = ConfigBuilder().included_model_types(['NN', 'NN']).build()
     assert acutal_config == expected_config
 
     expected_config = dict(excluded_model_types=['RF', 'XT', 'KNN', 'GBM', 'CAT', 'XGB', 'QNN', 'FASTAI', 'TRANSF', 'AG_TEXT_NN', 'AG_IMAGE_NN', 'FASTTEXT', 'VW'])
-    acutal_config = ConfigBuilder().with_included_model_types(['NN', 'LR']).build()
+    acutal_config = ConfigBuilder().included_model_types(['NN', 'LR']).build()
     assert acutal_config == expected_config
 
 
-def test_with_included_model_types_invalid_option():
+def test_included_model_types_invalid_option():
     with pytest.raises(AssertionError, match=r"The following model types are not recognized: .'unknown1'. - use one of the valid models: .*"):
-        ConfigBuilder().with_included_model_types('unknown1').build()
+        ConfigBuilder().included_model_types('unknown1').build()
 
     with pytest.raises(AssertionError, match=r"The following model types are not recognized: .'unknown2', 'unknown3'. - use one of the valid models: .*"):
-        ConfigBuilder().with_included_model_types(['RF', 'unknown2', 'unknown3']).build()
+        ConfigBuilder().included_model_types(['RF', 'unknown2', 'unknown3']).build()
 
 
-def test_with_time_limit():
+def test_time_limit():
     expected_config = dict(time_limit=10)
-    acutal_config = ConfigBuilder().with_time_limit(10).build()
+    acutal_config = ConfigBuilder().time_limit(10).build()
     assert acutal_config == expected_config
 
 
-def test_with_time_limit_invalid_option():
+def test_time_limit_invalid_option():
     with pytest.raises(AssertionError, match=r"time_limit must be greater than zero"):
-        ConfigBuilder().with_time_limit(-1).build()
+        ConfigBuilder().time_limit(-1).build()
 
 
-def test_with_hyperparameters_str():
+def test_hyperparameters_str():
     expected_config = dict(hyperparameters='very_light')
-    acutal_config = ConfigBuilder().with_hyperparameters('very_light').build()
+    acutal_config = ConfigBuilder().hyperparameters('very_light').build()
     assert acutal_config == expected_config
 
 
-def test_with_hyperparameters_dict():
+def test_hyperparameters_dict():
     expected_config = dict(hyperparameters={'NN': {}})
-    acutal_config = ConfigBuilder().with_hyperparameters({'NN': {}}).build()
+    acutal_config = ConfigBuilder().hyperparameters({'NN': {}}).build()
     assert acutal_config == expected_config
 
 
-def test_with_hyperparameters__invalid_option():
+def test_hyperparameters__invalid_option():
     with pytest.raises(ValueError, match=r"hyperparameters must be either str: .* or dict with keys of .*"):
-        ConfigBuilder().with_hyperparameters(42).build()
+        ConfigBuilder().hyperparameters(42).build()
 
     with pytest.raises(AssertionError, match=r"unknown is not one of the valid presets .*"):
-        ConfigBuilder().with_hyperparameters('unknown').build()
+        ConfigBuilder().hyperparameters('unknown').build()
 
     with pytest.raises(AssertionError, match=r"The following model types are not recognized: .'unknown'. - use one of the valid models: .*"):
-        ConfigBuilder().with_hyperparameters({'unknown': []}).build()
+        ConfigBuilder().hyperparameters({'unknown': []}).build()
