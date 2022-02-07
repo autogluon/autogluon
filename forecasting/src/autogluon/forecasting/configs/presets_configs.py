98554bc6b4eef8cc339ad5a1bbcb307e11d5d0b3
Caner Turkmen <turkmen.ac@gmail.com>
2022-02-07 08:35:54 +0100

    black configs

--- forecasting/src/autogluon/forecasting/configs/presets_configs.py
+++ forecasting/src/autogluon/forecasting/configs/presets_configs.py
@@ -1,34 +1,40 @@
 forecasting_presets_configs = dict(
-    best_quality={"hyperparameters": "default_hpo",
-                  "hyperparameter_tune_kwargs": {'scheduler': 'local',
-                                                'searcher': 'random',
-                                                'num_trials': 20
-                                                },
-                  "refit_full": True
-                  },
-    high_quality={"hyperparameters": "default_hpo",
-                  "hyperparameter_tune_kwargs": {'scheduler': 'local',
-                                                'searcher': 'random',
-                                                'num_trials': 10
-                                                },
-                  "refit_full": True
-                  },
-    good_quality={"hyperparameters": "default_hpo",
-                  "hyperparameter_tune_kwargs": {'scheduler': 'local',
-                                                'searcher': 'random',
-                                                'num_trials': 2
-                                                },
-                  "refit_full": True
-                  },
-    medium_quality={"hyperparameters": "default",
-                    "refit_full": True},
+    best_quality={
+        "hyperparameters": "default_hpo",
+        "hyperparameter_tune_kwargs": {
+            "scheduler": "local",
+            "searcher": "random",
+            "num_trials": 20,
+        },
+        "refit_full": True,
+    },
+    high_quality={
+        "hyperparameters": "default_hpo",
+        "hyperparameter_tune_kwargs": {
+            "scheduler": "local",
+            "searcher": "random",
+            "num_trials": 10,
+        },
+        "refit_full": True,
+    },
+    good_quality={
+        "hyperparameters": "default_hpo",
+        "hyperparameter_tune_kwargs": {
+            "scheduler": "local",
+            "searcher": "random",
+            "num_trials": 2,
+        },
+        "refit_full": True,
+    },
+    medium_quality={"hyperparameters": "default", "refit_full": True},
     low_quality={"hyperparameters": "toy"},
-    low_quality_hpo={"hyperparameters": "toy_hpo",
-                      "hyperparameter_tune_kwargs": {'scheduler': 'local',
-                                                     'searcher': 'random',
-                                                     'num_trials': 2
-                                                     },
-                      "refit_full": True
-                    },
-
+    low_quality_hpo={
+        "hyperparameters": "toy_hpo",
+        "hyperparameter_tune_kwargs": {
+            "scheduler": "local",
+            "searcher": "random",
+            "num_trials": 2,
+        },
+        "refit_full": True,
+    },
 )
