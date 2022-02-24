9e94dfc25433a1367cb11b3bc1e98d69a7e9978f
Xingjian Shi <xshiab@connect.ust.hk>
2022-02-24 15:14:02 -0800

    Update predictor.py

--- text/src/autogluon/text/automm/predictor.py
+++ text/src/autogluon/text/automm/predictor.py
@@ -475,10 +475,12 @@ class AutoMMPredictor:
             grad_steps = config.env.batch_size // (
                     config.env.per_gpu_batch_size * config.env.num_nodes
             )
+            precision = 32  # Force to use fp32 for training since AMP is not available in CPU
         else:
             grad_steps = config.env.batch_size // (
                     config.env.per_gpu_batch_size * num_gpus * config.env.num_nodes
             )
+            precision = config.env.precision
 
         if num_gpus <= 1:
             strategy = None
@@ -489,7 +491,7 @@ class AutoMMPredictor:
             gpus=num_gpus,
             auto_select_gpus=config.env.auto_select_gpus if num_gpus != 0 else False,
             num_nodes=config.env.num_nodes,
-            precision=config.env.precision,
+            precision=precision,
             strategy=strategy,
             benchmark=False,
             deterministic=config.env.deterministic,
