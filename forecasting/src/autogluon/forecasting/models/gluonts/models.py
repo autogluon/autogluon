ace2f5542fdffaef45df9434385fb5304c698b22
Caner Turkmen <turkmen.ac@gmail.com>
2022-03-23 09:56:14 +0100

    added prophet to the gluonts model zoo

--- forecasting/src/autogluon/forecasting/models/gluonts/models.py
+++ forecasting/src/autogluon/forecasting/models/gluonts/models.py
@@ -7,7 +7,8 @@ from autogluon.core.utils import warning_filter
 
 with warning_filter():
     from gluonts.model.deepar import DeepAREstimator
-    from gluonts.model.estimator import Estimator as GluonTSEstimator
+    from gluonts.model.estimator import Estimator as GluonTSEstimator, DummyEstimator
+    from gluonts.model.prophet import ProphetPredictor
     from gluonts.model.seq2seq import MQCNNEstimator
     from gluonts.model.simple_feedforward import SimpleFeedForwardEstimator
     from gluonts.mx.context import get_mxnet_context
@@ -159,6 +160,61 @@ class GenericGluonTSModel(AbstractGluonTSModel):
         params_dict = super().get_params()
         params_dict["gluonts_estimator_class"] = self.gluonts_estimator_class
         return params_dict
+
+
+class _ProphetDummyEstimator(DummyEstimator):
+    def train(self, train_data, validation_data=None, **kwargs):
+        return self.predictor
+
+
+class ProphetModel(AbstractGluonTSModel):
+    """Wrapper around `Prophet <https://github.com/facebook/prophet>`_, which wraps the
+    library through GluonTS's wrapper.
+
+    In order to use it you need to install the package::
+
+        # you can either install Prophet directly
+        pip install fbprophet
+
+        # or install gluonts with the Prophet extras
+        pip install gluonts[Prophet]
+
+    Other Parameters
+    ----------------
+    hyperparameters
+        Model hyperparameters that will be passed directly to the `Prophet`
+        class. See Prophet documentation for available parameters.
+    """
+    gluonts_estimator_class = _ProphetDummyEstimator
+    allowed_prophet_parameters = [
+        'growth',
+        'changepoints',
+        'n_changepoints',
+        'changepoint_range',
+        'yearly_seasonality',
+        'weekly_seasonality',
+        'daily_seasonality',
+        'holidays',
+        'seasonality_mode',
+        'seasonality_prior_scale',
+        'holidays_prior_scale',
+        'changepoint_prior_scale',
+        'mcmc_samples',
+        'interval_width',
+        'uncertainty_samples'
+    ]
+
+    def _get_estimator(self) -> GluonTSEstimator:
+        model_init_params = self._get_estimator_init_args()
+
+        return _ProphetDummyEstimator(
+            predictor_cls=ProphetPredictor,
+            freq=model_init_params['freq'],
+            prediction_length=model_init_params['prediction_length'],
+            prophet_params={
+                k: v for k, v in model_init_params.items() if k in self.allowed_prophet_parameters
+            }
+        )
 
 
 # TODO: AutoGluon Tabular will be removed from GluonTS to avoid circular dependencies
--- forecasting/tests/unittests/models/test_gluonts.py
+++ forecasting/tests/unittests/models/test_gluonts.py
@@ -6,13 +6,15 @@ from gluonts.model.seq2seq import MQRNNEstimator
 from gluonts.model.transformer import TransformerEstimator
 
 import autogluon.core as ag
+from autogluon.core.scheduler.scheduler_factory import scheduler_factory
 from autogluon.forecasting.models.gluonts import (
     DeepARModel,
     # AutoTabularModel,
-    SimpleFeedForwardModel,
+    GenericGluonTSModel,
     MQCNNModel,
+    ProphetModel,
+    SimpleFeedForwardModel,
 )
-from autogluon.forecasting.models.gluonts.models import GenericGluonTSModel
 
 from .common import DUMMY_DATASET
 
@@ -21,6 +23,7 @@ TESTABLE_MODELS = [
     DeepARModel,
     MQCNNModel,
     SimpleFeedForwardModel,
+    ProphetModel,
     partial(GenericGluonTSModel, gluonts_estimator_class=MQRNNEstimator),  # partial constructor for generic model
     partial(GenericGluonTSModel, gluonts_estimator_class=TransformerEstimator),
 ]
@@ -171,3 +174,72 @@ def test_when_fit_called_then_models_train_and_returned_predictor_inference_has_
         assert all(
             k in df.columns for _, df in predictions.items()
         )
+
+
+@pytest.mark.parametrize("growth", ["linear", "logistic"])
+@pytest.mark.parametrize("n_changepoints", [3, 5])
+def test_when_fit_called_on_prophet_then_hyperparameters_are_passed_to_underlying_model(
+    growth, n_changepoints, temp_model_path
+):
+    model = ProphetModel(
+        path=temp_model_path,
+        freq="H",
+        prediction_length=4,
+        hyperparameters={"growth": growth, "n_changepoints": n_changepoints},
+    )
+
+    model.fit(train_data=DUMMY_DATASET)
+
+    assert model.gts_predictor.prophet_params.get("growth") == growth  # noqa
+    assert model.gts_predictor.prophet_params.get("n_changepoints") == n_changepoints  # noqa
+
+
+@pytest.mark.parametrize("growth", ["linear", "logistic"])
+@pytest.mark.parametrize("n_changepoints", [3, 5])
+def test_when_prophet_model_saved_then_prophet_parameters_are_loaded(
+    growth, n_changepoints, temp_model_path
+):
+    model = ProphetModel(
+        path=temp_model_path,
+        freq="H",
+        quantile_levels=[0.1, 0.9],
+        hyperparameters={
+            "growth": growth, "n_changepoints": n_changepoints
+        },
+    )
+    model.fit(
+        train_data=DUMMY_DATASET,
+    )
+    model.save()
+
+    loaded_model = model.__class__.load(path=model.path)
+
+    assert loaded_model.gts_predictor.prophet_params.get("growth") == growth  # noqa
+    assert loaded_model.gts_predictor.prophet_params.get("n_changepoints") == n_changepoints  # noqa
+
+
+def test_when_hyperparameter_tune_called_on_prophet_then_hyperparameters_are_passed_to_underlying_model(
+    temp_model_path
+):
+    scheduler_options = scheduler_factory(hyperparameter_tune_kwargs="auto")
+
+    model = ProphetModel(
+        path=temp_model_path,
+        freq="H",
+        prediction_length=4,
+        hyperparameters={
+            "growth": "linear", "n_changepoints": ag.Int(3, 4)
+        },
+    )
+    _, _, results = model.hyperparameter_tune(
+        scheduler_options=scheduler_options,
+        time_limit=100,
+        train_data=DUMMY_DATASET,
+        val_data=DUMMY_DATASET,
+    )
+
+    assert len(results["config_history"]) == 2
+    assert results["config_history"][0]["n_changepoints"] == 3
+    assert results["config_history"][1]["n_changepoints"] == 4
+
+    assert all(c["growth"] == "linear" for c in results["config_history"].values())
