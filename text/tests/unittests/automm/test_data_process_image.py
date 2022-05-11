771799899ebe943b1bd633bb86e8d96496a4cf3a
XiaoLiang <74056923+Linuxdex@users.noreply.github.com>
2022-05-11 14:39:28 +0800

    Update test_data_process_image.py

--- text/tests/unittests/automm/test_data_process_image.py
+++ text/tests/unittests/automm/test_data_process_image.py
@@ -1,6 +1,18 @@
 import pytest
-
+import PIL
 from autogluon.text.automm.data.process_image import ImageProcessor
+
+from datasets import (
+    PetFinderDataset,
+    HatefulMeMesDataset,
+    AEDataset,
+)
+
+ALL_DATASETS = {
+    "petfinder": PetFinderDataset,
+    "hateful_memes": HatefulMeMesDataset,
+    "ae": AEDataset,
+}
 
 @pytest.mark.parametrize(
     "augmentations",
@@ -38,6 +50,8 @@ from autogluon.text.automm.data.process_image import ImageProcessor
 )
 
 def test_data_process_image(augmentations):
+    dataset = ALL_DATASETS["petfinder"]()
+    image = PIL.Image.open(dataset.train_df.Images[0]).convert("RGB")
 
     data_processors=ImageProcessor(
         image_column_names=['train_df.Images'],
@@ -47,6 +61,8 @@ def test_data_process_image(augmentations):
         size=224,
         norm_type="imagenet",
     )
+
+    transimage = data_processors.train_processor(image)
 
     assert len(data_processors.train_processor.transforms) == len(augmentations["model.timm_image.train_transform_types"])+2
 
