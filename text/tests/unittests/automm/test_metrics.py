85798216e554b05af3adb5fe0e403180f93ce0ec
XiaoLiang <74056923+Linuxdex@users.noreply.github.com>
2022-06-06 22:30:49 +0800

    Add test for BCEWithLogitsLoss

--- text/tests/unittests/automm/test_metrics.py
+++ text/tests/unittests/automm/test_metrics.py
@@ -3,7 +3,7 @@ import random
 import torch
 from torchmetrics import MeanMetric
 from sklearn.metrics import log_loss
-from autogluon.text.automm.optimization.utils import get_metric
+from autogluon.text.automm.optimization.utils import get_metric, get_loss_func
 from autogluon.text.automm.constants import MULTICLASS
 
 
@@ -34,6 +34,43 @@ def test_cross_entropy(metric_name, class_num):
 
     score1 = mean_metric.compute()
     preds = torch.cat(preds).softmax(dim=1)
+    targets = torch.cat(targets)
+    score2 = log_loss(
+        y_true=targets,
+        y_pred=preds,
+    )
+    assert pytest.approx(score1, 1e-6) == score2
+
+
+@pytest.mark.parametrize(
+    "problem_type,loss_func_replaced",
+    [
+        ("regression", "bcewithlogitsloss"),
+    ]
+)
+def test_bce_with_logits_loss(problem_type, loss_func_replaced):
+    preds = []
+    targets = []
+    random.seed(123)
+    torch.manual_seed(123)
+
+    for i in range(100):
+        bs = random.randint(1, 16)
+        preds.append(torch.randn(bs, 1))
+        targets.append(torch.rand(bs, 1))
+
+    loss_func = get_loss_func(
+        problem_type=problem_type,
+        mixup_active=False,
+        loss_func_replaced=loss_func_replaced,
+    )
+    mean_metric = MeanMetric()
+
+    for per_pred, per_target in zip(preds, targets):
+        mean_metric.update(loss_func(per_pred, per_target))
+
+    score1 = mean_metric.compute()
+    preds = torch.cat(preds).sigmoid()
     targets = torch.cat(targets)
     score2 = log_loss(
         y_true=targets,
