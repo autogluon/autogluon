53e94f36c4110b2f2b8042c796d09f128f694a45
lzcemma <zl71@rice.edu>
2022-06-10 00:04:39 +0000

    minor changes

--- text/src/autogluon/text/automm/data/utils.py
+++ text/src/autogluon/text/automm/data/utils.py
@@ -38,8 +38,6 @@ class InsertPunctuation(Augmenter):
     """
     Inherit nlpaug basic augmenter to support insert random punction at random location https://arxiv.org/pdf/2108.13230.pdf
 
-    aug_max = 0.3 * sentencelength
-
     example:
     a healthy ,clean , sweet little girl in Mantin . send me message if you can give her a nice home
     ? a ! healthy ,clean , sweet little : girl , in Mantin . send me message . if you ; can give her ? a nice home
@@ -48,24 +46,32 @@ class InsertPunctuation(Augmenter):
     def __init__(
         self,
         name="Insert_Punc",
-        action="insert",
         aug_min=1,
         aug_max=50,
         aug_p=0.3,
-        device="cpu",
-        include_detail=False,
-        verbose=0,
     ):
+        """
+        Parameters
+        ----------
+        name
+            name used when print out augmentation function
+        aug_min
+            minimum number of punctuation to insert
+        aug_max
+            maximum number of punctuation to insert
+        aug_p
+            how many punctuation to insert calcualted as aug_p * sentence length
+        """
         super().__init__(
             name=name,
             method=Method.WORD,
-            action=action,
+            action="insert",
             aug_min=aug_min,
             aug_max=aug_max,
             aug_p=aug_p,
-            device=device,
-            include_detail=include_detail,
-            verbose=verbose,
+            device="cpu",
+            include_detail=False,
+            verbose=0,
         )
         self.punc_list = [".", ",", "!", "?", ";", ":"]
 
