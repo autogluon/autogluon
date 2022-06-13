0954ddc98c39179565b71cc848a547126525c047
Xingjian Shi <xshiab@connect.ust.hk>
2022-06-12 17:33:27 -0700

    update

--- examples/automm/tabular_dl/example_tabular.py
+++ examples/automm/tabular_dl/example_tabular.py
@@ -31,7 +31,7 @@ TABULAR_DATASETS = {
     'ye': YearTabularDataset,
 }
 
-hyperparameters = {
+automm_hyperparameters = {
     'data.categorical.convert_to_text': False,
     'model.names': [
         "categorical_transformer",
@@ -44,7 +44,7 @@ hyperparameters = {
     'env.num_workers': 12,
     'env.num_workers_evaluation': 12,
     'env.num_gpus': 1,
-    'optimization.max_epochs': 200,  # Specify a large value to train until convergence
+    'optimization.max_epochs': 2000,  # Specify a large value to train until convergence
     'optimization.weight_decay': 1.0e-5,
     'optimization.lr_choice': None,
     'optimization.lr_schedule': "polynomial_decay",
@@ -52,6 +52,7 @@ hyperparameters = {
     'optimization.patience': 20,
     'optimization.top_k': 3,
 }
+
 
 def main(args):
     assert args.dataset_name in TABULAR_DATASETS.keys(), 'Unsupported dataset name.'
@@ -69,34 +70,79 @@ def main(args):
         'test', args.dataset_dir
     )
 
-    hyperparameters['optimization.learning_rate'] = args.lr
-    hyperparameters['optimization.end_lr'] = args.lr
+    automm_hyperparameters['optimization.learning_rate'] = args.lr
+    automm_hyperparameters['optimization.end_lr'] = args.lr
 
-    ### model initalization
-    predictor = AutoMMPredictor(
-        label=train_data.label_column, 
-        problem_type=train_data.problem_type,
-        eval_metric=train_data.metric,
-        path=args.exp_dir,
-        verbosity=4,
-    )
+    tabular_hyperparameters = {
+        'GBM': [
+            {},
+            {'extra_trees': True, 'ag_args': {'name_suffix': 'XT'}},
+        ],
+        'CAT': {},
+        'XGB': {},
+        'NN': {},
+        'AG_AUTOMM_NN': automm_hyperparameters,
+    }
 
-    ### model training
-    predictor.fit(
-        train_data=train_data.data,
-        tuning_data=val_data.data,
-        seed=args.seed,
-        hyperparameters=hyperparameters
-    )
+    if args.mode == 'single':
+        ### model initalization
+        predictor = AutoMMPredictor(
+            label=train_data.label_column,
+            problem_type=train_data.problem_type,
+            eval_metric=train_data.metric,
+            path=args.exp_dir,
+            verbosity=4,
+        )
 
-    ### model inference
+        ### model training
+        predictor.fit(
+            train_data=train_data.data,
+            tuning_data=val_data.data,
+            seed=args.seed,
+            hyperparameters=automm_hyperparameters
+        )
+
+        ### model inference
+        scores = predictor.evaluate(
+            data=test_data.data,
+            metrics=[test_data.metric]
+        )
+        with open(os.path.join(args.exp_dir, 'scores.json'), 'w') as f:
+            json.dump(scores, f)
+        print(scores)
+    elif args.mode == 'weighted' or args.mode == 'single_bag5' or args.mode == 'stack5':
+        if args.mode == 'single_bag5':
+            tabular_hyperparameters = {'AG_AUTOMM_NN': automm_hyperparameters}
+            num_bag_folds, num_stack_levels = 5, 0
+        elif args.mode == 'weighted':
+            num_bag_folds, num_stack_levels = None, None
+        elif args.mode == 'stack5':
+            num_bag_folds, num_stack_levels = 5, 1
+        else:
+            raise NotImplementedError
+        from autogluon.tabular import TabularPredictor
+        predictor = TabularPredictor(eval_metric=train_data.metric,
+                                     label=train_data.label_column,
+                                     path=args.exp_path)
+        predictor.fit(train_data.data,
+                      hyperparameters=tabular_hyperparameters,
+                      num_bag_folds=num_bag_folds,
+                      num_stack_levels=num_stack_levels)
+        leaderboard = predictor.leaderboard()
+        leaderboard.to_csv(os.path.join(args.exp_path, 'leaderboard.csv'))
+    else:
+        raise NotImplementedError
     scores = predictor.evaluate(
-        data=test_data.data,
-        metrics=[test_data.metric]
+        data=test_data.data
     )
     with open(os.path.join(args.exp_dir, 'scores.json'), 'w') as f:
         json.dump(scores, f)
     print(scores)
+
+    predictions = predictor.predict(
+        data=test_data.data
+    )
+    predictions.to_csv(args.exp_dir, 'predictions.csv')
 
 
 if __name__ == '__main__':
--- examples/automm/tabular_dl/run_all.sh
+++ examples/automm/tabular_dl/run_all.sh
@@ -18,5 +18,5 @@ DATASETS=(
 
 for dataset_name in ${DATASETS}
 do
-  python3 example_tabular.py --dataset_name ${dataset_name} --dataset_dir ./dataset/${dataset_name} --exp_dir ./result/${dataset_name} --lr 1E-4 --seed 0
+  python3 example_tabular.py --dataset_name ${dataset_name} --dataset_dir ./dataset/${dataset_name} --exp_dir ./result/${dataset_name} --lr 5E-4 --seed 0
 done
