c78262d813f8efed96951f42c18993a0a8e80491
James Sharpnack <jsharpna@amazon.com>
2022-06-16 16:51:21 -0700

    complete unit tests add pvalue script

--- shift/src/autogluon/shift/models/classifier2ST.py
+++ shift/src/autogluon/shift/models/classifier2ST.py
@@ -1,39 +1,17 @@
 from sklearn.metrics import accuracy_score
-
-"""
-def acc_test_stat(data, pred, split=0.5):
-    train, _ ,test = data.random_split(split)
-    pred.fit(train)
-    test_acc = pred.evaluate(test)
-    p = test_acc['top1']
-    n = test.shape[0]
-    se = (p * (1-p) / n)**0.5
-    return p, se
-
-def acc_test(data, pred, split=0.5, perm_num = 100):
-    train, _ ,test = data.random_split(split)
-    pred.fit(train)
-    test_acc = pred.evaluate(test)
-    p = test_acc['top1']
-    perm_p = [p]
-    for i in range(perm_num):
-        perm_data = test.copy()
-        perm_data['label'] = np.random.permutation(test['label'])
-        perm_p.append(pred.evaluate(perm_data)['top1'])
-    p_val = (p <= np.array(perm_p)).mean()
-    return p, p_val
-
-def sim_null_test(data, pred, split = 0.5, perm_num = 100):
-    perm_data = data.copy()
-    perm_data['label'] = np.random.permutation(data['label'])
-    return acc_test(perm_data, pred, split=split, perm_num=100)
-"""
+import numpy as np
 
 class Classifier2ST:
-    def __init__(self, classifier):
+    def __init__(self,
+                 classifier):
         self._classifier = classifier
 
-    def fit(self, data, sample_label = None, accuracy_metric = accuracy_score):
+    def fit(self,
+            data,
+            sample_label = None,
+            accuracy_metric = accuracy_score,
+            split=0.5):
+
         if sample_label:
             train, _, test = data.random_split(split)
         # need to catch all sorts of data cases
@@ -41,10 +19,34 @@ class Classifier2ST:
         self._classifier.fit(train)
         self._test = test
         yhat = self._classifier.predict(test)
-        self.test_stat = accuracy_metric(test[label], yhat)
+        self._accuracy_metric = accuracy_metric
+        self._sample_label = sample_label
+        self.test_stat = accuracy_metric(test[sample_label], yhat)
 
-    def pvalue(self, method='half permutation'):
-        pass
+    def _pvalue_half_permutation(self,
+                                 num_permutations=1000):
+        perm_stats = [self.test_stat]
+        for i in range(num_permutations):
+            perm_data = self._test.copy()
+            perm_data[self._sample_label] = \
+                np.random.permutation(perm_data[self._sample_label])
+            yhat = self._classifier.predict(perm_data)
+            perm_test_stat = self._accuracy_metric(
+                perm_data[self._sample_label],
+                yhat)
+            perm_stats.append(perm_test_stat)
+        p_val = (self.test_stat <= np.array(perm_stats)).mean()
+        return p_val
 
-    def sim_null(self):
-        pass
+    def pvalue(self,
+               method='half permutation',
+               num_permutations=1000):
+        valid_methods = [
+            'half permutation'
+        ]
+        assert method in valid_methods, \
+            'method must be one of ' + ', '.join(valid_methods)
+        if method == 'half permutation':
+            pval = self._pvalue_half_permutation(
+                num_permutations=num_permutations)
+        return pval
--- shift/tests/unittests/models/test_classifier2ST.py
+++ shift/tests/unittests/models/test_classifier2ST.py
@@ -1,13 +1,43 @@
-import pytest
 from autogluon.shift import Classifier2ST
 from autogluon.vision import ImageDataset, ImagePredictor
 import os
+import numpy as np
+import math
 
-data_dir = os.path.join('..','..','..','data')
+data_dir = os.path.join('..','..','data')
 
-def test_classifier2ST():
-    twodogs_dir = os.path.join(data_dir,'two_dogs')
+def get_dogs_data():
+    twodogs_dir = os.path.join(data_dir, 'two_dogs')
     data = ImageDataset.from_folder(twodogs_dir)
+    return data
+
+def fit_tst():
+    data = get_dogs_data()
     pred = ImagePredictor()
     tst = Classifier2ST(pred)
     tst.fit(data, sample_label='label')
+    return tst
+
+def test_classifier2ST_fit():
+    tst = fit_tst()
+    val = 0.82
+    assert math.isclose(tst.test_stat, val, abs_tol = 1e-1), \
+        f'test stat ({tst.test_stat}) is too far from similar ({val})'
+
+def test_null_test():
+    data = get_dogs_data()
+    data['label'] = np.random.permutation(data['label'])
+    pred = ImagePredictor()
+    tst = Classifier2ST(pred)
+    tst.fit(data, sample_label='label')
+    val = 0.5
+    assert math.isclose(tst.test_stat, val, abs_tol = 1e-1), \
+        f'test stat ({tst.test_stat}) is too far from similar ({val})'
+    print(f'succeeded with null stat of {tst.test_stat}')
+
+def test_classifier2ST_pvalue():
+    tst = fit_tst()
+    pval = tst.pvalue(num_permutations=100)
+    assert pval < 0.05
+
+
