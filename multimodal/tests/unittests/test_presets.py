8b154ee853ff0ae39a612cd56a1efd592c24a98d
Zhiqiang Tang <zhiqiang.tang@rutgers.edu>
2022-06-20 18:03:25 -0700

    add test

--- multimodal/tests/unittests/test_presets.py
+++ multimodal/tests/unittests/test_presets.py
@@ -1,17 +1,28 @@
 import pytest
+from omegaconf import OmegaConf
 from autogluon.multimodal.presets import (
     get_automm_preset,
     list_automm_presets,
 )
+from autogluon.multimodal.utils import get_config
 
 
 def test_presets():
-    all_model_presets = list_automm_presets()
-    for preset in all_model_presets:
-        config, overrides = get_automm_preset(preset)
+    all_presets = list_automm_presets()
+    for preset in all_presets:
+        basic_config, overrides = get_automm_preset(preset)
 
     # test non-existing types
     non_exist_types = ["hello", "haha"]
     for per_type in non_exist_types:
         with pytest.raises(ValueError):
-            config, overrides = get_automm_preset(per_type)
+            basic_config, overrides = get_automm_preset(per_type)
+
+
+def test_preset_to_config():
+    all_presets = list_automm_presets()
+    for preset in all_presets:
+        basic_config, overrides = get_automm_preset(preset)
+        config = get_config(preset=preset)
+        for k, v in overrides.items():
+            assert OmegaConf.select(config, k) == v
