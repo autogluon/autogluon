91ab82fcff42e74e0614c3cbcd9e58a04543e676
Xingjian Shi <xshiab@connect.ust.hk>
2022-06-21 23:23:21 -0700

    Initial AutoMM Document + Disable multi-gpu training in interactive environment (#1858)
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Update automm.md
    
    * fix

--- multimodal/tests/unittests/test_presets.py
+++ multimodal/tests/unittests/test_presets.py
@@ -1,7 +1,7 @@
 import pytest
 from omegaconf import OmegaConf
 from autogluon.multimodal.presets import (
-    get_automm_preset,
+    get_automm_presets,
     list_automm_presets,
 )
 from autogluon.multimodal.utils import get_config
@@ -10,19 +10,19 @@ from autogluon.multimodal.utils import get_config
 def test_presets():
     all_presets = list_automm_presets()
     for preset in all_presets:
-        basic_config, overrides = get_automm_preset(preset)
+        basic_config, overrides = get_automm_presets(preset)
 
     # test non-existing types
     non_exist_types = ["hello", "haha"]
     for per_type in non_exist_types:
         with pytest.raises(ValueError):
-            basic_config, overrides = get_automm_preset(per_type)
+            basic_config, overrides = get_automm_presets(per_type)
 
 
 def test_preset_to_config():
     all_presets = list_automm_presets()
     for preset in all_presets:
-        basic_config, overrides = get_automm_preset(preset)
-        config = get_config(preset=preset)
+        basic_config, overrides = get_automm_presets(preset)
+        config = get_config(presets=preset)
         for k, v in overrides.items():
             assert OmegaConf.select(config, k) == v
