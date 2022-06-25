f6ce6229904f6b52adae1f08bae19261b74adadf
Zhiqiang Tang <zhiqiang.tang@rutgers.edu>
2022-06-25 15:46:26 -0700

    Support distillation for text predictor and add default for distiller (#1895)
    * add default for distiller
    
    * update
    
    * add distillation for text predictor
    
    * add tests with teacher path
    
    * fix

--- text/tests/unittests/text_prediction/test_text_presets.py
+++ text/tests/unittests/text_prediction/test_text_presets.py
@@ -17,14 +17,13 @@ def test_presets():
     available_presets = list_text_presets(verbose=True)
 
     for preset, to_be_verified in available_presets.items():
-        automm_preset, overrides = get_text_preset(preset)
+        overrides = get_text_preset(preset)
         config = get_config(
-            config=automm_preset,
             overrides=overrides,
         )
         for k, v in to_be_verified.items():
             assert v == rgetattr(config, k)
-        assert sorted(config.model.names) == sorted(["hf_text", "numerical_mlp", "categorical_mlp", "fusion_mlp"])
+
     # test invalid presets
     presets = ["hello", "haha"]
     for preset in presets:
