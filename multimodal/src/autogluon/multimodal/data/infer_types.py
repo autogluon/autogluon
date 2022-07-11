89f4e1b35ca6dcb82e0bc4b3bf2c68ff5b315088
Zhiqiang Tang <zhiqiang.tang@rutgers.edu>
2022-07-11 16:34:06 -0700

    black style

--- multimodal/src/autogluon/multimodal/data/infer_types.py
+++ multimodal/src/autogluon/multimodal/data/infer_types.py
@@ -404,11 +404,7 @@ def infer_problem_type_output_shape(
             )
 
 
-def set_fallback_column_type(
-    column_types: Dict,
-    allowable_column_types: List[str],
-    fallback_column_type: str
-) -> Dict:
+def set_fallback_column_type(column_types: Dict, allowable_column_types: List[str], fallback_column_type: str) -> Dict:
     """
     Filter the auto-detected column types to make sure that all column types are allowable.
     Use the fallback type to replace those out of the allowable_column_types.
