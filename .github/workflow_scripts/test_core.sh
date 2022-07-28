f28fa029770c296092cfa62d2aa8c61314ec2d64
Weisu Yin <weisy@amazon.com>
2022-07-28 11:03:36 -0700

    Fix Platform Tests (#1991)
    * Update platform_tests-command.yml
    
    * Add addtional args to test scripts
    
    * Remove

--- .github/workflow_scripts/test_core.sh
+++ .github/workflow_scripts/test_core.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+ADDITIONAL_TEST_ARGS=$1
+
 source $(dirname "$0")/env_setup.sh
 
 setup_build_env
@@ -9,4 +11,4 @@ install_core_all_tests
 python3 -m pip install ray_lightning==0.2.0  # TODO Change this line once we support ray_lightning 0.3.0
 
 cd core/
-python3 -m pytest --junitxml=results.xml --runslow tests
+python3 -m pytest --junitxml=results.xml --runslow "$ADDITIONAL_TEST_ARGS" tests
