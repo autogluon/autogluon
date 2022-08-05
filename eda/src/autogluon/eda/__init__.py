3bc7722f661afecbe983af8fbbaa3e5b54e107c1
Alexander Shirkov <ashyrkou@amazon.com>
2022-08-04 18:08:50 -0700

    WIP

--- eda/src/autogluon/eda/__init__.py
+++ eda/src/autogluon/eda/__init__.py
@@ -1,2 +1,1 @@
 from .base import Analysis
-from .analysis.univariate import UnivariateAnalysis
--- eda/src/autogluon/eda/backend/jupyter.py
+++ eda/src/autogluon/eda/backend/jupyter.py
@@ -1,7 +1,7 @@
+import ipywidgets as widgets
+import matplotlib.pyplot as plt
 from IPython.display import display, HTML
 from pandas import DataFrame
-import matplotlib.pyplot as plt
-import ipywidgets as widgets
 
 from autogluon.eda.backend.base import RenderingBackend
 
@@ -18,25 +18,14 @@ class SimpleJupyterBackend(RenderingBackend):
         display(df)
 
     def render_histogram(self, df: DataFrame, **kwargs):
-        kwargs = kwargs.copy()
-        kwargs.pop('column')
         fig_params = kwargs.pop('fig_params', {})
-
         outs = [widgets.Output() for c in df.columns]
         tab = widgets.Tab(children=outs)
         for i, c in enumerate(df.columns):
             tab.set_title(i, c)
         display(tab)
-        print(kwargs)
         for c, out in zip(df.columns, outs):
             with out:
                 fig, ax = plt.subplots(**fig_params)
                 df.hist(ax=ax, column=c, **kwargs)
                 plt.show(fig)
-
-        # out = widgets.Output()
-        # with out:
-        #     fig, ax = plt.subplots()
-        # df.hist(ax=ax, **kwargs)
-        # plt.show(fig)
-
