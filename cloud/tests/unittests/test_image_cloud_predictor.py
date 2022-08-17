2c8167c446eae1313767421d5eab36a83326c6e9
Weisu Yin <weisy@amazon.com>
2022-08-17 14:03:16 -0700

    [Cloud] MultiModalCloud Predictor + Multimodal Support for TabularCloudPredictor (#2030)
    * check
    
    * multimodal cloud predictor
    
    * multimodal image and text only tests
    
    * multimodal text and image only test passed
    
    * checkpoint
    
    * tabular and move tests around
    
    * tabular multimodal
    
    * move predictors around
    
    * fix
    
    * minor fixes
    
    * add batch inference warning
    
    * fixes and cleanup
    
    * remove image_modality_only
    
    * base 85
    
    Co-authored-by: Weisu Yin <weisuyin96@gmail.com>

---
+++ cloud/tests/unittests/test_image_cloud_predictor.py
@@ -0,0 +1,46 @@
+import pytest
+import tempfile
+
+from autogluon.cloud import ImageCloudPredictor
+
+from utils import (
+    _prepare_data,
+    _test_functionality
+)
+
+
+@pytest.mark.cloud
+def test_image():
+    train_data = 'image_train_relative.csv'
+    train_image = 'shopee-iet.zip'
+    test_data = 'test_images/BabyPants_1035.jpg'
+    with tempfile.TemporaryDirectory() as root:
+        _prepare_data(train_data, train_image, test_data)
+        test_data = 'BabyPants_1035.jpg'
+        time_limit = 60
+
+        predictor_init_args = dict(
+            label='label',
+            eval_metric='acc'
+        )
+        predictor_fit_args = dict(
+            train_data=train_data,
+            time_limit=time_limit
+        )
+        cloud_predictor = ImageCloudPredictor(
+            cloud_output_path='s3://ag-cloud-predictor/test-image',
+            local_output_path='test_image_cloud_predictor'
+        )
+        cloud_predictor_no_train = ImageCloudPredictor(
+            cloud_output_path='s3://ag-cloud-predictor/test-image-no-train',
+            local_output_path='test_image_cloud_predictor_no_train'
+        )
+        _test_functionality(
+            cloud_predictor,
+            predictor_init_args,
+            predictor_fit_args,
+            cloud_predictor_no_train,
+            test_data,
+            image_path='shopee-iet.zip',
+            fit_instance_type='ml.g4dn.2xlarge'
+        )
