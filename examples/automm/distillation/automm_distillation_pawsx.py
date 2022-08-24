4f6e0f9cd3f51a97e87cd8d8e1c089a3ae3a66fd
Haoyang Fang <107515844+FANGAreNotGnu@users.noreply.github.com>
2022-08-23 22:38:43 -0700

    Finish Multilingual Distillation Example on PAWS-X dataset. (#2064)
    * add an example for automm distillation
    
    * Add an example of using automm with distillation on GLUE datasets
    
    * distiller example: add README and update code for new version
    
    * change some typos
    
    * uncomment some testing comments
    
    * minor fix in README
    
    * minor fixes in README
    
    * automatically infer soft label loss function and add trivial aug to distillation example
    
    * remove debugging printing and add an assert
    
    * allow eval_metric to be a str for AutoMMPredictor
    
    * fix bugs
    
    * add embedding l2 loss for distillation
    
    * changes for distillation with embedding loss
    
    * change default weight of embedding loss
    
    * change back the naming
    
    * fix typo
    
    * reformat 2 files
    
    * update README
    
    * change naming and add adaptor
    
    * add test for distillation with different model structures
    
    * formatting with black
    
    * change some variable namings
    
    * add RKD
    
    * skip fusion model for now
    
    * update README
    
    * change default rkd loss setting
    
    * add docstring and type
    
    * minor fixes
    
    * reformat with black
    
    * lint
    
    * add out_features in fusion model
    
    * some refactor
    
    * lint
    
    * combine rkd loss
    
    * minor fixes
    
    * update test
    
    * move init of adaptor and rkd_loss to predictor
    
    * lint check
    
    * fix typo
    
    * typo fixes and add to ignore list
    
    * “omegaconf”
    
    * lint
    
    * fix a wrong deletion and improve efficiency
    
    * add zero shot sentence similarity support and examples
    
    * add softmax regression loss and add distillation example on pawsx
    
    * change zero_shot user interface
    
    * update pawsx distillation example
    
    * add pipeline
    
    * fix timm test and black formatting
    
    * black
    
    * add todo label for fit
    
    * black
    
    * black
    
    * remove try except while get preset based on pipeline
    
    * fix clip zero shot tutorial and remove changes in predictor.evaluate
    
    * change clip tutorial
    
    * remove temperature for softmax regression
    
    * remove init_only
    
    * add paper reference
    
    * remove todo constants
    
    * add details for todo
    
    * handle and add warning for deprecated zero_shot flag
    
    * handle and add warning for deprecated zero_shot flag
    
    * finish pawsx example and update readme
    
    Co-authored-by: Haoyang Fang <haoyfang@amazon.com>

--- examples/automm/distillation/automm_distillation_pawsx.py
+++ examples/automm/distillation/automm_distillation_pawsx.py
@@ -16,17 +16,12 @@ def tasks_to_id(pawsx_tasks):
             id += task
     return id
 
-
-def main(args):
-    pawsx_tasks = args.pawsx_tasks
-    assert all(task in PAWS_TASKS for task in pawsx_tasks)
-    tasks_id = tasks_to_id(pawsx_tasks)
-
+def getDatasetSplits(pawsx_tasks):
     datasets = {}
     train_dfs = {}
     val_dfs = {}
     test_dfs = {}
-    for task in args.pawsx_tasks:
+    for task in pawsx_tasks:
         datasets[task] = load_dataset("paws-x", task)
         train_dfs[task] = datasets[task]["train"].to_pandas()
         val_dfs[task] = datasets[task]["validation"].to_pandas()
@@ -39,9 +34,22 @@ def main(args):
     val_df = pd.concat(val_dfs)
     test_dfs["all"] = pd.concat(test_dfs)
 
-    teacher_predictor_name = f"pawsx-{tasks_id}-{args.teacher_model.replace('/', '-')}"
+    return train_df, val_df, test_dfs
+
+def main(args):
+    pawsx_teacher_tasks = args.pawsx_teacher_tasks
+    assert all(task in PAWS_TASKS for task in pawsx_teacher_tasks)
+    teacher_tasks_id = tasks_to_id(pawsx_teacher_tasks)
+    pawsx_student_tasks = args.pawsx_student_tasks
+    assert all(task in PAWS_TASKS for task in pawsx_student_tasks)
+    student_tasks_id = tasks_to_id(pawsx_student_tasks)
+
+    teacher_train_df, teacher_val_df, teacher_test_dfs = getDatasetSplits(pawsx_teacher_tasks)
+    student_train_df, student_val_df, student_test_dfs = getDatasetSplits(pawsx_student_tasks)
+
+    teacher_predictor_name = f"pawsx-{teacher_tasks_id}-{args.teacher_model.replace('/', '-')}"
     teacher_predictor_path = os.path.join(args.save_path, teacher_predictor_name)
-    nodistill_predictor_name = f"pawsx-{tasks_id}-{args.student_model.replace('/', '-')}"
+    nodistill_predictor_name = f"pawsx-{student_tasks_id}-{args.student_model.replace('/', '-')}"
     nodistill_predictor_path = os.path.join(args.save_path, nodistill_predictor_name)
 
     teacher_result = {}
@@ -59,11 +67,11 @@ def main(args):
     if not resume_teacher:
         teacher_predictor = MultiModalPredictor(label="label", eval_metric="accuracy")
         teacher_predictor.fit(
-            train_df,
-            tuning_data=val_df,
+            teacher_train_df,
+            tuning_data=teacher_val_df,
             hyperparameters={
                 "env.num_gpus": args.num_gpu,
-                "env.precision": "bf16",
+                "env.precision": args.precision,
                 "env.per_gpu_batch_size": 6,
                 "model.hf_text.checkpoint_name": args.teacher_model,
                 "optimization.learning_rate": 1.0e-4,
@@ -73,9 +81,12 @@ def main(args):
             seed=args.seed,
         )
         teacher_predictor.save(teacher_predictor_path)
-    start = time()
-    for test_name, test_df in test_dfs.items():
+    for test_name, test_df in teacher_test_dfs.items():
         teacher_result[test_name] = teacher_predictor.evaluate(data=test_df, metrics="accuracy")
+    #use same dataset to measure computation time
+    start = time()
+    for test_name, test_df in student_test_dfs.items():
+        teacher_predictor.evaluate(data=test_df, metrics="accuracy")
     teacher_usedtime = time() - start
 
     ### Train and evaluate a smaller pretrained model
@@ -89,11 +100,11 @@ def main(args):
     if not resume_nodistill:
         nodistill_predictor = MultiModalPredictor(label="label", eval_metric="accuracy")
         nodistill_predictor.fit(
-            train_df,
-            tuning_data=val_df,
+            student_train_df,
+            tuning_data=student_val_df,
             hyperparameters={
                 "env.num_gpus": args.num_gpu,
-                "env.precision": "bf16",
+                "env.precision": args.precision,
                 "optimization.max_epochs": args.max_epochs,
                 "model.hf_text.checkpoint_name": args.student_model,
                 "optimization.learning_rate": 2.0e-4,
@@ -103,7 +114,7 @@ def main(args):
             seed=args.seed,
         )
         nodistill_predictor.save(nodistill_predictor_path)
-    for test_name, test_df in test_dfs.items():
+    for test_name, test_df in student_test_dfs.items():
         nodistill_result[test_name] = nodistill_predictor.evaluate(data=test_df, metrics="accuracy")
 
     ### Distill and evaluate a student model
@@ -118,14 +129,15 @@ def main(args):
     }
     student_predictor = MultiModalPredictor(label="label", eval_metric="accuracy")
     student_predictor.fit(
-        train_df,
-        tuning_data=val_df,
+        student_train_df,
+        tuning_data=student_val_df,
         config=config,
         hyperparameters={
             "env.num_gpus": args.num_gpu,
-            # "env.precision": "bf16",
+            "env.precision": args.precision,
             "optimization.max_epochs": args.max_epochs,
             "model.hf_text.checkpoint_name": args.student_model,
+            "model.hf_text.text_trivial_aug_maxscale": args.aug_scale,
             "optimization.learning_rate": 2.0e-4,
             "optimization.weight_decay": 2.0e-3,
             "distiller.temperature": args.temperature,
@@ -138,7 +150,7 @@ def main(args):
             "distiller.soft_label_loss_type": args.soft_label_loss_type,
             "distiller.softmax_regression_loss_type": args.softmax_regression_loss_type,
             "distiller.output_feature_loss_type": args.output_feature_loss_type,
-            "model.hf_text.text_trivial_aug_maxscale": 0.0,
+            "model.hf_text.text_trivial_aug_maxscale": args.aug_scale,
             # "optimization.top_k": 1,
             # "optimization.top_k_average_method": "best",
         },
@@ -147,12 +159,12 @@ def main(args):
         seed=args.seed,
     )
     start = time()
-    for test_name, test_df in test_dfs.items():
+    for test_name, test_df in student_test_dfs.items():
         student_result[test_name] = student_predictor.evaluate(data=test_df, metrics="accuracy")
     student_usedtime = time() - start
 
     ### Print distillation's performance
-    for test_name in test_dfs.keys():
+    for test_name in student_test_dfs.keys():
         print("Distillation Result (%s):" % test_name)
         print("Teacher Model: %s" % args.teacher_model)
         print("Student Model: %s" % args.student_model)
@@ -175,10 +187,12 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--pawsx_tasks", default=["en", "de", "es", "fr", "ja", "ko", "zh"], type=str)
+    parser.add_argument("--pawsx_teacher_tasks", nargs="+", default=["en", "de", "es", "fr", "ja", "ko", "zh"])
+    parser.add_argument("--pawsx_student_tasks", nargs="+", default=["en", "de", "es", "fr", "ja", "ko", "zh"])
     parser.add_argument("--teacher_model", default="microsoft/mdeberta-v3-base", type=str)
     parser.add_argument("--student_model", default="nreimers/mMiniLMv2-L6-H384-distilled-from-XLMR-Large", type=str)
     parser.add_argument("--seed", default=123, type=int)
+    parser.add_argument("--precision", default="bf16", type=str)
     parser.add_argument("--max_epochs", default=10, type=int)
     parser.add_argument("--time_limit", default=None, type=int)
     parser.add_argument("--num_gpu", default=-1, type=int)
@@ -198,6 +212,6 @@ if __name__ == "__main__":
         type=str,
     )
     parser.add_argument("--resume", action="store_true")
+    parser.add_argument("--aug_scale", default=0., type=float)
     args = parser.parse_args()
-
     main(args)
