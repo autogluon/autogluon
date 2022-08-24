bea49f0104643715c04cc48a385c2b26adf81aff
Oleksandr Shchur <shchuro@amazon.com>
2022-08-24 09:28:42 +0000

    Add TODO

--- timeseries/src/autogluon/timeseries/utils/hashing.py
+++ timeseries/src/autogluon/timeseries/utils/hashing.py
@@ -5,6 +5,7 @@ import pandas as pd
 from ..dataset.ts_dataframe import ITEMID, TIMESTAMP, TimeSeriesDataFrame
 
 
+# TODO: Move to autogluon.timeseries.models.statsmodels.helper after we drop the sktime dependency
 def hash_ts_dataframe_items(ts_dataframe: TimeSeriesDataFrame) -> pd.Series:
     """Hash each time series in the dataset to a 32-character hex string.
 
