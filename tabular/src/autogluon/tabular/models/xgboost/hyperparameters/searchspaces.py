8af8b083277ae3302877ca15cda80f747781c771
Innixma <neerick@amazon.com>
2022-09-11 15:07:43 -0700

    Simplify XGBoost HPO search space to be stronger with fewer trials

--- tabular/src/autogluon/tabular/models/xgboost/hyperparameters/searchspaces.py
+++ tabular/src/autogluon/tabular/models/xgboost/hyperparameters/searchspaces.py
@@ -24,11 +24,11 @@ def get_base_searchspace():
         'learning_rate': Real(lower=5e-3, upper=0.2, default=0.1, log=True),
         'max_depth': Int(lower=3, upper=10, default=6),
         'min_child_weight': Int(lower=1, upper=5, default=1),
-        'gamma': Real(lower=0, upper=5, default=0.01),
-        'subsample': Real(lower=0.5, upper=1.0, default=1.0),
         'colsample_bytree': Real(lower=0.5, upper=1.0, default=1.0),
-        'reg_alpha': Real(lower=0.0, upper=10.0, default=0.0),
-        'reg_lambda': Real(lower=0.0, upper=10.0, default=1.0),
+        # 'gamma': Real(lower=0, upper=5, default=0),
+        # 'subsample': Real(lower=0.5, upper=1.0, default=1.0),
+        # 'reg_alpha': Real(lower=0.0, upper=10.0, default=0.0),
+        # 'reg_lambda': Real(lower=0.0, upper=10.0, default=1.0),
     }
     return base_params
 
