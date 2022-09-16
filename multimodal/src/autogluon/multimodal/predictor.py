17a6371b8f53c791bbeda4cb7e254e164ec2a7c2
Zhiqiang Tang <zhiqiang.tang@rutgers.edu>
2022-09-15 18:13:59 -0700

    Improve averaging checkpoints and creating model (#2135)
    * improve ckpt averaging
    
    * fix
    
    * fix

--- multimodal/src/autogluon/multimodal/predictor.py
+++ multimodal/src/autogluon/multimodal/predictor.py
@@ -86,7 +86,7 @@ from .utils import (
     compute_num_gpus,
     compute_score,
     create_fusion_data_processors,
-    create_model,
+    create_fusion_model,
     data_to_df,
     extract_from_output,
     filter_search_space,
@@ -614,7 +614,7 @@ class MultiModalPredictor:
             # reload the predictor metadata
             predictor = MultiModalPredictor._load_metadata(predictor=self, path=best_trial_path)
             # construct the model
-            model = create_model(
+            model = create_fusion_model(
                 config=predictor._config,
                 num_classes=predictor._output_shape,
                 num_numerical_columns=len(predictor._df_preprocessor.numerical_feature_names),
@@ -861,7 +861,7 @@ class MultiModalPredictor:
         config = select_model(config=config, df_preprocessor=df_preprocessor)
 
         if self._model is None:
-            model = create_model(
+            model = create_fusion_model(
                 config=config,
                 num_classes=self._output_shape,
                 num_numerical_columns=len(df_preprocessor.numerical_feature_names),
@@ -1960,7 +1960,7 @@ class MultiModalPredictor:
         predictor = cls(label="dummy_label")
         predictor = cls._load_metadata(predictor=predictor, path=path, resume=resume, verbosity=verbosity)
 
-        model = create_model(
+        model = create_fusion_model(
             config=predictor._config,
             num_classes=predictor._output_shape,
             num_numerical_columns=len(predictor._df_preprocessor.numerical_feature_names),
