700d2e558f0c21b64bb0409be247311cc5b11112
Innixma <neerick@amazon.com>
2022-09-29 16:33:46 -0700

    minor test fix

--- tabular/tests/unittests/models/test_cascade.py
+++ tabular/tests/unittests/models/test_cascade.py
@@ -11,7 +11,7 @@ def test_cascade_binary(fit_helper):
     )
     dataset_name = 'adult'
     cascade = ['LightGBM', 'WeightedEnsemble_L2']
-    fit_helper.fit_and_validate_dataset_with_cascade(dataset_name=dataset_name, fit_args=fit_args, cascade=cascade, model_count=2)
+    fit_helper.fit_and_validate_dataset_with_cascade(dataset_name=dataset_name, fit_args=fit_args, cascade=cascade, expected_model_count=3)
 
 
 def test_cascade_multiclass(fit_helper):
@@ -23,4 +23,4 @@ def test_cascade_multiclass(fit_helper):
     )
     dataset_name = 'covertype_small'
     cascade = ['LightGBM', 'WeightedEnsemble_L2']
-    fit_helper.fit_and_validate_dataset_with_cascade(dataset_name=dataset_name, fit_args=fit_args, cascade=cascade, model_count=2)
+    fit_helper.fit_and_validate_dataset_with_cascade(dataset_name=dataset_name, fit_args=fit_args, cascade=cascade, expected_model_count=3)
