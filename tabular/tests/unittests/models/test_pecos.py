b7bc1f3b3f9d802e3c138b605ceb4bb9e5d26aab
Noah Jacobson <noahjaco@amazon.com>
2022-10-05 17:31:22 +0000

    Remove regression from unit test

--- tabular/tests/unittests/models/test_pecos.py
+++ tabular/tests/unittests/models/test_pecos.py
@@ -17,10 +17,3 @@ def test_pecos_multiclass(fit_helper):
     dataset_name = 'covertype_small'
     fit_helper.fit_and_validate_dataset(dataset_name=dataset_name, fit_args=fit_args)
 
-
-def test_pecos_regression(fit_helper):
-    fit_args = dict(
-        hyperparameters={PecosModel: {}},
-    )
-    dataset_name = 'ames'
-    fit_helper.fit_and_validate_dataset(dataset_name=dataset_name, fit_args=fit_args)
