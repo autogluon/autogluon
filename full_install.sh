7001041a8c3eeb64b7b77c18721efff1da29462b
Alexander Shirkov <ashyrkou@amazon.com>
2022-10-13 18:16:49 -0700

    [eda] updated CI config

--- full_install.sh
+++ full_install.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 python3 -m pip install -e common/[tests]
 python3 -m pip install -e core/[all,tests]
 python3 -m pip install -e features/
-python3 -m pip install -e eda/
 python3 -m pip install -e tabular/[all,tests]
 python3 -m pip install -e multimodal/[tests]
 python3 -m pip install -e text/[tests]
 python3 -m pip install -e vision/
 python3 -m pip install -e timeseries/[all,tests]
+python3 -m pip install -e eda/
 python3 -m pip install -e autogluon/
