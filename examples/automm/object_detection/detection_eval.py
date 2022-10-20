1624a82a2c6ae01c79cdf1428a9f2e16d05b49b9
FANGAreNotGnu <kevinfang97@gmail.com>
2022-10-19 23:52:31 -0700

    update detection eval example

--- examples/automm/object_detection/detection_eval.py
+++ examples/automm/object_detection/detection_eval.py
@@ -1,8 +1,27 @@
+"""
+The example to evaluate a pretrained object detection model in AutoMM.
+
+An example to evaluate an MMDetection model on COCO:
+    python detection_eval.py \
+        --test_path coco17/annotations/instances_val2017.json
+        --checkpoint_name yolov3_mobilenetv2_320_300e_coco
+
+An example to evaluate an MMDetection model on VOC:
+    python detection_eval.py \
+        --test_path VOCdevkit/VOCCOCO/voc07_test.json
+        --checkpoint_name faster_rcnn_r50_fpn_1x_voc0712
+
+Note that for now it's required to install nightly build torchmetrics.
+This will be solved in next pr. (MeanAveragePrecision will be moved to AG temporarily.)
+"""
+
+import argparse
+
 from autogluon.multimodal import MultiModalPredictor
 
 
 def detection_evaluation(
-    checkpoint_name="faster_rcnn_r50_fpn_2x_coco", anno_path="coco17/annotations/instances_val2017.json"
+    checkpoint_name="yolov3_mobilenetv2_320_300e_coco", test_path="coco17/annotations/instances_val2017.json"
 ):
     predictor = MultiModalPredictor(
         label="rois_label",
@@ -16,10 +35,14 @@ def detection_evaluation(
     import time
 
     start = time.time()
-    predictor.evaluate(anno_path)
+    predictor.evaluate(test_path)
     print("time usage: %.2f" % (time.time() - start))
 
 
 if __name__ == "__main__":
-    detection_evaluation("yolov3_mobilenetv2_320_300e_coco")
-    # detection_evaluation("faster_rcnn_r50_fpn_1x_voc0712", "VOCdevkit/VOCCOCO/voc07_test.json")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test_path", default="coco17/annotations/instances_val2017.json", type=str)
+    parser.add_argument("--checkpoint_name", default="yolov3_mobilenetv2_320_300e_coco", type=str)
+    args = parser.parse_args()
+
+    detection_evaluation(checkpoint_name=args.checkpoint_name, test_path=args.test_path)
