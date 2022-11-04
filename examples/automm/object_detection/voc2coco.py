a5ca590a86f1117c531f815e7ac0121aac4cd1a6
yongxinw <yongxinw@cs.cmu.edu>
2022-11-03 17:17:00 -0700

    adding python script to get all unique classes in voc format, and dump all annotation xml files

--- examples/automm/object_detection/voc2coco.py
+++ examples/automm/object_detection/voc2coco.py
@@ -26,6 +26,7 @@ import re
 import subprocess
 from tqdm import tqdm
 from typing import Dict, List
+from autogluon.multimodal.utils.object_detection import read_voc_class_names
 
 
 MIN_AREA = 4  # TODO: put in arg?
@@ -177,6 +178,16 @@ def main():
         assert args.train_ratio + args.val_ratio <= 1
         annpaths_list_path = os.path.join(args.root_dir, "pathlist.txt")
     labels_path = os.path.join(args.root_dir, "labels.txt")
+    annpaths_list_path = os.path.join(args.root_dir, "pathlist.txt")
+
+    ## generate labels.txt containing all unique class names
+    read_voc_class_names(voc_annotation_path=os.path.join(args.root_dir, "Annotations"),
+                         voc_class_names_output_path=labels_path,
+                         voc_annotation_xml_output_path=annpaths_list_path)
+
+    assert os.path.exists(labels_path), "FatalError: labels.txt does not exist!"
+    assert os.path.exists(annpaths_list_path), "FatalError: pathlist.txt does not exist!"
+
     output_path_fmt = os.path.join(args.root_dir, "Annotations", "coco_%s.json")
 
     label2id = get_label2id(labels_path=labels_path)
