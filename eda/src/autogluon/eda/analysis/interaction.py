09d6e2a1b0d7d1c9585255e686fbfab01bf2e37e
Alexander Shirkov <ashyrkou@amazon.com>
2022-11-07 14:50:29 -0800

    [eda] added correlation

---
+++ eda/src/autogluon/eda/analysis/interaction.py
@@ -0,0 +1,111 @@
+from typing import List, Optional
+
+import phik  # noqa - required for significance_matrix instrumentation on pandas dataframes
+
+from .base import AbstractAnalysis
+from .. import AnalysisState
+
+__all__ = ["Correlation", "CorrelationSignificance"]
+
+
+class Correlation(AbstractAnalysis):
+    """
+    Correlation analysis.
+
+    Parameters
+    ----------
+    method: str  {'pearson', 'kendall', 'spearman', 'phik'}, default='spearman'
+        Method of correlation:
+            * pearson : standard correlation coefficient
+            * kendall : Kendall Tau correlation coefficient
+            * spearman : Spearman rank correlation
+            * phik : phi_k correlation
+                Correlation matrix of bivariate gaussian derived from chi2-value Chi2-value
+                gets converted into correlation coefficient of bivariate gauss with correlation
+                value rho, assuming giving binning and number of records. Correlation coefficient
+                value is between 0 and 1. Bivariate gaussian's range is set to [-5,5] by construction.
+                See Also `phik <https://github.com/KaveIO/PhiK>`_ documentation.
+
+    focus_field: Optional[str], default = None
+        field name to focus. Specifying a field would filter all correlations only when they are >= `focus_field_threshold`
+        This is helpful when dealing with a large number of variables.
+    focus_field_threshold: float, default = 0.5
+        a cut-off threshold when `focus_field` is specified
+    parent: Optional[AbstractAnalysis], default = None
+        parent Analysis
+    children: List[AbstractAnalysis], default []
+        wrapped analyses; these will receive sampled `args` during `fit` call
+
+    See Also
+    --------
+    `phik <https://github.com/KaveIO/PhiK>`_ documentation
+
+    """
+
+    def __init__(
+        self,
+        method: str = "spearman",
+        focus_field: Optional[str] = None,
+        focus_field_threshold: float = 0.5,
+        parent: Optional[AbstractAnalysis] = None,
+        children: Optional[List[AbstractAnalysis]] = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(parent, children, **kwargs)
+        assert method in ["pearson", "kendall", "spearman", "phik"]
+        self.method = method
+        self.focus_field = focus_field
+        self.focus_field_threshold = focus_field_threshold
+
+    def can_handle(self, state: AnalysisState, args: AnalysisState) -> bool:
+        return True
+
+    def _fit(self, state: AnalysisState, args: AnalysisState, **fit_kwargs):
+        state.correlations = {}
+        state.correlations_method = self.method
+        for (ds, df) in self.available_datasets(args):
+            if self.method == "phik":
+                state.correlations[ds] = df.phik_matrix(**self.args, verbose=False)
+            else:
+                state.correlations[ds] = df.corr(method=self.method, **self.args)
+
+            if self.focus_field is not None and self.focus_field in state.correlations[ds].columns:
+                state.correlations_focus_field = self.focus_field
+                state.correlations_focus_field_threshold = self.focus_field_threshold
+                state.correlations_focus_high_corr = {}
+                df_corr = state.correlations[ds]
+                df_corr = df_corr[df_corr[self.focus_field].abs() >= self.focus_field_threshold]
+                keep_cols = df_corr.index.values
+                state.correlations[ds] = df_corr[keep_cols]
+
+                high_corr = (
+                    state.correlations[ds][[self.focus_field]]
+                    .sort_values(self.focus_field, ascending=False)
+                    .drop(self.focus_field)
+                )
+                state.correlations_focus_high_corr[ds] = high_corr
+
+
+class CorrelationSignificance(AbstractAnalysis):
+    """
+    Significance of correlation of all variable combinations in the DataFrame.
+    See :py:meth:`~phik.significance.significance_matrix` for more details.
+    This analysis requires :py:class:`~autogluon.eda.analysis.interaction.Correlation` results to be
+    available in the state.
+
+    See Also
+    --------
+    `phik <https://github.com/KaveIO/PhiK>`_ documentation
+    :py:meth:`~phik.significance.significance_matrix`
+    :py:class:`~autogluon.eda.analysis.interaction.Correlation`
+    """
+
+    def can_handle(self, state: AnalysisState, args: AnalysisState) -> bool:
+        return self.all_keys_must_be_present(state, "correlations", "correlations_method")
+
+    def _fit(self, state: AnalysisState, args: AnalysisState, **fit_kwargs):
+        state.significance_matrix = {}
+        for (ds, df) in self.available_datasets(args):
+            state.significance_matrix[ds] = df[state.correlations[ds].columns].significance_matrix(
+                **self.args, verbose=False
+            )
---
+++ eda/tests/unittests/analysis/test_interaction.py
@@ -0,0 +1,198 @@
+import pandas as pd
+
+import autogluon.eda.auto as auto
+from autogluon.eda import AnalysisState
+from autogluon.eda.analysis import Correlation, CorrelationSignificance
+
+
+def test_Correlation_spearman():
+    state = auto.analyze(train_data=__create_test_df(), return_state=True, anlz_facets=[Correlation()])
+    state.correlations.train_data = state.correlations.train_data.round(2)
+    expected = AnalysisState(
+        {
+            "correlations": {
+                "train_data": pd.DataFrame(
+                    index=list("abcd"),
+                    data={
+                        "a": [1.00, 0.0, 0.88, -0.88],
+                        "b": [0.00, 1.0, 0.40, -0.40],
+                        "c": [0.88, 0.4, 1.00, -1.00],
+                        "d": [-0.88, -0.4, -1.00, 1.00],
+                    },
+                )
+            },
+            "correlations_method": "spearman",
+        }
+    )
+    __compare_outputs(expected, state)
+
+
+def test_Correlation_pearson():
+    state = auto.analyze(train_data=__create_test_df(), return_state=True, anlz_facets=[Correlation(method="pearson")])
+    state.correlations.train_data = state.correlations.train_data.round(2)
+    expected = AnalysisState(
+        {
+            "correlations": {
+                "train_data": pd.DataFrame(
+                    index=list("abcd"),
+                    data={
+                        "a": [1.00, 0.03, 0.88, -0.88],
+                        "b": [0.03, 1.00, 0.43, -0.43],
+                        "c": [0.88, 0.43, 1.00, -1.00],
+                        "d": [-0.88, -0.43, -1.00, 1.00],
+                    },
+                )
+            },
+            "correlations_method": "pearson",
+        }
+    )
+    __compare_outputs(expected, state)
+
+
+def test_Correlation_kendall():
+    state = auto.analyze(train_data=__create_test_df(), return_state=True, anlz_facets=[Correlation(method="kendall")])
+    state.correlations.train_data = state.correlations.train_data.round(2)
+    expected = AnalysisState(
+        {
+            "correlations": {
+                "train_data": pd.DataFrame(
+                    index=list("abcd"),
+                    data={
+                        "a": [1.00, 0.00, 0.77, -0.77],
+                        "b": [0.00, 1.00, 0.36, -0.36],
+                        "c": [0.77, 0.36, 1.00, -1.00],
+                        "d": [-0.77, -0.36, -1.00, 1.00],
+                    },
+                )
+            },
+            "correlations_method": "kendall",
+        }
+    )
+    __compare_outputs(expected, state)
+
+
+def test_Correlation_phik():
+    state = auto.analyze(train_data=__create_test_df(), return_state=True, anlz_facets=[Correlation(method="phik")])
+    state.correlations.train_data = state.correlations.train_data.round(2)
+    expected = AnalysisState(
+        {
+            "correlations": {
+                "train_data": pd.DataFrame(
+                    index=list("abcdef"),
+                    data={
+                        "a": [1.0, 1.0, 1.00, 1.00, 1.00, 1.00],
+                        "b": [1.0, 1.0, 0.00, 0.00, 0.00, 0.00],
+                        "c": [1.0, 0.0, 1.00, 0.79, 0.79, 0.79],
+                        "d": [1.0, 0.0, 0.79, 1.00, 0.79, 0.79],
+                        "e": [1.0, 0.0, 0.79, 0.79, 1.00, 0.79],
+                        "f": [1.0, 0.0, 0.79, 0.79, 0.79, 1.00],
+                    },
+                )
+            },
+            "correlations_method": "phik",
+        }
+    )
+    __compare_outputs(expected, state)
+
+
+def test_Correlation_focus():
+    actual = auto.analyze(
+        train_data=(__create_test_df()),
+        return_state=True,
+        anlz_facets=[Correlation(focus_field="c", focus_field_threshold=0.5)],
+    )
+    print(actual)
+    actual.correlations.train_data = actual.correlations.train_data.round(2)
+    actual.correlations_focus_high_corr.train_data = actual.correlations_focus_high_corr.train_data.round(2)
+    expected = AnalysisState(
+        {
+            "correlations": {
+                "train_data": pd.DataFrame(
+                    index=list("acd"),
+                    data={
+                        "a": [1.00, 0.88, -0.88],
+                        "c": [0.88, 1.00, -1.00],
+                        "d": [-0.88, -1.00, 1.00],
+                    },
+                )
+            },
+            "correlations_method": "spearman",
+            "correlations_focus_field": "c",
+            "correlations_focus_field_threshold": 0.5,
+            "correlations_focus_high_corr": {"train_data": pd.DataFrame(index=list("ad"), data={"c": [0.88, -1.00]})},
+        }
+    )
+    assert actual.correlations_focus_high_corr.train_data.equals(
+        expected.correlations_focus_high_corr.train_data
+    )  # noqa
+    actual.correlations_focus_high_corr.train_data = "--"
+    expected.correlations_focus_high_corr.train_data = "--"
+    __compare_outputs(expected, actual)
+
+
+def test_CorrelationSignificance__can_handle():
+    args = AnalysisState()
+    assert (
+        CorrelationSignificance().can_handle(
+            AnalysisState({"correlations": 123, "correlations_method": "something"}), args
+        )
+        is True
+    )
+    assert (
+        CorrelationSignificance().can_handle(
+            AnalysisState({"something": 123, "correlations_method": "something"}), args
+        )
+        is False
+    )
+    assert (
+        CorrelationSignificance().can_handle(AnalysisState({"correlations": 123, "something": "something"}), args)
+        is False
+    )
+
+
+def test_CorrelationSignificance():
+    state = AnalysisState(
+        {
+            "correlations": {
+                "train_data": pd.DataFrame(
+                    index=list("acd"),
+                    data={
+                        "a": [1.00, 0.88, -0.88],
+                        "c": [0.88, 1.00, -1.00],
+                        "d": [-0.88, -1.00, 1.00],
+                    },
+                )
+            },
+            "correlations_method": "spearman",
+            "correlations_focus_field": "c",
+            "correlations_focus_field_threshold": 0.5,
+            "correlations_focus_high_corr": {"train_data": pd.DataFrame(index=list("ad"), data={"c": [0.88, -1.00]})},
+        }
+    )
+
+    result = auto.analyze(
+        train_data=__create_test_df(), state=state, return_state=True, anlz_facets=[CorrelationSignificance()]
+    )
+
+    assert result.significance_matrix.train_data.shape == (3, 3)
+
+
+def __create_test_df():
+    data = pd.DataFrame(
+        {
+            "a": [1, 2, 3, 4, 5, 6],
+            "b": [5, 3, 2, 6, 7, 2],
+            "c": [0, 0, 0, 1, 1, 1],
+            "d": [1, 1, 1, 0, 0, 0],
+            "e": ["a", "a", "a", "b", "b", "b"],
+            "f": ["b", "b", "b", "a", "a", "a"],
+        }
+    )
+    return data
+
+
+def __compare_outputs(expected: AnalysisState, actual: AnalysisState):
+    assert actual.correlations.train_data.equals(expected.correlations.train_data)  # noqa
+    actual.correlations.train_data = "--"
+    expected.correlations.train_data = "--"
+    assert actual == expected
