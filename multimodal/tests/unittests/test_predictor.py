d3ea45260dd94fe5d4ea2bf553ee287fb6142da1
Xingjian Shi <xshiab@connect.ust.hk>
2022-11-09 11:19:32 -0800

    Merge pipelines to problem_type (#2337)
    * update
    
    * co-list some pipelines that support .fit to problem_type
    
    * Update predictor.py
    
    * reformat
    
    * pipeline --> problem_type
    
    * improve runtime error
    
    * reformat file
    
    * fix matcher
    
    * skip test if mmocr is not installed
    
    * Update test_text_detection.py
    
    * Update test_text_detection.py
    
    * Update predictor.py
    
    * mark ocr as experimental
    
    * fix lint
    
    * fix NER
    
    * fix
    
    * fix
    
    * fix bug
    
    * add few-shot text classification
    
    * Update test_backward_compatibility.py
    
    * fix few-shot
    
    * Update test_backward_compatibility.py
    
    * Update README.md
    
    * Update README.md
    
    * reduce to train 10 seconds
    
    * revise
    
    * remove unused import
    
    * improve warning
    
    * Update test_data_augmentation.py
    
    * Update test_data_augmentation.py
    
    * Update test_data_augmentation.py
    
    * fix lint
    
    * fix
    
    * accelerate unittest
    
    * fix
    
    * Update test_hpo.py
    
    * fix
    
    * Update predictor.py
    
    * fix
    
    * update
    
    * accelerate test
    
    * try to fix
    
    * Update test_ner.py
    
    * lower the time_limit
    
    * fix lint
    
    * update
    
    * fix
    
    * Update matcher.py
    
    * fix
    
    * Update matcher.py
    
    * Update test_matcher.py
    
    * fix lint
    
    * Update test_matcher.py
    
    * accelerate test-case
    
    * use uuid
    
    * fix
    
    * Update test_ner.py

--- multimodal/tests/unittests/test_predictor.py
+++ multimodal/tests/unittests/test_predictor.py
@@ -3,6 +3,7 @@ import os
 import pickle
 import shutil
 import tempfile
+import uuid
 
 import numpy.testing as npt
 import pytest
@@ -40,23 +41,25 @@ ALL_DATASETS = {
 
 
 def verify_predictor_save_load(predictor, df, verify_embedding=True, cls=MultiModalPredictor):
-    with tempfile.TemporaryDirectory() as root:
-        predictor.save(root)
-        predictions = predictor.predict(df, as_pandas=False)
-        loaded_predictor = cls.load(root)
-        predictions2 = loaded_predictor.predict(df, as_pandas=False)
-        predictions2_df = loaded_predictor.predict(df, as_pandas=True)
-        npt.assert_equal(predictions, predictions2)
-        npt.assert_equal(predictions2, predictions2_df.to_numpy())
-        if predictor.problem_type in [BINARY, MULTICLASS]:
-            predictions_prob = predictor.predict_proba(df, as_pandas=False)
-            predictions2_prob = loaded_predictor.predict_proba(df, as_pandas=False)
-            predictions2_prob_df = loaded_predictor.predict_proba(df, as_pandas=True)
-            npt.assert_equal(predictions_prob, predictions2_prob)
-            npt.assert_equal(predictions2_prob, predictions2_prob_df.to_numpy())
-        if verify_embedding:
-            embeddings = predictor.extract_embedding(df)
-            assert embeddings.shape[0] == len(df)
+    root = str(uuid.uuid4())
+    os.makedirs(root, exist_ok=True)
+    predictor.save(root)
+    predictions = predictor.predict(df, as_pandas=False)
+    loaded_predictor = cls.load(root)
+    predictions2 = loaded_predictor.predict(df, as_pandas=False)
+    predictions2_df = loaded_predictor.predict(df, as_pandas=True)
+    npt.assert_equal(predictions, predictions2)
+    npt.assert_equal(predictions2, predictions2_df.to_numpy())
+    if predictor.problem_type in [BINARY, MULTICLASS]:
+        predictions_prob = predictor.predict_proba(df, as_pandas=False)
+        predictions2_prob = loaded_predictor.predict_proba(df, as_pandas=False)
+        predictions2_prob_df = loaded_predictor.predict_proba(df, as_pandas=True)
+        npt.assert_equal(predictions_prob, predictions2_prob)
+        npt.assert_equal(predictions2_prob, predictions2_prob_df.to_numpy())
+    if verify_embedding:
+        embeddings = predictor.extract_embedding(df)
+        assert embeddings.shape[0] == len(df)
+    shutil.rmtree(root)
 
 
 def verify_realtime_inference(predictor, df, verify_embedding=True):
