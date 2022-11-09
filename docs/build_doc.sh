307ed5f6e00b03da14c8b80701f55de2fb8c9340
Garrett Idler <gidler@amazon.com>
2022-11-09 10:40:24 -0700

    Remove pointers to mli file system

--- docs/build_doc.sh
+++ docs/build_doc.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# cp -r ~/Google\ Drive/My\ Drive/Colab\ Notebooks/autogluon_tutorials/* .
 rm -rf _build/
 rm -rf jupyter_execute/
 sphinx-autogen api/*.rst api/*/*.rst -t _templates/autosummary
