627e8cb00324a1cd84748528009e494aaca1c898
FANGAreNotGnu <kevinfang97@gmail.com>
2022-11-12 01:03:20 -0800

    fix

--- docs/build_pip_install.sh
+++ docs/build_pip_install.sh
@@ -32,7 +32,7 @@ cd multimodal/
 python3 -m pip install -e .
 # TODO: move mmlab packages installation to better place
 python3 -m pip install mmdet
-python3 -m mim install mmcv-full
+mim install mmcv-full
 cd ..
 
 cd text/
