df6c1eac83678d3f33f65e4faad8638a0f622012
Nick Erickson <neerick@amazon.com>
2022-11-14 18:45:46 -0800

    Fix FTTransformer errors, add max_features (#2389)
    * Fix FTTransformer errors, add max_features
    
    * Hide progress bar if verbosity<=2 FTT

--- tabular/src/autogluon/tabular/models/automm/ft_transformer.py
+++ tabular/src/autogluon/tabular/models/automm/ft_transformer.py
@@ -2,9 +2,13 @@
 import logging
 from typing import Dict
 
+from autogluon.common.features.types import R_INT, R_FLOAT, R_CATEGORY, \
+    S_TEXT_NGRAM, S_TEXT_SPECIAL
+
 from .automm_model import MultiModalPredictorModel
 
 logger = logging.getLogger(__name__)
+
 
 # TODO: Add unit tests
 class FTTransformerModel(MultiModalPredictorModel):
@@ -43,6 +47,21 @@ class FTTransformerModel(MultiModalPredictorModel):
         """
         super().__init__(**kwargs)
 
+    def _fit(self, X, num_gpus='auto', **kwargs):
+        if not isinstance(num_gpus, str):
+            if num_gpus == 0:
+                logger.log(30, f'WARNING: Training {self.name} on CPU (no GPU specified). This could take a long time. Use GPU to speed up training.')
+        super()._fit(X, num_gpus=num_gpus, **kwargs)
+
+    def _get_default_auxiliary_params(self) -> dict:
+        default_auxiliary_params = super()._get_default_auxiliary_params()
+        extra_auxiliary_params = dict(
+            valid_raw_types=[R_INT, R_FLOAT, R_CATEGORY],
+            ignored_type_group_special=[S_TEXT_NGRAM, S_TEXT_SPECIAL],
+        )
+        default_auxiliary_params.update(extra_auxiliary_params)
+        return default_auxiliary_params
+
     def _set_default_params(self):
         default_params = {
             "data.categorical.convert_to_text": False,
@@ -59,6 +78,7 @@ class FTTransformerModel(MultiModalPredictorModel):
             "optimization.warmup_steps": 0.0,
             "optimization.patience": 20,
             "optimization.top_k": 3,
+            '_max_features': 300,  # FIXME: This is a hack, move to AG_ARGS_FIT for v0.7
         }
         for param, val in default_params.items():
             self._set_default_param_value(param, val)
