59d7bde5ffef45f59ea81a2e0e8992de04109f28
Zhiqiang Tang <zhiqiang.tang@rutgers.edu>
2022-11-15 10:35:54 -0800

    save and load enable_progress_bar

--- multimodal/src/autogluon/multimodal/predictor.py
+++ multimodal/src/autogluon/multimodal/predictor.py
@@ -2687,6 +2687,7 @@ class MultiModalPredictor:
                     "classes": self._classes,
                     "save_path": self._save_path,
                     "pretrained_path": self._pretrained_path,
+                    "enable_progress_bar": self._enable_progress_bar,
                     "version": ag_version.__version__,
                 },
                 fp,
@@ -2886,6 +2887,8 @@ class MultiModalPredictor:
             predictor._classes = assets["classes"]
         predictor._column_types = assets["column_types"]
         predictor._validation_metric_name = assets["validation_metric_name"]
+        if "enable_progress_bar" in assets:
+            predictor._enable_progress_bar = assets["enable_progress_bar"]
         predictor._df_preprocessor = df_preprocessor
         predictor._data_processors = data_processors
 
