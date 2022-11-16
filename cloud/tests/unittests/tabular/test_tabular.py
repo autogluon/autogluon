301e8dfb06759407fcd92f722e62ace9176c2d37
Weisu Yin <weisuyin96@gmail.com>
2022-11-16 23:32:14 +0000

    initial
    fix
    
    fix
    
    fix
    
    checkpoint
    
    checkpoint
    
    checkpoint
    
    api
    
    cleanup old version code
    
    fix to local
    
    fix
    
    fix
    
    fix
    
    additional volume size
    
    fix
    
    fix
    
    tests

--- cloud/tests/unittests/tabular/test_tabular.py
+++ cloud/tests/unittests/tabular/test_tabular.py
@@ -34,7 +34,10 @@ def test_tabular(test_helper):
             predictor_init_args,
             predictor_fit_args,
             cloud_predictor_no_train,
-            test_data
+            test_data,
+            fit_kwargs=dict(custom_image_uri=test_helper.cpu_training_image),
+            deploy_kwargs=dict(custom_image_uri=test_helper.cpu_inference_image),
+            predict_kwargs=dict(custom_image_uri=test_helper.cpu_inference_image),
         )
 
 
@@ -76,9 +79,13 @@ def test_tabular_tabular_text_image(test_helper):
             test_data,
             image_path='tabular_text_image_images.zip',
             fit_instance_type='ml.g4dn.2xlarge',
-            fit_kwargs=dict(image_column='Images'),
+            fit_kwargs=dict(
+                image_column='Images',
+                custom_image_uri=test_helper.gpu_training_image
+            ),
             deploy_kwargs=dict(
                 instance_type='ml.g4dn.2xlarge',
+                custom_image_uri=test_helper.gpu_inference_image
             ),
             predict_real_time_kwargs=dict(
                 test_data_image_column='Images',
@@ -86,49 +93,53 @@ def test_tabular_tabular_text_image(test_helper):
             predict_kwargs=dict(
                 instance_type='ml.g4dn.2xlarge',
                 test_data_image_column='Images',
+                custom_image_uri=test_helper.gpu_inference_image
             ),
-            skip_predict=True  # TODO: remove this after autogluon 0.6 release. Currently, some issues cause some module's batch inference to fail
+            # skip_predict=True  # TODO: remove this after autogluon 0.6 release. Currently, some issues cause some module's batch inference to fail
         )
 
 
-def test_tabular_tabular_text(test_helper):
-    train_data = 'tabular_text_train.csv'
-    test_data = 'tabular_text_test.csv'
-    with tempfile.TemporaryDirectory() as root:
-        test_helper.prepare_data(train_data, test_data)
-        time_limit = 120
+# def test_tabular_tabular_text(test_helper):
+#     train_data = 'tabular_text_train.csv'
+#     test_data = 'tabular_text_test.csv'
+#     with tempfile.TemporaryDirectory() as root:
+#         test_helper.prepare_data(train_data, test_data)
+#         time_limit = 120
 
-        predictor_init_args = dict(
-            label='Sentiment',
-        )
-        predictor_fit_args = dict(
-            train_data=train_data,
-            time_limit=time_limit,
-            hyperparameters={
-                'XGB': {},
-                'AG_TEXT_NN': {'presets': 'medium_quality_faster_train'},
-            }
-        )
-        cloud_predictor = TabularCloudPredictor(
-            cloud_output_path='s3://autogluon-cloud-ci/test-tabular-tabular-text',
-            local_output_path='test_tabular_tabular_text_cloud_predictor'
-        )
-        cloud_predictor_no_train = TabularCloudPredictor(
-            cloud_output_path='s3://autogluon-cloud-ci/test-tabular-tabular-text-no-train',
-            local_output_path='test_tabular_tabular_text_cloud_predictor_no_train'
-        )
-        test_helper.test_functionality(
-            cloud_predictor,
-            predictor_init_args,
-            predictor_fit_args,
-            cloud_predictor_no_train,
-            test_data,
-            fit_instance_type='ml.g4dn.2xlarge',
-            deploy_kwargs=dict(
-                instance_type='ml.g4dn.2xlarge',
-            ),
-            predict_kwargs=dict(
-                instance_type='ml.g4dn.2xlarge',
-            ),
-            skip_predict=True  # TODO: remove this after autogluon 0.6 release. Currently, some issues cause some module's batch inference to fail
-        )
+#         predictor_init_args = dict(
+#             label='Sentiment',
+#         )
+#         predictor_fit_args = dict(
+#             train_data=train_data,
+#             time_limit=time_limit,
+#             hyperparameters={
+#                 'XGB': {},
+#                 'AG_TEXT_NN': {'presets': 'medium_quality_faster_train'},
+#             }
+#         )
+#         cloud_predictor = TabularCloudPredictor(
+#             cloud_output_path='s3://autogluon-cloud-ci/test-tabular-tabular-text',
+#             local_output_path='test_tabular_tabular_text_cloud_predictor'
+#         )
+#         cloud_predictor_no_train = TabularCloudPredictor(
+#             cloud_output_path='s3://autogluon-cloud-ci/test-tabular-tabular-text-no-train',
+#             local_output_path='test_tabular_tabular_text_cloud_predictor_no_train'
+#         )
+#         test_helper.test_functionality(
+#             cloud_predictor,
+#             predictor_init_args,
+#             predictor_fit_args,
+#             cloud_predictor_no_train,
+#             test_data,
+#             fit_instance_type='ml.g4dn.2xlarge',
+#             fit_kwargs=dict(custom_image_uri=test_helper.cpu_training_image),
+#             deploy_kwargs=dict(
+#                 instance_type='ml.g4dn.2xlarge',
+#                 custom_image_uri=test_helper.gpu_inference_image
+#             ),
+#             predict_kwargs=dict(
+#                 instance_type='ml.g4dn.2xlarge',
+#                 custom_image_uri=test_helper.gpu_inference_image
+#             ),
+#             skip_predict=True  # TODO: remove this after autogluon 0.6 release. Currently, some issues cause some module's batch inference to fail
+#         )
