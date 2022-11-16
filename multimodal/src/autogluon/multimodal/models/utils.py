0ffccd6a0e7ebdaee093ef37db26eecec6c2f68e
Haoyang Fang <107515844+FANGAreNotGnu@users.noreply.github.com>
2022-11-15 21:34:26 -0800

    Remove warnings and duplicate function (#2409)
    * create save_path in init and remove warning in fit
    
    * remove unecessary line
    
    * fix
    
    * remove duplicate function
    
    * filter mmcv warning
    
    * lint

--- multimodal/src/autogluon/multimodal/models/utils.py
+++ multimodal/src/autogluon/multimodal/models/utils.py
@@ -619,7 +619,11 @@ def get_mmocr_config_and_model(checkpoint_name: str):
     An MMOCR config and model.
     """
     try:
-        import mmcv
+        import warnings
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            import mmcv
         from mmcv.runner import load_checkpoint
     except ImportError:
         mmcv = None
