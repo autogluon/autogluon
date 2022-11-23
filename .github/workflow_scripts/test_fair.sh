82d78a071d708fb3d00c61ca9c6d4000723b0355
Weisu Yin <weisuyin96@gmail.com>
2022-11-23 23:50:50 +0000

    check

--- .github/workflow_scripts/test_fair.sh
+++ .github/workflow_scripts/test_fair.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-# set -ex
+set -ex
 
-# ADDITIONAL_TEST_ARGS=$1
+ADDITIONAL_TEST_ARGS=$1
 
-# source $(dirname "$0")/env_setup.sh
+source $(dirname "$0")/env_setup.sh
 
-# setup_build_env
-# export CUDA_VISIBLE_DEVICES=0
-# install_core_all_tests
-# install_common
-# install_features
-# install_tabular_all
-# install_fair
+setup_build_env
+export CUDA_VISIBLE_DEVICES=0
+install_core_all_tests
+install_common
+install_features
+install_tabular_all
+install_fair
 
-# cd fair/
-# if [ -n "$ADDITIONAL_TEST_ARGS" ]
-# then
-#     python3 -m pytest --junitxml=results.xml --runslow "$ADDITIONAL_TEST_ARGS" tests
-# else
-#     python3 -m pytest --junitxml=results.xml --runslow tests
-# fi
+if [ -d fair ]
+then
+    cd fair/
+    if [ -n "$ADDITIONAL_TEST_ARGS" ]
+    then
+        python3 -m pytest --junitxml=results.xml --runslow "$ADDITIONAL_TEST_ARGS" tests
+    else
+        python3 -m pytest --junitxml=results.xml --runslow tests
+    fi
+fi
