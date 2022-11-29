22d65ba2dafb8c8f2cd7de99ded5131d55cdb11b
tonyhoo <tonyhoo0410@gmail.com>
2022-11-29 11:59:36 -0800

    fix additional parameter passed to build_doc

--- .github/workflow_scripts/build_multimodal_tutorial.sh
+++ .github/workflow_scripts/build_multimodal_tutorial.sh
@@ -12,4 +12,4 @@ PR_NUMBER=$5  # For push events, PR_NUMBER will be empty
 source $(dirname "$0")/env_setup.sh
 source $(dirname "$0")/build_doc.sh
 
-build_doc multimodal $SUB_DOC $BRANCH $GIT_REPO $COMMIT_SHA ${PR_NUMBER:-""} $SUB_DOC
+build_doc multimodal $BRANCH $GIT_REPO $COMMIT_SHA ${PR_NUMBER:-""} $SUB_DOC
