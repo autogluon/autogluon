d70d89b0c76027f53e58e66ede771f3696094fa9
FANGAreNotGnu <kevinfang97@gmail.com>
2022-12-05 21:46:13 -0800

    black

--- multimodal/src/autogluon/multimodal/predictor.py
+++ multimodal/src/autogluon/multimodal/predictor.py
@@ -2854,7 +2854,9 @@ class MultiModalPredictor:
         with open(os.path.join(path, "df_preprocessor.pkl"), "rb") as fp:
             df_preprocessor = CustomUnpickler(fp).load()
             if (
-                not hasattr(df_preprocessor, "_rois_feature_names") and ROIS in df_preprocessor._image_feature_names
+                not hasattr(df_preprocessor, "_rois_feature_names")
+                and hasattr(df_preprocessor, "_image_feature_names")
+                and ROIS in df_preprocessor._image_feature_names
             ):  # backward compatibility for mmlab models
                 df_preprocessor._image_feature_names = [
                     name for name in df_preprocessor._image_feature_names if name != ROIS
