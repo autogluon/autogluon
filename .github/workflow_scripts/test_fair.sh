71a240f322e5a07721b79cd19ad3398e26f4f546
Chris Russell <christopher.m.russell@gmail.com>
2022-12-06 11:59:40 +0100

    remove runslow from test

--- .github/workflow_scripts/test_fair.sh
+++ .github/workflow_scripts/test_fair.sh
@@ -19,8 +19,8 @@ then
     cd fair/
     if [ -n "$ADDITIONAL_TEST_ARGS" ]
     then
-        python3 -m pytest --junitxml=results.xml --runslow "$ADDITIONAL_TEST_ARGS" tests
+        python3 -m pytest --junitxml=results.xml "$ADDITIONAL_TEST_ARGS" tests
     else
-        python3 -m pytest --junitxml=results.xml --runslow tests
+        python3 -m pytest --junitxml=results.xml tests
     fi
 fi
