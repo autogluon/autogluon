c2cbe87d7b8a0fd480b756a12d6cfdd23a79edc2
Weisu Yin <weisy@amazon.com>
2022-12-06 16:27:49 -0800

    remove (#2515)
    Co-authored-by: Weisu Yin <weisuyin96@gmail.com>

--- cloud/setup.py
+++ cloud/setup.py
@@ -23,7 +23,6 @@ install_requires = [
     "autogluon.common<0.7",
     "boto3",
     "numpy",
-    "opencv-python>=4.6,<4.7",
     "packaging<22",
     "pandas",
     # TODO: update to the latest after 0.6 container is out
