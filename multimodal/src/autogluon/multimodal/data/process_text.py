a7671fe1f924e677f574f5afc9bfb92cf481833f
FANGAreNotGnu <kevinfang97@gmail.com>
2022-12-09 15:27:50 -0800

    black

--- multimodal/src/autogluon/multimodal/data/process_text.py
+++ multimodal/src/autogluon/multimodal/data/process_text.py
@@ -155,7 +155,7 @@ class TextProcessor:
         else:
             if max_len < self.tokenizer.model_max_length:
                 # TODO, Consider to fix the logic
-                if self.tokenizer.model_max_length < 10 ** 6:
+                if self.tokenizer.model_max_length < 10**6:
                     warnings.warn(
                         f"provided max length: {max_len} "
                         f"is smaller than {model.checkpoint_name}'s default: {self.tokenizer.model_max_length}"
