67e75782feedb8cf2180442c930c2a5ccefb0f0d
Xingjian Shi <xshiab@connect.ust.hk>
2022-12-15 22:29:02 -0800

    fix lint

--- multimodal/tests/unittests/others/test_problem_types.py
+++ multimodal/tests/unittests/others/test_problem_types.py
@@ -4,11 +4,23 @@ from autogluon.multimodal.problem_types import problem_type_reg
 
 @pytest.mark.parametrize(
     "name",
-    ["classification", "multiclass", "binary", "regression",
-     "ner", "named_entity_recognition", "object_detection",
-     "text_similarity", "image_similarity", "image_text_similarity",
-     "feature_extraction", "zero_shot_image_classification", "few_shot_text_classification",
-     "ocr_text_detection", "ocr_text_recognition"]
+    [
+        "classification",
+        "multiclass",
+        "binary",
+        "regression",
+        "ner",
+        "named_entity_recognition",
+        "object_detection",
+        "text_similarity",
+        "image_similarity",
+        "image_text_similarity",
+        "feature_extraction",
+        "zero_shot_image_classification",
+        "few_shot_text_classification",
+        "ocr_text_detection",
+        "ocr_text_recognition",
+    ],
 )
 def test_get_problem_type(name):
     problem_type = problem_type_reg.get(name)
