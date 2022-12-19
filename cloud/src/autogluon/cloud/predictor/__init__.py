6e367011edb81adecc0c349fc8771569ff8d04dd
Weisu Yin <weisy@amazon.com>
2022-12-19 11:46:19 -0800

    remove cloud module (#2580)
    Co-authored-by: Weisu Yin <weisuyin96@gmail.com>

--- cloud/src/autogluon/cloud/predictor/__init__.py
+++
@@ -1,4 +0,0 @@
-from .image_cloud_predictor import ImageCloudPredictor
-from .multimodal_cloud_predictor import MultiModalCloudPredictor
-from .tabular_cloud_predictor import TabularCloudPredictor
-from .text_cloud_predictor import TextCloudPredictor
