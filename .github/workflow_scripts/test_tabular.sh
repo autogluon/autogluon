9e2eec0a3382109e6609302734181a8b9af17d8e
Weisu Yin <weisuyin96@gmail.com>
2023-01-03 19:10:07 +0000

    fix plat torch

--- .github/workflow_scripts/test_tabular.sh
+++ .github/workflow_scripts/test_tabular.sh
@@ -3,12 +3,19 @@
 set -ex
 
 ADDITIONAL_TEST_ARGS=$1
+IS_PLATFORM_TEST=$2
 
 source $(dirname "$0")/env_setup.sh
 
 setup_build_env
-setup_torch_gpu
-export CUDA_VISIBLE_DEVICES=0
+
+if [ "${IS_PLATFORM_TEST,,}" = "true" ]
+then
+    setup_torch_cpu_non_linux
+else
+    setup_torch_gpu
+fi
+
 install_local_packages "common/[tests]" "core/[all,tests]" "features/" "tabular/[all,tests]"
 install_multimodal "[tests]"
 install_local_packages "text/"
