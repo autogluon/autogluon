70dd543682a6ed0bb1173274e5de63071a71336e
Nick Erickson <neerick@amazon.com>
2023-01-05 16:12:14 -0800

    Tabular: Add num_folds (#2646)
    * Tabular: Add num_folds
    
    * cleanup interaction with HPO

--- tabular/tests/unittests/edgecases/test_edgecases.py
+++ tabular/tests/unittests/edgecases/test_edgecases.py
@@ -41,3 +41,55 @@ def test_max_sets(fit_helper):
     # 2 folds * 3 sets = 6
     assert leaderboard.iloc[0]['num_models'] == 6
     shutil.rmtree(predictor.path, ignore_errors=True)
+
+
+def test_num_folds(fit_helper):
+    """Tests that num_folds works"""
+    fit_args = dict(
+        hyperparameters={'GBM': {'ag_args_ensemble': {'num_folds': 3}}},
+        fit_weighted_ensemble=False,
+        num_bag_folds=7,
+        num_bag_sets=2,
+    )
+    dataset_name = 'adult'
+
+    predictor = fit_helper.fit_and_validate_dataset(
+        dataset_name=dataset_name,
+        fit_args=fit_args,
+        expected_model_count=1,
+        refit_full=False,
+        delete_directory=False,
+    )
+    leaderboard = predictor.leaderboard(extra_info=True)
+    # 3 folds * 2 sets = 6
+    assert leaderboard.iloc[0]['num_models'] == 6
+    shutil.rmtree(predictor.path, ignore_errors=True)
+
+
+def test_num_folds_hpo(fit_helper):
+    """Tests that num_folds works"""
+    fit_args = dict(
+        hyperparameters={'GBM': {'ag_args_ensemble': {'num_folds': 2}}},
+        fit_weighted_ensemble=False,
+        num_bag_folds=5,
+        num_bag_sets=2,
+        hyperparameter_tune_kwargs={
+            'searcher': 'random',
+            'scheduler': 'local',
+            'num_trials': 2,
+        },
+    )
+    dataset_name = 'adult'
+
+    predictor = fit_helper.fit_and_validate_dataset(
+        dataset_name=dataset_name,
+        fit_args=fit_args,
+        expected_model_count=2,
+        refit_full=False,
+        delete_directory=False,
+    )
+    leaderboard = predictor.leaderboard(extra_info=True)
+    # 2 folds * 2 sets = 4
+    assert leaderboard.iloc[0]['num_models'] == 4
+    assert leaderboard.iloc[1]['num_models'] == 4
+    shutil.rmtree(predictor.path, ignore_errors=True)
