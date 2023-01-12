90dd562fa824f2cda584c30fa7aafb0f8cd9c971
Oleksandr Shchur <shchuro@amazon.com>
2023-01-12 16:12:30 +0000

    Extend tests

--- timeseries/tests/unittests/models/test_autogluon_tabular.py
+++ timeseries/tests/unittests/models/test_autogluon_tabular.py
@@ -1,10 +1,13 @@
 from unittest import mock
 
+import numpy as np
+import pandas as pd
 import pytest
 
 from autogluon.timeseries.models.autogluon_tabular import AutoGluonTabularModel
+from autogluon.timeseries.utils.features import TimeSeriesFeatureGenerator
 
-from ..common import DATAFRAME_WITH_STATIC, DUMMY_VARIABLE_LENGTH_TS_DATAFRAME
+from ..common import DATAFRAME_WITH_STATIC, DUMMY_VARIABLE_LENGTH_TS_DATAFRAME, get_data_frame_with_variable_lengths
 
 TESTABLE_MODELS = [
     AutoGluonTabularModel,
@@ -46,17 +49,36 @@ def test_when_predict_is_called_then_get_features_dataframe_receives_correct_inp
                     assert len(new_timestamps.difference(original_timestamps)) == prediction_length
 
 
-def test_when_static_features_present_then_shape_is_correct(temp_model_path):
-    data = DATAFRAME_WITH_STATIC.copy()
-    model = AutoGluonTabularModel(path=temp_model_path)
+@pytest.mark.parametrize("known_covariates_names", [["known_1", "known_2"], []])
+@pytest.mark.parametrize("past_covariates_names", [["past_1", "past_2", "past_3"], []])
+@pytest.mark.parametrize("static_features_names", [["cat_1"], []])
+def test_when_covariates_and_features_present_then_shape_is_correct(
+    temp_model_path, known_covariates_names, past_covariates_names, static_features_names
+):
+    item_id_to_length = {1: 10, 5: 20, 2: 30}
+    data = get_data_frame_with_variable_lengths(
+        item_id_to_length, covariates_names=known_covariates_names + past_covariates_names
+    )
+    if static_features_names:
+        columns = {k: np.random.normal(size=len(item_id_to_length)) for k in static_features_names}
+        data.static_features = pd.DataFrame(columns, index=data.item_ids)
+
+    feat_gen = TimeSeriesFeatureGenerator(target="target", known_covariates_names=known_covariates_names)
+    data = feat_gen.fit_transform(data)
     # Initialize model._target_lag_indices and model._time_features from freq
+    model = AutoGluonTabularModel(path=temp_model_path, metadata=feat_gen.covariate_metadata)
     model.fit(train_data=data, time_limit=2)
     df = model._get_features_dataframe(data)
     expected_num_features = (
-        len(model._target_lag_indices) + len(model._time_features) + 1 + len(data.static_features.columns)
+        len(model._time_features)
+        + 1
+        + len(model._target_lag_indices)
+        + len(known_covariates_names) * len(model._known_covariates_lag_indices)
+        + len(past_covariates_names) * len(model._past_covariates_lag_indices)
+        + len(static_features_names)
     )
     assert len(df.columns) == expected_num_features
-    assert all(col in df.columns for col in data.static_features.columns)
+    assert all(col in df.columns for col in static_features_names)
 
 
 def test_when_static_features_present_then_prediction_works(temp_model_path):
