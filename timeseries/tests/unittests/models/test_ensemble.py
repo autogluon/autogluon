56debe2410f3d27e8cd8f29a455c87c6546674eb
Oleksandr Shchur <shchuro@amazon.com>
2023-01-17 16:02:02 +0000

    Create an abstract class for time series ensembles

--- timeseries/tests/unittests/models/test_ensemble.py
+++ timeseries/tests/unittests/models/test_ensemble.py
@@ -1,7 +1,7 @@
 import pytest
 
 from autogluon.timeseries.models import ETSModel
-from autogluon.timeseries.models.ensemble.greedy_ensemble import TimeSeriesEnsembleWrapper
+from autogluon.timeseries.models.ensemble.greedy_ensemble import TimeSeriesGreedyEnsemble
 
 from ..common import DUMMY_TS_DATAFRAME
 
@@ -9,13 +9,15 @@ from ..common import DUMMY_TS_DATAFRAME
 def test_when_some_base_models_fail_during_prediction_then_ensemble_can_still_predict():
     ets = ETSModel(prediction_length=1, freq=DUMMY_TS_DATAFRAME.freq)
     ets.fit(train_data=DUMMY_TS_DATAFRAME, hyperparameters={"maxiter": 1, "seasonal": None})
-    ensemble = TimeSeriesEnsembleWrapper(weights={"ARIMA": 0.5, "ETS": 0.5}, name="WeightedEnsemble")
+    ensemble = TimeSeriesGreedyEnsemble(name="WeightedEnsemble")
+    ensemble.model_to_weight = {"ARIMA": 0.5, "ETS": 0.5}
     ets_preds = ets.predict(DUMMY_TS_DATAFRAME)
     ensemble_preds = ensemble.predict(data={"ARIMA": None, "ETS": ets_preds})
     assert (ets_preds.values == ensemble_preds.values).all()
 
 
 def test_when_all_base_models_fail_during_prediction_then_ensemble_raises_runtime_error():
-    ensemble = TimeSeriesEnsembleWrapper(weights={"ARIMA": 0.5, "ETS": 0.5}, name="WeightedEnsemble")
+    ensemble = TimeSeriesGreedyEnsemble(name="WeightedEnsemble")
+    ensemble.model_to_weight = {"ARIMA": 0.5, "ETS": 0.5}
     with pytest.raises(RuntimeError):
         ensemble.predict(data={"ARIMA": None, "ETS": None})
