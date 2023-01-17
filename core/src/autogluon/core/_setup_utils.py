a98f7202dfd616ec39a71a87a5e3df5551de0787
Oleksandr Shchur <shchuro@amazon.com>
2023-01-17 11:52:01 +0100

    [timeseries] Bump pandas dependency to >=1.4.0 (#2703)

--- core/src/autogluon/core/_setup_utils.py
+++ core/src/autogluon/core/_setup_utils.py
@@ -17,7 +17,7 @@ PYTHON_REQUIRES = '>=3.7, <3.11'
 DEPENDENT_PACKAGES = {
     # note: if python 3.7 is used, the open CVEs are present: CVE-2021-41496 | CVE-2021-34141; fixes are available in 1.22.x, but python 3.8 only
     'numpy': '>=1.21,<1.24',
-    'pandas': '>=1.2.5,!=1.4.0,<1.6',
+    'pandas': '>1.4.0,<1.6',
     'scikit-learn': '>=1.0.0,<1.2',
     'scipy': '>=1.5.4,<1.10.0',
     'psutil': '>=5.7.3,<6',
