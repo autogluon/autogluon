669a3a07d0c5a43fc9140f36d86102016e1ad926
Xingjian Shi <xshiab@connect.ust.hk>
2023-01-19 22:58:19 -0800

    Update try_import.py

--- core/src/autogluon/core/utils/try_import.py
+++ core/src/autogluon/core/utils/try_import.py
@@ -61,7 +61,7 @@ def try_import_ray() -> ModuleType:
         Windows=RAY_MAX_VERSION,
         Linux=RAY_MAX_VERSION,
     )
-    ray_min_version = '2.0.0'
+    ray_min_version = '2.2.0'
     current_os = platform.system()
     ray_max_version = ray_max_version_os_map.get(current_os, RAY_MAX_VERSION)
     try:
