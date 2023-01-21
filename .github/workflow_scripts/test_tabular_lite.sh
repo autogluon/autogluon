e6f7ebedaeac346549130d9a1bb73ceac9193814
Yizhi Liu <liuyizhi@apache.org>
2023-01-20 17:22:05 -0800

    [CI] AutoGluonLite for tabular

---
+++ .github/workflow_scripts/test_tabular_lite.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ex
+
+source $(dirname "$0")/env_setup.sh
+
+setup_build_env
+export AUTOGLUON_PACKAGE_NAME="autogluon-lite"
+
+build_all
+
+DIST_DST=/pyodide/wheels
+mkdir -p $DIST_DST
+cp -f autogluon/dist/*.whl $DIST_DST/
+cp -f common/dist/*.whl $DIST_DST/
+cp -f core/dist/*.whl $DIST_DST/
+cp -f features/dist/*.whl $DIST_DST/
+cp -f tabular/dist/*.whl $DIST_DST/
+
+cp -f tabular/tests/regressiontests/test_tabular_lite.py /pyodide/
+
+cd /pyodide
+tools/pytest_wrapper.py test_tabular_lite -v \
+  --runtime 'firefox' --runner 'playwright' --junitxml=results.xml
