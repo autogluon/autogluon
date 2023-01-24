973ac38b377e2dc8bf49729beba9d342e6df55e9
Innixma <neerick@amazon.com>
2023-01-24 11:32:16 -0800

    Remove fairness module

--- full_install.sh
+++ full_install.sh
@@ -9,5 +9,4 @@ python3 -m pip install -e text/[tests]
 python3 -m pip install -e vision/
 python3 -m pip install -e timeseries/[all,tests]
 python3 -m pip install -e eda/
-python3 -m pip install -e fair/
 python3 -m pip install -e autogluon/
