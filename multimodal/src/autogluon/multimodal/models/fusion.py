c1d7c488ead8b5e96cc6bd9c4f2078c3e305698d
Su Zhou <zhousu@amazon.com>
2023-01-31 22:23:49 +0000

    add  fusion base class

--- multimodal/src/autogluon/multimodal/models/fusion.py
+++ multimodal/src/autogluon/multimodal/models/fusion.py
@@ -23,7 +23,67 @@ from .utils import init_weights, run_model
 logger = logging.getLogger(AUTOMM)
 
 
-class MultimodalFusionMLP(nn.Module):
+class BaseMultimodalFusionModel(nn.Module):
+    """
+    A base class to fuse different models' features (single-modal and multimodal).
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    @property
+    def label_key(self):
+        return f"{self.prefix}_{LABEL}"
+
+    def forward(self, *args, **kwargs):
+        pass
+
+    def get_layer_ids(
+        self,
+    ):
+        """
+        Assign an id to each layer. Layer ids will be used in layer-wise lr decay.
+        Basically, id gradually increases when going from the output end to
+        the input end.
+
+        It assumes that each individual model has the "name_to_id" attribute storing
+        the already computed model's layer ids. This function only collects those layer ids.
+        It also add prefixes for each model's parameter names since the fusion model wraps
+        those individual models, making the name scope changed. Configuring the optimizer
+        requires a full name of each parameter.
+
+        The layers defined in this class, e.g., head, adapter,
+        and, fusion_mlp, have id 0.
+
+        Returns
+        -------
+        A dictionary mapping the layer names (keys) to their ids (values).
+        """
+        model_prefix = "model"
+        names = [n for n, _ in self.named_parameters()]
+
+        outer_layer_names = [n for n in names if not n.startswith(model_prefix)]
+        name_to_id = {}
+        logger.debug(f"outer layers are treated as head: {outer_layer_names}")
+        for n in outer_layer_names:
+            name_to_id[n] = 0
+
+        for i, per_model in enumerate(self.model):
+            per_model_prefix = f"{model_prefix}.{i}"
+            if not hasattr(per_model, "name_to_id"):
+                raise ValueError(f"name_to_id attribute is missing in model: {per_model.__class__.__name__}")
+            for n, layer_id in per_model.name_to_id.items():
+                full_n = f"{per_model_prefix}.{n}"
+                name_to_id[full_n] = layer_id
+
+        # double check each parameter has been assigned an id
+        for n in names:
+            assert n in name_to_id
+
+        return name_to_id
+
+
+class MultimodalFusionMLP(BaseMultimodalFusionModel):
     """
     Use MLP to fuse different models' features (single-modal and multimodal).
     Specifically, it adapts the features of each model to specified dimensions,
@@ -131,10 +191,6 @@ class MultimodalFusionMLP(nn.Module):
         self.name_to_id = self.get_layer_ids()
         self.head_layer_names = [n for n, layer_id in self.name_to_id.items() if layer_id == 0]
 
-    @property
-    def label_key(self):
-        return f"{self.prefix}_{LABEL}"
-
     def forward(
         self,
         batch: dict,
@@ -178,52 +234,8 @@ class MultimodalFusionMLP(nn.Module):
         else:
             return fusion_output
 
-    def get_layer_ids(
-        self,
-    ):
-        """
-        Assign an id to each layer. Layer ids will be used in layer-wise lr decay.
-        Basically, id gradually increases when going from the output end to
-        the input end.
 
-        It assumes that each individual model has the "name_to_id" attribute storing
-        the already computed model's layer ids. This function only collects those layer ids.
-        It also add prefixes for each model's parameter names since the fusion model wraps
-        those individual models, making the name scope changed. Configuring the optimizer
-        requires a full name of each parameter.
-
-        The layers defined in this class, e.g., head, adapter,
-        and, fusion_mlp, have id 0.
-
-        Returns
-        -------
-        A dictionary mapping the layer names (keys) to their ids (values).
-        """
-        model_prefix = "model"
-        names = [n for n, _ in self.named_parameters()]
-
-        outer_layer_names = [n for n in names if not n.startswith(model_prefix)]
-        name_to_id = {}
-        logger.debug(f"outer layers are treated as head: {outer_layer_names}")
-        for n in outer_layer_names:
-            name_to_id[n] = 0
-
-        for i, per_model in enumerate(self.model):
-            per_model_prefix = f"{model_prefix}.{i}"
-            if not hasattr(per_model, "name_to_id"):
-                raise ValueError(f"name_to_id attribute is missing in model: {per_model.__class__.__name__}")
-            for n, layer_id in per_model.name_to_id.items():
-                full_n = f"{per_model_prefix}.{n}"
-                name_to_id[full_n] = layer_id
-
-        # double check each parameter has been assigned an id
-        for n in names:
-            assert n in name_to_id
-
-        return name_to_id
-
-
-class MultimodalFusionNER(MultimodalFusionMLP):
+class MultimodalFusionNER(BaseMultimodalFusionModel):
     """
     Use MLP to fuse different models' features (single-modal and multimodal) for NER.
     Specifically, it adapts the features of each model to specified dimensions,
@@ -274,17 +286,7 @@ class MultimodalFusionNER(MultimodalFusionMLP):
         loss_weight
             The weight of individual models.
         """
-        super().__init__(
-            prefix=prefix,
-            models=models,
-            hidden_features=hidden_features,
-            num_classes=num_classes,
-            adapt_in_features=adapt_in_features,
-            activation=activation,
-            dropout_prob=dropout_prob,
-            normalization=normalization,
-            loss_weight=None,
-        )
+        super().__init__()
         logger.debug("initializing MultimodalFusionNER")
 
         self.ner_model = None
@@ -373,7 +375,7 @@ class MultimodalFusionNER(MultimodalFusionMLP):
         return fusion_output
 
 
-class MultimodalFusionTransformer(nn.Module):
+class MultimodalFusionTransformer(BaseMultimodalFusionModel):
     """
     Use Transformer to fuse different models' features (single-modal and multimodal).
     Specifically, it adapts the features of each model to specified dimensions,
@@ -542,10 +544,6 @@ class MultimodalFusionTransformer(nn.Module):
         self.name_to_id = self.get_layer_ids()
         self.head_layer_names = [n for n, layer_id in self.name_to_id.items() if layer_id == 0]
 
-    @property
-    def label_key(self):
-        return f"{self.prefix}_{LABEL}"
-
     def forward(
         self,
         batch: dict,
@@ -580,47 +578,3 @@ class MultimodalFusionTransformer(nn.Module):
             return output
         else:
             return fusion_output
-
-    def get_layer_ids(
-        self,
-    ):
-        """
-        Assign an id to each layer. Layer ids will be used in layer-wise lr decay.
-        Basically, id gradually increases when going from the output end to
-        the input end.
-
-        It assumes that each individual model has the "name_to_id" attribute storing
-        the already computed model's layer ids. This function only collects those layer ids.
-        It also add prefixes for each model's parameter names since the fusion model wraps
-        those individual models, making the name scope changed. Configuring the optimizer
-        requires a full name of each parameter.
-
-        The layers defined in this class, e.g., head, adapter,
-        and, fusion_mlp, have id 0.
-
-        Returns
-        -------
-        A dictionary mapping the layer names (keys) to their ids (values).
-        """
-        model_prefix = "model"
-        names = [n for n, _ in self.named_parameters()]
-
-        outer_layer_names = [n for n in names if not n.startswith(model_prefix)]
-        name_to_id = {}
-        logger.debug(f"outer layers are treated as head: {outer_layer_names}")
-        for n in outer_layer_names:
-            name_to_id[n] = 0
-
-        for i, per_model in enumerate(self.model):
-            per_model_prefix = f"{model_prefix}.{i}"
-            if not hasattr(per_model, "name_to_id"):
-                raise ValueError(f"name_to_id attribute is missing in model: {per_model.__class__.__name__}")
-            for n, layer_id in per_model.name_to_id.items():
-                full_n = f"{per_model_prefix}.{n}"
-                name_to_id[full_n] = layer_id
-
-        # double check each parameter has been assigned an id
-        for n in names:
-            assert n in name_to_id
-
-        return name_to_id
