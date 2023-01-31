570816ea3f7a503abdbdf09cf99b347037e1de48
Zhiqiang Tang <zhiqiang.tang@rutgers.edu>
2023-01-30 22:18:25 -0800

    filter out unused model configs

--- multimodal/src/autogluon/multimodal/utils/model.py
+++ multimodal/src/autogluon/multimodal/utils/model.py
@@ -139,6 +139,13 @@ def select_model(
     for model_name in selected_model_names:
         logger.debug(f"model dtypes: {getattr(config.model, model_name).data_types}")
 
+    # clean up unused model configs
+    model_keys = list(config.model.keys())
+    model_keys.remove("names")
+    for model_name in model_keys:
+        if model_name not in selected_model_names:
+            delattr(config.model, model_name)
+
     return config
 
 
