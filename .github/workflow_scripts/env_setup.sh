18bdc8cbbadab7b12cf93574adee11effdb53eb2
Xingjian Shi <xshiab@connect.ust.hk>
2023-02-01 00:16:00 -0800

    add upper bound of black

--- .github/workflow_scripts/env_setup.sh
+++ .github/workflow_scripts/env_setup.sh
@@ -2,7 +2,7 @@ function setup_build_env {
     python3 -m pip install --upgrade pip
     python3 -m pip install tox
     python3 -m pip install flake8
-    python3 -m pip install black>=22.3
+    python3 -m pip install black>=22.3,<23.0
     python3 -m pip install isort>=5.10
     python3 -m pip install bandit
 }
