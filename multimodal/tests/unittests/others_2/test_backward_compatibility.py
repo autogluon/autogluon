b65d207cd934877a44fcbded118ed51ae3927841
Xingjian Shi <xshiab@connect.ust.hk>
2023-01-31 22:52:44 -0800

    Update test_backward_compatibility.py

--- multimodal/tests/unittests/others_2/test_backward_compatibility.py
+++ multimodal/tests/unittests/others_2/test_backward_compatibility.py
@@ -34,6 +34,7 @@ def test_load_old_checkpoint(cls):
         folder=save_path,
     )
     predictor = cls.load(checkpoint_path)
+    assert predictor._config.timm_image.image_size is None
     verify_predictor_save_load(predictor, dataset.test_df.sample(4), cls=cls)
 
     # continuous training
