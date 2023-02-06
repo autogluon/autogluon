fb6c7ef30043cb0768adadaa5220b290b84b8991
Alexander Shirkov <10080307+gradientsky@users.noreply.github.com>
2023-02-06 15:56:10 -0800

    [eda][build] dependencies cleanup, CI stability fixes, removed unused code (#2849)

--- eda/src/autogluon/eda/visualization/jupyter.py
+++ eda/src/autogluon/eda/visualization/jupyter.py
@@ -9,13 +9,6 @@ class JupyterMixin:
     def display_obj(obj):
         display(obj)
 
-    @staticmethod
-    def render_text(text, text_type=None):
-        if text_type in [f"h{r}" for r in range(1, 7)]:
-            display(HTML(f"<{text_type}>{text}</{text_type}>"))
-        else:
-            print(text)
-
     def render_header_if_needed(self, state, header_text):
         sample_size = state.get("sample_size", None)
         if self.headers:
