f5c4a5d1d1adf6f8827be46db69a4c79dc60378a
Xingjian Shi <xshiab@connect.ust.hk>
2023-02-06 22:16:59 -0800

    Update build_pip_install.sh

--- docs/build_pip_install.sh
+++ docs/build_pip_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 python3 -m pip uninstall -y autogluon
 python3 -m pip uninstall -y autogluon.eda
+python3 -m pip uninstall -y autogluon.vision
+python3 -m pip uninstall -y autogluon.text
 python3 -m pip uninstall -y autogluon.multimodal
 python3 -m pip uninstall -y autogluon.tabular
 python3 -m pip uninstall -y autogluon.timeseries
