deda833d64b197a22e63207bcbdf840bbe91cd09
Xingjian Shi <xshiab@connect.ust.hk>
2023-02-06 23:20:31 -0800

    Update test_predictor_regression_eval_metrics.py

--- multimodal/tests/unittests/others_2/test_predictor_regression_eval_metrics.py
+++ multimodal/tests/unittests/others_2/test_predictor_regression_eval_metrics.py
@@ -9,9 +9,10 @@ from torch import Tensor
 
 from autogluon.multimodal import MultiModalPredictor
 
+
 @pytest.mark.parametrize("eval_metric", ["spearmanr", "pearsonr"])
 def test_predictor_with_spearman_pearson_eval(eval_metric):
     train_df = load_dataset('SetFit/stsb', split='train').to_pandas()
     predictor = MultiModalPredictor(label="label", eval_metric=eval_metric)
     predictor.fit(train_df, presets="medium_quality", time_limit=5)
-
+    assert predictor._eval_metric_name == eval_metric
