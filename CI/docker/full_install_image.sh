86c73ba864917e5f40c914b6b33cd120308c8945
tonyhu <tonyhoo@users.noreply.github.com>
2023-02-15 10:44:47 -0800

    Remove text module from nightly image build (#2918)
    Co-authored-by: tonyhoo <tonyhoo0410@gmail.com>

--- CI/docker/full_install_image.sh
+++ CI/docker/full_install_image.sh
@@ -6,6 +6,5 @@ python3 -m pip install core/[all,tests]
 python3 -m pip install features/
 python3 -m pip install tabular/[all,tests]
 python3 -m pip install multimodal/[tests]
-python3 -m pip install text/[tests]
 python3 -m pip install timeseries/[all,tests]
 python3 -m pip install autogluon/
