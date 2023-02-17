26fb324e859f4f9251dd859f1d322bf8545bf737
Innixma <neerick@amazon.com>
2023-02-16 21:33:01 -0800

    Revert "Cleanup save/load code"
    This reverts commit 522166c8be3006ac93915204f314a777c1014d3b.

--- common/src/autogluon/common/loaders/load_s3.py
+++ common/src/autogluon/common/loaders/load_s3.py
@@ -2,8 +2,6 @@ import logging
 import os
 import pathlib
 
-from typing import List, Union
-
 logger = logging.getLogger(__name__)
 
 
@@ -29,85 +27,47 @@ def download(input_bucket, input_prefix, local_path):
     s3.Bucket(input_bucket).download_file(input_prefix, local_path)
 
 
-# TODO: Expand to support arbitrary list of candidate file paths
-# TODO: add local file path support
-# TODO: Consider renaming and deprecating old name
-def list_bucket_prefix_suffix_contains_s3(bucket: str,
-                                          prefix: str,
-                                          suffix: Union[str, List[str]] = None,
-                                          banned_suffixes: List[str] = None,
-                                          contains: Union[str, List[str]] = None,
-                                          banned_contains: List[str] = None) -> List[str]:
-    """
-    Returns a list of file paths within an S3 bucket that satisfies the constraints.
+def list_bucket_prefix_s3(bucket, prefix):
+    return list_bucket_prefix_suffix_s3(bucket=bucket, prefix=prefix)
 
-    Parameters
-    ----------
-    bucket : str
-        The S3 bucket to list files from.
-        You must have read permissions to the S3 bucket and its files for this function to work.
-    prefix : str
-        The string prefix to search for files within the S3 bucket. Any file outside of this prefix will not be considered.
-        For example, if `bucket='autogluon'` and `prefix='datasets/'`,
-        only files starting under `s3://autogluon/datasets/` will be considered.
-    suffix : str or List[str], default = None
-        If specified, filters files to ensure their paths end with the specified suffix (if str)
-        or at least one element of `suffix` (if list) in the post-prefix string path.
-    banned_suffixes : List[str], default = None
-        If specified, filters files to ensure their paths do not end with any element in `banned_suffixes`.
-    contains : str or List[str], default = None
-        If specified, will filter any result that doesn't contain `contains` (if str)
-        or at least one element of `contains` (if list) in the post-prefix string path.
-    banned_contains : List[str], default = None
-        If specified, filters files to ensure their paths do not contain any element in `banned_contains`.
 
-    Returns
-    -------
-    Returns a list of file paths within an S3 bucket that satisfies the constraints.
-
-    """
+def list_bucket_prefix_suffix_s3(bucket, prefix, suffix=None, banned_suffixes=None):
     import boto3
     if banned_suffixes is None:
         banned_suffixes = []
-    if banned_contains is None:
-        banned_contains = []
-    if suffix is not None and not isinstance(suffix, list):
-        suffix = [suffix]
-    if contains is not None and not isinstance(contains, list):
-        contains = [contains]
-
     s3 = boto3.resource('s3')
     my_bucket = s3.Bucket(bucket)
+    prefix = prefix
 
     files = []
     for object_summary in my_bucket.objects.filter(Prefix=prefix):
         suffix_full = object_summary.key.split(prefix, 1)[1]
         is_banned = False
-        for banned_s in banned_suffixes:
-            if suffix_full.endswith(banned_s):
+        for banned_suffix in banned_suffixes:
+            if banned_suffix in suffix_full:
                 is_banned = True
-                break
-        if is_banned:
-            continue
-        for banned_c in banned_contains:
-            if banned_c in suffix_full:
-                is_banned = True
-                break
-        if is_banned:
-            continue
-        if suffix is not None:
-            has_valid_suffix = False
-            for s in suffix:
-                if suffix_full.endswith(s):
-                    has_valid_suffix = True
-                    break
-            if not has_valid_suffix:
-                continue
-        if contains is None:
+        if (not is_banned) and ((suffix is None) or (suffix in suffix_full)):
             files.append(object_summary.key)
-        else:
-            for c in contains:
-                if c in suffix_full:
-                    files.append(object_summary.key)
-                    break
+
+    return files
+
+
+def list_bucket_prefix_suffix_contains_s3(bucket, prefix, suffix=None, banned_suffixes=None, contains=None):
+    import boto3
+    if banned_suffixes is None:
+        banned_suffixes = []
+    s3 = boto3.resource('s3')
+    my_bucket = s3.Bucket(bucket)
+    prefix = prefix
+
+    files = []
+    for object_summary in my_bucket.objects.filter(Prefix=prefix):
+        suffix_full = object_summary.key.split(prefix, 1)[1]
+        is_banned = False
+        for banned_suffix in banned_suffixes:
+            if banned_suffix in suffix_full:
+                is_banned = True
+        if (not is_banned) and ((suffix is None) or (suffix in suffix_full)) and (contains is None or contains in suffix_full):
+            files.append(object_summary.key)
+
     return files
