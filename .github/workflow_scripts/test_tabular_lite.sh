fe5a01ce2b2daf2a697961f2d259e6ae46e17e06
Yizhi Liu <liuyizhi@apache.org>
2023-03-12 15:25:19 -0700

    copy test script instead

--- .github/workflow_scripts/test_tabular_lite.sh
+++ .github/workflow_scripts/test_tabular_lite.sh
@@ -7,12 +7,12 @@ source $(dirname "$0")/env_setup.sh
 setup_build_env
 export AUTOGLUON_PACKAGE_NAME="autogluon-lite"
 
-install_local_packages "common/[tests]" "core/[all,tests]" "features/" "tabular/[all,tests]"
+#install_local_packages "common/[tests]" "core/[all,tests]" "features/" "tabular/[all,tests]"
 build_all
 
 PYODIDE_DIR=/src/pyodide
 
-DIST_DST=wheels
+DIST_DST=$PYODIDE_DIR/wheels
 mkdir -p $DIST_DST
 cp -f autogluon/dist/*.whl $DIST_DST/
 cp -f common/dist/*.whl $DIST_DST/
@@ -20,8 +20,10 @@ cp -f core/dist/*.whl $DIST_DST/
 cp -f features/dist/*.whl $DIST_DST/
 cp -f tabular/dist/*.whl $DIST_DST/
 
-#cp -f tabular/tests/regressiontests/test_tabular_lite*.py $PYODIDE_DIR/
+TEST_SCRIPT_DIR=$PYODIDE_DIR/autogluon_tests/
+mkdir -p $TEST_SCRIPT_DIR
+cp -f tabular/tests/regressiontests/*.py $TEST_SCRIPT_DIR/
 
 cd $PYODIDE_DIR
-tools/pytest_wrapper.py /src/autogluon/tabular/tests/regressiontests/test_tabular_lite2.py -v \
+tools/pytest_wrapper.py $TEST_SCRIPT_DIR/test_tabular_lite2.py -v \
   --runtime 'firefox' --runner 'playwright' --junitxml=results.xml
