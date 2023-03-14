4c3ff6f18b77d2e15101ba0ef333ce5fe759fd18
Yizhi Liu <liuyizhi@apache.org>
2023-03-14 09:42:15 -0700

    experiment using conftest.py in pyodide

--- .github/workflow_scripts/test_tabular_lite.sh
+++ .github/workflow_scripts/test_tabular_lite.sh
@@ -7,7 +7,7 @@ source $(dirname "$0")/env_setup.sh
 setup_build_env
 export AUTOGLUON_PACKAGE_NAME="autogluon-lite"
 
-install_local_packages "common/[tests]" "core/[all,tests]" "features/" "tabular/[all,tests]"
+#install_local_packages "common/[tests]" "core/[all,tests]" "features/" "tabular/[all,tests]"
 build_all
 
 PYODIDE_DIR=/src/pyodide
@@ -25,12 +25,13 @@ cp -f tabular/dist/*.whl $WHEEL_DST/
 TEST_SCRIPT_DIR=$PYODIDE_DIR/autogluon_tests/
 mkdir -p $TEST_SCRIPT_DIR
 # copy test scripts to a separate dir to avoid pytest picking up test settings that conflict with pyodide.
-cp -f tabular/tests/regressiontests/*.py $TEST_SCRIPT_DIR/
+#cp -f tabular/tests/regressiontests/*.py $TEST_SCRIPT_DIR/
+#-p no:$(pwd)/tabular/tests/conftest.py \
+#--dist-dir /src/pyodide \
+cp -rf tabular/tests $TEST_SCRIPT_DIR/
 
 cd $PYODIDE_DIR
 #tools/pytest_wrapper.py $TEST_SCRIPT_DIR/test_tabular_lite.py -v \
 #  --runtime 'firefox' --runner 'playwright' --junitxml=results.xml
-tools/pytest_wrapper.py /src/autogluon/tabular/tests/regressiontests/test_tabular_lite.py -v \
-  -p no:$(pwd)/tabular/tests/conftest.py \
-  --dist-dir /src/pyodide \
+tools/pytest_wrapper.py $TEST_SCRIPT_DIR/tests/regressiontests/test_tabular_lite.py -v \
   --runtime 'firefox' --runner 'playwright' --junitxml=results.xml --runpyodide
