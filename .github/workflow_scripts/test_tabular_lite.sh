138594080f53d991aba6341d94f2c3c859a1a714
Yizhi Liu <liuyizhi@apache.org>
2023-03-14 02:52:04 -0700

    do runpyodide

--- .github/workflow_scripts/test_tabular_lite.sh
+++ .github/workflow_scripts/test_tabular_lite.sh
@@ -31,4 +31,4 @@ cd $PYODIDE_DIR
 #tools/pytest_wrapper.py $TEST_SCRIPT_DIR/test_tabular_lite.py -v \
 #  --runtime 'firefox' --runner 'playwright' --junitxml=results.xml
 tools/pytest_wrapper.py /src/autogluon/tabular/tests/regressiontests/test_tabular_lite.py -v \
-  --runtime 'firefox' --runner 'playwright' --junitxml=results.xml
+  --runtime 'firefox' --runner 'playwright' --junitxml=results.xml --runpyodide
