4e846b80d7b796980e19488e1d4ac4b1d9d8963b
Yizhi Liu <liuyizhi@apache.org>
2023-03-13 19:03:23 -0700

    use updated pyodide container

--- CI/batch/submit-job.py
+++ CI/batch/submit-job.py
@@ -28,7 +28,7 @@ job_type_info = {
         'job_queue': 'CI-GPU'
     },
     'CI-WASM-PUSH': {
-        'job_definition': 'autogluon-ci-wasm-push:1',
+        'job_definition': 'autogluon-ci-wasm-push:2',
         'job_queue': 'CI-CPU'
     },
 }
