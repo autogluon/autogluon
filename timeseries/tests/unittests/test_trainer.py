0b77812e964d95ec23bc18aa38e96ed256b3c950
Oleksandr Shchur <shchuro@amazon.com>
2023-03-17 20:54:40 +0000

    Fix method name

--- timeseries/tests/unittests/test_trainer.py
+++ timeseries/tests/unittests/test_trainer.py
@@ -343,7 +343,7 @@ def test_when_trainer_fit_and_deleted_then_oof_predictions_can_be_loaded(temp_mo
     for m in model_names:
         oof_predictions = loaded_trainer.get_model_oof_predictions(m)
         assert isinstance(oof_predictions, TimeSeriesDataFrame)
-        loaded_trainer.score(DUMMY_TS_DATAFRAME, oof_predictions)
+        loaded_trainer.score_with_predictions(DUMMY_TS_DATAFRAME, oof_predictions)
 
 
 @pytest.mark.parametrize("failing_model", ["NaiveModel", "SeasonalNaiveModel"])
