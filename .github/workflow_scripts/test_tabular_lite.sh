663b2c9cbaf73de3bc4cbf3db2e2930c8b37adf0
Yizhi Liu <liuyizhi@apache.org>
2023-03-18 11:22:16 -0700

    [CI] Tabular regression test for Pyodide/WebAssembly (#3046)

---
+++ .github/workflow_scripts/test_tabular_lite.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+source $(dirname "$0")/env_setup.sh
+
+setup_build_env
+export AUTOGLUON_PACKAGE_NAME="autogluon-lite"
+
+install_local_packages "common/[tests]" "core/[all,tests]" "features/" "tabular/[all,tests]"
+build_pkg "common" "core" "features" "tabular" "autogluon"
+
+PYODIDE_DIR=/src/pyodide
+
+cd $PYODIDE_DIR
+tools/pytest_wrapper.py autogluon/tabular/tests/regressiontests/test_tabular_lite.py -v \
+  --runtime 'firefox' --runner 'playwright' --junitxml=results.xml --runpyodide
--- tabular/tests/conftest.py
+++ tabular/tests/conftest.py
@@ -23,19 +23,25 @@ def pytest_addoption(parser):
     parser.addoption(
         "--runregression", action="store_true", default=False, help="run regression tests"
     )
+    parser.addoption(
+        "--runpyodide", action="store_true", default=False, help="run Pyodide tests"
+    )
 
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "slow: mark test as slow to run")
     config.addinivalue_line("markers", "regression: mark test as regression test")
+    config.addinivalue_line("markers", "pyodide: mark test as pyodide test")
 
 
 def pytest_collection_modifyitems(config, items):
     skip_slow = pytest.mark.skip(reason="need --runslow option to run")
     skip_regression = pytest.mark.skip(reason="need --runregression option to run")
+    skip_pyodide = pytest.mark.skip(reason="need --runpyodide option to run")
     custom_markers = dict(
         slow=skip_slow,
-        regression=skip_regression
+        regression=skip_regression,
+        pyodide=skip_pyodide
     )
     if config.getoption("--runslow"):
         # --runslow given in cli: do not skip slow tests
@@ -43,6 +49,9 @@ def pytest_collection_modifyitems(config, items):
     if config.getoption("--runregression"):
         # --runregression given in cli: do not skip slow tests
         custom_markers.pop("regression", None)
+    if config.getoption("--runpyodide"):
+        # --runpyodide given in cli: do not skip pyodide tests
+        custom_markers.pop("pyodide", None)
 
     for item in items:
         for marker in custom_markers:
