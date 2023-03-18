9753df5a4bcde29eeb7869d00b102141d64741c6
Alexander Shirkov <ashyrkou@amazon.com>
2023-03-17 17:46:13 -0700

    explain improvements, updated waterfall to the modern API

--- eda/src/autogluon/eda/analysis/explain.py
+++ eda/src/autogluon/eda/analysis/explain.py
@@ -1,4 +1,5 @@
 import logging
+import warnings
 from typing import List, Optional
 
 import pandas as pd
@@ -118,7 +119,9 @@ class ShapAnalysis(AbstractAnalysis):
                 predicted_class = args.model.predict(_row).iloc[0]
             ag_wrapper = _ShapAutoGluonWrapper(args.model, args.train_data.columns, predicted_class)
             explainer = shap.KernelExplainer(ag_wrapper.predict_proba, baseline)
-            ke_shap_values = explainer.shap_values(_row[args.train_data.columns], silent=True)
+            with warnings.catch_warnings():
+                warnings.filterwarnings("ignore")
+                ke_shap_values = explainer.shap_values(_row[args.train_data.columns], silent=True)
             shap_data.append(
                 AnalysisState(
                     row=_row,
