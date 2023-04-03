a4199359f2f876788e4581e966caf1c7f5bacc45
FANGAreNotGnu <kevinfang97@gmail.com>
2023-04-03 12:25:41 -0700

    fix yolox-m and add yolox-nano

---
+++ multimodal/src/autogluon/multimodal/configs/pretrain/detection/yolox/yolox_nano_8x8_300e_coco.py
@@ -0,0 +1,28 @@
+_base_ = "./yolox_s_8x8_300e_coco.py"
+
+
+# model settings
+model = dict(
+    backbone=dict(deepen_factor=0.33, widen_factor=0.25, use_depthwise=True),
+    neck=dict(
+        in_channels=[64, 128, 256],
+        out_channels=64,
+        num_csp_blocks=1,
+        use_depthwise=True),
+    bbox_head=dict(in_channels=64, feat_channels=64, use_depthwise=True))
+
+test_pipeline = [
+    dict(type="LoadImageFromFile"),
+    dict(
+        type="MultiScaleFlipAug",
+        img_scale=(416, 416),  # Use 416x416 for inference
+        flip=False,
+        transforms=[
+            dict(type="Resize", keep_ratio=True),
+            dict(type="RandomFlip"),
+            dict(type="Pad", pad_to_square=True, pad_val=dict(img=(114.0, 114.0, 114.0))),
+            dict(type="DefaultFormatBundle"),
+            dict(type="Collect", keys=["img"]),
+        ],
+    ),
+]
--- multimodal/src/autogluon/multimodal/models/mmdet_image.py
+++ multimodal/src/autogluon/multimodal/models/mmdet_image.py
@@ -81,6 +81,7 @@ class MMDetAutoModelForObjectDetection(nn.Module):
         self.checkpoint = None
         self.checkpoint_name = checkpoint_name
         self.config_file = config_file
+        self.classes = classes
 
         if output_bbox_format.lower() in BBOX_FORMATS:
             self.output_bbox_format = output_bbox_format.lower()
@@ -183,6 +184,11 @@ class MMDetAutoModelForObjectDetection(nn.Module):
                 "url": "https://automl-mm-bench.s3.amazonaws.com/voc_script/faster_rcnn_r50_fpn_1x_voc0712_20220320_192712-54bef0f3.pth",
                 "config_file": os.path.join(mmdet_configs_dir, "voc", "faster_rcnn_r50_fpn_1x_voc0712.py"),
             },
+            "yolox_nano_8x8_300e_coco": {
+                "url": "https://github.com/Megvii-BaseDetection/YOLOX/releases/download/0.1.1rc0/yolox_nano.pth",
+                "config_file": os.path.join(mmdet_configs_dir, "yolox", "yolox_nano_8x8_300e_coco.py"),
+                "source": "MegVii",
+            },
             "yolox_tiny_8x8_300e_coco": {
                 "url": "https://download.openmmlab.com/mmdetection/v2.0/yolox/yolox_tiny_8x8_300e_coco/yolox_tiny_8x8_300e_coco_20211124_171234-b4047906.pth",
                 "config_file": os.path.join(mmdet_configs_dir, "yolox", "yolox_tiny_8x8_300e_coco.py"),
@@ -194,6 +200,7 @@ class MMDetAutoModelForObjectDetection(nn.Module):
             "yolox_m_8x8_300e_coco": {
                 "url": "https://github.com/Megvii-BaseDetection/YOLOX/releases/download/0.1.1rc0/yolox_m.pth",  # Megvii weight, need more verifications
                 "config_file": os.path.join(mmdet_configs_dir, "yolox", "yolox_m_8x8_300e_coco.py"),
+                "source": "MegVii",
             },
             "yolox_l_8x8_300e_coco": {
                 "url": "https://download.openmmlab.com/mmdetection/v2.0/yolox/yolox_l_8x8_300e_coco/yolox_l_8x8_300e_coco_20211126_140236-d3bd2b23.pth",
@@ -220,6 +227,8 @@ class MMDetAutoModelForObjectDetection(nn.Module):
                 checkpoint_file = download(
                     url=AG_CUSTOM_MODELS[checkpoint_name]["url"],
                 )
+                if "source" in AG_CUSTOM_MODELS[checkpoint_name] and AG_CUSTOM_MODELS[checkpoint_name]["source"] == "MegVii":
+                    checkpoint_file = self.convert_megvii_yolox(checkpoint_file)
             else:
                 # download config and checkpoint files using openmim
                 checkpoint_file = mimdownload(package="mmdet", configs=[checkpoint_name], dest_root=".")[0]
@@ -420,3 +429,131 @@ class MMDetAutoModelForObjectDetection(nn.Module):
         for n, _ in self.named_parameters():
             name_to_id[n] = value_to_id[name_to_value[n]]
         return name_to_id
+
+    def convert_megvii_yolox(self, source_path):
+        """
+        Convert YOLOX in megvii naming to mmdetection naming.
+        Using code script from: https://github.com/haiyang-tju/dl_tools/blob/master/megvii_nano_2_mmdet.py
+        """
+        sd = source_path
+
+        model_dict = torch.load(sd, map_location=torch.device('cpu'))
+        if "state_dict" in model_dict:
+            model_dict = model_dict["state_dict"]
+        if "model" in model_dict:
+            model_dict = model_dict["model"]
+
+        new_dict = dict()
+        for k, v in model_dict.items():
+            new_k = k
+
+            if "backbone.backbone." in k:
+                new_k = k.replace("backbone.backbone.", "backbone.")
+            if "backbone.dark2." in new_k:
+                new_k = new_k.replace("backbone.dark2.", "backbone.stage1.")
+            if "backbone.dark3." in new_k:
+                new_k = new_k.replace("backbone.dark3.", "backbone.stage2.")
+            if "backbone.dark4." in new_k:
+                new_k = new_k.replace("backbone.dark4.", "backbone.stage3.")
+            if "backbone.dark5." in new_k:
+                new_k = new_k.replace("backbone.dark5.", "backbone.stage4.")
+            if "dconv." in new_k:
+                new_k = new_k.replace("dconv.", "depthwise_conv.")
+            if "pconv." in new_k:
+                new_k = new_k.replace("pconv.", "pointwise_conv.")
+            if "backbone.stage1.1.conv1." in new_k:
+                new_k = new_k.replace("backbone.stage1.1.conv1.", "backbone.stage1.1.main_conv.")
+            if "backbone.stage1.1.conv2." in new_k:
+                new_k = new_k.replace("backbone.stage1.1.conv2.", "backbone.stage1.1.short_conv.")
+            if "backbone.stage1.1.conv3." in new_k:
+                new_k = new_k.replace("backbone.stage1.1.conv3.", "backbone.stage1.1.final_conv.")
+            if ".m." in new_k:
+                new_k = new_k.replace(".m.", ".blocks.")
+            if "backbone.stage2.1.conv1." in new_k:
+                new_k = new_k.replace("backbone.stage2.1.conv1.", "backbone.stage2.1.main_conv.")
+            if "backbone.stage2.1.conv2." in new_k:
+                new_k = new_k.replace("backbone.stage2.1.conv2.", "backbone.stage2.1.short_conv.")
+            if "backbone.stage2.1.conv3." in new_k:
+                new_k = new_k.replace("backbone.stage2.1.conv3.", "backbone.stage2.1.final_conv.")
+            if "backbone.stage3.1.conv1." in new_k:
+                new_k = new_k.replace("backbone.stage3.1.conv1.", "backbone.stage3.1.main_conv.")
+            if "backbone.stage3.1.conv2." in new_k:
+                new_k = new_k.replace("backbone.stage3.1.conv2.", "backbone.stage3.1.short_conv.")
+            if "backbone.stage3.1.conv3." in new_k:
+                new_k = new_k.replace("backbone.stage3.1.conv3.", "backbone.stage3.1.final_conv.")
+            if "backbone.stage4.2.conv1." in new_k:
+                new_k = new_k.replace("backbone.stage4.2.conv1.", "backbone.stage4.2.main_conv.")
+            if "backbone.stage4.2.conv2." in new_k:
+                new_k = new_k.replace("backbone.stage4.2.conv2.", "backbone.stage4.2.short_conv.")
+            if "backbone.stage4.2.conv3." in new_k:
+                new_k = new_k.replace("backbone.stage4.2.conv3.", "backbone.stage4.2.final_conv.")
+            if "backbone.lateral_conv0." in new_k:
+                new_k = new_k.replace("backbone.lateral_conv0.", "neck.reduce_layers.0.")
+            if "backbone.reduce_conv1." in new_k:
+                new_k = new_k.replace("backbone.reduce_conv1.", "neck.reduce_layers.1.")
+            if "backbone.C3_p4." in new_k:
+                new_k = new_k.replace("backbone.C3_p4.", "neck.top_down_blocks.0.")
+            if "neck.top_down_blocks.0.conv1." in new_k:
+                new_k = new_k.replace("neck.top_down_blocks.0.conv1.", "neck.top_down_blocks.0.main_conv.")
+            if "neck.top_down_blocks.0.conv2." in new_k:
+                new_k = new_k.replace("neck.top_down_blocks.0.conv2.", "neck.top_down_blocks.0.short_conv.")
+            if "neck.top_down_blocks.0.conv3." in new_k:
+                new_k = new_k.replace("neck.top_down_blocks.0.conv3.", "neck.top_down_blocks.0.final_conv.")
+            if "backbone.C3_p3." in new_k:
+                new_k = new_k.replace("backbone.C3_p3.", "neck.top_down_blocks.1.")
+            if "neck.top_down_blocks.1.conv1." in new_k:
+                new_k = new_k.replace("neck.top_down_blocks.1.conv1.", "neck.top_down_blocks.1.main_conv.")
+            if "neck.top_down_blocks.1.conv2." in new_k:
+                new_k = new_k.replace("neck.top_down_blocks.1.conv2.", "neck.top_down_blocks.1.short_conv.")
+            if "neck.top_down_blocks.1.conv3." in new_k:
+                new_k = new_k.replace("neck.top_down_blocks.1.conv3.", "neck.top_down_blocks.1.final_conv.")
+
+            if "backbone.bu_conv2." in new_k:
+                new_k = new_k.replace("backbone.bu_conv2.", "neck.downsamples.0.")
+            if "backbone.bu_conv1." in new_k:
+                new_k = new_k.replace("backbone.bu_conv1.", "neck.downsamples.1.")
+
+            if "backbone.C3_n3." in new_k:
+                new_k = new_k.replace("backbone.C3_n3.", "neck.bottom_up_blocks.0.")
+            if "neck.bottom_up_blocks.0.conv1." in new_k:
+                new_k = new_k.replace("neck.bottom_up_blocks.0.conv1.", "neck.bottom_up_blocks.0.main_conv.")
+            if "neck.bottom_up_blocks.0.conv2." in new_k:
+                new_k = new_k.replace("neck.bottom_up_blocks.0.conv2.", "neck.bottom_up_blocks.0.short_conv.")
+            if "neck.bottom_up_blocks.0.conv3." in new_k:
+                new_k = new_k.replace("neck.bottom_up_blocks.0.conv3.", "neck.bottom_up_blocks.0.final_conv.")
+            if "backbone.C3_n4." in new_k:
+                new_k = new_k.replace("backbone.C3_n4.", "neck.bottom_up_blocks.1.")
+            if "neck.bottom_up_blocks.1.conv1." in new_k:
+                new_k = new_k.replace("neck.bottom_up_blocks.1.conv1.", "neck.bottom_up_blocks.1.main_conv.")
+            if "neck.bottom_up_blocks.1.conv2." in new_k:
+                new_k = new_k.replace("neck.bottom_up_blocks.1.conv2.", "neck.bottom_up_blocks.1.short_conv.")
+            if "neck.bottom_up_blocks.1.conv3." in new_k:
+                new_k = new_k.replace("neck.bottom_up_blocks.1.conv3.", "neck.bottom_up_blocks.1.final_conv.")
+
+            if "head.stems." in new_k:
+                new_k = new_k.replace("head.stems.", "neck.out_convs.")
+            if "head.cls_convs." in new_k:
+                new_k = new_k.replace("head.cls_convs.", "bbox_head.multi_level_cls_convs.")
+            if "head.reg_convs." in new_k:
+                new_k = new_k.replace("head.reg_convs.", "bbox_head.multi_level_reg_convs.")
+            if "head.cls_preds." in new_k:
+                new_k = new_k.replace("head.cls_preds.", "bbox_head.multi_level_conv_cls.")
+            if "head.reg_preds." in new_k:
+                new_k = new_k.replace("head.reg_preds.", "bbox_head.multi_level_conv_reg.")
+            if "head.obj_preds." in new_k:
+                new_k = new_k.replace("head.obj_preds.", "bbox_head.multi_level_conv_obj.")
+
+            if "bbox_head.multi_level_conv_cls." in new_k:
+                if self.classes:
+                    new_dict[new_k] = v[:len(self.classes), ...]  # there take the num_classes
+                else:
+                    new_dict[new_k] = v
+            else:
+                new_dict[new_k] = v
+
+        data = {'state_dict': new_dict}
+
+        target_directory = os.path.splitext(sd)[0] + f'_cvt.pth'
+        torch.save(data, target_directory)
+
+        return target_directory
