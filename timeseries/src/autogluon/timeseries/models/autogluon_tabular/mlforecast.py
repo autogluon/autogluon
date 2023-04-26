1efba55c5ec458c004d0ad664b9b8774a347617d
Oleksandr Shchur <shchuro@amazon.com>
2023-04-26 10:20:30 +0000

    Add type hints

--- timeseries/src/autogluon/timeseries/models/autogluon_tabular/mlforecast.py
+++ timeseries/src/autogluon/timeseries/models/autogluon_tabular/mlforecast.py
@@ -1,6 +1,7 @@
 import logging
 from typing import Optional
 
+import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator
 
@@ -23,20 +24,20 @@ class TabularEstimator(BaseEstimator):
         self.predictor_init_kwargs = predictor_init_kwargs if predictor_init_kwargs is not None else {}
         self.predictor_fit_kwargs = predictor_fit_kwargs if predictor_fit_kwargs is not None else {}
 
-    def get_params(self, deep: bool = True):
+    def get_params(self, deep: bool = True) -> dict:
         return {
             "predictor_init_kwargs": self.predictor_init_kwargs,
             "predictor_fit_kwargs": self.predictor_fit_kwargs,
         }
 
-    def fit(self, X, y):
+    def fit(self, X: pd.DataFrame, y: pd.Series) -> "TabularEstimator":
         assert isinstance(X, pd.DataFrame) and isinstance(y, pd.Series)
         df = pd.concat([X, y.rename(self._label_column_name).to_frame()], axis=1)
         self.predictor = TabularPredictor(label=self._label_column_name, **self.predictor_init_kwargs)
         self.predictor.fit(df, **self.predictor_fit_kwargs)
         return self
 
-    def predict(self, X):
+    def predict(self, X: pd.DataFrame) -> np.ndarray:
         assert isinstance(X, pd.DataFrame)
         return self.predictor.predict(X)
 
