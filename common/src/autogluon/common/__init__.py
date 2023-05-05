168ea9855e117edf88bc07c7091f69c25ac41fe2
Weisu Yin <weisy@amazon.com>
2023-05-05 23:01:53 +0000

    address comments

--- common/src/autogluon/common/__init__.py
+++ common/src/autogluon/common/__init__.py
@@ -1,7 +1,6 @@
 from .version import __version__
 
 from .utils.log_utils import _add_stream_handler, fix_logging_if_kaggle as __fix_logging_if_kaggle
-from .space import Space, Categorical, Real, Int, Bool
 
 # Fixes logger in Kaggle to show logs in notebook.
 __fix_logging_if_kaggle()
