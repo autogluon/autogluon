16b91242fe3ee1f379274d1e81cf42f2a7e9dbf1
yongxinw <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>
2023-05-09 22:18:23 +0000

    adding ner/detection learner

---
+++ multimodal/src/autogluon/multimodal/learners/object_detection_learner.py
@@ -0,0 +1,33 @@
+from typing import Optional
+from .default_learner import DefaultLearner
+
+
+class ObjectDetectionLearner(DefaultLearner):
+    def __init__(
+        self,
+        problem_type: str,
+        label: Optional[str] = None,
+        # column_types: Optional[dict] = None,  # this is now inferred in learner
+        # query: Optional[Union[str, List[str]]] = None,
+        # response: Optional[Union[str, List[str]]] = None,
+        # match_label: Optional[Union[int, str]] = None,
+        # pipeline: Optional[str] = None,
+        presets: Optional[str] = None,
+        eval_metric: Optional[str] = None,  # this is now inferred in learner
+        path: Optional[str] = None,
+        verbosity: Optional[int] = 2,
+        num_classes: Optional[int] = None,
+        classes: Optional[list] = None,
+        enable_progress_bar: Optional[bool] = True,
+    ):
+        super().__init__(
+            problem_type=problem_type,
+            label=label,
+            presets=presets,
+            eval_metric=eval_metric,
+            path=path,
+            verbosity=verbosity,
+            num_classes=num_classes,
+            classes=classes,
+            enable_progress_bar=enable_progress_bar,
+        )
