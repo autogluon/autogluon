0c266eefe35c721a81cb3dc08bf56d82a0d6e5d8
FANGAreNotGnu <kevinfang97@gmail.com>
2023-05-25 20:29:23 +0000

    ../../../multimodal/src/autogluon/multimodal/models/mmdet_image.py

--- multimodal/src/autogluon/multimodal/models/mmdet_image.py
+++ multimodal/src/autogluon/multimodal/models/mmdet_image.py
@@ -29,7 +29,6 @@ except ImportError as e:
     mmdet = None
 
 from ..constants import BBOX, BBOX_FORMATS, COLUMN, IMAGE, IMAGE_VALID_NUM, LABEL, XYXY
-
 from .utils import freeze_model_layers, lookup_mmdet_config, update_mmdet_config
 
 logger = logging.getLogger(__name__)
