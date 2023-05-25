f43099d102cafc3bd73b6680a5bd0eeeeb99f508
FANGAreNotGnu <kevinfang97@gmail.com>
2023-05-25 20:04:10 +0000

    remove deprecated codes in mmcv.py

--- multimodal/src/autogluon/multimodal/utils/mmcv.py
+++ multimodal/src/autogluon/multimodal/utils/mmcv.py
@@ -1,8 +1,6 @@
 import functools
-from collections import OrderedDict
-from typing import Callable, Type, Union
+from typing import Callable
 
-import numpy as np
 import torch
 
 
@@ -32,28 +30,7 @@ class CollateMMDet:
         self.samples_per_gpu = samples_per_gpu
 
     def __call__(self, x):
-        ret = collate(x)
-
-        return ret
-
-        if isinstance(ret["inputs"], list):
-            img_metas = ret["img_metas"][0]
-            imgs = [ret["inputs"][0][0].float()]
-            return dict(imgs=imgs, img_metas=img_metas)
-        else:
-            raise NotImplementedError
-            img_metas = ret["img_metas"][0]
-            img = ret["img"][0].float()
-            batch_size = img.shape[0]
-            gt_bboxes = []
-            gt_labels = []
-            for i in range(batch_size):
-                gt_bboxes.append(ret["gt_bboxes"][0][i].float())
-                gt_labels.append(ret["gt_labels"][0][i].long())
-
-            return dict(img=img, img_metas=img_metas, gt_bboxes=gt_bboxes, gt_labels=gt_labels)
-
-        return ret
+        return collate(x)
 
 
 class CollateMMOcr:
