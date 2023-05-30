d9e7beca5a9df9cbe8543f5750f02a47201403e8
Zhiqiang Tang <zhiqiang.tang@rutgers.edu>
2023-05-30 10:32:41 -0700

    [AutoMM] Support loading specific checkpoints (#3244)

--- multimodal/tests/unittests/predictor/test_predictor.py
+++ multimodal/tests/unittests/predictor/test_predictor.py
@@ -708,3 +708,21 @@ def test_fit_with_data_path():
     predictor = MultiModalPredictor(label="label")
     predictor.fit(train_data=train_csv_file, time_limit=0)
     predictor.fit(train_data=train_csv_file, tuning_data=train_csv_file, time_limit=0)
+
+
+def test_load_ckpt():
+    download_dir = "./"
+    train_data, test_data = shopee_dataset(download_dir=download_dir)
+    predictor = MultiModalPredictor(label="label")
+    predictor.fit(train_data=train_data, time_limit=20)
+    src_file = os.path.join(predictor.path, "model.ckpt")
+    dest_file = os.path.join(predictor.path, "epoch=8-step=18.ckpt")
+    shutil.copy(src_file, dest_file)
+    loaded_predictor = MultiModalPredictor.load(path=dest_file)
+
+    predictions = predictor.predict(test_data, as_pandas=False)
+    predictions2 = loaded_predictor.predict(test_data, as_pandas=False)
+    npt.assert_equal(predictions, predictions2)
+    predictions_prob = predictor.predict_proba(test_data, as_pandas=False)
+    predictions2_prob = loaded_predictor.predict_proba(test_data, as_pandas=False)
+    npt.assert_equal(predictions_prob, predictions2_prob)
