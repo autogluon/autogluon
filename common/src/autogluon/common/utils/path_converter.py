55a256b7e971fb4a31d9020bbac3a6ad3ba2e68a
Weisu Yin <weisuyin96@gmail.com>
2023-05-30 18:45:13 +0000

    add unit tests

--- common/src/autogluon/common/utils/path_converter.py
+++ common/src/autogluon/common/utils/path_converter.py
@@ -1,23 +1,22 @@
-from pathlib import PurePosixPath, PureWindowsPath
-
 import platform
+from pathlib import PurePosixPath, PureWindowsPath
 
 
 class PathConverter:
     """Util class to convert a given path to a path to the corresponding OS"""
-    
+
     @staticmethod
     def _is_windows():
         return platform.system() == "Windows"
-    
+
     @staticmethod
     def to_windows(path: str) -> str:
         return str(PureWindowsPath(PurePosixPath(path)))
-    
+
     @staticmethod
     def to_posix(path: str) -> str:
         return str(PurePosixPath(PureWindowsPath(path)))
-    
+
     @staticmethod
     def to_current(path: str) -> str:
         return PathConverter.to_windows(path) if PathConverter._is_windows() else PathConverter.to_posix(path)
---
+++ common/tests/unittests/test_path_converter.py
@@ -0,0 +1,41 @@
+from unittest.mock import patch
+
+import pytest
+
+from autogluon.common.utils.path_converter import PathConverter
+
+
+@pytest.mark.parametrize(
+    "og_path, expected_path",
+    [
+        ("dummy", "dummy"),
+        ("dummy/foo", "dummy\\foo")
+    ] 
+)
+def test_to_windows(og_path, expected_path):
+    assert PathConverter.to_windows(og_path) == expected_path
+    
+
+@pytest.mark.parametrize(
+    "og_path, expected_path",
+    [
+        ("dummy", "dummy"),
+        ("dummy\\foo", "dummy/foo"),
+    ] 
+)
+def test_to_posix(og_path, expected_path):
+    assert PathConverter.to_posix(og_path) == expected_path
+    
+
+@pytest.mark.parametrize(
+    "og_path, expected_path, mock_system",
+    [
+        ("dummy", "dummy", "Windows"),
+        ("dummy/foo", "dummy\\foo", "Windows"),
+        ("dummy", "dummy", "Linux"),
+        ("dummy\\foo", "dummy/foo", "Linux"),
+    ] 
+)
+def test_to_current(og_path, expected_path, mock_system):
+    with patch("platform.system", return_value=mock_system):
+        assert PathConverter.to_current(og_path) == expected_path
