a4677dbb7b418091d5cb0fcfe0efdf805512b408
innixma <innixma@gmail.com>
2023-06-04 03:21:49 +0000

    Add TabPFN

--- core/src/autogluon/core/utils/utils.py
+++ core/src/autogluon/core/utils/utils.py
@@ -4,7 +4,7 @@ import pickle
 import time
 import random
 import sys
-from typing import Callable, List, Tuple
+from typing import Callable, List, Union, Tuple
 
 import numpy as np
 import pandas as pd
@@ -394,7 +394,7 @@ def generate_train_test_split_combined(data: DataFrame,
 def generate_train_test_split(X: DataFrame,
                               y: Series,
                               problem_type: str,
-                              test_size: float = 0.1,
+                              test_size: Union[float, int] = 0.1,
                               random_state=0,
                               min_cls_count_train=1) -> Tuple[DataFrame, DataFrame, Series, Series]:
     """
@@ -414,6 +414,9 @@ def generate_train_test_split(X: DataFrame,
     test_size : float, default = 0.1
         The proportion of data to use for the test set.
         The remaining (1 - test_size) of data will be used for the training set.
+    test_size : float or int, default = 0.1
+        If float, should be between 0.0 and 1.0 and represent the proportion of the dataset to include in the test split.
+        If int, represents the absolute number of test samples.
     random_state : int, default = 0
         Random seed to use during the split.
     min_cls_count_train : int, default = 1
@@ -428,8 +431,11 @@ def generate_train_test_split(X: DataFrame,
         The train_data and test_data after performing the split, separated into X and y.
 
     """
-    if (test_size <= 0.0) or (test_size >= 1.0):
-        raise ValueError("fraction of data to hold-out must be specified between 0 and 1")
+    if isinstance(test_size, float):
+        if (test_size <= 0.0) or (test_size >= 1.0):
+            raise ValueError("fraction of data to hold-out must be specified between 0 and 1")
+    elif isinstance(test_size, int):
+        assert test_size > 0
     valid_problem_types = [BINARY, MULTICLASS, REGRESSION, SOFTCLASS, QUANTILE]
     assert problem_type in valid_problem_types, f'Unknown problem type "{problem_type}" | Valid problem types: {valid_problem_types}'
 
