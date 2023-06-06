5bb72a48ddd7e0619332022cde2986c4f1c77fb4
Weisu Yin <weisuyin96@gmail.com>
2023-06-06 23:34:05 +0000

    fix

--- .github/workflow_scripts/env_setup.sh
+++ .github/workflow_scripts/env_setup.sh
@@ -5,6 +5,7 @@ function setup_build_env {
     python3 -m pip install "black>=22.3,<23.0"
     python3 -m pip install isort>=5.10
     python3 -m pip install bandit
+    python3 -m pip install packaging
 }
 
 function setup_build_contrib_env {
