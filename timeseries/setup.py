674c2bb80e82df2556712eea70f55d058a331738
Oleksandr Shchur <shchuro@amazon.com>
2023-06-07 15:44:29 +0000

    Update SF version range

--- timeseries/setup.py
+++ timeseries/setup.py
@@ -32,7 +32,7 @@ install_requires = [
     "torch>=1.9,<1.14",
     "pytorch-lightning>=1.7.4,<1.10.0",
     "networkx",  # version range defined in `core/_setup_utils.py`
-    "statsforecast>=1.5.0,<1.6",
+    "statsforecast>=1.4.0,<1.5",
     "mlforecast>=0.7.0,<0.7.4",
     "tqdm",  # version range defined in `core/_setup_utils.py`
     "ujson>=5,<6",  # needed to silence GluonTS warning
