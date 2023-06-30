8f48830aa267f5e11c09251772964f44155b08b7
Weisu Yin <weisy@amazon.com>
2023-06-30 17:52:24 +0000

    lower cap

--- core/setup.py
+++ core/setup.py
@@ -52,8 +52,8 @@ extras_require = {
         # version range set to align with ray's updated version range:
         # https://github.com/ray-project/ray/blob/master/python/setup.py#L259-L261
         # TODO: remove the pin once issue resolved
-        "grpcio >= 1.42.0, <= 1.50.0",
-        "pydantic<2.0"  # https://github.com/ray-project/ray/issues/36990
+        "grpcio>=1.42.0,<=1.50.0",
+        "pydantic>=1.10.4,<2.0",  # https://github.com/ray-project/ray/issues/36990
     ],
     "raytune": [
         "ray[tune]>=2.3,<2.4",
