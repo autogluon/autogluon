7393a19d487e6ae2758611ff10aa714ac001559e
Zhiqiang Tang <zhiqiang.tang@rutgers.edu>
2023-06-30 10:26:54 -0700

    [Merge after v0.8.1][AutoMM]  Unpin sentencepiece (#3368)

--- core/setup.py
+++ core/setup.py
@@ -53,7 +53,6 @@ extras_require = {
         # https://github.com/ray-project/ray/blob/master/python/setup.py#L259-L261
         # TODO: remove the pin once issue resolved
         "grpcio >= 1.42.0, <= 1.50.0",
-        "protobuf>=3.15.3,<=3.20.2",  # pin version to allign with transformers
     ],
     "raytune": [
         "ray[tune]>=2.3,<2.4",
