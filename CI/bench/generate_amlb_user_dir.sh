90c1d502455c096ccc30ef7572bb7904ab265c87
Weisu Yin <weisuyin96@gmail.com>
2023-07-18 00:28:24 +0000

    workflow

--- CI/bench/generate_amlb_user_dir.sh
+++ CI/bench/generate_amlb_user_dir.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-MODULE=$1
-REPOSITORY=$2
-BRANCH=$3
-PR_NUMBER=$4
+REPOSITORY=$1
+BRANCH=$2
+PR_NUMBER=$3
 
-if [ $MODULE == "tabular" ]
-then
-    python tabular/generate_framework.py --repository $REPOSITORY --branch $BRANCH
-    aws s3 cp --recursive $(dirname "$0")/tabular/amlb_user_dir/* s3://autogluon-ci-benchmark/configs/$MODULE/$PR_NUMBER/
+# generate tabular configs
+python tabular/generate_framework.py --repository $REPOSITORY --branch $BRANCH
+aws s3 cp --recursive $(dirname "$0")/tabular/amlb_user_dir/* s3://autogluon-ci-benchmark/configs/$MODULE/$PR_NUMBER/
