5aea12fb98fd38f3218f59a89d27a34521111c4d
Weisu Yin <weisuyin96@gmail.com>
2023-07-19 21:30:44 +0000

    fix

--- CI/bench/generate_amlb_user_dir.sh
+++ CI/bench/generate_amlb_user_dir.sh
@@ -8,8 +8,8 @@ PR_NUMBER=$3
 python $(dirname "$0")/tabular/generate_framework.py --repository $REPOSITORY --branch $BRANCH
 if [ -n "$PR_NUMBER" ]
 then
-    CONFIG_PATH=$MODULE/$PR_NUMBER/
+    CONFIG_PATH=tabular/$PR_NUMBER/
 else
-    CONFIG_PATH=$MODULE/$BRANCH/
+    CONFIG_PATH=tabular/$BRANCH/
 fi
 aws s3 cp --recursive $(dirname "$0")/tabular/amlb_user_dir/ s3://autogluon-ci-benchmark/configs/$CONFIG_PATH
