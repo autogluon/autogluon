50a88572a99a0b877b26e4ef82108f0349a02611
Weisu Yin <weisy@amazon.com>
2023-08-04 20:00:53 +0000

    fix

--- .github/workflow_scripts/run_benchmark.sh
+++ .github/workflow_scripts/run_benchmark.sh
@@ -14,7 +14,7 @@ source $(dirname "$0")/env_setup.sh
 setup_benchmark_env
 
 /bin/bash CI/bench/generate_bench_config.sh $MODULE $PRESET $BENCHMARK $TIME_LIMIT $BRANCH_OR_PR_NUMBER
-agbench run $MODULE_cloud_configs.yaml --wait
+agbench run $MODULE"_cloud_configs.yaml" --wait
 
 python CI/bench/evaulate.py --config_path ./ag_bench_runs/tabular/ --time_limit $TIME_LIMIT
 aws s3 cp --recursive ./results s3://autogluon-ci-benchmark/cleaned/$BRANCH_OR_PR_NUMBER/$SHA/
