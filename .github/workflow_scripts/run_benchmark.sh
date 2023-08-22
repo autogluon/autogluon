f974d07c7f0afa8e23c5b9f18eb6980ee9a4a94e
Shreyash Sridhar Iyengar <54525027+shreyash2106@users.noreply.github.com>
2023-08-22 16:15:44 -0400

    Dashboard CI Integration (#3457)

--- .github/workflow_scripts/run_benchmark.sh
+++ .github/workflow_scripts/run_benchmark.sh
@@ -20,3 +20,10 @@ python CI/bench/evaluate.py --config_path ./ag_bench_runs/tabular/ --time_limit 
 aws s3 cp --recursive ./results s3://autogluon-ci-benchmark/cleaned/$BRANCH_OR_PR_NUMBER/$SHA/
 aws s3 rm --recursive s3://autogluon-ci-benchmark/cleaned/$BRANCH_OR_PR_NUMBER/latest/
 aws s3 cp --recursive ./results s3://autogluon-ci-benchmark/cleaned/$BRANCH_OR_PR_NUMBER/latest/
+
+cwd=`pwd`
+ls data/results/output/openml/ag_eval/pairwise/* | grep .csv > $cwd/agg_csv.txt
+filename=`head -1 $cwd/agg_csv.txt`
+prefix=$BRANCH_OR_PR_NUMBER/$SHA
+agdash --per_dataset_csv  'data/results/output/openml/ag_eval/results_ranked_by_dataset_all.csv' --agg_dataset_csv $filename --s3_prefix benchmark-dashboard/$prefix --s3_bucket autogluon-staging --s3_region us-west-2 > $cwd/out.txt
+tail -1 $cwd/out.txt > $cwd/website.txt
