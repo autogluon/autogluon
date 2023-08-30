610a50ee77aed6bb00ba77c84c4da69a5bd1dc7c
Weisu Yin <weisy@amazon.com>
2023-08-30 19:02:24 +0000

    diable lite

--- .github/workflow_scripts/test_tabular_lite.sh
+++ .github/workflow_scripts/test_tabular_lite.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+# TODO: re-enable when pandas 2.0 supported in browser
 
-set -ex
+# set -ex
 
-source $(dirname "$0")/env_setup.sh
+# source $(dirname "$0")/env_setup.sh
 
-setup_build_env
-export AUTOGLUON_PACKAGE_NAME="autogluon-lite"
+# setup_build_env
+# export AUTOGLUON_PACKAGE_NAME="autogluon-lite"
 
-install_local_packages "common/[tests]" "core/[all,tests]" "features/" "tabular/[all,tests]"
-build_pkg "common" "core" "features" "tabular" "autogluon"
+# install_local_packages "common/[tests]" "core/[all,tests]" "features/" "tabular/[all,tests]"
+# build_pkg "common" "core" "features" "tabular" "autogluon"
 
-PYODIDE_DIR=/src/pyodide
+# PYODIDE_DIR=/src/pyodide
 
-cd $PYODIDE_DIR
-tools/pytest_wrapper.py autogluon/tabular/tests/regressiontests/test_tabular_lite.py -v \
-  --runtime 'firefox' --runner 'playwright' --junitxml=results.xml --runpyodide
+# cd $PYODIDE_DIR
+# tools/pytest_wrapper.py autogluon/tabular/tests/regressiontests/test_tabular_lite.py -v \
+#   --runtime 'firefox' --runner 'playwright' --junitxml=results.xml --runpyodide
