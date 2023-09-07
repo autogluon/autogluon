a12cbe10dd9e0fd8cc082565087047b98e974783
Weisu Yin <weisuyin96@gmail.com>
2023-09-07 21:35:27 +0000

    fix

--- core/src/autogluon/core/models/ensemble/fold_fitting_strategy.py
+++ core/src/autogluon/core/models/ensemble/fold_fitting_strategy.py
@@ -486,7 +486,7 @@ class ParallelFoldFittingStrategy(FoldFittingStrategy):
         data_mem_est = self._estimate_data_memory_usage()
         mem_available = ResourceManager.get_available_virtual_mem()
         # Train 1 fold at least as the estimation might be off
-        max_folds_to_train_with_mem = min(1, int((mem_available * self.max_memory_usage_ratio) / (model_mem_est + data_mem_est)))
+        max_folds_to_train_with_mem = max(1, int((mem_available * self.max_memory_usage_ratio) / (model_mem_est + data_mem_est)))
         num_folds_parallel = user_specified_num_folds_parallel
         if max_folds_to_train_with_mem < user_specified_num_folds_parallel:
             # If memory is not sufficient to train num_folds_parallel, reduce to max power of 2 folds that's smaller than folds_can_be_fit_in_parallel.
