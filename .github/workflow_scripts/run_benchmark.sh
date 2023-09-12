bd6811a8416a281b79427d5fcf86fefdcf5218d0
EC2 Default User <ec2-user@ip-172-31-10-49.us-west-2.compute.internal>
2023-09-12 23:05:58 +0000

    adding fixes to main

--- .github/workflow_scripts/run_benchmark.sh
+++ .github/workflow_scripts/run_benchmark.sh
@@ -32,15 +32,14 @@ fi
 python CI/bench/evaluate.py --config_path ./ag_bench_runs/tabular/ --time_limit $TIME_LIMIT --branch_name $BRANCH_OR_PR_NUMBER
 
 for file in ./results/*; do
+    echo "File Name: $file"
     # Check if the file does not start with "master"
     if [[ "$(basename "$file")" != "master"* ]]
     then
-        echo "I am not in master!!!"
         aws s3 cp "$file" "s3://autogluon-ci-benchmark/cleaned/$BRANCH_OR_PR_NUMBER/$SHA/$(basename "$file")"
         aws s3 rm --recursive s3://autogluon-ci-benchmark/cleaned/$BRANCH_OR_PR_NUMBER/latest/$(basename "$file")
         aws s3 cp --recursive ./results s3://autogluon-ci-benchmark/cleaned/$BRANCH_OR_PR_NUMBER/latest/$(basename "$file")
     else
-        echo "I am in master!!!"
         aws s3 cp "$file" "s3://autogluon-ci-benchmark/cleaned/master/$SHA/$(basename "$file")"
         aws s3 rm --recursive s3://autogluon-ci-benchmark/cleaned/master/latest/$(basename "$file")
         aws s3 cp --recursive ./results s3://autogluon-ci-benchmark/cleaned/master/latest/$(basename "$file")
