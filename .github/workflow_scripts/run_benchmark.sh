001a21876879210632e2fa799e1595424c210481
EC2 Default User <ec2-user@ip-172-31-10-49.us-west-2.compute.internal>
2023-09-13 18:29:40 +0000

    adding more changes

--- .github/workflow_scripts/run_benchmark.sh
+++ .github/workflow_scripts/run_benchmark.sh
@@ -39,12 +39,12 @@ for file in ./results/*; do
     if [[ "$(basename "$file")" != "master"* ]]
     then
         aws s3 cp "$file" "s3://autogluon-ci-benchmark/cleaned/$BRANCH_OR_PR_NUMBER/$SHA/$(basename "$file")"
-        aws s3 rm --recursive s3://autogluon-ci-benchmark/cleaned/$BRANCH_OR_PR_NUMBER/latest/$(basename "$file")
-        aws s3 cp --recursive ./results s3://autogluon-ci-benchmark/cleaned/$BRANCH_OR_PR_NUMBER/latest/$(basename "$file")
+        aws s3 rm --recursive s3://autogluon-ci-benchmark/cleaned/$BRANCH_OR_PR_NUMBER/latest/
+        aws s3 cp "$file" s3://autogluon-ci-benchmark/cleaned/$BRANCH_OR_PR_NUMBER/latest/$(basename "$file")
     else
         aws s3 cp "$file" "s3://autogluon-ci-benchmark/cleaned/master/$SHA/$(basename "$file")"
-        aws s3 rm --recursive s3://autogluon-ci-benchmark/cleaned/master/latest/$(basename "$file")
-        aws s3 cp --recursive ./results s3://autogluon-ci-benchmark/cleaned/master/latest/$(basename "$file")
+        aws s3 rm --recursive s3://autogluon-ci-benchmark/cleaned/master/latest/
+        aws s3 cp "$file" s3://autogluon-ci-benchmark/cleaned/master/latest/$(basename "$file")
     fi
 done
 
