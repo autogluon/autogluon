fed81c22d398235bd414826cde5cfa409ec4737c
Ubuntu <ubuntu@ip-172-31-9-154.us-west-2.compute.internal>
2023-09-29 23:39:21 +0000

    changing job type

--- CI/batch/submit-job.py
+++ CI/batch/submit-job.py
@@ -35,7 +35,7 @@ job_type_info = {
         'job_definition': 'autogluon-ci-wasm-push:2',
         'job_queue': 'CI-CPU'
     },
-    'CI-MULTI-GPU': {
+    'CI-MULTI-GPU-PUSH': {
         'job_definition': 'autogluon-ci-multi-gpu:3',
         'job_queue': 'CI-MULTI-CPU'
     },
