229f6680717d59e99d21bde0deeed10d59187666
Ubuntu <ubuntu@ip-172-31-9-154.us-west-2.compute.internal>
2023-10-11 17:28:45 +0000

    Addressing comments

--- multimodal/tests/conftest.py
+++ multimodal/tests/conftest.py
@@ -3,24 +3,24 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption("--runslow", action="store_true", default=False, help="run slow tests")
-    parser.addoption("--runsgpu", action="store_true", default=False, help="run single-gpu tests")
+    parser.addoption("--runonegpu", action="store_true", default=False, help="run single-gpu tests")
 
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "slow: mark test as slow to run")
-    config.addinivalue_line("markers", "sgpu: mark test as single-gpu to run")
+    config.addinivalue_line("markers", "single_gpu: mark test as single-gpu to run")
 
 
 def pytest_collection_modifyitems(config, items):
     skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-    skip_sgpu = pytest.mark.skip(reason="need --runsgpu option to run")
-    custom_markers = dict(slow=skip_slow, sgpu=skip_sgpu)
+    skip_sgpu = pytest.mark.skip(reason="need --runonegpu option to run")
+    custom_markers = dict(slow=skip_slow, single_gpu=skip_sgpu)
     if config.getoption("--runslow"):
         # --runslow given in cli: do not skip slow tests
         custom_markers.pop("slow", None)
-    if config.getoption("--runsgpu", None):
+    if config.getoption("--runonegpu", None):
         # --runslow given in cli: do not skip single-gpu tests
-        custom_markers.pop("sgpu", None)
+        custom_markers.pop("single_gpu", None)
     for item in items:
         for marker in custom_markers:
             if marker in item.keywords:
