1961171a1956a3945ee8502d09aa359f0bd3890f
Oleksandr Shchur <shchuro@amazon.com>
2023-10-18 14:51:36 +0000

    Fix typo

--- timeseries/src/autogluon/timeseries/metrics/quantile.py
+++ timeseries/src/autogluon/timeseries/metrics/quantile.py
@@ -17,9 +17,9 @@ class QuantileForecastMetric(TimeSeriesMetric):
     ) -> float:
         y_true = data_future[target]
         quantile_pred_columns = [col for col in predictions.columns if col != "mean"]
-        y_pred = predictions[quantile_pred_columns]
+        q_pred = predictions[quantile_pred_columns]
         quantile_levels = np.array([float(q) for q in quantile_pred_columns], dtype=float)
-        return self._compute_quantile_metric(y_true=y_true, y_pred=y_pred, quantile_levels=quantile_levels)
+        return self._compute_quantile_metric(y_true=y_true, q_pred=q_pred, quantile_levels=quantile_levels)
 
 
 class WQL(TimeSeriesMetric):
