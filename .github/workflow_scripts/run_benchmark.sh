21da96e69e67d299b7d6a003568526761e07a61d
Ubuntu <ubuntu@ip-172-31-9-154.us-west-2.compute.internal>
2023-10-18 22:44:13 +0000

    cleanup

--- .github/workflow_scripts/run_benchmark.sh
+++ .github/workflow_scripts/run_benchmark.sh
@@ -14,10 +14,6 @@ source $(dirname "$0")/env_setup.sh
 setup_benchmark_env
 
 /bin/bash CI/bench/generate_bench_config.sh $MODULE $PRESET $BENCHMARK $TIME_LIMIT $BRANCH_OR_PR_NUMBER
-echo "Printing All Files"
-ls
-echo "Printing Cloud Configs"
-cat $MODULE"_cloud_configs.yaml"
 agbench run $MODULE"_cloud_configs.yaml" --wait
 
 # If it is a PR, fetch the cleaned file of master-evaluation
