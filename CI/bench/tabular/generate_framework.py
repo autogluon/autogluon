95ddc7540ddd94575c3e32d3ac94f98aeec654a6
Ubuntu <ubuntu@ip-172-31-9-154.us-west-2.compute.internal>
2023-10-23 01:29:52 +0000

    Addressing comments

--- CI/bench/tabular/generate_framework.py
+++ CI/bench/tabular/generate_framework.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFo
 
 parser.add_argument("--repository", help="git repository to run autogluon on", type=str, required=True)
 parser.add_argument("--branch", help="git branch to run autogluon on", type=str, required=True)
-parser.add_argument("--folds_to_run", help="number of folds to run", type=int, required=True)
+parser.add_argument("--folds_to_run", help="number of folds to run", type=int, required=False)
 
 args = parser.parse_args()
 
@@ -20,17 +20,18 @@ framework_template_file = os.path.join(os.path.dirname(__file__), "amlb_user_dir
 framework_benchmark_file = os.path.join(os.path.dirname(framework_template_file), "frameworks_benchmark.yaml")
 constraints_file = os.path.join(os.path.dirname(__file__), "amlb_user_dir", "constraints.yaml")
 
+if folds_to_run is not None:
+    constraints = {}
+    with open(constraints_file, "r") as f:
+        constraints = yaml.safe_load(f)
+
+    for constraint in constraints.values():
+        constraint["folds"] = folds_to_run
+
+    with open(constraints_file, "w") as f:
+        yaml.safe_dump(constraints, f)
+
 frameworks = {}
-constraints = {}
-with open(constraints_file, "r") as f:
-    constraints = yaml.safe_load(f)
-
-for constraint in constraints.values():
-    constraint["folds"] = folds_to_run
-
-with open(constraints_file, "w") as f:
-    yaml.safe_dump(constraints, f)
-
 with open(framework_template_file, "r") as f:
     frameworks = yaml.safe_load(f)
 
