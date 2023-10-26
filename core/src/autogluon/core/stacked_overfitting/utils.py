dad40cd35f90555dfdc0646bab14f610468922cc
Lennart Purucker <purucker@amazon.com>
2023-10-26 11:28:13 -0700

    rename utils

--- core/src/autogluon/core/stacked_overfitting/utils.py
+++ core/src/autogluon/core/stacked_overfitting/utils.py
@@ -6,51 +6,52 @@ import pandas as pd
 logger = logging.getLogger(__name__)
 
 
-def get_non_leaking_and_leaking_model_names(leaderboard: pd.DataFrame) -> Tuple[List[str], List[str]]:
-    non_leaking = []
-    leaking = []
+def get_affected_stacked_overfitting_model_names(leaderboard: pd.DataFrame) -> Tuple[List[str], List[str]]:
+    non_affected = []
+    affected = []
     for model_name in set(leaderboard["model"]):
         # TODO: move away from using names to metadata properties (somehow).
-        if model_name.startswith("WeightedEnsemble") and model_name.endswith("L2"):
-            non_leaking.append(model_name)
+        #   - include something like linear models once we are sure that they cannot leak
+        if (model_name.startswith("WeightedEnsemble") and model_name.endswith("L2")) or model_name.endswith("L1"):
+            non_affected.append(model_name)
         else:
-            leaking.append(model_name)
+            affected.append(model_name)
 
-    return non_leaking, leaking
+    return non_affected, affected
 
 
 def get_best_val_models(leaderboard: pd.DataFrame) -> Tuple[List[str], List[str], bool]:
-    non_leaking_names, leaking_names = get_non_leaking_and_leaking_model_names(leaderboard)
+    non_affected, affected = get_affected_stacked_overfitting_model_names(leaderboard)
 
-    best_non_leaking_model = leaderboard[leaderboard["model"].isin(non_leaking_names)].sort_values(by="score_val", ascending=False).iloc[0].loc["model"]
+    best_non_affected_model = leaderboard[leaderboard["model"].isin(non_affected)].sort_values(by="score_val", ascending=False).iloc[0].loc["model"]
 
-    leaking_models_exist = len(leaking_names) > 0
-    best_leaking_model = None
-    if leaking_models_exist:
-        best_leaking_model = leaderboard[leaderboard["model"].isin(leaking_names)].sort_values(by="score_val", ascending=False).iloc[0].loc["model"]
+    affected_models_exist = len(affected) > 0
+    best_affected_model = None
+    if affected_models_exist:
+        best_affected_model = leaderboard[leaderboard["model"].isin(affected)].sort_values(by="score_val", ascending=False).iloc[0].loc["model"]
 
-    return best_non_leaking_model, best_leaking_model, leaking_models_exist
+    return best_non_affected_model, best_affected_model, affected_models_exist
 
 
-def _check_stacked_overfitting_for_models(best_non_leaking_model: List[str], best_leaking_model: List[str], leaderboard: pd.DataFrame) -> bool:
-    score_non_leaking_oof = leaderboard.loc[leaderboard["model"] == best_non_leaking_model, "score_val"].iloc[0]
-    score_non_leaking_test = leaderboard.loc[leaderboard["model"] == best_non_leaking_model, "score_test"].iloc[0]
+def _check_stacked_overfitting_for_models(best_non_affected_model: List[str], best_affected_model: List[str], leaderboard: pd.DataFrame) -> bool:
+    score_non_affected_val = leaderboard.loc[leaderboard["model"] == best_non_affected_model, "score_val"].iloc[0]
+    score_non_affected_test = leaderboard.loc[leaderboard["model"] == best_non_affected_model, "score_test"].iloc[0]
 
-    score_leaking_oof = leaderboard.loc[leaderboard["model"] == best_leaking_model, "score_val"].iloc[0]
-    score_leaking_test = leaderboard.loc[leaderboard["model"] == best_leaking_model, "score_test"].iloc[0]
+    score_affected_val = leaderboard.loc[leaderboard["model"] == best_affected_model, "score_val"].iloc[0]
+    score_affected_test = leaderboard.loc[leaderboard["model"] == best_affected_model, "score_test"].iloc[0]
 
     # l1 worse val score than l2+
-    stacked_overfitting = score_non_leaking_oof < score_leaking_oof
+    stacked_overfitting = score_non_affected_val < score_affected_val
     # l2+ worse test score than L1
-    stacked_overfitting = stacked_overfitting and (score_non_leaking_test >= score_leaking_test)
+    stacked_overfitting = stacked_overfitting and (score_non_affected_test >= score_affected_test)
 
     return stacked_overfitting
 
 
 def check_stacked_overfitting_from_leaderboard(leaderboard: pd.DataFrame) -> bool:
-    best_non_leaking_model, best_leaking_model, leaking_models_exist = get_best_val_models(leaderboard)
+    best_non_affected_model, best_affected_model, affected_models_exist = get_best_val_models(leaderboard)
 
-    if not leaking_models_exist:
+    if not affected_models_exist:
         return False
 
-    return _check_stacked_overfitting_for_models(best_non_leaking_model, best_leaking_model, leaderboard)
+    return _check_stacked_overfitting_for_models(best_non_affected_model, best_affected_model, leaderboard)
