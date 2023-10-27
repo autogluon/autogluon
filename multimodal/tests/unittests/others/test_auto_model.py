f8f17f5facb08cbc00144840f3781945370031b3
Taojiannan Yang <vectoryyyy@gmail.com>
2023-10-27 22:34:41 +0000

    cleanup

--- multimodal/tests/unittests/others/test_auto_model.py
+++ multimodal/tests/unittests/others/test_auto_model.py
@@ -2,10 +2,7 @@ import pytest
 import torch
 
 from autogluon.multimodal.constants import FEATURES, LOGITS
-from autogluon.multimodal.models import (
-    HFAutoModelForTextPrediction,
-    TimmAutoModelForImagePrediction,
-)
+from autogluon.multimodal.models import HFAutoModelForTextPrediction, TimmAutoModelForImagePrediction
 
 
 @pytest.mark.parametrize(
