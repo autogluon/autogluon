f2f7f89d641dc85d94b6f129f74c2832c6054986
innixma <innixma@gmail.com>
2023-11-08 20:06:08 +0000

    Add json save/load to S3

--- common/src/autogluon/common/loaders/load_json.py
+++ common/src/autogluon/common/loaders/load_json.py
@@ -1,12 +1,22 @@
 import json
 import logging
 
+from ..utils import s3_utils
+
 logger = logging.getLogger(__name__)
 
 
-def load(path, *, verbose=True):
+def load(path: str, *, verbose=True) -> dict:
     if verbose:
         logger.log(15, "Loading: %s" % path)
-    with open(path, "r") as f:
-        out = json.load(f)
+    is_s3_path = s3_utils.is_s3_url(path)
+    if is_s3_path:
+        import boto3
+        bucket, key = s3_utils.s3_path_to_bucket_prefix(path)
+        s3_client = boto3.client('s3')
+        result = s3_client.get_object(Bucket=bucket, Key=key)
+        out = json.loads(result["Body"].read())
+    else:
+        with open(path, "r") as f:
+            out = json.load(f)
     return out
