737aa3e31aa94a050aadf196c581274479338c2a
Ubuntu <ubuntu@ip-172-31-1-2.us-west-2.compute.internal>
2023-11-09 03:57:23 +0000

    test

--- .github/workflow_scripts/build_tabular_prediction_tutorial.sh
+++ .github/workflow_scripts/build_tabular_prediction_tutorial.sh
@@ -13,4 +13,10 @@ source $(dirname "$0")/build_doc.sh
 
 export CUDA_VISIBLE_DEVICES=0
 
+python3 -m pip3 install --upgrade pip
+# python3 -m pip install --upgrade awscli
+python3 -m pip3 install awscli==1.18.105
+python3 -m pip3 install botocore==1.17.28
+
+
 build_doc tabular $BRANCH $GIT_REPO $COMMIT_SHA $PR_NUMBER
