91205ccf1dea1cdb0ceb9dc39aea65b506fee4bb
Ubuntu <ubuntu@ip-172-31-1-2.us-west-2.compute.internal>
2023-11-14 22:36:42 +0000

    addressing comments

--- tabular/setup.py
+++ tabular/setup.py
@@ -33,7 +33,7 @@ install_requires = [
 
 extras_require = {
     "lightgbm": [
-        "lightgbm>=3.3,<4.2",  # {N+1} upper cap, where N is the latest released minor version
+        "lightgbm>=3.3,<4.2",  # <{N+1} upper cap, where N is the latest released minor version
     ],
     "catboost": [
         # CatBoost wheel build is not working correctly on darwin for CatBoost 1.2, so use old version in this case.
@@ -46,7 +46,7 @@ extras_require = {
     #  It is possibly only present on MacOS, haven't tested linux.
     # XGBoost made API breaking changes in 1.6 with custom metric and callback support, so we don't support older versions.
     "xgboost": [
-        "xgboost>=1.6,<2.1",  # {N+1} upper cap, where N is the latest released minor version
+        "xgboost>=1.6,<2.1",  # <{N+1} upper cap, where N is the latest released minor version
     ],
     "fastai": [
         "torch",  # version range defined in `core/_setup_utils.py`
