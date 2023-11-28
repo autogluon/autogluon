84e5953d19be3a4c55bfc47a54fd6dc4cdd1905c
Ubuntu <ubuntu@ip-172-31-4-25.us-west-2.compute.internal>
2023-11-28 22:31:08 +0000

    black

--- multimodal/src/autogluon/multimodal/configs/pretrain/detection/dino/dino-5scale_swin-l_8xb2-12e_coco.py
+++ multimodal/src/autogluon/multimodal/configs/pretrain/detection/dino/dino-5scale_swin-l_8xb2-12e_coco.py
@@ -1,12 +1,12 @@
-_base_ = './dino-4scale_r50_8xb2-12e_coco.py'
+_base_ = "./dino-4scale_r50_8xb2-12e_coco.py"
 
-pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_large_patch4_window12_384_22k.pth'  # noqa
+pretrained = "https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_large_patch4_window12_384_22k.pth"  # noqa
 num_levels = 5
 model = dict(
     num_feature_levels=num_levels,
     backbone=dict(
         _delete_=True,
-        type='SwinTransformer',
+        type="SwinTransformer",
         pretrain_img_size=384,
         embed_dims=192,
         depths=[2, 2, 18, 2],
@@ -15,8 +15,8 @@ model = dict(
         mlp_ratio=4,
         qkv_bias=True,
         qk_scale=None,
-        drop_rate=0.,
-        attn_drop_rate=0.,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
         drop_path_rate=0.2,
         patch_norm=True,
         out_indices=(0, 1, 2, 3),
@@ -24,7 +24,9 @@ model = dict(
         # in FPN, otherwise some parameter will not be used
         with_cp=True,
         convert_weights=True,
-        init_cfg=dict(type='Pretrained', checkpoint=pretrained)),
+        init_cfg=dict(type="Pretrained", checkpoint=pretrained),
+    ),
     neck=dict(in_channels=[192, 384, 768, 1536], num_outs=num_levels),
     encoder=dict(layer_cfg=dict(self_attn_cfg=dict(num_levels=num_levels))),
-    decoder=dict(layer_cfg=dict(cross_attn_cfg=dict(num_levels=num_levels))))
+    decoder=dict(layer_cfg=dict(cross_attn_cfg=dict(num_levels=num_levels))),
+)
