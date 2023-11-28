b6b73e3daafb4a033987ad1e018ab925769952b2
ddelange <14880945+ddelange@users.noreply.github.com>
2023-11-28 13:10:50 +0200

    Add comment

--- core/setup.py
+++ core/setup.py
@@ -47,10 +47,10 @@ install_requires = (
 
 extras_require = {
     "ray": [
-        "ray[default]>=2.6.3,<2.8",
+        "ray[default]>=2.6.3,<2.8",  # sync with common/src/autogluon/common/utils/try_import.py
     ],
     "raytune": [
-        "ray[default,tune]>=2.6.3,<2.8",
+        "ray[default,tune]>=2.6.3,<2.8",  # sync with common/src/autogluon/common/utils/try_import.py
         # TODO: consider alternatives as hyperopt is not actively maintained.
         "hyperopt>=0.2.7,<0.2.8",  # This is needed for the bayes search to work.
         # 'GPy>=1.10.0,<1.11.0'  # TODO: Enable this once PBT/PB2 are supported by ray lightning
