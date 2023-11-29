f473e33763a0d6e4b64d2ce30e94dcba75b492cb
Oleksandr Shchur <shchuro@amazon.com>
2023-11-29 20:09:09 +0100

    [core] Add async_timeout to requirements (#3782)

--- .github/workflow_scripts/env_setup.sh
+++ .github/workflow_scripts/env_setup.sh
@@ -7,7 +7,6 @@ function setup_build_env {
     python3 -m pip install bandit
     python3 -m pip install packaging
     python3 -m pip install ruff
-    python3 -m pip install async-timeout
 }
 
 function setup_build_contrib_env {
