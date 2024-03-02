df0833e344a4c0100d3f7834313bf517fe53aa85
Tony Hu <tonyhu@amazon.com>
2024-03-02 04:55:13 +0000

    use environment markers for package installation

--- core/setup.py
+++ core/setup.py
@@ -47,23 +47,20 @@ install_requires = (
     ]
 )
 
-# Check for Windows and Python 3.11
-is_windows_and_py311 = platform.system().lower() == "windows" and sys.version_info.major == 3 and sys.version_info.minor == 11
-if not is_windows_and_py311:
-    extras_require = {
+
+extras_require = {
         "ray": [
-            "ray[default]>=2.6.3,<2.7",
+            #TODO: remove this once ray release the support on windows and Python 3.11
+            "ray[default]>=2.6.3,<2.7; sys_platform != 'win32' or python_version < '3.11'",
         ],
         "raytune": [
-            "ray[default,tune]>=2.6.3,<2.7",
+            #TODO: remove this once ray release the support on windows and Python 3.11
+            "ray[default,tune]>=2.6.3,<2.7; sys_platform != 'win32' or python_version < '3.11'", 
             # TODO: consider alternatives as hyperopt is not actively maintained.
             "hyperopt>=0.2.7,<0.2.8",  # This is needed for the bayes search to work.
             # 'GPy>=1.10.0,<1.11.0'  # TODO: Enable this once PBT/PB2 are supported by ray lightning
         ],
     }
-else:
-    # we don't install ray and raytune on Windows with Python 3.11 due to issue https://github.com/autogluon/autogluon/issues/3807
-    extras_require = {}
 
 tests_require = [
     "pytest",
