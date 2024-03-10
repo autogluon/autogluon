6329df6e95b0e908b9ea46bf7efd77d252b53033
“FANGAreNotGnu” <kevinfang97@gmail.com>
2024-03-10 02:56:30 +0000

    address comments

--- examples/automm/object_detection/benchmarking.py
+++ examples/automm/object_detection/benchmarking.py
@@ -5,15 +5,16 @@ import time
 
 from autogluon.multimodal import MultiModalPredictor
 
-BENCH_ROOT = "/media/ag/data/AutoMLDetBench"
 
-def main(dataset_name, presets, seed):
-    train_path = os.path.join(BENCH_ROOT, dataset_name, "annotations", "train_train.json")
-    val_path = os.path.join(BENCH_ROOT, dataset_name, "annotations", "train_val.json")
+def main(benchmark_root, dataset_name, presets, seed):
+
+
+    train_path = os.path.join(benchmark_root, dataset_name, "annotations", "train_train.json")
+    val_path = os.path.join(benchmark_root, dataset_name, "annotations", "train_val.json")
     if not os.path.exists(val_path):
         val_path = None
         print(f"Validation path {val_path} does not exist.")
-    test_path = os.path.join(BENCH_ROOT, dataset_name, "annotations", "test.json")
+    test_path = os.path.join(benchmark_root, dataset_name, "annotations", "test.json")
 
     # Init predictor
     predictor = MultiModalPredictor(
@@ -38,12 +39,14 @@ def main(dataset_name, presets, seed):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--benchmark_root", default="/media/ag/data/AutoMLDetBench", type=str)
     parser.add_argument("-d", "--dataset_name", default=None, type=str)
     parser.add_argument("-p", "--presets", default="best_quality", type=str)
     parser.add_argument("-s", "--seed", default=0, type=int)
     args = parser.parse_args()
 
     main(
+        benchmark_root=args.benchmark_root,
         dataset_name=args.dataset_name,
         presets=args.presets,
         seed=args.seed,
