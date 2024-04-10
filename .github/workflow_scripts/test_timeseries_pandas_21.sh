9ef911701d2e49df0c1a1a64ad94a021289341ca
Oleksandr Shchur <shchuro@amazon.com>
2024-04-10 07:55:15 +0000

    Add GitHub workflow for TS/pandas 2.1

---
+++ .github/workflow_scripts/test_timeseries_pandas_21.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ex
+
+ADDITIONAL_TEST_ARGS=$1
+
+source $(dirname "$0")/env_setup.sh
+
+setup_build_env
+export CUDA_VISIBLE_DEVICES=0
+install_local_packages "common/[tests]" "core/[all,tests]" "features/" "tabular/[all,tests]" "timeseries/[all,tests]"
+python -m pip install -U pandas~=2.1.0
+
+cd timeseries/
+if [ -n "$ADDITIONAL_TEST_ARGS" ]
+then
+    python -m pytest --junitxml=results.xml --runslow "$ADDITIONAL_TEST_ARGS" tests
+else
+    python -m pytest --junitxml=results.xml --runslow tests
+fi
