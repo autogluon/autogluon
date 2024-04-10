24e6e2dbaacafcec27769238f3cef54b5f7c1b74
Oleksandr Shchur <shchuro@amazon.com>
2024-04-10 16:14:43 +0000

    Remove scale feature

--- timeseries/tests/unittests/models/test_mlforecast.py
+++ timeseries/tests/unittests/models/test_mlforecast.py
@@ -49,7 +49,7 @@ def test_when_covariates_and_features_present_then_train_and_val_dfs_have_correc
     )
     # Initialize model._target_lags and model._date_features from freq
     model.fit(train_data=data, time_limit=3)
-    train_df, val_df = model._generate_train_val_dfs(model._add_scale_as_static_feature(data))
+    train_df, val_df = model._generate_train_val_dfs(data)
     expected_num_features = (
         len(lags)
         + len(known_covariates_names)
@@ -57,7 +57,6 @@ def test_when_covariates_and_features_present_then_train_and_val_dfs_have_correc
         + len(static_features_names)
         + len(model._date_features)
         + 2  # target, item_id
-        + 2  # mean/scale of the target
     )
     # sum(differences) rows  dropped per item, prediction_length rows are reserved for validation
     expected_num_train_rows = len(data) - (sum(differences) + model.prediction_length) * data.num_items
