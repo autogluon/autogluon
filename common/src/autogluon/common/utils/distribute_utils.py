9d5a70b039a3cd0c0a6ee80e5c49a0d33df450e0
Lennart Purucker <contact@lennart-purucker.com>
2024-05-10 17:11:38 +0200

    enable distributed fold fitting with a NFS

--- common/src/autogluon/common/utils/distribute_utils.py
+++ common/src/autogluon/common/utils/distribute_utils.py
@@ -2,19 +2,33 @@ import os
 
 
 class DistributedContext:
-    """Class to manage distributed context"""
+    """Class to manage distributed context."""
 
     @staticmethod
     def get_util_path() -> str:
-        """Return the S3 path to store utils generated in distributed training"""
+        """Return the path to store utils generated in distributed training. So far only used for HPO.
+
+        Options for the environment variable `AG_UTIL_PATH`:
+            - str, an S3 path if using AWS.
+        """
         return os.environ.get("AG_UTIL_PATH")
 
     @staticmethod
     def get_model_sync_path() -> str:
-        """Return the S3 path to sync the model artifacts generated in distributed training"""
+        """Return a path to sync the model artifacts generated in distributed training.
+
+        Options for the environment variable `AG_MODEL_SYNC_PATH`:
+            - str, an S3 path if using AWS.
+        """
         return os.environ.get("AG_MODEL_SYNC_PATH")
+
+
+    @staticmethod
+    def is_shared_network_file_system() -> bool:
+        """Return if the current context is using a shared (network) file system."""
+        return os.environ.get("AG_DISTRIBUTED_MODE_NFS", False)
 
     @staticmethod
     def is_distributed_mode() -> bool:
-        """Return if the current context is in distributed mode or not"""
+        """Return if the current context is in distributed mode or not."""
         return os.environ.get("AG_DISTRIBUTED_MODE", False)
