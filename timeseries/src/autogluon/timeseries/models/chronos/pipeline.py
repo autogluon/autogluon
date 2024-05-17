[timeseries] fix off-by-one bug in Chronos inference

timeseries/src/autogluon/timeseries/models/chronos/pipeline.py
@@ -163,7 +163,7 @@ class MeanScaleUniformBins(ChronosTokenizer):
     def output_transform(self, samples: torch.Tensor, scale: torch.Tensor) -> torch.Tensor:
         scale_unsqueezed = scale.unsqueeze(-1).unsqueeze(-1)
         indices = torch.clamp(
-            samples - self.config.n_special_tokens,
+            samples - self.config.n_special_tokens - 1,
             min=0,
             max=len(self.centers) - 1,
         )