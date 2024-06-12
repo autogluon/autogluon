782522921ea92b20e927763d6817aee1f5bb9f4a
Su Zhou <zhousu@amazon.com>
2024-06-12 17:04:54 +0000

    update multimodal install

--- CI/docker/full_install_image.sh
+++ CI/docker/full_install_image.sh
@@ -11,5 +11,3 @@ python3 -m pip install autogluon/
 
 mim install "mmcv==2.1.0" --timeout 60
 python3 -m pip install --upgrade "mmdet==3.2.0"
-python3 -m pip install -U "mmengine<0.8"  # TODO: remove once compatible with the image
-# python3 -m pip install --upgrade "mmocr<1.0"  # not compatible with mmcv 2.0
