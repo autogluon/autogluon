af23aa5dc3d2922ef8bdd5d1ebdba940fc62f8f3
Oleksandr Shchur <shchuro@amazon.com>
2024-08-20 11:38:45 +0000

    Keep flake8

--- .github/workflow_scripts/env_setup.sh
+++ .github/workflow_scripts/env_setup.sh
@@ -1,6 +1,7 @@
 function setup_build_env {
     python -m pip install --upgrade pip
     python -m pip install tox
+    python -m pip install flake8
     python -m pip install bandit
     python -m pip install packaging
     python -m pip install ruff
