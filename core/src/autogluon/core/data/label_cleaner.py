17de4021b74de0cd7884743832bf1555191abea4
Su Zhou <zhousu@amazon.com>
2024-09-26 16:56:50 +0000

    fixup! ensure dtype consistency

--- core/src/autogluon/core/data/label_cleaner.py
+++ core/src/autogluon/core/data/label_cleaner.py
@@ -1,5 +1,6 @@
 import copy
 import logging
+import pandas as pd
 from typing import Union
 
 import numpy as np
@@ -138,7 +139,7 @@ class LabelCleanerMulticlass(LabelCleaner):
         Inverse transforms uncleaned internal labels to external labels.
         """
         y = self._convert_to_valid_series(y=y)
-        y = y.map(self.cat_mappings_dependent_var_uncleaned).astype('int64')
+        y = y.map(self.cat_mappings_dependent_var_uncleaned).astype(pd.Int64Dtype())
         return y
 
     # TODO: Unused? There are not many reasonable situations that seem to require this method.
