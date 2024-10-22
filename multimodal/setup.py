73c3e9432a4298f507f542bbd39b06ffa26b884f
Su Zhou <zhousu@amazon.com>
2024-10-22 19:24:47 +0000

    trigger CI

--- multimodal/setup.py
+++ multimodal/setup.py
@@ -62,7 +62,7 @@ install_requires = ag.get_dependency_version_ranges(install_requires)
 tests_require = [
     "ruff",
     "datasets>=2.10.0,<2.15.0",
-    "onnx>=1.13.0,<1.14.0",
+    "onnx>=1.13.0,<1.14.0",  # trigger
     "onnxruntime>=1.15.0,<1.16.0;platform_system=='Darwin'",
     "onnxruntime-gpu>=1.15.0,<1.16.0;platform_system!='Darwin'",
     "tensorrt>=8.6.0,<10.3;platform_system=='Linux' and python_version<'3.11'",
