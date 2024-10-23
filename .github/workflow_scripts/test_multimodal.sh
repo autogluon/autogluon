fb16acd7856ceaa131a9f88f7a1526cd4f275467
Su Zhou <zhousu@amazon.com>
2024-10-23 00:09:59 +0000

    test

--- .github/workflow_scripts/test_multimodal.sh
+++ .github/workflow_scripts/test_multimodal.sh
@@ -5,7 +5,6 @@ function test_multimodal {
     source $(dirname "$0")/env_setup.sh
 
     setup_build_env
-    setup_hf_model_mirror "$SUB_FOLDER"
     # Use all available GPUs
     unset CUDA_VISIBLE_DEVICES
     install_local_packages "common/[tests]" "core/[all,tests]" "features/"
@@ -14,8 +13,8 @@ function test_multimodal {
     cd multimodal/
     if [ -n "$ADDITIONAL_TEST_ARGS" ]
     then
-        python -m pytest --junitxml=results.xml --runslow "$ADDITIONAL_TEST_ARGS" tests/unittests/"$SUB_FOLDER"/
+        python -m pytest --junitxml=results.xml --runslow "$ADDITIONAL_TEST_ARGS" tests/unittests/"$SUB_FOLDER"
     else
-        python -m pytest --junitxml=results.xml --runslow tests/unittests/"$SUB_FOLDER"/
+        python -m pytest --junitxml=results.xml --runslow tests/unittests/"$SUB_FOLDER"
     fi   
 }
