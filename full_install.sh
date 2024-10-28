bf6ce35f64de12c7d51b2975fdb3adfcf681c091
innixma <innixma@gmail.com>
2024-10-28 22:55:37 +0000

    Fix source install venv location

--- full_install.sh
+++ full_install.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 if ! command -v uv &> /dev/null
 then
     echo "uv could not be found. Installing uv..."
-    python3 -m pip install uv
+    python -m pip install uv
 fi
 
 # Use uv to install packages
-python3 -m uv pip install --refresh -e common/[tests] -e core/[all,tests] -e features/ -e tabular/[all,tests] -e multimodal/[tests] -e timeseries/[all,tests] -e eda/ -e autogluon/
+python -m uv pip install --refresh -e common/[tests] -e core/[all,tests] -e features/ -e tabular/[all,tests] -e multimodal/[tests] -e timeseries/[all,tests] -e eda/ -e autogluon/
