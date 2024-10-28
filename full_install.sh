a08f7705f0fd96a34d2e4340609ce1fea85263be
innixma <innixma@gmail.com>
2024-10-28 23:59:17 +0000

    edit full_install.sh

--- full_install.sh
+++ full_install.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+EDITABLE="true"
+
+while test $# -gt 0
+do
+    case "$1" in
+        --non-editable) EDITABLE="false";;
+        *) echo "Error: Unused argument: $1" >&2
+           exit 1;;
+    esac
+    shift
+done
+
 # Check if uv is installed
 if ! command -v uv &> /dev/null
 then
@@ -9,9 +21,18 @@ then
 fi
 
 # Use uv to install packages
+# TODO: We should simplify this by having a single setup.py at project root, and let user call `pip install -e .`
+if [ "$EDITABLE" == "true" ]; then
+  # install common first to avoid bugs with parallelization
+  python -m uv pip install --refresh -e common/[tests]
 
-# install common first to avoid bugs with parallelization
-python -m uv pip install --refresh -e common/[tests]
+  # install the rest
+  python -m uv pip install -e core/[all,tests] -e features/ -e tabular/[all,tests] -e multimodal/[tests] -e timeseries/[all,tests] -e eda/ -e autogluon/
 
-# install the rest
-python -m uv pip install -e core/[all,tests] -e features/ -e tabular/[all,tests] -e multimodal/[tests] -e timeseries/[all,tests] -e eda/ -e autogluon/
+else
+  # install common first to avoid bugs with parallelization
+  python -m uv pip install --refresh common/[tests]
+
+  # install the rest
+  python -m uv pip install core/[all,tests] features/ tabular/[all,tests] multimodal/[tests] timeseries/[all,tests] eda/ autogluon/
+fi
