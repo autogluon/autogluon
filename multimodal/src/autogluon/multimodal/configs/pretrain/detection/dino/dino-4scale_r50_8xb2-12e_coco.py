98ca74f0b26b94cad2844a65dfccb1e384996791
Haoyang Fang <107515844+FANGAreNotGnu@users.noreply.github.com>
2024-11-07 13:52:40 -0800

    [AutoMM] Add COCO Format Saving Support and Update Object Detection I/O Handling (#3811)

--- multimodal/src/autogluon/multimodal/configs/pretrain/detection/dino/dino-4scale_r50_8xb2-12e_coco.py
+++ multimodal/src/autogluon/multimodal/configs/pretrain/detection/dino/dino-4scale_r50_8xb2-12e_coco.py
@@ -84,7 +84,7 @@ model = dict(
 # train_pipeline, NOTE the img_scale and the Pad's size_divisor is different
 # from the default setting in mmdet.
 train_pipeline = [
-    dict(type="LoadImageFromFile", backend_args={{_base_.backend_args}}),
+    dict(type="LoadImageFromFile", backend_args={{_base_.backend_args}}),  # nosec
     dict(type="LoadAnnotations", with_bbox=True),
     dict(type="RandomFlip", prob=0.5),
     dict(
