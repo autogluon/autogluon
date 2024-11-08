b3642272b4f7930aef2bec35981859bb205ebb5d
FANGAreNotGnu <kevinfang97@gmail.com>
2024-11-08 05:00:12 +0000

    fix bandit

--- .github/workflow_scripts/lint_check.sh
+++ .github/workflow_scripts/lint_check.sh
@@ -22,6 +22,6 @@ function lint_check_all {
     # lint_check tabular
 }
 
-bandit -r multimodal/src -ll --exclude-dirs multimodal/src/autogluon/multimodal/configs/pretrain
+bandit -r multimodal/src -ll --exclude "multimodal/src/autogluon/multimodal/configs/pretrain/*"
 lint_check_all
 ruff check timeseries/
