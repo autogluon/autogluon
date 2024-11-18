93342bd5b0f8fb8561982645b30a66fc4e06ec71
Su Zhou <zhousu@amazon.com>
2024-11-18 20:18:53 +0000

    remove setuptool

--- common/setup.py
+++ common/setup.py
@@ -28,16 +28,12 @@ install_requires = (
         "tqdm",  # version range defined in `core/_setup_utils.py`
         # s3fs is removed due to doubling install time due to version range resolution
         # "s3fs",  # version range defined in `core/_setup_utils.py`
-        "setuptools<60.0; python_version < '3.12'",
-        "setuptools>=60.0; python_version >= '3.12'",
     ]
     if not ag.LITE_MODE
     else {
         "numpy",  # version range defined in `core/_setup_utils.py`
         "pandas",  # version range defined in `core/_setup_utils.py`
         "tqdm",  # version range defined in `core/_setup_utils.py`
-        "setuptools<60.0; python_version < '3.12'",
-        "setuptools>=60.0; python_version >= '3.12'",
     }
 )
 
