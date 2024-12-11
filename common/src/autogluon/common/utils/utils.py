9e246466eb351ffd888a9d769345ace5cc1336ed
Su Zhou <zhousu@amazon.com>
2024-12-11 23:08:52 +0000

    add condition for s3

--- common/src/autogluon/common/utils/utils.py
+++ common/src/autogluon/common/utils/utils.py
@@ -61,7 +61,9 @@ def setup_outputdir(path, warn_if_exist=True, create_dir=True, path_suffix=None)
             logger.warning(
                 f'Warning: path already exists! This predictor may overwrite an existing predictor! path="{path}"'
             )
-    path = os.path.expanduser(path)  # replace ~ with absolute path if it exists
+    if not path.lower().startswith("s3://"):
+        path = os.path.expanduser(path)  # replace ~ with absolute path if it exists
+        path = os.path.abspath(path)
     return path
 
 
