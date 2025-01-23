30ea811a7614deb3f15e191f99efe2289362959b
Caner Turkmen <turkmen.ac@gmail.com>
2025-01-23 09:47:37 +0000

    [timeseries] add pytest-xdist to accelerate tests

--- .github/workflow_scripts/test_timeseries.sh
+++ .github/workflow_scripts/test_timeseries.sh
@@ -9,11 +9,12 @@ source $(dirname "$0")/env_setup.sh
 setup_build_env
 export CUDA_VISIBLE_DEVICES=0
 install_local_packages "common/[tests]" "core/[all,tests]" "features/" "tabular/[all,tests]" "timeseries/[all,tests]"
+python -m pip install --upgrade pytest-xdist
 
 cd timeseries/
 if [ -n "$ADDITIONAL_TEST_ARGS" ]
 then
-    python -m pytest --junitxml=results.xml --runslow "$ADDITIONAL_TEST_ARGS" tests
+    python -m pytest --junitxml=results.xml --runslow "$ADDITIONAL_TEST_ARGS" --numprocesses 4 tests
 else
-    python -m pytest --junitxml=results.xml --runslow tests
+    python -m pytest --junitxml=results.xml --runslow --numprocesses 4 tests
 fi
