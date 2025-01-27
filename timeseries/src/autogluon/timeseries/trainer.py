0274705d700e7e343622be430e5ff39271c9804d
Caner Turkmen <turkmen.ac@gmail.com>
2025-01-27 12:19:46 +0000

    remove get_models_attribute_dict from time series

--- timeseries/src/autogluon/timeseries/trainer.py
+++ timeseries/src/autogluon/timeseries/trainer.py
@@ -205,7 +205,9 @@ class TimeSeriesTrainer(AbstractTrainer[AbstractTimeSeriesModel]):
             raise ValueError("Trainer has no fit models that can predict.")
         if len(models) == 1:
             return models[0]
-        model_performances = self.get_models_attribute_dict(attribute="val_score")
+        model_performances = {
+            m: self.model_graph.nodes[m]["val_score"] for m in self.get_model_names()
+        }
         model_levels = self._get_model_levels()
         model_name_score_level_list = [
             (m, model_performances[m], model_levels.get(m, 0)) for m in models if model_performances[m] is not None
