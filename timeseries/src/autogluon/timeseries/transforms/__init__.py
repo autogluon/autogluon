1490a80c8d6adb865505a840ca80071052fdb5a6
Oleksandr Shchur <shchuro@amazon.com>
2025-04-28 13:13:45 +0200

    [timeseries] Do not ignore __init__ files with ruff (#5098)

--- timeseries/src/autogluon/timeseries/transforms/__init__.py
+++ timeseries/src/autogluon/timeseries/transforms/__init__.py
@@ -1,2 +1,2 @@
 from .covariate_scaler import CovariateScaler, get_covariate_scaler
-from .target_scaler import TargetScaler, get_target_scaler 
+from .target_scaler import TargetScaler, get_target_scaler
