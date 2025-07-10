e5ec9ae172c5e70b3f0bbbd7bc430fe86837cf48
ubuntu <innixma@gmail.com>
2025-07-10 17:16:40 +0000

    Test kditransform

--- tabular/tests/unittests/models/test_tabpfnv2.py
+++ tabular/tests/unittests/models/test_tabpfnv2.py
@@ -9,3 +9,22 @@ def test_tabpfnv2():
     model_hyperparameters = toy_model_params
 
     FitHelper.verify_model(model_cls=model_cls, model_hyperparameters=model_hyperparameters)
+
+
+def test_tabpfnv2_kditransform():
+    model_cls = TabPFNV2Model
+    model_hyperparameters = {
+        "n_estimators": 1,
+        # Check custom KDITransformer
+        "inference_config/PREPROCESS_TRANSFORMS": [
+            {
+                "append_original": True,
+                "categorical_name": "none",
+                "global_transformer_name": None,
+                "name": "kdi",
+                "subsample_features": -1,
+            }
+        ],
+    }
+
+    FitHelper.verify_model(model_cls=model_cls, model_hyperparameters=model_hyperparameters)
