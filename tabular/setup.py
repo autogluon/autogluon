0dbd701ffb34c0df0177e01431dedcc9a75ed80a
xiyuanzh <xiz032@ucsd.edu>
2025-07-30 17:25:40 +0000

    add einops in mitra install

--- tabular/setup.py
+++ tabular/setup.py
@@ -73,6 +73,7 @@ extras_require = {
         "torch",
         "transformers",
         "huggingface_hub[torch]",
+        "einops>=0.7,<0.9",
     ],
     "tabicl": [
         "tabicl>=0.1.3,<0.2",  # 0.1.3 added a major bug fix to multithreading.
