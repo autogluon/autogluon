cc151c0e4fd8a3a654ae20561cd51f84851a731c
LennartPurucker <contact@lennart-purucker.com>
2025-08-16 10:31:37 +0200

    fix: linting

--- common/tests/unittests/test_setup_outputdir.py
+++ common/tests/unittests/test_setup_outputdir.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from unittest.mock import patch
 
 import pytest
+
 from autogluon.common.utils.utils import DEFAULT_BASE_PATH, setup_outputdir
 
 
@@ -15,15 +16,11 @@ class SetupOutputDirTestCase(unittest.TestCase):
         # checks that setup_outputdir raises when incorrect type is given
         with pytest.raises(Exception):
             path = 2.2
-            setup_outputdir(
-                path, warn_if_exist=True, create_dir=False, path_suffix=None
-            )
+            setup_outputdir(path, warn_if_exist=True, create_dir=False, path_suffix=None)
 
         # checks that setup_outputdir returns a path AutogluonModels/ag-* when no path is given
         path = None
-        returned_path = setup_outputdir(
-            path, warn_if_exist=True, create_dir=False, path_suffix=None
-        )
+        returned_path = setup_outputdir(path, warn_if_exist=True, create_dir=False, path_suffix=None)
         assert os.path.join(DEFAULT_BASE_PATH, "ag") in returned_path
 
         # checks that setup_outputdir returns a path CustomPath/ag-* when base path is given
@@ -39,24 +36,18 @@ class SetupOutputDirTestCase(unittest.TestCase):
 
         # checks that setup_outputdir returns the path given as input when given a path of type `str`
         path = tempfile.TemporaryDirectory().name
-        returned_path = setup_outputdir(
-            path, warn_if_exist=True, create_dir=False, path_suffix=None
-        )
+        returned_path = setup_outputdir(path, warn_if_exist=True, create_dir=False, path_suffix=None)
         assert str(Path(returned_path)) == path
 
         # checks that setup_outputdir returns the path given as input when given a path of type `pathlib.Path`
         path = Path(tempfile.TemporaryDirectory().name)
-        returned_path = setup_outputdir(
-            path, warn_if_exist=True, create_dir=False, path_suffix=None
-        )
+        returned_path = setup_outputdir(path, warn_if_exist=True, create_dir=False, path_suffix=None)
         assert str(Path(returned_path)) == str(path)
 
         # checks behavior of path_suffix logic
         path = tempfile.TemporaryDirectory().name
         path_suffix = f"my_subdir{os.path.sep}"
-        returned_path = setup_outputdir(
-            path, warn_if_exist=True, create_dir=False, path_suffix=path_suffix
-        )
+        returned_path = setup_outputdir(path, warn_if_exist=True, create_dir=False, path_suffix=path_suffix)
         assert not returned_path.endswith(os.path.sep)
         assert "my_subdir" in returned_path
 
@@ -64,17 +55,13 @@ class SetupOutputDirTestCase(unittest.TestCase):
         path = "s3://test-bucket/test-folder"
         # checks no local dir is created
         with patch("os.makedirs") as mock_makedirs:
-            returned_path = setup_outputdir(
-                path, warn_if_exist=True, create_dir=True, path_suffix=None
-            )
+            returned_path = setup_outputdir(path, warn_if_exist=True, create_dir=True, path_suffix=None)
             mock_makedirs.assert_not_called()
             assert returned_path == path
 
         # checks behavior of path_suffix logic
         path_suffix = "my_subdir/"
-        returned_path = setup_outputdir(
-            path, warn_if_exist=True, create_dir=False, path_suffix=path_suffix
-        )
+        returned_path = setup_outputdir(path, warn_if_exist=True, create_dir=False, path_suffix=path_suffix)
         assert not returned_path.endswith("/")
         assert "my_subdir" in returned_path
 
