8c1ad794f51bcd74d06665e2231cc6ce4c89ccb2
Nick Erickson <neerick@amazon.com>
2025-08-20 17:04:12 -0700

    Update tabular/tests/unittests/models/test_ebm.py

--- tabular/tests/unittests/models/test_ebm.py
+++ tabular/tests/unittests/models/test_ebm.py
@@ -1,6 +1,15 @@
 from autogluon.tabular.models import EBMModel
 from autogluon.tabular.testing import FitHelper
 
+toy_model_params = {}
+
+
+def test_ebm():
+    model_cls = EBMModel
+    model_hyperparameters = toy_model_params
+
+    FitHelper.verify_model(model_cls=model_cls, model_hyperparameters=model_hyperparameters)
+
 
 def test_ebm_binary():
     fit_args = dict(
