e483d1578691945c2f3ea2acbe877905af17ef00
Oleksandr Shchur <shchuro@amazon.com>
2025-08-26 07:36:37 +0000

    Try pinning package versions

--- core/setup.py
+++ core/setup.py
@@ -56,6 +56,8 @@ extras_require = {
         # TODO: consider alternatives as hyperopt is not actively maintained.
         "hyperopt>=0.2.7,<0.2.8",  # This is needed for the bayes search to work.
         # 'GPy>=1.10.0,<1.11.0'  # TODO: Enable this once PBT/PB2 are supported by ray lightning
+        "typing-extensions<4.15",
+        "stevedore<5.5",
     ],
 }
 
