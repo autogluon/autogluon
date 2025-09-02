6723c8503199c7c14fe01d652f61cc3d9da98ff1
Oleksandr Shchur <shchuro@amazon.com>
2025-09-02 10:15:24 +0000

    Clean up the quantile patching method

--- timeseries/src/autogluon/timeseries/models/chronos/pipeline/__init__.py
+++ timeseries/src/autogluon/timeseries/models/chronos/pipeline/__init__.py
@@ -1,10 +1,11 @@
 from .base import BaseChronosPipeline, ForecastType
 from .chronos import ChronosPipeline
-from .chronos_bolt import ChronosBoltPipeline
+from .chronos_bolt import ChronosBoltPipeline, patch_chronos_bolt_output_quantiles
 
 __all__ = [
     "BaseChronosPipeline",
     "ChronosBoltPipeline",
     "ChronosPipeline",
     "ForecastType",
+    "patch_chronos_bolt_output_quantiles",
 ]
