6fe94d1ac10b21d80d1f1c0a61b1588fcaabfcbb
Oleksandr Shchur <shchuro@amazon.com>
2025-11-26 16:11:16 +0000

    Fix default ensemble

--- timeseries/src/autogluon/timeseries/trainer/trainer.py
+++ timeseries/src/autogluon/timeseries/trainer/trainer.py
@@ -415,8 +415,7 @@ class TimeSeriesTrainer(AbstractTrainer[TimeSeriesModelBase]):
 
         # Handle ensemble hyperparameters
         if ensemble_hyperparameters is None:
-            # ensemble_hyperparameters = {"GreedyEnsemble": {}}
-            ensemble_hyperparameters = {"PerItemGreedyEnsemble": {}}
+            ensemble_hyperparameters = {"GreedyEnsemble": {}}
         ensemble_hyperparameters = validate_ensemble_hyperparameters(ensemble_hyperparameters)
 
         time_start = time.time()
