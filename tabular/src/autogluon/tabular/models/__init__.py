b290c75d8913e2112977978bc330cb89483b4967
ubuntu <innixma@gmail.com>
2025-11-29 06:01:24 +0000

    Add TabDPT (WIP)

--- tabular/src/autogluon/tabular/models/__init__.py
+++ tabular/src/autogluon/tabular/models/__init__.py
@@ -21,6 +21,7 @@ from .lgb.lgb_model import LGBModel
 from .lr.lr_model import LinearModel
 from .realmlp.realmlp_model import RealMLPModel
 from .rf.rf_model import RFModel
+from .tabdpt.tabdpt_model import TabDPTModel
 from .tabicl.tabicl_model import TabICLModel
 from .tabm.tabm_model import TabMModel
 from .tabpfnv2.tabpfnv2_model import TabPFNV2Model
