eb1621f234a1b7105c35358bc09c4aaf483782a9
ubuntu <innixma@gmail.com>
2025-12-03 17:22:08 +0000

    update tests

--- tabular/tests/unittests/models/test_realmlp.py
+++ tabular/tests/unittests/models/test_realmlp.py
@@ -8,4 +8,8 @@ def test_realmlp():
     model_cls = RealMLPModel
     model_hyperparameters = toy_model_params
 
-    FitHelper.verify_model(model_cls=model_cls, model_hyperparameters=model_hyperparameters)
+    FitHelper.verify_model(
+        model_cls=model_cls,
+        model_hyperparameters=model_hyperparameters,
+        # verify_load_wo_cuda=True,  # TODO: RealMLP doesn't yet work for GPU -> CPU
+    )
