64fc14339665295b4d03ff690c3daf645247dc87
ubuntu <innixma@gmail.com>
2025-12-19 04:26:34 +0000

    Update memory estimate for TabPFN

--- tabular/src/autogluon/tabular/models/tabpfnv2/tabpfnv2_5_model.py
+++ tabular/src/autogluon/tabular/models/tabpfnv2/tabpfnv2_5_model.py
@@ -271,7 +271,7 @@ class TabPFNModel(AbstractModel):
 
         model_mem = 14489108  # Based on TabPFNv2 default
 
-        n_samples, n_features = X.shape[0], min(X.shape[1], 500)
+        n_samples, n_features = X.shape[0], min(X.shape[1], 2000)
         n_feature_groups = (
             n_features
         ) / features_per_group + 1  # TODO: Unsure how to calculate this
@@ -404,3 +404,42 @@ class RealTabPFNv2Model(TabPFNModel):
         if not _HAS_LOGGED_TABPFN_LICENSE:
             logger.log(20, "\tBuilt with PriorLabs-TabPFN")  # Aligning with TabPFNv2 license requirements
             _HAS_LOGGED_TABPFN_LICENSE = True  # Avoid repeated logging
+
+    # FIXME: Avoid code dupe. This one has 500 features max, 2.5 has 2000.
+    @classmethod
+    def _estimate_memory_usage_static(
+            cls,
+            *,
+            X: pd.DataFrame,
+            hyperparameters: dict | None = None,
+            **kwargs,
+    ) -> int:
+        """Heuristic memory estimate based on TabPFN's memory estimate logic in:
+        https://github.com/PriorLabs/TabPFN/blob/57a2efd3ebdb3886245e4d097cefa73a5261a969/src/tabpfn/model/memory.py#L147.
+
+        This is based on GPU memory usage, but hopefully with overheads it also approximates CPU memory usage.
+        """
+        # TODO: update, this is not correct anymore, consider using internal TabPFN functions directly.
+        features_per_group = 3  # Based on TabPFNv2 default (unused)
+        n_layers = 12  # Based on TabPFNv2 default
+        embedding_size = 192  # Based on TabPFNv2 default
+        dtype_byte_size = 2  # Based on TabPFNv2 default
+
+        model_mem = 14489108  # Based on TabPFNv2 default
+
+        n_samples, n_features = X.shape[0], min(X.shape[1], 500)
+        n_feature_groups = (
+                               n_features
+                           ) / features_per_group + 1  # TODO: Unsure how to calculate this
+
+        X_mem = n_samples * n_feature_groups * dtype_byte_size
+        activation_mem = (
+                n_samples * n_feature_groups * embedding_size * n_layers * dtype_byte_size
+        )
+
+        baseline_overhead_mem_est = 1e9  # 1 GB generic overhead
+
+        # Add some buffer to each term + 1 GB overhead to be safe
+        return int(
+            model_mem + 4 * X_mem + 2 * activation_mem + baseline_overhead_mem_est
+        )
