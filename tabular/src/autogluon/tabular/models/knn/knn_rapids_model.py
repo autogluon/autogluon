473030917256e64df7cb37ea58eb490382945f27
Ubuntu <ubuntu@ip-172-31-0-113.us-west-2.compute.internal>
2025-12-19 18:53:45 +0000

    remove pre-release flags

--- tabular/src/autogluon/tabular/models/knn/knn_rapids_model.py
+++ tabular/src/autogluon/tabular/models/knn/knn_rapids_model.py
@@ -23,7 +23,7 @@ class KNNRapidsModel(RapidsModelMixin, KNNModel):
 
     conda create -n rapids-21.06 -c rapidsai -c nvidia -c conda-forge rapids=21.06 python=3.8 cudatoolkit=11.2
     conda activate rapids-21.06
-    pip install --pre autogluon.tabular[all]
+    pip install autogluon.tabular[all]
     """
 
     def _get_model_type(self):
