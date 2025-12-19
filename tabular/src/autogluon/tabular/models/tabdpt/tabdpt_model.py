fefe8f7e6b051b07d3f1e661dca66a4caba8d43b
Nick Erickson <neerick@amazon.com>
2025-12-19 04:57:48 -0800

    [tabular] Add v1.5.0 Presets  (#5505)

--- tabular/src/autogluon/tabular/models/tabdpt/tabdpt_model.py
+++ tabular/src/autogluon/tabular/models/tabdpt/tabdpt_model.py
@@ -182,6 +182,10 @@ class TabDPTModel(AbstractTorchModel):
     def supported_problem_types(cls) -> list[str] | None:
         return ["binary", "multiclass", "regression"]
 
+    @classmethod
+    def _class_tags(cls):
+        return {"can_estimate_memory_usage_static": True}
+
     def _more_tags(self) -> dict:
         return {"can_refit_full": True}
 
@@ -204,3 +208,46 @@ class TabDPTModel(AbstractTorchModel):
         }
         default_ag_args_ensemble.update(extra_ag_args_ensemble)
         return default_ag_args_ensemble
+
+    # FIXME: This is copied from TabPFN, but TabDPT is not the same
+    @classmethod
+    def _estimate_memory_usage_static(
+        cls,
+        *,
+        X: pd.DataFrame,
+        hyperparameters: dict | None = None,
+        **kwargs,
+    ) -> int:
+        """Heuristic memory estimate based on TabPFN's memory estimate logic in:
+        https://github.com/PriorLabs/TabPFN/blob/57a2efd3ebdb3886245e4d097cefa73a5261a969/src/tabpfn/model/memory.py#L147.
+
+        This is based on GPU memory usage, but hopefully with overheads it also approximates CPU memory usage.
+        """
+        # TODO: update, this is not correct anymore, consider using internal TabPFN functions directly.
+        features_per_group = 3  # Based on TabPFNv2 default (unused)
+        n_layers = 12  # Based on TabPFNv2 default
+        embedding_size = 192  # Based on TabPFNv2 default
+        dtype_byte_size = 2  # Based on TabPFNv2 default
+
+        model_mem = 14489108  # Based on TabPFNv2 default
+
+        n_samples, n_features = X.shape[0], min(X.shape[1], 500)
+        n_feature_groups = (
+            n_features
+        ) / features_per_group + 1  # TODO: Unsure how to calculate this
+
+        X_mem = n_samples * n_feature_groups * dtype_byte_size
+        activation_mem = (
+            n_samples * n_feature_groups * embedding_size * n_layers * dtype_byte_size
+        )
+
+        baseline_overhead_mem_est = 1e9  # 1 GB generic overhead
+
+        # Add some buffer to each term + 1 GB overhead to be safe
+        memory_estimate = model_mem + 4 * X_mem + 2 * activation_mem + baseline_overhead_mem_est
+
+        # TabDPT memory estimation is very inaccurate because it is using TabPFN memory estimate. Double it to be safe.
+        memory_estimate = memory_estimate * 2
+
+        # Note: This memory estimate is way off if `context_size` is not None
+        return int(memory_estimate)
