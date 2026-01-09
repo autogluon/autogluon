6b60df20c19264be1280691c4f5ac89464acbd58
Nick Erickson <neerick@amazon.com>
2026-01-08 16:30:28 -0800

    Install `features` before `core` (#5536)

--- .github/workflow_scripts/test_eda.sh
+++ .github/workflow_scripts/test_eda.sh
@@ -8,7 +8,7 @@ source $(dirname "$0")/env_setup.sh
 
 setup_build_env
 export CUDA_VISIBLE_DEVICES=0
-install_local_packages "common/[tests]" "core/[all,tests]" "features/" "tabular/[all,tests]" "eda/[tests]"
+install_local_packages "common/[tests]" "features/" "core/[all,tests]" "tabular/[all,tests]" "eda/[tests]"
 
 cd eda/
 python -m tox -e lint,typecheck,format,testenv
