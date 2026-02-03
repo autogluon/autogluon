d4bd96e2a39cb4303585a45c0d1abe1e4a240bba
Oleksandr Shchur <shchuro@amazon.com>
2026-02-03 13:24:26 +0000

    Cache HF datasets for AutoMM tests

--- CI/hf_mirror/download_hf_models.py
+++ CI/hf_mirror/download_hf_models.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import yaml
 
 from huggingface_hub import snapshot_download
@@ -8,10 +9,12 @@ from huggingface_hub.utils import RepositoryNotFoundError
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument('--model_list_file', help='file containing list of models to download.', type=str, required=True)
+parser.add_argument('--dataset_list_file', help='file containing list of datasets to download.', type=str, default=None)
 
 args = parser.parse_args()
 
 model_list_file = args.model_list_file
+dataset_list_file = args.dataset_list_file
 
 with open(model_list_file, 'r') as fp:
     model_list = yaml.safe_load(fp)  # a dict containing models for different sub_folders
@@ -25,3 +28,35 @@ for model in model_list:
         print(f"Finished downloading {model}")
     except RepositoryNotFoundError:
         print(f"Invalid repo_id: {model}. Failed to download")
+
+# Download HuggingFace datasets if dataset list is provided
+if dataset_list_file and os.path.exists(dataset_list_file):
+    from datasets import load_dataset
+    from datasets.exceptions import DatasetNotFoundError
+
+    with open(dataset_list_file, 'r') as fp:
+        dataset_list = yaml.safe_load(fp)
+        dataset_list = list(dataset_list.values())
+        dataset_list = set(sum(dataset_list, []))  # concatenate inner dataset lists
+
+    datasets_cache_dir = "/mnt/efs/datasets"
+    os.makedirs(datasets_cache_dir, exist_ok=True)
+
+    for dataset_spec in dataset_list:
+        # Handle dataset specs like "glue/mrpc" -> dataset_name="glue", config="mrpc"
+        parts = dataset_spec.split("/")
+        if len(parts) == 2 and parts[0] in ["glue", "super_glue"]:
+            dataset_name, config = parts
+        else:
+            dataset_name = dataset_spec
+            config = None
+
+        print(f"Downloading dataset: {dataset_spec}")
+        try:
+            if config:
+                load_dataset(dataset_name, config, cache_dir=datasets_cache_dir)
+            else:
+                load_dataset(dataset_name, cache_dir=datasets_cache_dir)
+            print(f"Finished downloading dataset: {dataset_spec}")
+        except (DatasetNotFoundError, Exception) as e:
+            print(f"Failed to download dataset {dataset_spec}: {e}")
--- CI/hf_mirror/download_hf_models.sh
+++ CI/hf_mirror/download_hf_models.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if grep -qs '/mnt/efs ' /proc/mounts; 
-then 
+if grep -qs '/mnt/efs ' /proc/mounts;
+then
     echo EFS attached
 else
     echo EFS failed to attach
     exit 1
 fi
 git clone https://github.com/autogluon/autogluon.git
-python3 download_hf_models.py --model_list_file autogluon/multimodal/tests/hf_model_list.yaml
+python3 download_hf_models.py \
+    --model_list_file autogluon/multimodal/tests/hf_model_list.yaml \
+    --dataset_list_file autogluon/multimodal/tests/hf_dataset_list.yaml
 aws s3 sync --delete /mnt/efs s3://autogluon-hf-model-mirror
