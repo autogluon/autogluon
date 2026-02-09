1faa62bf598310cead52422ee62c8b27a97dc3a2
CelestinoXP <41735311+celestinoxp@users.noreply.github.com>
2026-02-09 22:47:19 +0000

    [Core] Refactor greedy ensemble selection to use np.isclose for floating point comparisons (#5556)
    Co-authored-by: Nick Erickson <neerick@amazon.com>

--- core/src/autogluon/core/models/greedy_ensemble/ensemble_selection.py
+++ core/src/autogluon/core/models/greedy_ensemble/ensemble_selection.py
@@ -137,7 +137,7 @@ class EnsembleSelection(AbstractWeightedEnsemble):
                 if round_scores:
                     scores[j] = scores[j].round(round_decimals)
 
-            all_best = np.argwhere(scores == np.nanmin(scores)).flatten()
+            all_best = np.argwhere(np.isclose(scores, np.nanmin(scores), atol=0, rtol=1e-12)).flatten()
 
             if (len(all_best) > 1) and used_models:
                 # If tie, prioritize models already in ensemble to avoid unnecessarily large ensemble
@@ -163,7 +163,9 @@ class EnsembleSelection(AbstractWeightedEnsemble):
                             scores_tiebreak[k] = self._calculate_regret(
                                 y_true=labels, y_pred_proba=fant_ensemble_prediction, metric=secondary_metric
                             )
-                        all_best_tiebreak = np.argwhere(scores_tiebreak == np.nanmin(scores_tiebreak)).flatten()
+                        all_best_tiebreak = np.argwhere(
+                            np.isclose(scores_tiebreak, np.nanmin(scores_tiebreak), atol=0, rtol=1e-12)
+                        ).flatten()
                         all_best = [index_map[index] for index in all_best_tiebreak]
 
             best = self.random_state.choice(all_best)
