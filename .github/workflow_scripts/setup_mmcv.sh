e657d86eea1c7d7927c96c07ac2991933bd0902f
Ubuntu <ubuntu@ip-172-31-0-99.us-west-2.compute.internal>
2026-02-10 21:26:32 +0000

    downgrade setuptools

--- .github/workflow_scripts/setup_mmcv.sh
+++ .github/workflow_scripts/setup_mmcv.sh
@@ -4,7 +4,8 @@ function setup_mmcv {
     return 0
   fi
   # Install MMEngine from PyPI wheel to avoid setuptools>=82 removing pkg_resources
+  python3 -m pip install "setuptools<82"
   python3 -m pip install "mmengine==0.10.5"
-  pip install "mmcv==2.1.0" --no-build-isolation --timeout 60
+  python3 -m pip install "mmcv==2.1.0" --no-build-isolation --timeout 60
   python3 -m pip install "mmdet==3.2.0"
 }
