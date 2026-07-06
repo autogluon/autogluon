4a990dd73962718acac439bd19db8a9d23fe5489
Oleksandr Shchur <shchuro@amazon.com>
2026-07-06 11:08:17 +0000

    Bump skl2onnx

--- tabular/setup.py
+++ tabular/setup.py
@@ -88,7 +88,7 @@ extras_require = {
         "imodels>=1.3.10,<2.1.0",  # 1.3.8/1.3.9 either remove/renamed attribute `complexity_` causing failures. https://github.com/csinva/imodels/issues/147
     ],
     "skl2onnx": [
-        "skl2onnx>=1.15.0,<1.20.0",
+        "skl2onnx>=1.15.0,<1.21.0",
         # Sync ONNX requirements with multimodal/setup.py
         "onnx>=1.13.0,!=1.16.2,<1.21.0;platform_system=='Windows'",  # exclude 1.16.2 for issue https://github.com/onnx/onnx/issues/6267
         "onnx>=1.13.0,<1.21.0;platform_system!='Windows'",
