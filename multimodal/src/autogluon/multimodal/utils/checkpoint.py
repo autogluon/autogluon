9903e33aec01eaf66dd0bcffb4df60921474b35a
Atharva Kale <athrajan@amazon.com>
2026-07-10 12:17:22 -0700

    applied ruff

--- multimodal/src/autogluon/multimodal/utils/checkpoint.py
+++ multimodal/src/autogluon/multimodal/utils/checkpoint.py
@@ -74,9 +74,9 @@ def average_checkpoints(
         for k in avg_state_dict:
             avg_state_dict[k].clamp_(float32_info.min, float32_info.max).to(dtype=torch.float32)
     else:
-        avg_state_dict = torch.load(
-            checkpoint_paths[0], map_location=torch.device("cpu"), weights_only=True
-        )["state_dict"]
+        avg_state_dict = torch.load(checkpoint_paths[0], map_location=torch.device("cpu"), weights_only=True)[
+            "state_dict"
+        ]
 
     return avg_state_dict
 
