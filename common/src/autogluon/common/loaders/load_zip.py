aae54f3e122309d5967eb64f3f724c728e8966eb
Oleksandr Shchur <shchuro@amazon.com>
2026-07-15 07:46:00 +0000

    Add type hints to safe_extractall

--- common/src/autogluon/common/loaders/load_zip.py
+++ common/src/autogluon/common/loaders/load_zip.py
@@ -1,11 +1,12 @@
 import logging
 import os
 import stat
+import zipfile
 
 logger = logging.getLogger(__name__)
 
 
-def safe_extractall(zf, dest_dir):
+def safe_extractall(zf: zipfile.ZipFile, dest_dir: str) -> None:
     """Extract zip file with path traversal and symlink validation."""
     dest_dir = os.path.realpath(dest_dir)
     for member in zf.infolist():
