244f9b7f47c02e186513c871d0fb1377680e0ded
Oleksandr Shchur <shchuro@amazon.com>
2026-09-18 10:40:13 +0200

    [1.6.3] Bump Ray version cap to 2.59 (#5928)

--- core/setup.py
+++ core/setup.py
@@ -40,12 +40,12 @@ extras_require = {
     "ray": [
         # Ray<=2.53 does not support py313 on Windows
         # Lower bound to exclude CVE-2026-41486
-        "ray[default]>=2.55.0,<2.57; platform_system != 'Windows' or python_version != '3.13'",  # sync with common/src/autogluon/common/utils/try_import.py
+        "ray[default]>=2.55.0,<2.59; platform_system != 'Windows' or python_version != '3.13'",  # sync with common/src/autogluon/common/utils/try_import.py
     ],
     "raytune": [
         "pyarrow>=15.0.0",  # cap Pyarrow to fix source installation - https://github.com/autogluon/autogluon/issues/4519
         # Lower bound to exclude CVE-2026-41486
-        "ray[default,tune]>=2.55.0,<2.57; platform_system != 'Windows' or python_version != '3.13'",  # sync with common/src/autogluon/common/utils/try_import.py
+        "ray[default,tune]>=2.55.0,<2.59; platform_system != 'Windows' or python_version != '3.13'",  # sync with common/src/autogluon/common/utils/try_import.py
         # TODO: consider alternatives as hyperopt is not actively maintained.
         "hyperopt>=0.2.7,<0.2.8",  # This is needed for the bayes search to work.
         # 'GPy>=1.10.0,<1.11.0'  # TODO: Enable this once PBT/PB2 are supported by ray lightning
